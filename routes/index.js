0d19f9386b74a036e40a796c5fa2f6241667dbf5
rojonaitor <bryanlundberg@outlook.com>
2023-01-23 17:36:56 -0600

    routes

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+const homeController = require("../controllers/homeController")
+const loginController = require("../controllers/loginController")
+const registerController = require("../controllers/registerController")
+
+router.get('/', homeController.home_get);
 
 module.exports = router;
