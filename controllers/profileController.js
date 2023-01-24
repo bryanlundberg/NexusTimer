79049df728141366f330022e87de931ad79864d3
rojonaitor <bryanlundberg@outlook.com>
2023-01-23 18:53:19 -0600

    models

--- controllers/profileController.js
+++ controllers/profileController.js
@@ -1,3 +1,12 @@
-exports.profile_get = (req, res) => {
-	res.json({msg: "pagina en blanco"})
+const User = require("../models/User");
+
+exports.profile_get = async (req, res) => {
+	try {
+		const user = await new User ({username: "anna", email: "ssasaasaas@gmail.com", password: "fdbsafbd"})
+		await user.save()
+		console.log(user)
+		res.redirect("/")
+	}	catch (error) {
+			res.json({error: `${error}`})
+	}
 }
---
+++ models/User.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+	username: {
+		type: String,
+		unique: true,
+		required: true
+	},
+	
+	email: {
+		type: String,
+		unique: true,
+		required: true
+	},
+	
+	password: {
+		type: String,
+		required: true
+	}
+})
+
+
+module.exports = mongoose.model("Users", UserSchema)
+
+
