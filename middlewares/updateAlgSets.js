21d76fcb7723708c0d64ad6f475118535f6576b5
rojonaitor <bryanlundberg@outlook.com>
2023-02-03 07:55:59 -0600

    pll db & images

--- middlewares/updateAlgSets.js
+++ middlewares/updateAlgSets.js
@@ -2,6 +2,8 @@ const User = require("../models/User");
 const AlgSet = require("../models/AlgSet");
 const Algorithm = require("../models/Algorithm");
 const ollAlgorithms = require("../algs/ollAlgs");
+const pllAlgorithms = require("../algs/pllAlgs");
+
 
 module.exports = async (req, res, next) => {
   try {
@@ -9,15 +11,22 @@ module.exports = async (req, res, next) => {
     if (!user) {
       throw new Error("User not found");
     }
+	const sets = ["OLL", "PLL"];
 
+	for (let i = 0; i < sets.length; i++) {
+	  let set = await AlgSet.findOne({ name: sets[i] });
+	  if (!set) {
+		set = new AlgSet({
+		  name: sets[i],
+		});
+		await set.save();
+		console.log(`Algorithm set ${sets[i]} created`);
+	  }
+	}
+
+	//POPULATE OLLSET & INSERT USER LEARN STATUS
     let ollSet = await AlgSet.findOne({ name: "OLL" });
-    if (!ollSet) {
-      ollSet = new AlgSet({
-        name: "OLL",
-      });
-      await ollSet.save();
-      console.log("alg set oll created");
-    }
+    if (!ollSet) {throw new Error("something went wrong")}
 
     let algorithmsOll = await Algorithm.find({ algSet: ollSet._id });
     if (algorithmsOll.length !== ollAlgorithms.length) {
@@ -37,7 +46,6 @@ module.exports = async (req, res, next) => {
         });
       }
       await Algorithm.create(createAlgs);
-      console.log("created algs");
     } else {
       let pendingStatus = [];
       algorithmsOll.forEach((oll) => {
@@ -54,10 +62,51 @@ module.exports = async (req, res, next) => {
           { _id: { $in: pendingStatus } },
           { $addToSet: { learnStatus: { user: user._id, status: "off" } } }
         );
-        console.log("actualizados");
       }
-      console.log(pendingStatus);
     }
+	
+	//POPULATE PLLSET & INSERT USER LEARN STATUS
+    let pllSet = await AlgSet.findOne({ name: "PLL" });
+    if (!pllSet) {throw new Error("something went wrong")}
+
+    let algorithmsPll = await Algorithm.find({ algSet: pllSet._id });
+    if (algorithmsPll.length !== pllAlgorithms.length) {
+      const createAlgs = [];
+      for (let i = 0; i < pllAlgorithms.length; i++) {
+        createAlgs.push({
+          algSet: pllSet._id,
+          name: `PLL${i + 1}`,
+          thumbnail: `/images/collection/pll/${i + 1}.png`,
+          alg: pllAlgorithms[i].alg,
+          learnStatus: [
+            {
+              user: user._id,
+              status: "off",
+            },
+          ],
+        });
+      }
+      await Algorithm.create(createAlgs);
+    } else {
+      let pendingStatus = [];
+      algorithmsPll.forEach((oll) => {
+        const status = oll.learnStatus.find(
+          (state) => state.user.toString() === user._id.toString()
+        );
+        if (!status) {
+          pendingStatus.push(oll._id);
+        }
+      });
+
+      if (pendingStatus.length !== 0) {
+        await Algorithm.updateMany(
+          { _id: { $in: pendingStatus } },
+          { $addToSet: { learnStatus: { user: user._id, status: "off" } } }
+        );
+      }
+    }
+
+	//POPULATE OTHERS
 
     return next();
   } catch (error) {
