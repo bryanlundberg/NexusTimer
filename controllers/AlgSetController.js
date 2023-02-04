ff163854753b1d499a5eedb5e45feb58a7e3632b
rojonaitor <bryanlundberg@outlook.com>
2023-02-04 07:51:25 -0600

    algController

--- controllers/AlgSetController.js
+++ controllers/AlgSetController.js
@@ -4,29 +4,15 @@ const Algorithm = require("../models/Algorithm");
 exports.oll_get = async (req, res) => {
   try {
 	const user = await User.findById(req.params.idUser);
-	const set = await AlgSet.findOne({ name: "OLL" });
-
-	if (!user) {
-	  throw new Error("User not found");
-	}
-	if (!set) {
-	  throw new Error("OLL set not found");
-	}
+	if (!user) {throw new Error("User not found");}
 	
-	const algs = await Algorithm.find({ algSet: set._id });
-	if (!algs) {
-	  throw new Error("algs set not found");
-	}
-	const sdsa = await algs.filter(e => { e.user == user._id})
-	
-	console.log(`fOUND ${learnState.length}`)
-	console.log(learnState[1])
+	let userAlgs = await Algorithm.find({ algSet: "OLL", owner: user._id });
+	if (!userAlgs) {throw new Error("OLL set not found");}
 	
 	res.render("profile_alg-set", {
 		title: "Your profile",
 		user,
-		learnState,
-		set
+		userAlgs
 	})
 	
   } catch (error) {
@@ -38,34 +24,19 @@ exports.oll_get = async (req, res) => {
 exports.pll_get = async (req, res) => {
   try {
 	const user = await User.findById(req.params.idUser);
-	const set = await AlgSet.findOne({ name: "PLL" });
-
-	if (!user) {
-	  throw new Error("User not found");
-	}
-	if (!set) {
-	  throw new Error("PLL set not found");
-	}
-
-	const algs = await Algorithm.find({
-	  algSet: set._id,
-	  "learnStatus.user": user._id
-	});
+	if (!user) {throw new Error("User not found");}
 	
-	if (!algs) {throw new Error("Something went wrong");}
-	
-	console.log(`fOUND ${algs.length}`)
-	console.log(algs)
+	let userAlgs = await Algorithm.find({ algSet: "PLL", owner: user._id });
+	if (!userAlgs) {throw new Error("PLL set not found");}
 	
 	res.render("profile_alg-set", {
 		title: "Your profile",
 		user,
-		algs,
-		set
+		userAlgs
 	})
 	
   } catch (error) {
-	  console.log(error.message)
+	  console.log(error)
 	  res.redirect("/")
   }
 }
