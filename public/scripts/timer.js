7920a2143d5980d479ce8f8d62e41f49456fdf21
rojonaitor <bryanlundberg@outlook.com>
2023-02-06 20:59:19 -0600

    effect

--- public/scripts/timer.js
+++ public/scripts/timer.js
@@ -1,3 +1,4 @@
+//falta añadir metas semanales en el perfil tambien si se me olvida deje un buen dashboard de eejmplo en los pin de chrome
 function generateScramble(scrambleLength) {
   const possibleMoves = ["U", "U2", "U'", "R", "R2", "R'", "F", "F2", "F'", "D", "D2", "D'", "L", "L2", "L'", "B", "B2", "B'"];
   const scramble = [];
@@ -15,45 +16,82 @@ function generateScramble(scrambleLength) {
   return scramble.join(" ");
 }
 
-function runTimer() {
-  
-    if (!timerId) {
-	  document.querySelector("#timer").classList.remove("text-success")
-      startTime = Date.now();
-      timerId = setInterval(() => {
-        currentTime = Date.now();
-        elapsedTime = currentTime - startTime;
-        document.querySelector("#timer").innerHTML = (elapsedTime / 1000).toFixed(2);
-      }, 10);
-    } else {
-      clearInterval(timerId);
-      timerId = null;
-	  document.querySelector("#timer").classList.remove("text-success")
-	  document.querySelector("#scramble").textContent = generateScramble(20)
-    }
-  
+function updateTimer() {
+  let solveTime = (Date.now() - startTime) / 1000;
+  document.querySelector("#timer").textContent = solveTime.toFixed(2);
 }
+
+
+function runTimer() {
+      startTime = Date.now();
+      timerId = setInterval(updateTimer,10);
+}
+
+function stopTimer() {
+	  clearInterval(timerId)
+}
+
 
 
 document.querySelector("#scramble").textContent = generateScramble(20)
 
-let timerId;
-let startTime;
-let currentTime;
-let elapsedTime = 0;
-let running = false;
+
+let holdStartTime = 0;
+let isRunning = false;
+let isHolding = false
+let solveTime = 0;
 
 document.addEventListener("keydown", function(event) {
-	if (event.code === "Space") {
+  if (event.code === "Space") {
+    if (isRunning === false && isHolding === false) {
+      holdStartTime = Date.now()
+	  isHolding = true
+	  
+    } 
+	
+	if (isRunning === true && isHolding === false) {
+      stopTimer();
+      isRunning = false;
+	  document.querySelector("#timer").classList.remove("text-success")
+	  document.querySelector("#timer").classList.remove("text-danger")
+	  document.querySelector("#scramble").textContent = generateScramble(20)
+	  
+    }
+	
+	if (isRunning === false && isHolding === true && ((Date.now() - holdStartTime)/1000) <= 1) {
+	  document.querySelector("#timer").classList.remove("text-success")
+      document.querySelector("#timer").classList.add("text-danger")
+	  
+    }
+	
+	if (isRunning === false && isHolding === true && ((Date.now() - holdStartTime)/1000) >= 1) {
+		document.querySelector("#timer").classList.remove("text-danger")
 		document.querySelector("#timer").classList.add("text-success")
 	}
+  
+  }
 });
 
 document.addEventListener("keyup", function(event) {
-	if (event.code === "Space" && running == false) {
-	  runTimer()
+  if (event.code === "Space") {
+	
+    let difference = (Date.now() - holdStartTime)/1000;
+    if (isRunning === false && isHolding === true && difference >= 1) {
+		console.log("hold 1+s")
+      isRunning = true;
+	  isHolding = false;
+      runTimer();
+    } 
+	
+	if (difference <= 1) {
+      isRunning = false;
+	  isHolding = false;
+	  document.querySelector("#timer").classList.remove("text-danger")
+	  document.querySelector("#timer").classList.remove("text-success")
 	}
+  }
 });
+
 
 document.addEventListener("click", (e) => {
 	const timeBody = document.querySelector("#timer-vh")
@@ -67,52 +105,4 @@ document.addEventListener("keydown", function(event) {
 		document.querySelector("#timer").textContent = `0.00`
 	}
 });
-
-
-/*
-
-let timerId;
-let startTime;
-let currentTime;
-let elapsedTime = 0;
-
-document.addEventListener("keyup", function(event) {
-	if (event.code === "Space") {
-		if (!timerId) {
-			startTime = Date.now();
-			timerId = setInterval(() => {
-				currentTime = Date.now();
-				elapsedTime = currentTime - startTime;
-				document.getElementById("timer").innerHTML = (elapsedTime / 1000).toFixed(2) + "s";
-			}, 10);
-		} else {
-			clearInterval(timerId);
-			timerId = null;
-			let solveTime = (elapsedTime / 1000).toFixed(2);
-			let input1 = document.getElementById("input1").value;
-			let input2 = document.getElementById("input2").value;
-			fetch('/api/registerSolve', {
-				method: 'POST',
-				headers: {
-					'Content-Type': 'application/json'
-				},
-				body: JSON.stringify({
-					solveTime,
-					input1,
-					input2
-				})
-			})
-				.then(response => response.json())
-				.then(data => {
-					console.log(data);
-				})
-				.catch(error => {
-					console.error(error);
-				});
-		}
-	}
-});
-
-*/
-
 
