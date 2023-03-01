2d75b44d927bf4b6d3affe8af3fc9f108f8ccb10
rojonaitor <bryanlundberg@outlook.com>
2023-03-01 10:26:43 -0600

    connect mongo

--- src/middlewares/sessions.js
+++ src/middlewares/sessions.js
@@ -1,11 +1,19 @@
 const session = require("express-session");
+const MongoStore = require('connect-mongo');
 
-const sessionsExpress =
-	  session({
-    secret: "sessionSecreta",
-    resave: true,
-    saveUninitialized: true,
-    name: "secreto-nombre-session",
-  })
+const USER = "usernametest"
+const PASSWORD = "passwordtest"
+const DB = "cubestats"
+
+const sessionsExpress = session({
+  secret: "sessionSecreta",
+  resave: false,
+  saveUninitialized: false,
+  name: "secreto-nombre-session",
+  store: MongoStore.create({
+    mongoUrl:
+      `mongodb+srv://${USER}:${PASSWORD}@cluster0.dltd4ag.mongodb.net/${DB}?retryWrites=true&w=majority`,
+  }),
+});
 
 module.exports = { sessionsExpress }
