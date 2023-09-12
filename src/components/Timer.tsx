68c053e00f2a6e0f3848eadabff1938b00c256a5
rojonaitor <bryanlundberg@outlook.com>
2023-09-11 20:24:07 -0600

    minor changes removing code

--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -1,27 +1,20 @@
 import { useEffect, useRef, useState } from "react";
 import SolveOptions from "./SolveOptions";
-import { stringify } from "querystring";
-
-interface Solve {
-  scramble: string;
-  startTime: number;
-  endTime: number;
-}
+import { Solve } from "@/interfaces/Solve";
 
 export default function Timer() {
-  const [solvingTime, setSolvingTime] = useState(0);
+  const [solvingTime, setSolvingTime] = useState<number>(0);
   const holdingTimeRef = useRef<number>(0);
   const startTime = useRef<number>(0);
   const runningTimeId = useRef<any>(null);
   const [solves, setSolves] = useState<Solve[]>([]);
-  const delayHold = useRef<number>(1000);
+  const delayHold = useRef<number>(500);
 
   const isSolving = useRef<boolean>(false);
   const isHoldingSpace = useRef<boolean>(false);
 
   const handleSpaceKeyDown = (event: KeyboardEvent) => {
     if (event.code !== "Space") return;
-    console.log("Space key down");
     const now = Date.now();
     if (!isHoldingSpace.current && !isSolving.current) {
       holdingTimeRef.current = now;
@@ -32,62 +25,44 @@ export default function Timer() {
     if (!isHoldingSpace.current && isSolving.current) {
       clearInterval(runningTimeId.current);
       isSolving.current = false;
+      const lastSolve: Solve = {
+        startTime: startTime.current,
+        endTime: Date.now(),
+        scramble: "F' U' F2 U2 R2 U' R' F R'",
+      };
+      setSolves([...solves, lastSolve]);
+      startTime.current = 0;
+      holdingTimeRef.current = 0;
       return;
     }
-
-    // if (startTime.current > 0) {
-    //   console.log("stop");
-
-    //   const lastSolve: Solve = {
-    //     startTime: startTime.current,
-    //     endTime: Date.now(),
-    //     scramble: "jgfslkhgsd",
-    //   };
-    //   startTime.current = 0;
-    //   holdingTimeRef.current = 0;
-    //   clearInterval(runningTimeId.current);
-    //   setSolves([...solves, lastSolve]);
-    // }
   };
 
   const handleSpaceKeyUp = (event: KeyboardEvent) => {
     if (event.code === "Space") {
       const now = Date.now();
-      const difference = now - holdingTimeRef.current;
-      console.log(difference);
+      const difference: number = now - holdingTimeRef.current;
+
       if (isHoldingSpace.current && !isSolving.current) {
         if (difference >= delayHold.current) {
           isSolving.current = true;
           isHoldingSpace.current = false;
+          holdingTimeRef.current = 0;
           startTime.current = Date.now();
           runningTimeId.current = setInterval(() => {
             setSolvingTime(Date.now() - (startTime.current || 0));
-            console.log(solvingTime);
           });
           return;
         }
-      }
 
-      if (difference <= delayHold.current) {
-        isSolving.current = false;
-        isHoldingSpace.current = false;
-        holdingTimeRef.current = 0;
+        if (difference <= delayHold.current) {
+          isSolving.current = false;
+          isHoldingSpace.current = false;
+          holdingTimeRef.current = 0;
+          return;
+        }
       }
-
-      // if (now - holdingTimeRef.current <= delayHold.current) {
-      //   console.log("early", now - (holdingTimeRef.current || 0));
-      //   holdingTimeRef.current = 0;
-      // } else if (now - (holdingTimeRef.current || 0) >= delayHold.current) {
-      //   console.log("readyyy", now - (holdingTimeRef.current || 0));
-      //   startTime.current = now;
-      //   runningTimeId.current = setInterval(() => {
-      //     setSolvingTime(now - (startTime.current || 0));
-      //   });
-      // }
     }
   };
-
-  console.log(solvingTime);
 
   useEffect(() => {
     console.log("Effect setup");
