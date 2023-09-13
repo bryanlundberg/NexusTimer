602e787bd9c61428e364a828435d25dc642ab6d2
rojonaitor <bryanlundberg@outlook.com>
2023-09-13 05:43:15 -0600

    add better design navigation bar

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -9,23 +9,39 @@ export default function Navigation() {
     <nav>
       <ul className="flex p-3 gap-5 justify-center">
         <li className="p-4 hover:bg-gray-800">
-          <Link href="/">
+          <Link
+            href="/"
+            className="flex flex-col justify-center items-center font-medium"
+          >
             <Clock />
+            <div>Timer</div>
           </Link>
         </li>
         <li className="p-4 hover:bg-gray-800">
-          <Link href="/solves">
+          <Link
+            href="/solves"
+            className="flex flex-col justify-center items-center font-medium"
+          >
             <Stack />
+            <div>Historial</div>
           </Link>
         </li>
         <li className="p-4 hover:bg-gray-800">
-          <Link href="/stats">
+          <Link
+            href="/stats"
+            className="flex flex-col justify-center items-center font-medium"
+          >
             <Metrics />
+            <div>Statistics</div>
           </Link>
         </li>
         <li className="p-4 hover:bg-gray-800">
-          <Link href="/cubes">
+          <Link
+            href="/cubes"
+            className="flex flex-col justify-center items-center font-medium"
+          >
             <Cubes />
+            <div>Cubes</div>
           </Link>
         </li>
       </ul>
