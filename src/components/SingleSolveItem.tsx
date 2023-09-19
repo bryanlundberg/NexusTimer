31e893b3c69ed0fc1cfed75424a677b9502267a9
rojonaitor <bryanlundberg@outlook.com>
2023-09-19 09:25:09 -0600

    changed solver order

--- src/components/SingleSolveItem.tsx
+++ src/components/SingleSolveItem.tsx
@@ -4,7 +4,7 @@ export default function SingleSolveItem({ solve }: { solve: Solve }) {
   return (
     <>
       <div className="text-lg font-medium relative flex items-center justify-center p-1 rounded-md bg-zinc-700 text-center w-28 h-12">
-        9.12
+        {(solve.time / 1000).toFixed(2)}
         <div className="absolute font-light top-1 left-1 text-xs">11/08</div>
       </div>
     </>
