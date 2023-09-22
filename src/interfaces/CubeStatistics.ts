bd3b554e87f5376ead55afe9486d39f22e0d849e
rojonaitor <bryanlundberg@outlook.com>
2023-09-22 06:38:40 -0600

    calculate averages

---
+++ src/interfaces/CubeStatistics.ts
@@ -0,0 +1,10 @@
+interface CubeStatistics {
+  count: number;
+  best: number;
+  ao3: number;
+  ao5: number;
+  ao12: number;
+  ao50: number;
+  ao100: number;
+  ao1000: number;
+}
---
+++ src/lib/calcStatistics.ts
@@ -0,0 +1,71 @@
+import { sort } from "fast-sort";
+import findCube from "./findCube";
+import { Solve } from "@/interfaces/Solve";
+
+export default function calcStatistics({
+  cubeId,
+  typeSearch = "session",
+}: {
+  cubeId: string;
+  typeSearch: "session" | "all";
+}): CubeStatistics {
+  const defaultResult: CubeStatistics = {
+    count: 0,
+    best: 0,
+    ao3: 0,
+    ao5: 0,
+    ao12: 0,
+    ao50: 0,
+    ao100: 0,
+    ao1000: 0,
+  };
+
+  const cube = findCube({ cubeId: cubeId });
+  if (!cube) return defaultResult;
+
+  if (typeSearch === "session") {
+    defaultResult.count = cube.solves.session.length;
+    const sortByTime = sort(cube.solves.session).asc((u) => u.time);
+    if (sortByTime.length <= 0) return defaultResult;
+    defaultResult.best = sortByTime[0].time;
+    const sortByDate = sort(cube.solves.session).desc((u) => u.endTime);
+    if (sortByDate.length >= 3) {
+      defaultResult.ao3 = calculateCurrentAo(sortByDate, 3);
+    }
+    if (sortByDate.length >= 5) {
+      defaultResult.ao5 = calculateCurrentAo(sortByDate, 5);
+    }
+    if (sortByDate.length >= 12) {
+      defaultResult.ao12 = calculateCurrentAo(sortByDate, 12);
+    }
+    if (sortByDate.length >= 50) {
+      defaultResult.ao50 = calculateCurrentAo(sortByDate, 50);
+    }
+    if (sortByDate.length >= 100) {
+      defaultResult.ao100 = calculateCurrentAo(sortByDate, 100);
+    }
+    if (sortByDate.length >= 1000) {
+      defaultResult.ao1000 = calculateCurrentAo(sortByDate, 100);
+    }
+  } else {
+    const sortByTime = sort(cube.solves.all).asc((u) => u.time);
+    if (sortByTime.length <= 0) return defaultResult;
+    defaultResult.best = sortByTime[0].time;
+  }
+  return defaultResult;
+}
+
+function calculateCurrentAo(solves: Solve[], ao: number) {
+  let result = 0;
+  if (solves.length < ao) {
+    return result;
+  }
+
+  const cubeAo = solves.slice(0, ao);
+  const sum = cubeAo.reduce(
+    (accumulator, currentValue) => accumulator + currentValue.time,
+    0
+  );
+  result = sum / ao;
+  return result;
+}
