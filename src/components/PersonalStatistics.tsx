d1744b18a5f2c9e92d71d105e6001c4fdee392f0
rojonaitor <bryanlundberg@outlook.com>
2023-09-23 18:10:36 -0600

    remove experiments charts

--- src/components/PersonalStatistics.tsx
+++ src/components/PersonalStatistics.tsx
@@ -1,40 +1,8 @@
-import { Doughnut } from "react-chartjs-2";
-import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
 import { useTimerStore } from "@/store/timerStore";
 import CardStatistic from "./CardStatistic";
-import AreaChart from "./AreaChart";
-
-ChartJS.register(ArcElement, Tooltip, Legend);
 
 export default function PersonalStatistics() {
   const { cubes } = useTimerStore();
-  const data = {
-    labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-    datasets: [
-      {
-        label: "# of Votes",
-        data: [12, 19, 3, 5, 2, 53],
-        backgroundColor: [
-          "rgba(255, 99, 132, 0.2)",
-          "rgba(54, 162, 235, 0.2)",
-          "rgba(255, 206, 86, 0.2)",
-          "rgba(75, 192, 192, 0.2)",
-          "rgba(153, 102, 255, 0.2)",
-          "rgba(255, 159, 64, 0.2)",
-        ],
-        borderColor: [
-          "rgba(255, 99, 132, 1)",
-          "rgba(54, 162, 235, 1)",
-          "rgba(255, 206, 86, 1)",
-          "rgba(75, 192, 192, 1)",
-          "rgba(153, 102, 255, 1)",
-          "rgba(255, 159, 64, 1)",
-        ],
-        borderWidth: 1,
-      },
-    ],
-  };
-
   return (
     <>
       <div className="flex flex-col gap-3 px-3 py-3 grow overflow-auto">
@@ -49,11 +17,11 @@ export default function PersonalStatistics() {
           <CardStatistic label="Success Rate" total={"98.8%"} />
         </div>
         <div className="flex flex-col lg:flex-row gap-3">
-          <div className="flex flex-col border rounded-md border-zinc-800 p-3 w-full sm:h-96 justify-between items-center mx-auto">
-            {cubes && <Doughnut data={data} />}
+          <div className="flex flex-col border rounded-md border-zinc-800 p-3 w-full sm:h-96 justify-center items-center">
+            <div>Pending Dounough Chart</div>
           </div>
-          <div className="flex flex-col border rounded-md border-zinc-800 p-3 w-full sm:h-96 justify-center items-center mx-auto">
-            {cubes && <AreaChart />}
+          <div className="flex flex-col border rounded-md border-zinc-800 p-3 w-full sm:h-96 justify-center items-center">
+            <div>Pending Area Chart</div>
           </div>
         </div>
         <LastActivity />
