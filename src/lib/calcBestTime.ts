2b83ac8bc5631ad50438b46821434268aad5271f
rojonaitor <bryanlundberg@outlook.com>
2023-10-05 12:11:42 -0600

    fix calc best time

--- src/lib/calcBestTime.ts
+++ src/lib/calcBestTime.ts
@@ -1,5 +1,6 @@
 import { Categories } from "@/interfaces/Categories";
 import getSolvesMetrics from "./getSolvesMetrics";
+import { sort } from "fast-sort";
 
 export default function calcBestTime(category: Categories, cubeName: string) {
   const { global, session, cubeAll, cubeSession } = getSolvesMetrics(
@@ -7,10 +8,15 @@ export default function calcBestTime(category: Categories, cubeName: string) {
     cubeName
   );
 
+  const bestGlobal = sort(global).asc((u) => u.time);
+  const bestSession = sort(global).asc((u) => u.time);
+  const bestCubeAll = sort(global).asc((u) => u.time);
+  const bestCubeSession = sort(global).asc((u) => u.time);
+
   return {
-    global: global.length > 0 ? global[0].time / 1000 : "--",
-    session: session.length > 0 ? session[0].time / 1000 : "--",
-    cubeAll: cubeAll.length > 0 ? cubeAll[0].time / 1000 : "--",
-    cubeSession: cubeSession.length > 0 ? cubeSession[0].time / 1000 : "--",
+    global: global.length > 0 ? bestGlobal[0].time / 1000 : "--",
+    session: session.length > 0 ? bestSession[0].time / 1000 : "--",
+    cubeAll: cubeAll.length > 0 ? bestCubeAll[0].time / 1000 : "--",
+    cubeSession: cubeSession.length > 0 ? bestCubeSession[0].time / 1000 : "--",
   };
 }
