015613538387b601c15a983a37798cacec5eb7ec
Kritesh Timsina <kriteshtimsina55@gmail.com>
2023-10-11 18:33:44 +0545

    feat:change color of active tabs

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -5,25 +5,27 @@ import Stack from "@/icons/Stack";
 import { useSettingsModalStore } from "@/store/SettingsModalStore";
 import Link from "next/link";
 import translation from "@/translations/global.json";
+import { usePathname } from "next/navigation";
 
 export default function Navigation() {
   const { lang } = useSettingsModalStore();
+  const pathname = usePathname();
   return (
     <nav>
       <ul className="flex justify-center gap-5 ">
-        <NavItem url="/">
+        <NavItem pathname={pathname} url="/">
           <Clock />
           <div>{translation.timer["header"][lang]}</div>
         </NavItem>
-        <NavItem url="/solves">
+        <NavItem pathname={pathname} url="/solves">
           <Stack />
           <div>{translation.solves["header"][lang]}</div>
         </NavItem>
-        <NavItem url="/stats">
+        <NavItem pathname={pathname} url="/stats">
           <Metrics />
           <div>{translation.metrics["header"][lang]}</div>
         </NavItem>
-        <NavItem url="/cubes">
+        <NavItem pathname={pathname} url="/cubes">
           <Cubes />
           <div>{translation.cubes["header"][lang]}</div>
         </NavItem>
@@ -35,16 +37,20 @@ export default function Navigation() {
 function NavItem({
   children,
   url,
+  pathname,
 }: {
   children: React.ReactNode;
   url: string;
+  pathname: string;
 }) {
   return (
     <>
       <li>
         <Link
           href={url}
-          className="min-w-[70px] py-2 rounded-md hover:bg-zinc-800 flex flex-col justify-center items-center font-medium"
+          className={`${
+            pathname === url ? "text-neutral-100" : "text-neutral-400"
+          } min-w-[70px] py-2 rounded-md hover:bg-zinc-800 flex flex-col justify-center items-center font-medium`}
         >
           {children}
         </Link>
