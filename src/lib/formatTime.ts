b5978ac8a4a7019f2728ee4c54e3e3c4be1b2081
Diwash Dahal <diwashdahal75@gmail.com>
2023-10-12 22:52:12 +0545

    improves time readability

---
+++ src/lib/formatTime.ts
@@ -0,0 +1,17 @@
+export default function formatTime(timeInMs: number): string {
+  const milliseconds = timeInMs % 1000;
+  const seconds = Math.floor((timeInMs / 1000) % 60);
+  const minutes = Math.floor((timeInMs / (60 * 1000)) % 60);
+
+  const formattedTime =
+    (minutes > 0 ? padTo2Digits(minutes) + ":" : "") +
+    padTo2Digits(seconds) +
+    "." +
+    (milliseconds / 1000).toFixed(2).slice(2);
+
+  return formattedTime;
+}
+
+function padTo2Digits(num: number): string {
+  return num.toString().padStart(2, "0");
+}
