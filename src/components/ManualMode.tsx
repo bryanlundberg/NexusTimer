c682a772f7cbfb37467a344975a7e58ddbc729b3
rojonaitor <bryanlundberg@outlook.com>
2023-10-15 15:19:44 -0600

    abstractTime to ms

--- src/components/ManualMode.tsx
+++ src/components/ManualMode.tsx
@@ -1,3 +1,7 @@
+import addSolve from "@/lib/addSolve";
+import convertToMs from "@/lib/convertToMs";
+import formatTime from "@/lib/formatTime";
+import genId from "@/lib/genId";
 import { useTimerStore } from "@/store/timerStore";
 import { useState } from "react";
 
@@ -11,6 +15,24 @@ export default function ManualMode() {
         onSubmit={(e) => {
           e.preventDefault();
           setValue("");
+
+          if (!selectedCube) return;
+
+          const msTime = convertToMs(value);
+          // const lastSolve: Solve = {
+          //   id: genId(),
+          //   startTime: startTime.current,
+          //   endTime: endTimeRef.current,
+          //   scramble: scramble,
+          //   bookmark: false,
+          //   time: solvingTime,
+          //   dnf: false,
+          //   plus2: false,
+          //   rating: Math.floor(Math.random() * 20) + scramble.length,
+          //   category: selectedCube.category,
+          //   cubeId: selectedCube.id,
+          // };
+          // addSolve({cubeId: selectedCube.id, solve: })
         }}
         className="flex flex-col items-center"
       >
@@ -23,11 +45,15 @@ export default function ManualMode() {
           className="w-full max-w-[750px] h-20 text-6xl font-medium text-center border rounded-md outline-none appearance-none cursor-pointer bg-zinc-900 focus:cursor-text py-14 border-zinc-800 focus:border-neutral-300 text-nexutral-200"
           onChange={(e) => {
             if (!selectedCube) return;
-            setValue(e.target.value);
+            if (parseInt(e.target.value) <= 595959 || e.target.value === "") {
+              setValue(e.target.value);
+            }
           }}
         />
         {value !== "" ? (
-          <div className="text-center mt-1">Preview: 0:00.00 </div>
+          <div className="mt-1 text-center">
+            Preview: {formatTime(convertToMs(value))}{" "}
+          </div>
         ) : null}
       </form>
     </>
---
+++ src/lib/convertToMs.ts
@@ -0,0 +1,66 @@
+export default function convertToMs(number: string): number {
+  const time = {
+    hh: 0,
+    mm: 0,
+    ss: 0,
+    ms: 0,
+  };
+  if (number.length <= 2) {
+    const ms = parseInt(number);
+    time.ms = ms;
+  }
+
+  if (number.length === 3) {
+    const ss = parseInt(number.slice(0, 1));
+    const ms = parseInt(number.slice(1, 3));
+    time.ss = ss;
+    time.ms = ms;
+  }
+
+  if (number.length === 4) {
+    const ss = parseInt(number.slice(0, 2));
+    const ms = parseInt(number.slice(2, 4));
+    if (ss >= 60) {
+      const mm = Math.floor(ss / 60);
+      const sss = ss % 60;
+      time.mm += mm;
+      time.ss += sss;
+    } else {
+      time.ss += ss;
+    }
+    time.ms += ms;
+  }
+
+  if (number.length === 5) {
+    const mm = parseInt(number.slice(0, 1));
+    const ss = parseInt(number.slice(1, 3));
+    const ms = parseInt(number.slice(3, 5));
+    if (ss >= 60) {
+      const mm = Math.floor(ss / 60);
+      const sss = ss % 60;
+      time.mm += mm;
+      time.ss += sss;
+    } else {
+      time.ss += ss;
+    }
+    time.mm += mm;
+    time.ms += ms;
+  }
+
+  if (number.length === 6) {
+    const mm = parseInt(number.slice(0, 2));
+    const ss = parseInt(number.slice(2, 4));
+    const ms = parseInt(number.slice(4, 6));
+    if (ss >= 60) {
+      const mm = Math.floor(ss / 60);
+      const sss = ss % 60;
+      time.mm += mm;
+      time.ss += sss;
+    } else {
+      time.ss += ss;
+    }
+    time.mm += mm;
+    time.ms += ms;
+  }
+  return time.mm * 60000 + time.ss * 1000 + time.ms * 10;
+}
