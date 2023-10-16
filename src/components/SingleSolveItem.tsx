180c7a83e6a74b0bd321c8a7cbbe37ff62230a25
abishek0057 <abishekparajuli64@gmail.com>
2023-10-16 08:41:24 +0545

    fix: format time where time in ms

--- src/components/SingleSolveItem.tsx
+++ src/components/SingleSolveItem.tsx
@@ -1,6 +1,7 @@
 import { Solve } from "@/interfaces/Solve";
 import formatDate from "@/lib/formatDate";
 import { useSolvesStore } from "@/store/SolvesStore";
+import formatTime from "@/lib/formatTime";
 
 export default function SingleSolveItem({ solve }: { solve: Solve }) {
   const { setStatus, setSolve } = useSolvesStore();
@@ -13,7 +14,7 @@ export default function SingleSolveItem({ solve }: { solve: Solve }) {
         }}
         className="relative flex items-center justify-center w-auto p-1 text-lg font-medium text-center rounded-md cursor-pointer z-1 bg-neutral-300 hover:bg-neutral-400 text-zinc-800 h-14"
       >
-        {(solve.time / 1000).toFixed(2)}
+        {formatTime(solve.time)}
         {solve.plus2 ? <span className="text-sm text-red-600">+2</span> : null}
         <div className="absolute z-20 text-xs top-1 left-1">
           {formatDate(solve.endTime).slice(0, 5)}
