0e325e3cbf8ab0c7c290262150f8a41d9b6c5701
rojonaitor <bryanlundberg@outlook.com>
2023-10-24 06:11:31 -0600

    updated button classNames

--- src/components/menu-settings/DataImportExport.tsx
+++ src/components/menu-settings/DataImportExport.tsx
@@ -6,28 +6,28 @@ import { useRef } from "react";
 import importDataFromFile from "@/lib/importDataFromFile";
 
 interface DataImportExport {
-  lang: Language
+  lang: Language;
 }
 
 export function DataImportExport({ lang }: DataImportExport) {
   const dataInputRef = useRef<HTMLInputElement>(null);
 
   return (
-    <div className="flex gap-2 justify-center">
+    <div className="flex justify-center gap-2">
       <input
         type="file"
         accept=".txt"
         ref={dataInputRef}
-        style={{ display: 'none' }} // Hide the file input element
+        className="hidden"
         onChange={importDataFromFile}
       />
       <Button
-        className="font-normal"
+        className="font-normal transition duration-400 dark:hover:bg-blue-500 light:hover:bg-blue-500 light:hover:text-neutral-100 dark:hover:text-neutral-100 dark:border-neutral-200 light:border-neutral-200"
         label={translation.settings["import-from-file"][lang]}
         onClick={() => dataInputRef.current && dataInputRef.current.click()}
       />
       <Button
-        className="font-normal"
+        className="font-normal transition duration-400 dark:hover:bg-blue-500 light:hover:bg-blue-500 light:hover:text-neutral-100 dark:hover:text-neutral-100 dark:border-neutral-200 light:border-neutral-200"
         label={translation.settings["export-to-file"][lang]}
         onClick={exportDataToFile}
       />
