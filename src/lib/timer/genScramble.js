32adf92fe8580c68a88c880370c2bcb27340bf33
rojonaitor <bryanlundberg@outlook.com>
2023-10-29 14:44:34 -0600

    fix scramble generator for 333oh category

--- src/lib/timer/genScramble.js
+++ src/lib/timer/genScramble.js
@@ -1,7 +1,13 @@
 import { Scrambow } from "scrambow";
+import { cubeCollection } from "../cubeCollection";
 
 export default function genScramble(category) {
-  const scramble = new Scrambow(category);
+  const eventId = cubeCollection.find((cube) => cube.name === category);
+  if (!eventId) {
+    console.error("Evento no encontrado");
+    return "Error: Scrambler not available for this category.";
+  }
+  const scramble = new Scrambow(eventId.event);
   const newScramble = scramble.get(1);
   return newScramble[0].scramble_string;
 }
