24694c0663367d20a154813053db933ba316e9b1
Alagappan N <alagappanforwork@gmail.com>
2023-11-17 20:20:33 +0530

    cleanup: Adds semoicolans and maintains format throughout the code

--- src/components/charts/LineCharter.tsx
+++ src/components/charts/LineCharter.tsx
@@ -18,17 +18,17 @@ const chartOptions: any = {
   },
   localization: {
     priceFormatter: (time: number) => {
-      const timeT = formatTime(time)
-      console.log(time)
-      return timeT
+      const timeT = formatTime(time);
+      console.log(time);
+      return timeT;
     },
     timeFormatter: (time: number) => {
-      return time.toString()
+      return time.toString();
     },
   },
   timeScale: {
     tickMarkFormatter: (time: number) => {
-      return time.toString()
+      return time.toString();
     },
   },
   priceScale: {
@@ -53,23 +53,23 @@ export default function LineCharter({
   useEffect(() => {
     const container = chartContainerRef.current;
     if (container) {
-      container.innerHTML = ""
-      const chart = createChart(container, chartOptions)
-      const dataArray = cubeSelected ? data.cubeAll : data.global
-      const structuredData: any[] = []
+      container.innerHTML = "";
+      const chart = createChart(container, chartOptions);
+      const dataArray = cubeSelected ? data.cubeAll : data.global;
+      const structuredData: any[] = [];
       dataArray.forEach((i: Solve, index: number) => {
-        console.log({ time: index, value: i.time })
+        console.log({ time: index, value: i.time });
         structuredData.unshift({
           time: dataArray.length - index,
           value: i.time,
-        })
-      })
+        });
+      });
 
       const lineSeries = chart.addLineSeries({
         color: cubeSelected ? "#2962FF" : "#F4D03F",
         lastValueVisible: false,
         priceLineVisible: false,
-      })
+      });
 
       const getMeanTime = (data: TimeObject[]) => {
         return data.length
@@ -78,8 +78,8 @@ export default function LineCharter({
                 total + timeObject.value,
               0
             ) / data.length
-          : 0
-      }
+          : 0;
+      };
 
       const meanTimeLine: CreatePriceLineOptions = {
         price: getMeanTime(structuredData),
@@ -88,24 +88,24 @@ export default function LineCharter({
         lineStyle: 0,
         axisLabelVisible: true,
         title: "Mean Time",
-      }
+      };
 
-      lineSeries.setData(structuredData)
-      lineSeries.createPriceLine(meanTimeLine)
+      lineSeries.setData(structuredData);
+      lineSeries.createPriceLine(meanTimeLine);
 
-      chart.autoSizeActive()
-      chart.timeScale().fitContent()
+      chart.autoSizeActive();
+      chart.timeScale().fitContent();
 
       // Make chart responsive with screen resize
       new ResizeObserver((entries) => {
         if (entries.length === 0 || entries[0].target !== container) {
-          return
+          return;
         }
-        const newRect = entries[0].contentRect
-        chart.applyOptions({ height: newRect.height, width: newRect.width })
-      }).observe(container)
+        const newRect = entries[0].contentRect;
+        chart.applyOptions({ height: newRect.height, width: newRect.width });
+      }).observe(container);
     }
-  }, [data, cubeSelected])
+  }, [data, cubeSelected]);
 
-  return <div ref={chartContainerRef} className="w-full h-full"></div>
+  return <div ref={chartContainerRef} className="w-full h-full"></div>;
 }
