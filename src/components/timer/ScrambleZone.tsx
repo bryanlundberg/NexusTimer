6d00989becad32fe13e260117d7e11c0adb40890
rojonaitor <bryanlundberg@outlook.com>
2023-11-18 11:18:16 -0600

    fix: scroll scramble

--- src/components/timer/ScrambleZone.tsx
+++ src/components/timer/ScrambleZone.tsx
@@ -8,7 +8,6 @@ export function ScrambleZone() {
   return (
     <>
       <div
-        id="touch"
         className={
           settings.features.scrambleBackground.status
             ? "h-auto p-2 overflow-auto text-2xl font-medium text-center rounded-md min-w-auto sm:max-w-screen-sm md:max-w-screen-md lg:max-w-screen-lg max-h-52 dark:bg-zinc-900 light:bg-neutral-100"
