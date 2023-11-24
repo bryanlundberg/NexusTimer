2a1bfa531480d4d3713d56f8a74e1add3239460a
rojonaitor <bryanlundberg@outlook.com>
2023-11-24 07:37:29 -0600

    feat: add best average text alert

--- src/components/timer/TimerWidgets.tsx
+++ src/components/timer/TimerWidgets.tsx
@@ -7,13 +7,19 @@ export default function TimerWidgets() {
   const { isSolving } = useTimerStore();
   if (isSolving) return null;
   return (
-    <div
-      id="touch"
-      className="flex items-center justify-between w-full h-20 text-xs sm:h-20 md:h-24 lg:h-32 md:text-sm"
-    >
-      <OverviewPanel />
-      <ScramblePanel />
-      <StatisticsPanel />
-    </div>
+    <>
+      <div className="flex flex-col gap-1 sm:gap-0" id="touch">
+        <div className="flex justify-end">
+          <div className="px-1 text-xs border rounded-md">
+            New best average!
+          </div>
+        </div>
+        <div className="flex items-center justify-between w-full h-20 text-xs sm:h-20 md:h-24 lg:h-32 md:text-sm">
+          <OverviewPanel />
+          <ScramblePanel />
+          <StatisticsPanel />
+        </div>
+      </div>
+    </>
   );
 }
