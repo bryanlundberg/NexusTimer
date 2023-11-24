9bf0a37d88dac752b777d2092bdd20448a212519
rojonaitor <bryanlundberg@outlook.com>
2023-11-24 07:43:35 -0600

    feat: add translation for alert msg

--- src/components/timer/TimerWidgets.tsx
+++ src/components/timer/TimerWidgets.tsx
@@ -2,18 +2,23 @@ import StatisticsPanel from "./StatisticsPanel";
 import OverviewPanel from "./OverviewPanel";
 import ScramblePanel from "./ScrambleImagePanel";
 import { useTimerStore } from "@/store/timerStore";
+import translation from "@/translations/global.json";
+import { useSettingsModalStore } from "@/store/SettingsModalStore";
 
 export default function TimerWidgets() {
   const { isSolving } = useTimerStore();
+  const { lang, settings } = useSettingsModalStore();
   if (isSolving) return null;
   return (
     <>
-      <div className="flex flex-col gap-1 sm:gap-0" id="touch">
-        <div className="flex justify-end">
-          <div className="px-1 text-xs border rounded-md">
-            New best average!
+      <div className="flex flex-col gap-1" id="touch">
+        {settings.alerts.bestAverage.status && (
+          <div className="flex justify-end">
+            <div className="px-1 text-xs border rounded-md">
+              {translation.timer["new_best_average"][lang]}
+            </div>
           </div>
-        </div>
+        )}
         <div className="flex items-center justify-between w-full h-20 text-xs sm:h-20 md:h-24 lg:h-32 md:text-sm">
           <OverviewPanel />
           <ScramblePanel />
