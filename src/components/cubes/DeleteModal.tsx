d34f7b86b2d3c161023148069f8db20eabf86160
rojonaitor <bryanlundberg@outlook.com>
2023-11-26 17:12:23 -0600

    style: delete modal

--- src/components/cubes/DeleteModal.tsx
+++ src/components/cubes/DeleteModal.tsx
@@ -12,36 +12,55 @@ export default function DeleteModal({
   return (
     <>
       <div className="fixed top-0 left-0 z-50 flex items-center justify-center w-full h-screen text-black bg-opacity-80 bg-neutral-900">
-        <div className="flex flex-col w-auto h-auto gap-3 p-4 bg-white rounded-lg shadow-lg">
+        <div className="flex flex-col w-full h-auto gap-3 p-3 m-8 bg-white rounded-lg shadow-lg sm:w-96">
+          <div className="text-lg font-bold text-center">
+            Are you sure you want to delete?
+          </div>
+
           <div className="flex gap-2">
             <div className="flex-1 text-end">
-              <div className="text-black">Name:</div>
+              <div className="w-full h-5 text-black">Name:</div>
               <div className="text-black">Category: </div>
               <div className="text-black">Best time: </div>
               <div className="text-black">Best Ao5: </div>
               <div className="text-black">Count: </div>
             </div>
 
-            <div className="flex-1">
-              <div className="text-black">{cubeData.name}</div>
-              <div className="text-black">{cubeData.category}</div>
-              <div className="text-black">{cubeData.best}</div>
-              <div className="text-black">{cubeData.ao5}</div>
-              <div className="text-black">{cubeData.count}</div>
+            <div className="flex-1 overflow-hidden">
+              <div className="w-full h-5 overflow-hidden text-black">
+                {cubeData.name}
+              </div>
+              <div className="w-full overflow-hidden text-black">
+                {cubeData.category}
+              </div>
+              <div className="w-auto overflow-hidden text-black">
+                {cubeData.best}
+              </div>
+              <div className="w-auto overflow-hidden text-black">
+                {cubeData.ao5}
+              </div>
+              <div className="w-auto overflow-hidden text-black">
+                {cubeData.count}
+              </div>
             </div>
           </div>
 
-          <div className="flex justify-end w-full h-10 gap-3">
+          <div className="w-11/12 mx-auto text-xs text-center">
+            This irreversible action will delete all solve history for this cube
+            and may impact your statistics.
+          </div>
+
+          <div className="flex justify-center w-full h-10 gap-3">
             <button
               onClick={cancelDelete}
-              className="px-4 py-2 rounded-lg bg-neutral-300 text-neutral-900 hover:bg-neutral-400"
+              className="px-4 py-2 transition duration-200 rounded-lg bg-neutral-200 text-neutral-900 hover:bg-neutral-300"
             >
               Cancel
             </button>
 
             <button
               onClick={confirmDelete}
-              className="px-4 py-2 text-white bg-red-600 rounded-lg hover-bg-red-700"
+              className="px-4 py-2 text-white transition duration-200 bg-red-600 rounded-lg hover:bg-red-700"
             >
               Confirm
             </button>
