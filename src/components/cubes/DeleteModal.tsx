dce91882d64cd25405eca5fa8117e162c07e369f
rojonaitor <bryanlundberg@outlook.com>
2023-11-27 18:42:39 -0600

    feat: add translations for delete modal text

--- src/components/cubes/DeleteModal.tsx
+++ src/components/cubes/DeleteModal.tsx
@@ -1,4 +1,7 @@
 import { DeleteCubeDetails } from "@/interfaces/DeleteCubeDetails";
+import { useSettingsModalStore } from "@/store/SettingsModalStore";
+import translation from "@/translations/global.json";
+
 export default function DeleteModal({
   confirmDelete,
   cancelDelete,
@@ -8,13 +11,14 @@ export default function DeleteModal({
   cancelDelete: () => void;
   cubeData: DeleteCubeDetails | null;
 }) {
+  const { lang } = useSettingsModalStore();
   if (!cubeData) return;
   return (
     <>
       <div className="fixed top-0 left-0 z-50 flex items-center justify-center w-full h-screen text-black bg-opacity-80 bg-neutral-900">
         <div className="flex flex-col w-full h-auto gap-3 p-3 m-8 bg-white rounded-lg shadow-lg sm:w-96">
           <div className="text-lg font-medium text-center">
-            Are you sure you want to delete?
+            {translation.cubes.modal["question-delete"][lang]}
           </div>
 
           <div className="px-2 mx-auto font-mono text-center text-black bg-yellow-300 w-fit text-md ">
@@ -23,10 +27,18 @@ export default function DeleteModal({
 
           <div className="flex justify-center gap-2 ">
             <div className="flex flex-col text-end">
-              <div className="text-black">Category: </div>
-              <div className="text-black">Best time: </div>
-              <div className="text-black">Best Ao5: </div>
-              <div className="text-black">Count: </div>
+              <div className="text-black">
+                {translation.cubes.table["category"][lang]}:{" "}
+              </div>
+              <div className="text-black">
+                {translation.settings["best-time"][lang]}:{" "}
+              </div>
+              <div className="text-black">
+                {translation.timer["best"][lang]} Ao5:{" "}
+              </div>
+              <div className="text-black">
+                {translation.timer["counter"][lang]}:{" "}
+              </div>
             </div>
 
             <div className="flex flex-col text-start">
@@ -46,8 +58,7 @@ export default function DeleteModal({
           </div>
 
           <div className="w-11/12 mx-auto text-xs text-center">
-            This irreversible action will delete all solve history for this cube
-            and may impact your statistics.
+            {translation.cubes.modal["warning-delete"][lang]}
           </div>
 
           <div className="flex justify-center w-full h-10 gap-3">
@@ -55,14 +66,14 @@ export default function DeleteModal({
               onClick={cancelDelete}
               className="px-4 py-2 transition duration-200 rounded-lg bg-neutral-200 text-neutral-900 hover:bg-neutral-300"
             >
-              Cancel
+              {translation.inputs["cancel"][lang]}
             </button>
 
             <button
               onClick={confirmDelete}
               className="px-4 py-2 text-white transition duration-200 bg-red-600 rounded-lg hover:bg-red-700"
             >
-              Confirm
+              {translation.inputs["confirm"][lang]}
             </button>
           </div>
         </div>
