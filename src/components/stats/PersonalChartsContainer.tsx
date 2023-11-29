f9b3ddf12733247a67711c8c4c3ddb736b1b3279
rojonaitor <bryanlundberg@outlook.com>
2023-11-28 18:42:05 -0600

    style: remove points chart from metrics layout

--- src/components/stats/PersonalChartsContainer.tsx
+++ src/components/stats/PersonalChartsContainer.tsx
@@ -5,7 +5,9 @@ interface PersonalChartsContainer {
 export function PersonalChartsContainer({ children }: PersonalChartsContainer) {
   return (
     <>
-      <div className="flex flex-col gap-3 md:flex-row">{children}</div>
+      <div className="w-full h-full p-3 border rounded-md light:border-neutral-200 dark:border-zinc-800 dark:bg-zinc-950 light:bg-neutral-100">
+        {children}
+      </div>
     </>
   );
 }
