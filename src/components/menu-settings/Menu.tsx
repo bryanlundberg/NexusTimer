a408c6cc327a34da78ead73731911607aeef7529
rojonaitor <bryanlundberg@outlook.com>
2023-11-30 07:20:37 -0600

    feat: add about section to the project

--- src/components/menu-settings/Menu.tsx
+++ src/components/menu-settings/Menu.tsx
@@ -17,6 +17,9 @@ import { MenuOption } from "./MenuOption";
 import { DataImportExport } from "./DataImportExport";
 import { useTimerStore } from "@/store/timerStore";
 import Folder from "@/icons/Folder";
+import Shield from "@/icons/Shield";
+import GithubIcon from "@/icons/GithubIcon";
+import Link from "next/link";
 
 export default function MenuSettings() {
   const { settingsOpen, setSettingsOpen, settings, setSettings, lang } =
@@ -132,6 +135,23 @@ export default function MenuSettings() {
             title={translation.settings["data"][lang]}
           >
             <DataImportExport lang={lang} />
+          </MenuSection>
+          <MenuSection icon={<Shield />} title="About">
+            <div className="flex flex-col justify-center items-center gap-3">
+              <div className="text-center w-11/12 italic">
+                NexusTimer is a fast, unopinionated, minimalist and opensource
+                web tool for SpeedCubing.
+              </div>
+              <div>Suggest a feature</div>
+              <div>Report a bug</div>
+              <Link
+                href="https://github.com/bryanlundberg/NexusTimer"
+                target="_blank"
+                className="hover:text-zinc-500 transition duration-300"
+              >
+                <GithubIcon />
+              </Link>
+            </div>
           </MenuSection>
         </div>
         {/* Area to the right  -> Its a transparent layer next to menu */}
