92f2389a021e6d488855dac99b6abb52abc5e4c2
rojonaitor <bryanlundberg@outlook.com>
2023-12-20 09:35:56 -0600

    style: modified entire layout.

---
+++ src/icons/ChevronUp.tsx
@@ -0,0 +1,18 @@
+export default function ChevronUp() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth={1.5}
+      stroke="currentColor"
+      className="w-6 h-6"
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="m4.5 15.75 7.5-7.5 7.5 7.5"
+      />
+    </svg>
+  );
+}
