d6529cc166e7e2df68e7722b8d2712b41e2d5f1d
rojonaitor <bryanlundberg@outlook.com>
2023-12-27 09:28:04 -0600

    feat: improve stats view layout

--- src/app/stats/page.tsx
+++ src/app/stats/page.tsx
@@ -1,40 +1,18 @@
 "use client";
 import translation from "@/translations/global.json";
 import { useSettingsModalStore } from "@/store/SettingsModalStore";
-import { Select } from "@/components/select/index";
-import useSelect from "@/hooks/useSelect";
 import { OverallContainer } from "@/components/OverallContainer";
 import { OverallHeader } from "@/components/OverallHeader";
-import { MetricsContent } from "@/components/stats/MetricsContent";
+import CategoryStatistics from "@/components/stats/CategoryStatistics";
 
 export default function StatsPage() {
   const { lang } = useSettingsModalStore();
-  const { selectedValue, handleSelect } = useSelect(
-    translation.metrics["header-select"]["personal"][lang]
-  );
-  const options = [
-    {
-      name: translation.metrics["header-select"]["personal"][lang],
-      id: translation.metrics["header-select"]["personal"][lang],
-    },
-    {
-      name: translation.metrics["header-select"]["category"][lang],
-      id: translation.metrics["header-select"]["category"][lang],
-    },
-  ];
 
   return (
     <>
       <OverallContainer>
-        <OverallHeader title={translation.solves["header"][lang]}>
-          <Select
-            defaultLabel={selectedValue}
-            list={options}
-            className="w-36 sm:w-56"
-            onChange={(selection) => handleSelect(selection)}
-          />
-        </OverallHeader>
-        <MetricsContent selectedValue={selectedValue} />
+        <OverallHeader title={translation.solves["header"][lang]} />
+        <CategoryStatistics />
       </OverallContainer>
     </>
   );
--- src/hooks/useMetricsSwitch.ts
+++ src/hooks/useMetricsSwitch.ts
@@ -1,9 +1,18 @@
 import { Categories } from "@/interfaces/Categories";
+import { Solve } from "@/interfaces/Solve";
+import calcAoStatistics from "@/lib/calcAoStatistics";
+import calcAverageStatistics from "@/lib/calcAverageStatistics";
+import calcBestTime from "@/lib/calcBestTime";
+import calcDeviation from "@/lib/calcDeviation";
+import calcSuccessRate from "@/lib/calcSuccessRate";
+import calcTimeSpentStatistics from "@/lib/calcTimeSpentStatistics";
+import calcTotalSolvesStatistics from "@/lib/calcTotalSolvesStatistics";
 import { cubeCollection } from "@/lib/cubeCollection";
+import getSolvesMetrics from "@/lib/getSolvesMetrics";
 import { useSettingsModalStore } from "@/store/SettingsModalStore";
 import { useTimerStore } from "@/store/timerStore";
 import translation from "@/translations/global.json";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function useMetricsSwitch() {
   const { lang } = useSettingsModalStore();
@@ -12,10 +21,41 @@ export default function useMetricsSwitch() {
   const [filterCube, setFilterCube] = useState(
     translation.solves.filter["all"][lang]
   );
+
   const [optInChart, setOptInChart] = useState({
     mean: true,
     best: false,
   });
+
+  const defaultValues: any = {
+    global: 0,
+    session: 0,
+    cubeSession: 0,
+    cubeAll: 0,
+  };
+
+  interface ChartData {
+    global: Solve[];
+    session: Solve[];
+    cubeSession: Solve[];
+    cubeAll: Solve[];
+  }
+
+  const defaultChartValues: ChartData = {
+    global: [],
+    session: [],
+    cubeSession: [],
+    cubeAll: [],
+  };
+
+  const [average, setAverage] = useState(defaultValues);
+  const [timeSpent, setTimeSpent] = useState(defaultValues);
+  const [counter, setCounter] = useState(defaultValues);
+  const [stats, setStats] = useState(defaultValues);
+  const [deviation, setDeviation] = useState(defaultValues);
+  const [successRate, setSuccessRate] = useState(defaultValues);
+  const [best, setBest] = useState(defaultValues);
+  const [data, setData] = useState(defaultChartValues);
 
   const categoryOptions = loadCategoryOptions();
   const cubeOptions = loadCubeOptions();
@@ -52,14 +92,52 @@ export default function useMetricsSwitch() {
     setFilterCube(value);
   };
 
+  useEffect(() => {
+    setFilterCube(translation.solves.filter["all"][lang]);
+  }, [lang]);
+
+  useEffect(() => {
+    const calculatedAverage = calcAverageStatistics(filterCategory, filterCube);
+    const calculatedTimeSpent = calcTimeSpentStatistics(
+      filterCategory,
+      filterCube
+    );
+    const calculatedCounter = calcTotalSolvesStatistics(
+      filterCategory,
+      filterCube
+    );
+    const calculatedStats = calcAoStatistics(filterCategory, filterCube);
+    const calculatedDeviation = calcDeviation(filterCategory, filterCube);
+    const calculatedSuccessRate = calcSuccessRate(filterCategory, filterCube);
+    const calculatedBest = calcBestTime(filterCategory, filterCube);
+    const calculatedData = getSolvesMetrics(filterCategory, filterCube);
+
+    setAverage(calculatedAverage);
+    setTimeSpent(calculatedTimeSpent);
+    setCounter(calculatedCounter);
+    setStats(calculatedStats);
+    setDeviation(calculatedDeviation);
+    setSuccessRate(calculatedSuccessRate);
+    setBest(calculatedBest);
+    setData(calculatedData);
+  }, [filterCategory, filterCube]);
+
   return {
     filterCategory,
     filterCube,
     handleChangeCategory,
     handleChangeCube,
-    categoryOptions, // Corrected variable name
-    cubeOptions, // Corrected variable name
+    categoryOptions,
+    cubeOptions,
     optInChart,
     setOptInChart,
+    average,
+    timeSpent,
+    counter,
+    stats,
+    deviation,
+    successRate,
+    best,
+    data,
   };
 }
