cffec5147045a088ef3ff76875b6b2e26dd4d281
rojonaitor <bryanlundberg@outlook.com>
2023-12-30 07:10:45 -0600

    style: disabled colors

--- src/components/button/ButtonContainer.tsx
+++ src/components/button/ButtonContainer.tsx
@@ -9,14 +9,14 @@ export function ButtonContainer({
   className,
   children,
   handleClick,
-  isDisabled
+  isDisabled,
 }: ButtonContainer) {
   return (
     <>
       <button
         type="button"
-        className={`min-h-9 px-3 transition duration-200 rounded-md border font-medium justify-center align-middle light:hover:bg-neutral-200 light:text-neutral-950 light:border-neutral-200 dark:hover:bg-zinc-700 dark:hover:border-zinc-500 dark:border-zinc-800 text-md disabled:bg-zinc-900 ${className} disabled:cursor-not-allowed`}
-        onClick={!isDisabled ? handleClick : undefined}
+        className={`min-h-9 px-3 transition duration-200 rounded-md border font-medium justify-center align-middle light:hover:bg-neutral-200 light:hover:border-neutral-400 light:text-neutral-950 light:border-neutral-200 dark:hover:bg-zinc-700 dark:hover:border-zinc-500 dark:border-zinc-800 text-md disabled:bg-zinc-900 dark:disabled:border-zinc-500 dark:disabled:bg-zinc-700 light:disabled:bg-neutral-200 light:disabled:border-neutral-300 ${className} disabled:cursor-normal`}
+        onClick={handleClick}
         disabled={isDisabled}
       >
         {children}
