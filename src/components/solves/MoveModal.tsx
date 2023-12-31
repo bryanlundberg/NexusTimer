03f0b07ea79f6f44d200c2cd687157145100b326
rojonaitor <bryanlundberg@outlook.com>
2023-12-31 10:27:02 -0600

    feat: move alert translations

--- src/components/solves/MoveModal.tsx
+++ src/components/solves/MoveModal.tsx
@@ -28,7 +28,7 @@ export default function MoveModal({
       <div className="fixed top-0 left-0 z-50 flex items-center justify-center w-full h-screen text-black bg-opacity-75 bg-neutral-900">
         <div className="flex flex-col w-full h-auto gap-3 p-3 m-8 bg-white rounded-lg shadow-lg sm:w-96">
           <div className="text-lg font-medium text-center">
-            Finalize session
+            {translation.solves["finalize-session"][lang]}
           </div>
 
           <div className="flex justify-center gap-2 ">
@@ -64,7 +64,7 @@ export default function MoveModal({
           </div>
 
           <div className="w-11/12 mx-auto text-xs text-center">
-            This action will store all active sessions from SQ-1 category.
+            {translation.solves["archive-sessions-legend"][lang]}
           </div>
 
           <div className="flex justify-center w-full h-10 gap-3">
@@ -72,7 +72,7 @@ export default function MoveModal({
               onClick={onCancel}
               className="px-5 py-2 text-sm font-medium text-center transition duration-300 rounded-lg text-neutral-800 bg-neutral-200 hover:bg-neutral-300"
             >
-              Cancel
+              {translation.inputs["cancel"][lang]}
             </button>
 
             <button
@@ -80,7 +80,7 @@ export default function MoveModal({
               className="px-5 py-2 text-sm font-medium text-center text-white transition duration-200 border rounded-md bg-lime-500 border-lime-500 hover:border-lime-600 hover:bg-lime-600"
               autoFocus={true}
             >
-              Confirm
+              {translation.inputs["confirm"][lang]}
             </button>
           </div>
         </div>
