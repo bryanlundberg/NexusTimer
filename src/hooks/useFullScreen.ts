db0d707360e37b6c7006bcca3fc7abc3310732d6
rojonaitor <bryanlundberg@outlook.com>
2023-12-31 07:38:39 -0600

    feat: add Fullscreen mode
    Co-Authored-By: RigvedRocks <43703382+rigvedrocks@users.noreply.github.com>

---
+++ src/hooks/useFullScreen.ts
@@ -0,0 +1,66 @@
+import { useState, useEffect } from "react";
+
+export function useFullScreen() {
+  const [isFullScreen, setIsFullScreen] = useState<boolean>(true);
+
+  useEffect(() => {
+    const handleFullScreenChange = () => {
+      setIsFullScreen(!!document.fullscreenElement);
+    };
+
+    handleFullScreenChange();
+
+    document.addEventListener("fullscreenchange", handleFullScreenChange);
+    document.addEventListener("mozfullscreenchange", handleFullScreenChange);
+    document.addEventListener("webkitfullscreenchange", handleFullScreenChange);
+    document.addEventListener("msfullscreenchange", handleFullScreenChange);
+
+    return () => {
+      document.removeEventListener("fullscreenchange", handleFullScreenChange);
+      document.removeEventListener(
+        "mozfullscreenchange",
+        handleFullScreenChange
+      );
+      document.removeEventListener(
+        "webkitfullscreenchange",
+        handleFullScreenChange
+      );
+      document.removeEventListener(
+        "msfullscreenchange",
+        handleFullScreenChange
+      );
+    };
+  }, []);
+
+  const toggleFullScreen = () => {
+    const element = document.documentElement;
+
+    if (!document.fullscreenElement) {
+      // Enter full-screen mode
+      if (element.requestFullscreen) {
+        element.requestFullscreen();
+      } else if ((element as any).mozRequestFullScreen) {
+        (element as any).mozRequestFullScreen();
+      } else if ((element as any).webkitRequestFullscreen) {
+        (element as any).webkitRequestFullscreen();
+      } else if ((element as any).msRequestFullscreen) {
+        (element as any).msRequestFullscreen();
+      }
+    } else {
+      // Exit full-screen mode
+      if (document.exitFullscreen) {
+        document.exitFullscreen();
+      } else if ((document as any).mozCancelFullScreen) {
+        // Use type assertion for older Firefox
+        (document as any).mozCancelFullScreen();
+      } else if ((document as any).webkitExitFullscreen) {
+        (document as any).webkitExitFullscreen();
+      } else if ((document as any).msExitFullscreen) {
+        // Use type assertion for older IE
+        (document as any).msExitFullscreen();
+      }
+    }
+  };
+
+  return { isFullScreen, toggleFullScreen };
+}
