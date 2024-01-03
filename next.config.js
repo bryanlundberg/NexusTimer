e9a9932f3a23936c60551bd181179c4b6b2dfc83
rojonaitor <bryanlundberg@outlook.com>
2024-01-03 11:57:36 -0600

    initial pwa config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
 
-module.exports = nextConfig;
+const withPWA = require("@ducanh2912/next-pwa").default({
+  dest: "public",
+});
+
+module.exports = withPWA({
+  // Your Next.js config
+});
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,52 @@
 import "./globals.css";
-import type { Metadata } from "next";
+import type { Metadata, Viewport } from "next";
 import PreloadSettings from "@/components/PreloadSettings";
 
+const APP_NAME = "Nexus Timer";
+const APP_DEFAULT_TITLE = "Nexus Timer";
+const APP_TITLE_TEMPLATE = "Nexus Timer";
+const APP_DESCRIPTION =
+  "Nexus Timer is a platform for recording and viewing Rubik's cube statistics. It also includes tools to improve Rubik's cube skills.";
+
 export const metadata: Metadata = {
-  title: "Nexus Timer | For SpeedCubers",
-  description:
-    "Nexus Timer is a platform for recording and viewing Rubik's cube statistics. It also includes tools to improve Rubik's cube skills.",
+  metadataBase: new URL("https://acme.com"),
+  applicationName: APP_NAME,
+  title: {
+    default: APP_DEFAULT_TITLE,
+    template: APP_TITLE_TEMPLATE,
+  },
+  description: APP_DESCRIPTION,
+  manifest: "/manifest.json",
+  appleWebApp: {
+    capable: true,
+    statusBarStyle: "default",
+    title: APP_DEFAULT_TITLE,
+    // startUpImage: [],
+  },
+  formatDetection: {
+    telephone: false,
+  },
+  openGraph: {
+    type: "website",
+    siteName: APP_NAME,
+    title: {
+      default: APP_DEFAULT_TITLE,
+      template: APP_TITLE_TEMPLATE,
+    },
+    description: APP_DESCRIPTION,
+  },
+  twitter: {
+    card: "summary",
+    title: {
+      default: APP_DEFAULT_TITLE,
+      template: APP_TITLE_TEMPLATE,
+    },
+    description: APP_DESCRIPTION,
+  },
+};
+
+export const viewport: Viewport = {
+  themeColor: "#181818",
 };
 
 export default function RootLayout({
