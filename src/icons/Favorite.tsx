fa374c5f90a463be2ed29fc814f08eced94cc6ca
rojonaitor <bryanlundberg@outlook.com>
2024-01-09 09:28:15 -0600

    feat: add icons to solves-area, referencing comment and bookmark.

--- src/icons/Favorite.tsx
+++ src/icons/Favorite.tsx
@@ -4,7 +4,7 @@ export default function Favorite() {
       xmlns="http://www.w3.org/2000/svg"
       viewBox="0 0 24 24"
       fill="currentColor"
-      className="w-6 h-6"
+      className="w-4 h-4"
     >
       <path
         fillRule="evenodd"
