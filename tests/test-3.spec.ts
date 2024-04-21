4becaaf5df60120c1249c420c6edcc31510de12c
rojonaitor <bryanlundberg@outlook.com>
2024-04-21 05:53:04 -0600

    test: initial (e2e) implementation

---
+++ tests/test-3.spec.ts
@@ -0,0 +1,11 @@
+import { test, expect } from "@playwright/test";
+
+test("Should create a cube from cubes-page", async ({ page }) => {
+  await page.goto("http://localhost:3000/cubes");
+  await expect(page.getByText("Cubes", { exact: true })).toBeVisible();
+  await page.getByRole("button").click();
+  await page.getByPlaceholder("Brand | Model | Version |").fill("test");
+  await page.getByRole("img", { name: "3x3", exact: true }).click();
+  await page.getByRole("button", { name: "Create" }).click();
+  await expect(page.getByText("test")).toBeVisible();
+});
