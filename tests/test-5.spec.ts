c7547e1e3a73b23d0caf303915e5517bb2c07c71
rojonaitor <bryanlundberg@outlook.com>
2024-04-23 19:08:11 -0600

    change description

--- tests/test-5.spec.ts
+++ tests/test-5.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from "@playwright/test";
 
-test.describe("Should check all timer settings", () => {
+test.describe("Should try all settings", () => {
   test("Should change language", async ({ page }) => {
     await page.goto("http://localhost:3000/");
     await expect(page.getByText("Choose a cube to load a")).toBeVisible();
