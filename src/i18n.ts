307d8b58404b79b7beb3840280c8f7ef96a8e371
rojonaitor <bryanlundberg@outlook.com>
2024-05-30 08:37:23 -0600

    add "english" as fallback language for empty strings

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,15 +1,17 @@
 import { notFound } from "next/navigation";
 import { getRequestConfig } from "next-intl/server";
 import { locales } from "./navigation";
-
-// Can be imported from a shared config
-// locales
+import { merge } from "ts-deepmerge";
 
 export default getRequestConfig(async ({ locale }) => {
   // Validate that the incoming `locale` parameter is valid
   if (!locales.includes(locale as any)) notFound();
 
+  const userMessages = (await import(`../messages/${locale}.json`)).default;
+  const defaultMessages = (await import(`../messages/en.json`)).default;
+  const messages = merge(defaultMessages, userMessages);
+
   return {
-    messages: (await import(`../messages/${locale}.json`)).default,
+    messages,
   };
 });
