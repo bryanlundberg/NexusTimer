872fbdbd1bcecc5ca8bef3cd8b3db5eec8d83bdc
Andrey <gbou883@gmail.com>
2024-07-15 12:47:10 +0300

    fix: refactor styles, highlight selected cube, and close select on outside click

--- src/components/menu-settings/MenuSelectPreferences.tsx
+++ src/components/menu-settings/MenuSelectPreferences.tsx
@@ -2,11 +2,10 @@ import { MenuSection } from "./MenuSection";
 import { useTranslations } from "next-intl";
 import { useRef, useState } from "react";
 import { useSettingsModalStore } from "@/store/SettingsModalStore";
-import { ChevronUpDownIcon, HeartIcon } from "@heroicons/react/24/solid";
+import { HeartIcon } from "@heroicons/react/24/solid";
 import useClickOutside from "@/hooks/useClickOutside";
 import { useTimerStore } from "@/store/timerStore";
 import { MiniatureIcon } from "../Select";
-import { useBackgroundImageStore } from "@/store/BackgroundThemeStore";
 import { AnimatePresence, motion } from "framer-motion";
 import loadSettings from "@/lib/loadSettings";
 import { Cube } from "@/interfaces/Cube";
@@ -17,8 +16,7 @@ export default function MenuSelectPreferences() {
 
   const [open, setOpen] = useState(false);
   const { cubes } = useTimerStore();
-  const componentRef = useRef<HTMLDivElement | null>(null);
-  const { backgroundImage } = useBackgroundImageStore();
+  const componentRef = useRef<HTMLButtonElement | null>(null);
 
   const handleClose = () => {
     setOpen(false);
@@ -32,6 +30,8 @@ export default function MenuSelectPreferences() {
     handleClose();
   };
 
+  const defaultCube = settings.preferences.defaultCube.cube;
+
   useClickOutside(componentRef, handleClose);
 
   return (
@@ -41,32 +41,21 @@ export default function MenuSelectPreferences() {
     >
       <div className="flex justify-between me-6">
         <div className="ms-12">{t("Settings-menu.default-cube")}</div>
-        <div
-          className="flex justify-end relative sm:w-[400px]"
-          ref={componentRef}
-        >
+        <div className="flex justify-end relative sm:w-[400px]">
           <button
             onClick={() => setOpen(!open)}
-            className={`grow text-md appearance-none max-w-40 border font-medium rounded-md px-3 transition duration-200 light:shadow-black  light:hover:bg-neutral-200 dark:hover:bg-zinc-800 dark:hover:border-zinc-500 light:hover:border-neutral-400 h-10 ${
-              backgroundImage ? "opacity-90 hover:opacity-100" : ""
-            } ${
+            ref={componentRef}
+            className={`grow text-md max-w-40 border font-medium rounded-md px-3 transition duration-200 shadow-black hover:bg-neutral-200 hover:border-neutral-400 h-10 ${
               open
-                ? "dark:bg-zinc-800 dark:border-zinc-500 light:bg-neutral-200 light:border-neutral-400"
-                : "dark:bg-zinc-900 dark:border-zinc-600 light:bg-neutral-100 light:border-neutral-300"
+                ? "bg-neutral-200 border-neutral-400"
+                : "bg-neutral-100 border-neutral-300"
             }`}
           >
-            <div className="flex items-center justify-between gap-2">
-              {settings.preferences.defaultCube.cube && (
-                <MiniatureIcon
-                  category={settings.preferences.defaultCube.cube.category}
-                />
-              )}
-              <div>
-                {settings.preferences.defaultCube.cube
-                  ? settings.preferences.defaultCube.cube.name
-                  : t("Inputs.none")}
+            <div className="flex items-center gap-2">
+              {defaultCube && <MiniatureIcon category={defaultCube.category} />}
+              <div className="truncate" title={defaultCube?.name}>
+                {defaultCube ? defaultCube.name : t("Inputs.none")}
               </div>
-              <ChevronUpDownIcon className="w-5 h-5" />
             </div>
           </button>
           <AnimatePresence>
@@ -76,21 +65,27 @@ export default function MenuSelectPreferences() {
                 animate={{ y: 0, scale: 1, opacity: 1 }}
                 exit={{ x: 0, scale: 0.9, opacity: 0 }}
                 id="list-options"
-                className="absolute z-40 overflow-auto max-h-[400px] p-1 top-12 mt-1 right-0 w-full h-auto border rounded-md light:bg-neutral-200 light:border-neutral-400 light:text-neutral-900 dark:bg-zinc-900 dark:border-zinc-700 dark:text-slate-100"
+                className="absolute overflow-auto max-h-[400px] p-1 top-12 mt-1 right-0 w-full h-auto border rounded-md bg-neutral-200 border-neutral-400 text-neutral-900"
               >
                 <div
                   onClick={() => handleCubeSelect(null)}
-                  className="cursor-pointer transition duration-200 p-1 select-none rounded-md ps-2 flex items-center justify-between overflow-hidden light:bg-transparent light:text-neutral-900 light:hover:bg-neutral-500 light:hover:text-neutral-100 dark:bg-transparent dark:hover:bg-zinc-700 dark:text-neutral-50"
+                  className={`cursor-pointer transition duration-200 p-1 select-none rounded-md ps-2 flex overflow-hidden ${
+                    defaultCube === null
+                      ? "bg-neutral-700 text-neutral-50"
+                      : "text-neutral-900 hover:bg-neutral-500 hover:text-neutral-100"
+                  }`}
                 >
-                  <div className="flex justify-start gap-3">
-                    <div className="overflow-hidden">{t("Inputs.none")}</div>
-                  </div>
+                  <div className="overflow-hidden">{t("Inputs.none")}</div>
                 </div>
                 {cubes?.map((cube) => (
                   <div
                     key={cube.id}
                     onClick={() => handleCubeSelect(cube)}
-                    className="cursor-pointer transition duration-200 p-1 select-none rounded-md ps-2 flex items-center justify-between overflow-hidden light:bg-transparent light:text-neutral-900 light:hover:bg-neutral-500 light:hover:text-neutral-100 dark:bg-transparent dark:hover:bg-zinc-700 dark:text-neutral-50"
+                    className={`cursor-pointer transition duration-200 p-1 select-none rounded-md ps-2 flex overflow-hidden ${
+                      defaultCube?.id === cube.id
+                        ? "bg-neutral-700 text-neutral-50"
+                        : "text-neutral-900 hover:bg-neutral-500 hover:text-neutral-100"
+                    }`}
                   >
                     <div className="flex justify-start gap-3">
                       <MiniatureIcon category={cube.category} />
