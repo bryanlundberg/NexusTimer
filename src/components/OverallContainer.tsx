4ea1a5a982dfb8abec7eb9a08562f6039dbff547
Bryan Lundberg <bryanlundberg@outlook.com>
2024-08-08 17:59:14 -0600

    refactor: component to improve prop handling and class merging.

--- src/components/OverallContainer.tsx
+++ src/components/OverallContainer.tsx
@@ -1,10 +1,23 @@
-interface Container {
+import { twMerge } from "tailwind-merge";
+
+interface OverallContainerProps extends React.HTMLAttributes<HTMLDivElement> {
+  className?: string;
   children: React.ReactNode;
 }
-export function OverallContainer({ children }: Container) {
+export function OverallContainer({
+  className,
+  children,
+  ...rest
+}: OverallContainerProps) {
   return (
     <>
-      <div className="flex flex-col w-full min-h-full mx-auto rounded-md grow xl:max-w-5xl xl:border light:border-neutral-200 dark:border-zinc-800 xl:mt-3 dark:bg-zinc-950 light:bg-neutral-50">
+      <div
+        {...rest}
+        className={twMerge(
+          "flex flex-col w-full min-h-full mx-auto rounded-md grow xl:max-w-5xl xl:border light:border-neutral-200 dark:border-zinc-800 xl:mt-3 dark:bg-zinc-950 light:bg-neutral-50",
+          className
+        )}
+      >
         {children}
       </div>
     </>
