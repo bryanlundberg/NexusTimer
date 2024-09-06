20c059d84c097dfa7005039fdba5f9af97027df3
Bryan Lundberg <bryanlundberg@outlook.com>
2024-09-05 20:45:55 -0600

    fix test

--- __tests__/getDeviation.test.js
+++ __tests__/getDeviation.test.js
@@ -1,6 +1,6 @@
-// import { FAKE_SESSION } from "./session";
 import getDeviation from "../src/lib/getDeviation";
 import { FAKE_SESSION } from "../data/FAKE_SESSION";
+
 test("Should get the deviation standard from an array of solves", () => {
   expect(getDeviation([])).toBe(0);
   expect(getDeviation([...FAKE_SESSION])).toBe(1432.117977536666);
@@ -8,4 +8,5 @@ test("Should get the deviation standard from an array of solves", () => {
   expect(getDeviation([...FAKE_SESSION].slice(5, 12))).toBe(1319.705883539063);
   expect(getDeviation([...FAKE_SESSION].slice(0, 12))).toBe(1403.2537730272281);
   expect(getDeviation([...FAKE_SESSION].slice(0, 30))).toBe(1373.9241429477513);
+  expect(getDeviation()).toBe(0);
 });
