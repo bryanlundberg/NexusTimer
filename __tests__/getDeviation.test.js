9927c9fb9e7b0f5ee48b13d28ea43411999216a6
Bryan Lundberg <bryanlundberg@outlook.com>
2024-09-06 13:19:48 -0600

    test: update deviation test cases

--- __tests__/getDeviation.test.js
+++ __tests__/getDeviation.test.js
@@ -1,12 +1,48 @@
 import getDeviation from "../src/lib/getDeviation";
 import { FAKE_SESSION } from "../data/FAKE_SESSION";
 
-test("Should get the deviation standard from an array of solves", () => {
-  expect(getDeviation([])).toBe(0);
-  expect(getDeviation([...FAKE_SESSION])).toBe(1432.117977536666);
-  expect(getDeviation([...FAKE_SESSION].slice(0, 5))).toBe(1217.350113977076);
-  expect(getDeviation([...FAKE_SESSION].slice(5, 12))).toBe(1319.705883539063);
-  expect(getDeviation([...FAKE_SESSION].slice(0, 12))).toBe(1403.2537730272281);
-  expect(getDeviation([...FAKE_SESSION].slice(0, 50))).toBe(1390.4659076756118);
-  expect(getDeviation()).toBe(0);
+describe("Should get the deviation standard from an array of solves", () => {
+  test("Passing an empty array", () => {
+    expect(getDeviation([])).toBe(0);
+  });
+
+  test("Passing a full session", () => {
+    expect(getDeviation([...FAKE_SESSION])).toBe(1432.117977536666);
+  });
+
+  test("Deviation of 5 values", () => {
+    expect(getDeviation([...FAKE_SESSION].slice(0, 5))).toBe(1217.350113977076);
+  });
+
+  test("Deviation of 12 values", () => {
+    expect(getDeviation([...FAKE_SESSION].slice(0, 12))).toBe(
+      1403.2537730272281
+    );
+  });
+
+  test("Deviation of 50 values", () => {
+    expect(getDeviation([...FAKE_SESSION].slice(0, 50))).toBe(
+      1390.4659076756118
+    );
+  });
+
+  test("without parameters", () => {
+    expect(getDeviation()).toBe(0);
+  });
+
+  test("passing undefined value as parameter", () => {
+    expect(getDeviation(undefined)).toBe(0);
+  });
+
+  test("passing null value as parameter", () => {
+    expect(getDeviation(null)).toBe(0);
+  });
+
+  test("passing string value as parameter", () => {
+    expect(getDeviation("string")).toBe(0);
+  });
+
+  test("passing number value as parameter", () => {
+    expect(getDeviation(34)).toBe(0);
+  });
 });
