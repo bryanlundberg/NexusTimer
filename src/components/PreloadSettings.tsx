e71f4fa8ff2fd67c854d849f28a93faacc6c7fc2
Bryan Lundberg <bryanlundberg@outlook.com>
2024-09-08 20:44:47 -0600

    initial shadcn setup

--- src/components/PreloadSettings.tsx
+++ src/components/PreloadSettings.tsx
@@ -4,6 +4,7 @@ import { Navbar } from "@/components/navbar/index";
 import { useSettingsModalStore } from "@/store/SettingsModalStore";
 import { useTimerStore } from "@/store/timerStore";
 import { useBackgroundImageStore } from "@/store/BackgroundThemeStore";
+import { ThemeProvider } from "./theme-provider";
 export default function PreloadSettings({
   children,
 }: {
@@ -18,21 +19,28 @@ export default function PreloadSettings({
 
   return (
     <>
-      <div className={`${theme}`}>
-        <div
-          className="flex flex-col justify-between max-h-dvh min-h-dvh gap-2 overflow-hidden select-none light:bg-neutral-50 light:text-zinc-950 dark:bg-zinc-950 dark:text-slate-50"
-          style={{
-            backgroundImage: backgroundImage ? `url(${backgroundImage})` : "",
-            backgroundPosition: backgroundImage ? "center" : "",
-            backgroundAttachment: backgroundImage ? "fixed" : "",
-            backgroundRepeat: backgroundImage ? "no-repeat" : "",
-            backgroundSize: backgroundImage ? "cover" : "",
-          }}
-        >
-          {children}{" "}
-          <>{!isSolving && timerStatus === "IDLE" ? <Navbar /> : null}</>
+      <ThemeProvider
+        attribute="class"
+        defaultTheme={settings.theme.background.color || "light"}
+        enableSystem
+        disableTransitionOnChange
+      >
+        <div className={`${theme}`}>
+          <div
+            className="flex flex-col justify-between max-h-dvh min-h-dvh gap-2 overflow-hidden select-none light:bg-neutral-50 light:text-zinc-950 dark:bg-zinc-950 dark:text-slate-50"
+            style={{
+              backgroundImage: backgroundImage ? `url(${backgroundImage})` : "",
+              backgroundPosition: backgroundImage ? "center" : "",
+              backgroundAttachment: backgroundImage ? "fixed" : "",
+              backgroundRepeat: backgroundImage ? "no-repeat" : "",
+              backgroundSize: backgroundImage ? "cover" : "",
+            }}
+          >
+            {children}{" "}
+            <>{!isSolving && timerStatus === "IDLE" ? <Navbar /> : null}</>
+          </div>
         </div>
-      </div>
+      </ThemeProvider>
     </>
   );
 }
