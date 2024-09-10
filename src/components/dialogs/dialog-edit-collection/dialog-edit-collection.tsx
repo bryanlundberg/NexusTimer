a1dd357aba055da6c9a9acb072aec394f158a8ed
Bryan Lundberg <bryanlundberg@outlook.com>
2024-09-09 22:21:31 -0600

    refactor: dialog for editing the cube info

--- src/components/dialogs/dialog-edit-collection/dialog-edit-collection.tsx
+++ src/components/dialogs/dialog-edit-collection/dialog-edit-collection.tsx
@@ -17,11 +17,88 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
+import { getAllCubes, saveCube } from "@/db/dbOperations";
 import { cubeCollection } from "@/lib/const/cubeCollection";
 import { useDialogCubesOptions } from "@/store/DialogCubesOptions";
+import { useSettingsModalStore } from "@/store/SettingsModalStore";
+import { useTimerStore } from "@/store/timerStore";
+import { useEffect, useState } from "react";
 
 export default function DialogEditCollection() {
-  const { cube } = useDialogCubesOptions();
+  const { cube, closeDialog } = useDialogCubesOptions();
+  const { cubes, setCubes, selectedCube, setSelectedCube, setTimerStatistics } =
+    useTimerStore();
+  const { settings, setSettings } = useSettingsModalStore();
+  const [form, setForm] = useState({
+    name: cube?.name || "",
+    category: cube?.category || "2x2",
+  });
+  const [error, setError] = useState({
+    status: false,
+    message: "",
+  });
+  const handleSubmitEditCubeCollection = async () => {
+    try {
+      // verify if its repeated the name
+      if (cubes?.some((e) => e.name === form.name)) {
+        setError((prev) => ({
+          ...prev,
+          status: true,
+          message: "That name already exist.",
+        }));
+        return;
+      }
+
+      // update cube info that its linked to settings "preferences" the default start cube
+      if (cube && cube.id === settings.preferences.defaultCube.cube?.id) {
+        const newSettings = {
+          ...settings,
+          preferences: {
+            ...settings.preferences,
+            defaultCube: {
+              ...settings.preferences.defaultCube,
+              cube: {
+                ...settings.preferences.defaultCube.cube,
+                name: form.name,
+                category: form.category,
+              },
+            },
+          },
+        };
+        setSettings(newSettings);
+        window.localStorage.setItem("settings", JSON.stringify(newSettings));
+      }
+
+      await saveCube({
+        ...cube,
+        name: form.name.trim(),
+        category: form.category,
+      });
+
+      const cubesDB = await getAllCubes();
+      setCubes(cubesDB);
+
+      if (cube && cube.id === selectedCube?.id) {
+        setSelectedCube(null);
+        setTimerStatistics();
+      }
+
+      // update states
+      closeDialog();
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  // helps to refresh input (name) when re-open the dialog
+  useEffect(() => {
+    setForm((prev) => ({
+      ...prev,
+      category: cube?.category || "2x2",
+      name: cube?.name || "",
+    }));
+  }, [cube]);
+
   return (
     <>
       <DialogContent className="sm:max-w-[425px]">
@@ -37,17 +114,31 @@ export default function DialogEditCollection() {
         </Alert>
 
         <Label>Name</Label>
-        <Input />
+        <Input
+          defaultValue={cube?.name}
+          onChange={(e) => {
+            setForm((prev) => ({ ...prev, name: e.target.value }));
+          }}
+        />
+
+        {error && error.status && (
+          <p className="text-destructive text-sm">{error.message}</p>
+        )}
 
         <Label>Category</Label>
-        <Select>
+        <Select
+          defaultValue={form.category}
+          onValueChange={(e) =>
+            setForm((prev) => ({ ...prev, category: e as any }))
+          }
+        >
           <SelectTrigger>
             <SelectValue placeholder="Select an option" />
           </SelectTrigger>
           <SelectContent>
             {cubeCollection.map((cube) => {
               return (
-                <SelectItem key={cube.id} value={cube.displayId}>
+                <SelectItem key={cube.id} value={cube.name}>
                   {cube.name}
                 </SelectItem>
               );
@@ -60,7 +151,12 @@ export default function DialogEditCollection() {
               <Button variant={"outline"}>Cancel</Button>
             </DialogClose>
 
-            <Button variant={"default"}>Continue</Button>
+            <Button
+              variant={"default"}
+              onClick={handleSubmitEditCubeCollection}
+            >
+              Continue
+            </Button>
           </div>
         </DialogFooter>
       </DialogContent>
