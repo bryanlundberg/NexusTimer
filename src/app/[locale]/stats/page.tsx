ff80b0464673b24850100c8e50803d5421f02eb4
Bryan Lundberg <bryanlundberg@outlook.com>
2024-09-14 20:33:46 -0600

    refactor stats page

--- src/app/[locale]/stats/page.tsx
+++ src/app/[locale]/stats/page.tsx
@@ -1,19 +1,43 @@
 "use client";
 
-import { OverallContainer } from "@/components/OverallContainer";
-import { OverallHeader } from "@/components/OverallHeader";
 import CategoryStatistics from "@/components/stats/CategoryStatistics";
+import { Card } from "@/components/ui/card";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
 import { useTranslations } from "next-intl";
 
-export default function StatsPage() {
-  const t = useTranslations("Index.StatsPage");
-
+export default function Page() {
+  const t = useTranslations("Index");
   return (
     <>
-      <OverallContainer>
-        <OverallHeader title={t("title")} />
+      {/* container */}
+      <div className="max-w-7xl mx-auto px-2 pt-2 flex flex-col w-full min-h-full">
+        {/* header */}
+        <Card className="w-full mb-2 border p-3 flex flex-col gap-2">
+          <div className="flex justify-between gap-10">
+            <h2 className="font-black text-xl">Stats</h2>
+            <div className="flex items-center gap-2 w-full justify-end">
+              <Select defaultValue="overall">
+                <SelectTrigger className="w-[180px]">
+                  <SelectValue placeholder="Select" />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectItem value="overall">Overall</SelectItem>
+                  <SelectItem value="advanced" disabled>
+                    Advanced
+                  </SelectItem>
+                </SelectContent>
+              </Select>
+            </div>
+          </div>
+        </Card>
         <CategoryStatistics />
-      </OverallContainer>
+      </div>
     </>
   );
 }
