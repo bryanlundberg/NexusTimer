072ab4cec0b7286ad4b4af7ffb2f98317873c167
Bryan Lundberg <bryanlundberg@outlook.com>
2024-09-16 07:18:50 -0600

    build: update dependencies

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,8 @@
-const createNextIntlPlugin = require("next-intl/plugin");
+import createNextIntlPlugin from "next-intl/plugin";
 
 const withNextIntl = createNextIntlPlugin();
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-module.exports = withNextIntl(nextConfig);
+export default withNextIntl(nextConfig);
