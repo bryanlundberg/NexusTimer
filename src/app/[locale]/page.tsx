386a2bdd3fd8996a9ac1e62252cca0de9aca0135
Bryan Lundberg <bryanlundberg@outlook.com>
2024-10-07 08:52:53 -0600

    feat: add dialog to timer page

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -10,10 +10,31 @@ import ScrambleModal from "@/components/timer/ScrambleModal";
 import useInitializeTimer from "@/hooks/useInitializeHint";
 import ImportModal from "@/components/menu-settings/ImportModal";
 import useForceHashSettings from "@/hooks/useForceHashSettings";
+import { useEffect, useState } from "react";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+} from "@/components/ui/alert-dialog";
 
 export default function Home() {
   useInitializeTimer();
   useForceHashSettings();
+
+  // Temporal code until 2nd November domain migration ends.
+  const [openMigrationDialog, setOpenMigrationDialog] = useState(false);
+  useEffect(() => {
+    const hostname = window.location.hostname;
+    if (hostname === "www.nexustimer.pro") {
+      setOpenMigrationDialog(true);
+    }
+  }, []);
+
   return (
     <>
       <TimerContainer>
@@ -26,6 +47,28 @@ export default function Home() {
       <FullscreenOption />
       <ScrambleModal />
       <ImportModal />
+
+      {/* Temporal alert */}
+      <AlertDialog
+        open={openMigrationDialog}
+        onOpenChange={setOpenMigrationDialog}
+      >
+        <AlertDialogContent>
+          <AlertDialogHeader>
+            <AlertDialogTitle>Domain Migration</AlertDialogTitle>
+            <AlertDialogDescription>Dear User,</AlertDialogDescription>
+            <AlertDialogDescription>
+              We are migrating to (nexustimer.com) domain. To ensure a smooth
+              transition, please back up your data and click continue. The
+              current domain (.pro) will be available until 2nd November.
+            </AlertDialogDescription>
+          </AlertDialogHeader>
+          <AlertDialogFooter>
+            <AlertDialogCancel>Download Backup</AlertDialogCancel>
+            <AlertDialogAction>Continue</AlertDialogAction>
+          </AlertDialogFooter>
+        </AlertDialogContent>
+      </AlertDialog>
     </>
   );
 }
