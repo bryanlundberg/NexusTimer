c7de2b2dda99e0b94d4b688cbb18fe46a77b1b6c
Bryan Lundberg <bryanlundberg@outlook.com>
2024-10-20 06:18:41 -0600

    feat: added quick access to favorite cubes

--- src/components/MainCubeSelector.tsx
+++ src/components/MainCubeSelector.tsx
@@ -1,7 +1,9 @@
 import {
   Select,
   SelectContent,
+  SelectGroup,
   SelectItem,
+  SelectLabel,
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
@@ -12,6 +14,7 @@ import { useTranslations } from "next-intl";
 import Image from "next/image";
 import { Button } from "./ui/button";
 import { PlusIcon } from "@radix-ui/react-icons";
+import { Cube } from "@/interfaces/Cube";
 
 export default function MainCubeSelector() {
   const t = useTranslations("Index");
@@ -38,48 +41,82 @@ export default function MainCubeSelector() {
           <SelectValue placeholder={t("Inputs.select")} />
         </SelectTrigger>
         <SelectContent>
-          {cubes && cubes.length > 0 ? (
-            cubes
-              .sort((a: any, b: any) => a.category.localeCompare(b.category))
-              .map((cube) => {
-                return (
-                  <SelectItem key={cube.id} value={cube.id}>
-                    <div className="flex flex-row items-center justify-between gap-2">
-                      {(() => {
-                        const foundCube = cubeCollection.find(
-                          (i) => i.name === cube.category
-                        );
-                        if (foundCube) {
-                          return (
-                            <Image
-                              src={foundCube.src}
-                              alt={foundCube.name}
-                              width={24}
-                              height={24}
-                              className="object-scale-down"
-                            />
-                          );
-                        }
-                        return null;
-                      })()}
+          {/* favorites cubes */}
 
-                      <p className="">{cube.name}</p>
-                    </div>
-                  </SelectItem>
-                );
-              })
-          ) : (
-            <Link href={"/cubes"} className="">
-              <Button variant={"outline"} className="w-full">
-                <div className="flex items-center justify-center gap-1">
-                  <PlusIcon />
-                  {t("CubesPage.new-collection")}
-                </div>
-              </Button>
-            </Link>
+          {cubes && cubes.length > 0 && cubes.some((c: Cube) => c.favorite) && (
+            <>
+              <SelectGroup>
+                <SelectLabel>Favoritos</SelectLabel>
+                {cubes
+                  .filter((cube: Cube) => cube.favorite)
+                  .sort((a: Cube, b: Cube) =>
+                    a.category.localeCompare(b.category)
+                  )
+                  .map((cube) => {
+                    return (
+                      <SelectCubeItemWidthImage cube={cube} key={cube.id} />
+                    );
+                  })}
+              </SelectGroup>
+            </>
           )}
+          {/* normal cubes list */}
+          <SelectGroup>
+            <SelectLabel>
+              Your collections {cubes && cubes.length <= 0 && "(Empty)"}
+            </SelectLabel>
+            {cubes && cubes.length > 0 ? (
+              cubes
+                .filter((cube: Cube) => !cube.favorite)
+                .sort((a: Cube, b: Cube) =>
+                  a.category.localeCompare(b.category)
+                )
+                .map((cube) => {
+                  return <SelectCubeItemWidthImage cube={cube} key={cube.id} />;
+                })
+            ) : (
+              <Link href={"/cubes"} className="">
+                <Button variant={"outline"} className="w-full">
+                  <div className="flex items-center justify-center gap-1">
+                    <PlusIcon />
+                    {t("CubesPage.new-collection")}
+                  </div>
+                </Button>
+              </Link>
+            )}
+          </SelectGroup>
         </SelectContent>
       </Select>
+    </>
+  );
+}
+
+function SelectCubeItemWidthImage({ cube }: { cube: Cube }) {
+  return (
+    <>
+      <SelectItem key={cube.id} value={cube.id}>
+        <div className="flex flex-row items-center justify-between gap-2">
+          {(() => {
+            const foundCube = cubeCollection.find(
+              (i) => i.name === cube.category
+            );
+            if (foundCube) {
+              return (
+                <Image
+                  src={foundCube.src}
+                  alt={foundCube.name}
+                  width={24}
+                  height={24}
+                  className="object-scale-down"
+                />
+              );
+            }
+            return null;
+          })()}
+
+          <p className="">{cube.name}</p>
+        </div>
+      </SelectItem>
     </>
   );
 }
