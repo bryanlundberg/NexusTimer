417f25db0cd26281114b706758a0af7eef080df9
Bryan Lundberg <bryanlundberg@outlook.com>
2024-10-30 11:46:01 -0600

    style: hide custom bg image option in settings

--- src/app/settings/layout.tsx
+++ src/app/settings/layout.tsx
@@ -4,7 +4,7 @@ export default async function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="overflow-y-auto">
+    <div className="overflow-y-auto bg-background min-h-dvh">
       <div className="max-w-md mx-auto bg-background/90 backdrop-blur-lg pt-2">
         <div className="p-2">{children}</div>
       </div>
