9039a8b87111deb577f1b2df3e4089fc63b63aaf
Bryan Lundberg <bryanlundberg@outlook.com>
2024-11-05 07:19:08 -0600

    set tooltips

--- src/components/timer/ScrambleZone.tsx
+++ src/components/timer/ScrambleZone.tsx
@@ -8,13 +8,19 @@ import { Button } from "../ui/button";
 import DrawerHintPanel from "../drawners/drawer-hint-panel";
 import { Dialog, DialogTrigger } from "@/components/ui/dialog";
 import DialogEnterNewScramble from "../dialogs/dialog-enter-new-scramble/dialog-enter-new-scramble";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "../ui/tooltip";
 
 export function ScrambleZone() {
   const { selectedCube, scramble, displayHint, setHints, isSolving } =
     useTimerStore();
 
   const { settings } = useSettingsModalStore();
-  const t = useTranslations("Index.HomePage");
+  const t = useTranslations("Index");
   return (
     <>
       <div className="relative mx-auto">
@@ -24,19 +30,29 @@ export function ScrambleZone() {
           }`}
         >
           <p data-testid="scramble-text-zone">
-            {selectedCube ? scramble : t("empty-scramble")}
+            {selectedCube ? scramble : t("HomePage.empty-scramble")}
           </p>
         </div>
 
         <div className="absolute bottom-0 right-0 cursor-pointer duration-300 transition translate-y-10 flex gap-3">
           {!isSolving && selectedCube && (
             <Dialog>
-              <DialogTrigger asChild>
-                <Button variant={"ghost"} size={"icon"} onClick={() => {}}>
-                  <Pencil2Icon />
-                </Button>
-              </DialogTrigger>
-              <DialogEnterNewScramble />
+              <TooltipProvider>
+                <Tooltip>
+                  <TooltipTrigger asChild>
+                    <DialogTrigger asChild>
+                      <Button variant={"ghost"} size={"icon"}>
+                        <Pencil2Icon />
+                      </Button>
+                    </DialogTrigger>
+                  </TooltipTrigger>
+
+                  <DialogEnterNewScramble />
+                  <TooltipContent>
+                    <p>Edit scramble</p>
+                  </TooltipContent>
+                </Tooltip>
+              </TooltipProvider>
             </Dialog>
           )}
 
@@ -45,23 +61,33 @@ export function ScrambleZone() {
             !displayHint &&
             !isSolving && (
               <Drawer>
-                <DrawerTrigger asChild>
-                  <Button
-                    variant={"ghost"}
-                    size={"icon"}
-                    onClick={() => {
-                      if (!selectedCube) return;
-                      genSolution(
-                        selectedCube.category,
-                        scramble,
-                        "yellow"
-                      ).then((res: CrossSolutions) => setHints(res));
-                    }}
-                  >
-                    <Component1Icon />
-                  </Button>
-                </DrawerTrigger>
-                <DrawerHintPanel />
+                <TooltipProvider>
+                  <Tooltip>
+                    <TooltipTrigger asChild>
+                      <DrawerTrigger asChild>
+                        <Button
+                          variant={"ghost"}
+                          size={"icon"}
+                          onClick={() => {
+                            if (!selectedCube) return;
+                            genSolution(
+                              selectedCube.category,
+                              scramble,
+                              "yellow"
+                            ).then((res: CrossSolutions) => setHints(res));
+                          }}
+                        >
+                          <Component1Icon />
+                        </Button>
+                      </DrawerTrigger>
+                    </TooltipTrigger>
+
+                    <DrawerHintPanel />
+                    <TooltipContent>
+                      <p>Hints</p>
+                    </TooltipContent>
+                  </Tooltip>
+                </TooltipProvider>
               </Drawer>
             )}
         </div>
