c4001ab0271d60d7419bf254c6c81ab3118d7957
Bryan Lundberg <bryanlundberg@outlook.com>
2025-03-15 20:05:56 -0600

    refactor: simplify personal best condition in HeaderTimer component

--- src/components/timer/HeaderTimer.tsx
+++ src/components/timer/HeaderTimer.tsx
@@ -4,29 +4,25 @@ import { ScrambleZone } from "./ScrambleZone";
 import { useTranslations } from "next-intl";
 import Navigation from "../navigation/navigation";
 import { TimerStatus } from "@/enums/TimerStatus";
-
 export default function HeaderTimer() {
-  const { isSolving, timerStatus } = useTimerStore();
+  const { isSolving, timerStatus, lastSolve, timerStatistics } = useTimerStore();
   const { settings } = useSettingsModalStore();
-  const { lastSolve, timerStatistics } = useTimerStore();
   const t = useTranslations("Index.HomePage");
 
   if (isSolving || timerStatus !== TimerStatus.IDLE) return null;
 
+  const isPersonalBest = lastSolve != null && lastSolve.time <= timerStatistics.global.best && settings.alerts.bestTime.status;
+
   return (
     <>
       <Navigation />
-
       <ScrambleZone />
-
-      {lastSolve != null &&
-      lastSolve.time <= timerStatistics.global.best &&
-      settings.alerts.bestTime.status ? (
+      {isPersonalBest && (
         <div id="touch" className="mt-10 text-center">
           <p>{t("congratulations")}</p>
           <p>{t("personal_best")}</p>
         </div>
-      ) : null}
+      )}
     </>
   );
 }
