1b391804408f095b2ca0a04da0e1e2d79b05200d
Bryan Lundberg <bryanlundberg@outlook.com>
2025-05-24 04:58:02 -0600

    refactor: `useTimer` to improve modularity and readability
    Abstracted timer functionalities into separate hooks: `useTimerControls`, `useInspection`, `useHoldToStart`, `useSolveData`, and `useEventHandlers`. This simplifies the main code, reduces duplication, and enhances maintainability.

--- src/hooks/useTimer.ts
+++ src/hooks/useTimer.ts
@@ -1,327 +1,94 @@
-import { getAllCubes, getCubeById, saveCube } from "@/db/dbOperations";
-import { Solve } from "@/interfaces/Solve";
-import genId from "@/lib/genId";
-import { useSettingsModalStore } from "@/store/SettingsModalStore";
+import { useEffect, useCallback } from "react";
 import { useTimerStore } from "@/store/timerStore";
-import { useEffect, useRef, useState } from "react";
-import { TimerMode } from "@/enums/TimerMode";
+import { useSettingsModalStore } from "@/store/SettingsModalStore";
 import { TimerStatus } from "@/enums/TimerStatus";
+import useTimerControls from "./useTimerControls";
+import useInspection from "./useInspection";
+import useHoldToStart from "./useHoldToStart";
+import useSolveData from "./useSolveData";
+import useEventHandlers from "./useEventHandlers";
 
 export default function useTimer() {
   const {
-    solvingTime,
-    setSolvingTime,
     isSolving,
     setIsSolving,
     setTimerStatus,
-    setNewScramble,
     selectedCube,
-    scramble,
-    setCubes,
-    setSelectedCube,
-    setLastSolve,
-    displayHint,
-    cubes,
-    timerMode,
     setTimerStatistics,
   } = useTimerStore();
 
   const { settings } = useSettingsModalStore();
-  const holdTimeRequired = settings.timer.holdToStart.status ? 500 : 0;
   const inspectionRequired = settings.timer.inspection.status;
-  const inspectionDuration = 16000;
-  const startSolveTime = useRef<number | null>(null);
-  const solveTimeId = useRef<any>(null);
-  const startInspectionTime = useRef<number | null>(null);
-  const inspectionId = useRef<any>(null);
-  const [inspectionTime, setInspectionTime] =
-    useState<number>(inspectionDuration);
-  const startHoldingTime = useRef<number | null>(null);
-  const holdingTimeId = useRef<any>(null);
-  const [holdingTime, setHoldingTime] = useState<number | null>(10);
-  const relasedKey = useRef<boolean>(true);
 
-  useEffect(() => {
-    if (timerMode === TimerMode.STACKMAT) return;
-    const startTimer = () => {
-      setIsSolving(true);
+  const { startTimer, resetTimer, stopTimer, startSolveTime } = useTimerControls();
+  const { inspectionTime, startInspection, removeInspection, inspectionId } = useInspection();
+  const { startHold, removeHolding, holdingTime, holdTimeRequired, holdingTimeId } = useHoldToStart();
+  const { saveSolveData } = useSolveData();
+
+  // MAIN HOLD CONTROL
+  const handleHold = useCallback((isReleased: boolean) => {
+    if (!selectedCube) return;
+    if (isSolving && isReleased) {
+      stopTimer();
+      saveSolveData(startSolveTime.current as number);
+      resetTimer();
+    }
+    if (!isReleased) return;
+    if (!isSolving) {
+      startHold();
+    }
+  }, [isSolving, resetTimer, saveSolveData, selectedCube, startHold, startSolveTime, stopTimer]);
+
+  // MAIN RELEASE CONTROL
+  const handleRelease = useCallback(() => {
+    if (!selectedCube) return;
+    if (!holdingTimeId.current) return;
+    if (typeof holdingTime === "number" && holdingTime <= holdTimeRequired) {
+      removeHolding();
+      inspectionId.current
+        ? setTimerStatus(TimerStatus.SOLVING)
+        : setTimerStatus(TimerStatus.IDLE);
+      return;
+    }
+    if (!inspectionId.current && inspectionRequired) {
+      startInspection();
+      removeHolding();
       setTimerStatus(TimerStatus.SOLVING);
-      startSolveTime.current = Date.now() - 1;
-      solveTimeId.current = setInterval(() => {
-        if (startSolveTime.current) {
-          const now = Date.now();
-          const difference = now - startSolveTime.current;
-          setSolvingTime(difference);
-        }
-      });
-    };
-
-    const startInspection = () => {
-      startInspectionTime.current = Date.now() - 1;
-      setTimerStatus(TimerStatus.INSPECTING);
-      let reproduced8 = false;
-      let reproduced12 = false;
-      inspectionId.current = setInterval(() => {
-        if (startInspectionTime.current) {
-          const now = Date.now();
-          const difference =
-            inspectionDuration - (now - startInspectionTime.current);
-
-          const timeRemaining = difference / 1000;
-
-          if (settings.timer.startCue.status) {
-            if (timeRemaining <= 9 && !reproduced8) {
-              reproduced8 = true;
-              const audio12 = new Audio("./sounds/en/8.wav");
-              audio12.play();
-            }
-
-            if (timeRemaining <= 4 && !reproduced12) {
-              reproduced12 = true;
-              const audio12 = new Audio("./sounds/en/12.wav");
-              audio12.play();
-            }
-          }
-
-          setInspectionTime(timeRemaining);
-          if (difference <= 0) {
-            setTimerStatus(TimerStatus.INSPECTING);
-            setSolvingTime(0);
-            resetTimer();
-            const audio = new Audio("./sounds/en/reset.wav");
-            audio.play();
-          }
-        }
-      }, 10);
-    };
-
-    const resetTimer = () => {
-      clearInterval(holdingTimeId.current);
-      clearInterval(solveTimeId.current);
-      clearInterval(inspectionId.current);
-      startHoldingTime.current = null;
-      setInspectionTime(inspectionDuration);
-      setIsSolving(false);
-      holdingTimeId.current = null;
-      solveTimeId.current = null;
-      inspectionId.current = null;
-      setTimerStatus(TimerStatus.IDLE);
-    };
-
-    const startHold = () => {
-      if (!holdingTimeId.current) {
-        startHoldingTime.current = Date.now() - 1;
-        holdingTimeId.current = setInterval(() => {
-          if (startHoldingTime.current) {
-            const now = Date.now();
-            const difference = now - startHoldingTime.current;
-            setHoldingTime(difference);
-            if (difference >= holdTimeRequired) {
-              setTimerStatus(TimerStatus.READY);
-            } else {
-              setTimerStatus(TimerStatus.HOLDING);
-            }
-          }
-        }, 10);
-      }
-    };
-
-    const removeInspection = () => {
-      startInspectionTime.current = null;
-      clearInterval(inspectionId.current);
-      inspectionId.current = null;
-      setInspectionTime(inspectionDuration);
-      setIsSolving(false);
-    };
-
-    const removeHolding = () => {
-      clearInterval(holdingTimeId.current);
-      holdingTimeId.current = null;
-      startHoldingTime.current = null;
-      setHoldingTime(0);
-      setIsSolving(false);
-    };
-
-    const stopTimer = async () => {
-      clearInterval(solveTimeId.current);
-      // save solve
-      if (
-        selectedCube &&
-        scramble &&
-        typeof startSolveTime.current === "number"
-      ) {
-        const lastSolve: Solve = {
-          id: genId(),
-          startTime: startSolveTime.current,
-          endTime: Date.now(),
-          scramble: scramble,
-          bookmark: false,
-          time: solvingTime,
-          dnf: false,
-          plus2: false,
-          rating: Math.floor(Math.random() * 20) + scramble.length,
-          cubeId: selectedCube.id,
-          comment: "",
-        };
-
-        setLastSolve({ ...lastSolve });
-
-        const cube = cubes?.find((u) => u.id === selectedCube.id);
-
-        if (cube) {
-          await saveCube({
-            ...cube,
-            solves: {
-              ...cube.solves,
-              session: [...cube.solves.session, lastSolve],
-            },
-          });
-        }
-
-        const updatedCubes = await getAllCubes();
-        setCubes(updatedCubes);
-
-        const updatedCube = await getCubeById(selectedCube.id);
-        setSelectedCube(updatedCube);
-      }
-      setNewScramble(selectedCube);
-      solveTimeId.current = null;
-      startSolveTime.current = null;
-    };
-
-    // MAIN HOLD CONTROL
-    const handleHold = () => {
-      if (!selectedCube) return;
-      if (isSolving && relasedKey.current) {
-        stopTimer();
-        resetTimer();
-        relasedKey.current = false;
-      }
-      if (!relasedKey.current) return;
-      if (!isSolving) {
-        startHold();
-      }
-    };
-
-    // MAIN RELEASE CONTROL
-    const handleRelease = () => {
-      if (!selectedCube) return;
-      relasedKey.current = true;
-      if (!holdingTimeId.current) return;
-      if (typeof holdingTime === "number" && holdingTime <= holdTimeRequired) {
-        removeHolding();
-        inspectionId.current
-          ? setTimerStatus(TimerStatus.SOLVING)
-          : setTimerStatus(TimerStatus.IDLE);
-        return;
-      }
-      if (!inspectionId.current && inspectionRequired) {
-        startInspection();
-        removeHolding();
-        setTimerStatus(TimerStatus.SOLVING);
-        return;
-      }
-      if (inspectionId.current && inspectionRequired) {
-        removeInspection();
-        removeHolding();
-        setTimerStatus(TimerStatus.READY);
-        startTimer();
-        return;
-      }
-      if (!inspectionRequired) {
-        removeInspection();
-        removeHolding();
-        setTimerStatus(TimerStatus.READY);
-        startTimer();
-        return;
-      }
-    };
-
-    const handleTouchStart = (event: TouchEvent): void => {
-      event.preventDefault();
-      const quickActionButtons = document.querySelector(
-        "#quick-action-buttons"
-      ) as HTMLElement | null;
-      if (
-        quickActionButtons &&
-        quickActionButtons.contains(event.target as Node)
-      ) {
-        // nothing
-      } else {
-        handleHold();
-      }
-    };
-
-    const handleTouchEnd = (event: TouchEvent): void => {
-      event.preventDefault();
-      handleRelease();
-    };
-
-    const handleKeyDown = (event: KeyboardEvent) => {
-      if (event.code === "Escape") {
-        resetTimer();
-        return;
-      }
-      if (event.code !== "Space") {
-        return;
-      }
-      handleHold();
-    };
-
-    const handleKeyUp = (event: KeyboardEvent) => {
-      if (event.code === "Escape") {
-        resetTimer();
-        return;
-      }
-      if (event.code !== "Space") {
-        return;
-      }
-      handleRelease();
-    };
-
-    if (displayHint) return;
-
-    const touchElements = document.querySelectorAll("#touch");
-
-    window.addEventListener("keydown", handleKeyDown);
-    window.addEventListener("keyup", handleKeyUp);
-    touchElements.forEach((element: any) => {
-      element.addEventListener("touchstart", handleTouchStart);
-      element.addEventListener("touchend", handleTouchEnd);
-    });
-    return () => {
-      window.removeEventListener("keydown", handleKeyDown);
-      window.removeEventListener("keyup", handleKeyUp);
-      touchElements.forEach((element: any) => {
-        element.removeEventListener("touchstart", handleTouchStart);
-        element.removeEventListener("touchend", handleTouchEnd);
-      });
-    };
+      return;
+    }
+    if (inspectionId.current && inspectionRequired) {
+      removeInspection();
+      removeHolding();
+      setTimerStatus(TimerStatus.READY);
+      startTimer();
+      return;
+    }
+    if (!inspectionRequired) {
+      removeInspection();
+      removeHolding();
+      setTimerStatus(TimerStatus.READY);
+      startTimer();
+      return;
+    }
   }, [
-    holdingTime,
     holdTimeRequired,
-    isSolving,
-    setIsSolving,
-    setSolvingTime,
+    holdingTime,
+    holdingTimeId,
+    inspectionId,
     inspectionRequired,
+    removeHolding,
+    removeInspection,
     selectedCube,
-    setCubes,
-    scramble,
-    setLastSolve,
-    setNewScramble,
-    setSelectedCube,
-    solvingTime,
     setTimerStatus,
-    displayHint,
-    cubes,
-    settings.timer.startCue.status,
-    timerMode,
+    startInspection,
+    startTimer
   ]);
 
+  // Event handlers
+  useEventHandlers(handleHold, handleRelease, resetTimer);
+
   useEffect(() => {
-    if (selectedCube && !isSolving) {
-      setTimerStatistics();
-    }
+    if (selectedCube && !isSolving) setTimerStatistics();
   }, [selectedCube, isSolving, setTimerStatistics]);
 
   return {
