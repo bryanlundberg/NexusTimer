a2a6b7726da49f90368d8b09981703515d5fa428
Bryan Lundberg <bryanlundberg@outlook.com>
2025-05-24 04:57:30 -0600

    feat: Add `useHoldToStart` custom hook for timer hold functionality
    This hook manages the "hold to start" functionality for the timer, including tracking hold duration and updating timer status. It integrates with timer and settings state stores for seamless behavior.

---
+++ src/hooks/useHoldToStart.ts
@@ -0,0 +1,48 @@
+import { useRef, useState } from "react";
+import { useTimerStore } from "@/store/timerStore";
+import { useSettingsModalStore } from "@/store/SettingsModalStore";
+import { TimerStatus } from "@/enums/TimerStatus";
+
+export default function useHoldToStart() {
+  const { setTimerStatus } = useTimerStore();
+  const { settings } = useSettingsModalStore();
+  
+  const holdTimeRequired = settings.timer.holdToStart.status ? 500 : 0;
+  const startHoldingTime = useRef<number | null>(null);
+  const holdingTimeId = useRef<any>(null);
+  const [holdingTime, setHoldingTime] = useState<number | null>(10);
+
+  const startHold = () => {
+    if (!holdingTimeId.current) {
+      startHoldingTime.current = Date.now() - 1;
+      holdingTimeId.current = setInterval(() => {
+        if (startHoldingTime.current) {
+          const now = Date.now();
+          const difference = now - startHoldingTime.current;
+          setHoldingTime(difference);
+          if (difference >= holdTimeRequired) {
+            setTimerStatus(TimerStatus.READY);
+          } else {
+            setTimerStatus(TimerStatus.HOLDING);
+          }
+        }
+      }, 10);
+    }
+  };
+
+  const removeHolding = () => {
+    clearInterval(holdingTimeId.current);
+    holdingTimeId.current = null;
+    startHoldingTime.current = null;
+    setHoldingTime(0);
+  };
+
+  return {
+    startHold,
+    removeHolding,
+    holdingTime,
+    holdTimeRequired,
+    holdingTimeId,
+    startHoldingTime,
+  };
+}
