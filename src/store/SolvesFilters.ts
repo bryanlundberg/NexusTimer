f1646cc27c1d01043fb34904799ff2bd69e27f96
Bryan Lundberg <bryanlundberg@outlook.com>
2025-05-31 07:02:25 -0600

    refactor: simplify SolvesFilters store structure**
    Removed unused properties and methods from the SolvesFilters store to streamline its structure. Focused on retaining only elements related to the `isOpenMoveSolvesDialog` state for clarity and efficiency.

--- src/store/SolvesFilters.ts
+++ src/store/SolvesFilters.ts
@@ -1,39 +1,13 @@
 import { create } from "zustand";
 
-type Direction = "asc" | "desc";
-type SortVariant = "date" | "time";
-type TabVariant = "session" | "all";
+
 interface SolvesFiltersProps {
-  tab: TabVariant;
-  query: string;
-  sortType: SortVariant;
-  order: Direction;
   isOpenMoveSolvesDialog: boolean;
-  handleSearch: (query: string) => void;
-  handleChangeOrder: (order: Direction) => void;
-  handleChangeSortType: (sortType: SortVariant) => void;
-  handleChangeTab: (newTab: TabVariant) => void;
   handleChangeIsOpenMoveSolvesDialog: () => void;
 }
 
 export const useSolveFiltersStore = create<SolvesFiltersProps>((set) => ({
-  tab: "session",
-  sortType: "date",
-  order: "desc",
-  query: "",
   isOpenMoveSolvesDialog: false,
-  handleSearch: (query: string) => {
-    set((prev) => ({ ...prev, query }));
-  },
-  handleChangeOrder: (order: Direction) => {
-    set((prev) => ({ ...prev, order }));
-  },
-  handleChangeSortType: (sortType: SortVariant) => {
-    set((prev) => ({ ...prev, sortType }));
-  },
-  handleChangeTab: (newTab: TabVariant) => {
-    set((prev) => ({ ...prev, tab: newTab }));
-  },
   handleChangeIsOpenMoveSolvesDialog: () => {
     set((prev) => ({
       ...prev,
