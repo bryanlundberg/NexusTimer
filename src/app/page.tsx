3638d2bff3f2e757c56d26baae3f692944fc09c6
Bryan Lundberg <bryanlundberg@outlook.com>
2025-05-31 14:59:28 -0600

    `feat: wrap home components with FadeIn animation`
    Introduce a FadeIn wrapper around home page components for smoother animations. This enhances the user experience with a visually appealing transition effect.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,17 +5,20 @@ import TimerContainer from "@/components/timer/TimerContainer";
 import { MainTimer } from "@/components/timer/MainTimer";
 import HintPanel from "@/components/timer/HintPanel";
 import ScrambleModal from "@/components/timer/ScrambleModal";
+import FadeIn from "@/components/fade-in/fade-in";
 
 export default function Home() {
   return (
     <>
-      <TimerContainer>
-        <HeaderTimer />
-        <MainTimer />
-        <TimerWidgets />
-      </TimerContainer>
-      <HintPanel />
-      <ScrambleModal />
+      <FadeIn className={"flex flex-col grow"}>
+        <TimerContainer>
+          <HeaderTimer/>
+          <MainTimer/>
+          <TimerWidgets/>
+        </TimerContainer>
+        <HintPanel/>
+        <ScrambleModal/>
+      </FadeIn>
     </>
   );
 }
