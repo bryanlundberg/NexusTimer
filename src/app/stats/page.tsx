3029552b94adcc25080a3a55e3efb937d542f2c5
Bryan Lundberg <bryanlundberg@outlook.com>
2025-05-31 14:59:13 -0600

    `feat(stats): wrap stats page with fade-in animation`
    Added a `FadeIn` component to wrap the stats page, enabling a smooth entry animation. This enhances the user experience by providing a visually appealing transition effect.

--- src/app/stats/page.tsx
+++ src/app/stats/page.tsx
@@ -1,11 +1,11 @@
 "use client";
 import Navigation from "@/components/navigation/navigation";
 import CategoryStatistics from "@/components/stats/CategoryStatistics";
-import { useTranslations } from "next-intl";
+import FadeIn from "@/components/fade-in/fade-in";
 
 export default function Page() {
   return (
-    <>
+    <FadeIn className="flex flex-col grow">
       {/* container */}
       <div className="overflow-auto pb-4">
         <div className="max-w-7xl mx-auto px-2 pt-2 flex flex-col w-full min-h-full">
@@ -14,6 +14,6 @@ export default function Page() {
           <CategoryStatistics />
         </div>
       </div>
-    </>
+    </FadeIn>
   );
 }
