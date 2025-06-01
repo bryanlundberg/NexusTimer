9d9d8a7b4cf03619d670d1d443d2a6f92eb49e75
Bryan Lundberg <bryanlundberg@outlook.com>
2025-05-31 19:20:17 -0600

    `fix: adjust sticky navigation position by 1px`
    Updated the top position of the sticky navigation bar from `top-0` to `top-1`. This ensures better alignment and avoids layout issues on certain screen sizes.

--- src/components/navigation/navigation.tsx
+++ src/components/navigation/navigation.tsx
@@ -16,7 +16,7 @@ export default function Navigation({
 
   return (
     <>
-      <div className="w-full max-w-7xl border mx-auto flex flex-col rounded-lg bg-background/90 backdrop-blur-lg p-2 gap-2 mb-2 sticky top-0 left-0  z-50">
+      <div className="w-full max-w-7xl border mx-auto flex flex-col rounded-lg bg-background/90 backdrop-blur-lg p-2 gap-2 mb-2 sticky top-1 left-0 z-50">
         <div className="flex justify-center items-center gap-2">
           {/* menu button */}
           <ButtonNavbar />
