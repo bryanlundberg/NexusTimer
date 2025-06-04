a64801d033b34822629ebca337165237fd973fc5
Bryan Lundberg <bryanlundberg@outlook.com>
2025-06-04 06:29:51 -0600

    feat: Update Navigation component to support conditional button rendering

--- src/components/navigation/navigation.tsx
+++ src/components/navigation/navigation.tsx
@@ -4,34 +4,37 @@ import ButtonDisplayType from "./buttons/button-display-type";
 import ButtonCreateCollection from "./buttons/button-create-collection";
 import ButtonNextScramble from "./buttons/button-next-scramble";
 import ButtonNavbar from "./buttons/button-navbar";
-import { usePathname } from "next/navigation";
 import ButtonSelectMode from "./buttons/button-select-mode";
+import { ReactNode } from "react";
 
 export default function Navigation({
   children,
+  showMenu = true,
+  showMainCubeSelector = false,
+  showButtonNextScramble = false,
+  showButtonDisplayType = false,
+  showButtonCreateCollection = false,
+  showButtonSelectMode = false
 }: {
-  children?: React.ReactNode;
-}) {
-  const path = usePathname();
+  children?: ReactNode;
+  showMenu?: boolean;
+  showMainCubeSelector?: boolean;
+  showButtonNextScramble?: boolean;
+  showButtonDisplayType?: boolean;
+  showButtonCreateCollection?: boolean;
+  showButtonSelectMode?: boolean;
 
+}) {
   return (
     <>
       <div className="w-full max-w-7xl border mx-auto flex flex-col rounded-lg bg-background/90 backdrop-blur-lg p-2 gap-2 mb-2 sticky top-1 left-0 z-50">
         <div className="flex justify-center items-center gap-2">
-          {/* menu button */}
-          <ButtonNavbar />
-
-          <MainCubeSelector />
-
-          {path === "/" && <ButtonNextScramble />}
-
-          {path === "/stats" || path === "/solves" ? (
-            <ButtonDisplayType />
-          ) : null}
-
-          {path === "/cubes" && <ButtonCreateCollection />}
-
-          {path === "/" && <ButtonSelectMode />}
+          {showMenu && <ButtonNavbar/>}
+          {showMainCubeSelector && <MainCubeSelector/>}
+          {showButtonNextScramble && <ButtonNextScramble/>}
+          {showButtonDisplayType && <ButtonDisplayType/>}
+          {showButtonCreateCollection && <ButtonCreateCollection/>}
+          {showButtonSelectMode && <ButtonSelectMode/>}
         </div>
         {children}
       </div>
