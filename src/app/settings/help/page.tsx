4752d11e6076fdef7522748ce6320a8d40d5dc0e
Bryan Lundberg <bryanlundberg@outlook.com>
2025-06-11 06:17:01 -0600

    feat: add GitHub logo icon to source link in help page

--- src/app/settings/help/page.tsx
+++ src/app/settings/help/page.tsx
@@ -3,6 +3,7 @@ import AccountHeader from "@/components/account/account-header/account-header";
 import { Button } from "@/components/ui/button";
 import { useTranslations } from "next-intl";
 import Link from "next/link";
+import { GitHubLogoIcon } from "@radix-ui/react-icons";
 
 export default function Page() {
   const t = useTranslations("Index");
@@ -26,9 +27,10 @@ export default function Page() {
         <Link
           href={"https://github.com/bryanlundberg/NexusTimer"}
           className="w-full"
+          target={"_blank"}
         >
           <Button variant={"secondary"} className="w-full">
-            {t("SettingsPage.source")}
+            <GitHubLogoIcon className={"mr-2"}/> {t("SettingsPage.source")}
           </Button>
         </Link>
       </div>
