d5f416d15bca95c38406ea035dc385517c42374b
Bryan Lundberg <bryanlundberg@outlook.com>
2025-06-14 05:33:50 -0600

    refactor: useAudioTrigger to leverage useAudioPlayer
    Replaced manual audio handling with the `useAudioPlayer` hook for improved audio management. Added support for an optional `autoplay` parameter and updated code documentation for better clarity.

--- src/hooks/useAudioTrigger.ts
+++ src/hooks/useAudioTrigger.ts
@@ -1,23 +1,28 @@
 "use client";
 
-import { useRef, useEffect } from "react";
+import { useEffect, useRef } from "react";
+import { useAudioPlayer } from "react-use-audio-player";
 
+/**
+ * Hook to play an audio when a trigger condition changes from false to true.
+ *
+ * @param {Object} props - The hook properties
+ * @param {string} props.audioSrc - The path to the audio file to play
+ * @param {boolean} props.trigger - The condition that triggers the audio playback
+ * @param {boolean} [props.autoplay=true] - Whether to autoplay the audio when loaded
+ */
 type UseAudioTriggerProps = {
   audioSrc: string;
   trigger: boolean;
+  autoplay?: boolean;
 };
 
-export const useAudioTrigger = ({
-  audioSrc,
-  trigger,
-}: UseAudioTriggerProps) => {
-  const prev = useRef(trigger);
+export const useAudioTrigger = ({ audioSrc, trigger, autoplay = true }: UseAudioTriggerProps) => {
+  const previousTriggerState = useRef(trigger);
+  const { load } = useAudioPlayer();
 
   useEffect(() => {
-    if (!prev.current && trigger) {
-      const audio = new Audio(audioSrc);
-      audio.play();
-    }
-    prev.current = trigger;
-  }, [trigger, audioSrc]);
+    if (trigger && !previousTriggerState.current) load(audioSrc, { autoplay });
+    previousTriggerState.current = trigger;
+  }, [trigger, audioSrc, autoplay, load]);
 };
