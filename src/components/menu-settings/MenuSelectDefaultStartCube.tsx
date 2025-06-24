fb5614a18994f8361f6c5915c34ae543dd37170d
Bryan Lundberg <bryanlundberg@outlook.com>
2025-06-24 06:11:04 -0600

    `feat(ui): improve layout and add description for cube selection`
    Updated the layout of the default cube selection component for better structure and added a descriptive text to improve user clarity. Ensures alignment with design guidelines and enhances the user experience.

--- src/components/menu-settings/MenuSelectDefaultStartCube.tsx
+++ src/components/menu-settings/MenuSelectDefaultStartCube.tsx
@@ -29,27 +29,32 @@ export default function MenuSelectDefaultStartCube() {
   const defaultCube = settings.preferences.defaultCube;
 
   return (
-    <div className="flex justify-between items-center mb-1 mx-3">
-      <div className="grow">{t("Settings-menu.auto-select")}</div>
-      <Select
-        defaultValue={defaultCube || "none"}
-        value={defaultCube || "none"}
-        onValueChange={handleCubeSelect}
-      >
-        <SelectTrigger className="w-[180px] bg-background">
-          <SelectValue />
-        </SelectTrigger>
-        <SelectContent>
-          <SelectItem value="none">{t("Inputs.none")}</SelectItem>
-          {cubes?.map((cube) => {
-            return (
-              <SelectItem value={cube.id} key={cube.id}>
-                {cube.name}
-              </SelectItem>
-            );
-          })}
-        </SelectContent>
-      </Select>
+    <div className="mx-3 mb-3">
+      <div className="flex justify-between items-center mb-1">
+        <div className="grow">{t("Settings-menu.auto-select")}</div>
+        <Select
+          defaultValue={defaultCube || "none"}
+          value={defaultCube || "none"}
+          onValueChange={handleCubeSelect}
+        >
+          <SelectTrigger className="w-[180px] bg-background">
+            <SelectValue />
+          </SelectTrigger>
+          <SelectContent>
+            <SelectItem value="none">{t("Inputs.none")}</SelectItem>
+            {cubes?.map((cube) => {
+              return (
+                <SelectItem value={cube.id} key={cube.id}>
+                  {cube.name}
+                </SelectItem>
+              );
+            })}
+          </SelectContent>
+        </Select>
+      </div>
+      <div className="text-xs text-muted-foreground">
+        {t("Settings-descriptions.auto-select-description")}
+      </div>
     </div>
   );
 }
