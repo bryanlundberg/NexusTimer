878da608983cede210d259750003eb10aec875d9
Bryan Lundberg <bryanlundberg@outlook.com>
2025-06-28 18:36:04 -0600

    **feat: add framer-motion for navigation container animation**
    Introduce `framer-motion` to enhance the navigation container with opacity and vertical slide-in animations. This improves the UI's interactivity and visual appeal with smooth transitions.

--- src/components/navigation/navigation.tsx
+++ src/components/navigation/navigation.tsx
@@ -6,6 +6,7 @@ import ButtonNextScramble from "./buttons/button-next-scramble";
 import ButtonNavbar from "./buttons/button-navbar";
 import ButtonSelectMode from "./buttons/button-select-mode";
 import { ReactNode } from "react";
+import { motion } from "framer-motion";
 
 export default function Navigation({
   children,
@@ -27,7 +28,12 @@ export default function Navigation({
 }) {
   return (
     <>
-      <div className="w-full max-w-7xl border mx-auto flex flex-col rounded-lg bg-background/50 backdrop-blur-lg p-2 gap-2 mb-2 sticky top-1 left-0 z-50">
+      <motion.div
+        className="w-full max-w-7xl border mx-auto flex flex-col rounded-lg bg-background/50 backdrop-blur-lg p-2 gap-2 mb-2 sticky top-1 left-0 z-50"
+        initial={{ opacity: 0.5 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.2, ease: "easeOut" }}
+      >
         {(showMenu || showMainCubeSelector || showButtonNextScramble || showButtonDisplayType || showButtonCreateCollection || showButtonSelectMode) && (
           <div className="flex justify-center items-center gap-2">
             {showMenu && <ButtonNavbar/>}
@@ -39,7 +45,7 @@ export default function Navigation({
           </div>
         )}
         {children}
-      </div>
+      </motion.div>
     </>
   );
 }
