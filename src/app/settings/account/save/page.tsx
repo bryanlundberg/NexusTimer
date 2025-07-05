6c98a5c71f30ccb0862a8dcebb01a30ca0007479
Bryan Lundberg <bryanlundberg@outlook.com>
2025-07-04 20:32:32 -0600

    refactor: backup handler and improve error handling
    Refactored the backup logic into a dedicated `handleBackup` function for improved readability and reusability. Added error toast notification for handling cases where cube or session data is invalid, enhancing user feedback and reliability. Updated formatting and alignment for consistent code style.

--- src/app/settings/account/save/page.tsx
+++ src/app/settings/account/save/page.tsx
@@ -1,55 +1,53 @@
-"use client";
-import { createOrUpdateBackup } from "@/actions/actions";
-import AccountHeader from "@/components/account/account-header/account-header";
-import { Button } from "@/components/ui/button";
-import { Card } from "@/components/ui/card";
-import { getAllCubes } from "@/db/dbOperations";
-import { useSession } from "next-auth/react";
-import { useTranslations } from "next-intl";
-import Link from "next/link";
-import { redirect, useRouter } from "next/navigation";
+'use client';
+import { createOrUpdateBackup } from '@/actions/actions';
+import AccountHeader from '@/components/account/account-header/account-header';
+import { Button } from '@/components/ui/button';
+import { Card } from '@/components/ui/card';
+import { getAllCubes } from '@/db/dbOperations';
+import { useSession } from 'next-auth/react';
+import { useTranslations } from 'next-intl';
+import Link from 'next/link';
+import { redirect, useRouter } from 'next/navigation';
+import { toast } from 'sonner';
 
 export default function Page() {
   const { data: session } = useSession();
-  const t = useTranslations("Index");
+  const t = useTranslations('Index');
   const router = useRouter();
-  if (!session) redirect("/settings");
+  if (!session) redirect('/settings');
+
+  const handleBackup = async () => {
+    const cubes = await getAllCubes();
+
+    if (!cubes || !session || !session.user || typeof session.user.email !== 'string') {
+      return toast.error('Failed to retrieve cubes or session data.');
+    }
+
+    await createOrUpdateBackup({
+      email: session.user.email,
+      data: JSON.stringify(cubes),
+    });
+
+    router.push('/');
+  }
+
   return (
     <>
-      <AccountHeader back="/" label={t("SettingsPage.save-data-title")} />
-
+      <AccountHeader back="/" label={t('SettingsPage.save-data-title')}/>
       <Card className="p-3 bg-secondary/10">
-        <p>{t("SettingsPage.save-data-description")}</p>
-        <p className="text-yellow-600">{t("SettingsPage.save-data-warning")}</p>
-
+        <p>{t('SettingsPage.save-data-description')}</p>
+        <p className="text-yellow-600">{t('SettingsPage.save-data-warning')}</p>
         <div className="flex gap-2 w-full justify-between mt-5 flex-col-reverse sm:flex-row">
-          <Link href={"/"} className="grow">
-            <Button variant={"secondary"} className="w-full">
-              {t("Inputs.back")}
+          <Link href={'/'} className="grow">
+            <Button variant={'secondary'} className="w-full">
+              {t('Inputs.back')}
             </Button>
           </Link>
           <Button
             className="grow"
-            onClick={async () => {
-              const cubes = await getAllCubes();
-              if (
-                !cubes ||
-                !session ||
-                !session.user ||
-                typeof session.user.email !== "string"
-              ) {
-                return; // send toast error to screen
-              }
-
-              await createOrUpdateBackup({
-                email: session.user.email,
-                data: JSON.stringify(cubes),
-              });
-
-              router.push("/");
-            }}
+            onClick={handleBackup}
           >
-            {t("Inputs.continue")}
+            {t('Inputs.continue')}
           </Button>
         </div>
       </Card>
