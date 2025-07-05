8226944c0053ec7bc9a98815970ad4e94e084745
Bryan Lundberg <bryanlundberg@outlook.com>
2025-07-04 22:44:47 -0600

    `refactor: streamline cube synchronization with syncBackup`
    Replaced manual cube merging logic with `syncBackup` hook to simplify code and enhance maintainability. This ensures consistency and reduces redundancy when syncing backup data with existing cubes. All related dependencies and unused imports were cleaned up accordingly.

--- src/app/settings/account/load/page.tsx
+++ src/app/settings/account/load/page.tsx
@@ -4,20 +4,21 @@ import AccountHeader from "@/components/account/account-header/account-header";
 import { Button } from "@/components/ui/button";
 import { Card } from "@/components/ui/card";
 import { clearCubes, getAllCubes, saveBatchCubes } from '@/db/dbOperations';
-import { Cube } from "@/interfaces/Cube";
 import { useTimerStore } from "@/store/timerStore";
 import { useSession } from "next-auth/react";
 import { useTranslations } from "next-intl";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
-import { formatCubesDatesAndOrder, importNexusTimerData } from '@/lib/importDataFromFile';
-import _, { uniqBy } from 'lodash';
+import { importNexusTimerData } from '@/lib/importDataFromFile';
+import { useSyncBackup } from '@/hooks/useSyncBackup';
 
 export default function Page() {
   const t = useTranslations("Index");
   const { data: session } = useSession();
   const router = useRouter();
   const { setCubes } = useTimerStore();
+  const { syncBackup } = useSyncBackup();
+
   const handleDownloadData = async () => {
     if (!session || !session.user || !session.user.email) return;
 
@@ -28,24 +29,7 @@ export default function Page() {
     const backupData = importNexusTimerData(jsonBackup.data);
     const existingCubes = await getAllCubes();
 
-    let newCubes = _.cloneDeep(existingCubes) as Cube[];
-
-    for(let i = 0; i < backupData.length; i++) {
-      const backupCube = backupData[i];
-      const existingCube = newCubes.find(cube => cube.id === backupCube.id);
-
-      if (existingCube) {
-        newCubes[newCubes.indexOf(existingCube)].solves = {
-          session: uniqBy([...existingCube.solves.session, ...backupCube.solves.session], 'id'),
-          all: uniqBy([...existingCube.solves.all, ...backupCube.solves.all], 'id'),
-        }
-      } else {
-        newCubes.push(backupCube);
-      }
-    }
-
-    newCubes = formatCubesDatesAndOrder(newCubes);
-
+    const newCubes = await syncBackup(backupData, existingCubes);
     await clearCubes();
     await saveBatchCubes(newCubes);
     setCubes(newCubes);
