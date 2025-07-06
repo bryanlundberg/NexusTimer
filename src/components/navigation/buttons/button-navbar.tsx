0b12416ca366fa0c1db733f6124ad28f88f4fadb
Bryan Lundberg <bryanlundberg@outlook.com>
2025-07-06 13:50:24 -0600

    feat: add social links for Discord and GitHub to navbar
    Added a new "Socials" section in the navbar, allowing users to quickly access the project's Discord and GitHub pages. This improves accessibility to community and collaboration resources.

--- src/components/navigation/buttons/button-navbar.tsx
+++ src/components/navigation/buttons/button-navbar.tsx
@@ -21,20 +21,19 @@ import {
   CircleIcon,
   CubeIcon,
   DesktopIcon,
-  EnterFullScreenIcon,
+  EnterFullScreenIcon, GitHubLogoIcon,
   MixerHorizontalIcon,
   MoonIcon,
   PersonIcon,
   SunIcon,
   TokensIcon
-} from "@radix-ui/react-icons";
+} from '@radix-ui/react-icons';
 
 import { ArrowRightLeftIcon } from "lucide-react";
 
 import Link from "next/link";
 import { useRouter } from "next/navigation";
 import { useTheme } from "next-themes";
-import genId from "@/lib/genId";
 import { useTranslations } from "next-intl";
 import { useFullScreen } from "@/hooks/useFullScreen";
 import useWebsiteColors from "@/hooks/useWebsiteColors";
@@ -195,6 +194,40 @@ export default function ButtonNavbar() {
             <CommandItem onPointerDown={() => handleThemeChange("system")}>
               <DesktopIcon/>
               {t("Settings-menu.system")}
+            </CommandItem>
+          </CommandGroup>
+          <CommandSeparator/>
+          <CommandGroup heading={"Socials"}>
+            <CommandItem>
+              <Link
+                href="https://discord.gg/cUECp4fr"
+                target="_blank"
+                rel="noopener noreferrer"
+                className={"flex items-center gap-2 w-full"}>
+              <svg
+                width="15"
+                height="15"
+                viewBox="0 0 24 24"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+                className="mr-1"
+              >
+                <path
+                  d="M20.317 4.37a19.791 19.791 0 0 0-4.885-1.515.074.074 0 0 0-.079.037c-.21.375-.444.864-.608 1.25a18.27 18.27 0 0 0-5.487 0 12.64 12.64 0 0 0-.617-1.25.077.077 0 0 0-.079-.037A19.736 19.736 0 0 0 3.677 4.37a.07.07 0 0 0-.032.027C.533 9.046-.32 13.58.099 18.057a.082.082 0 0 0 .031.057 19.9 19.9 0 0 0 5.993 3.03.078.078 0 0 0 .084-.028c.462-.63.874-1.295 1.226-1.994a.076.076 0 0 0-.041-.106 13.107 13.107 0 0 1-1.872-.892.077.077 0 0 1-.008-.128 10.2 10.2 0 0 0 .372-.292.074.074 0 0 1 .077-.01c3.928 1.793 8.18 1.793 12.062 0a.074.074 0 0 1 .078.01c.12.098.246.198.373.292a.077.077 0 0 1-.006.127 12.299 12.299 0 0 1-1.873.892.077.077 0 0 0-.041.107c.36.698.772 1.362 1.225 1.993a.076.076 0 0 0 .084.028 19.839 19.839 0 0 0 6.002-3.03.077.077 0 0 0 .032-.054c.5-5.177-.838-9.674-3.549-13.66a.061.061 0 0 0-.031-.03zM8.02 15.33c-1.183 0-2.157-1.085-2.157-2.419 0-1.333.956-2.419 2.157-2.419 1.21 0 2.176 1.096 2.157 2.42 0 1.333-.956 2.418-2.157 2.418zm7.975 0c-1.183 0-2.157-1.085-2.157-2.419 0-1.333.955-2.419 2.157-2.419 1.21 0 2.176 1.096 2.157 2.42 0 1.333-.946 2.418-2.157 2.418z"
+                  fill="currentColor"
+                />
+              </svg>
+              Join Discord
+              </Link>
+            </CommandItem>
+            <CommandItem>
+              <Link
+                href={"https://github.com/bryanlundberg/NexusTimer"}
+                target="_blank"
+                rel="noopener noreferrer"
+                className={"flex items-center gap-2 w-full"}>
+                <GitHubLogoIcon className={"mr-2"}/> Github
+              </Link>
             </CommandItem>
           </CommandGroup>
         </CommandList>
