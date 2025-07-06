44e256fa1f810c48ac655a115e33e038779db27b
Bryan Lundberg <bryanlundberg@outlook.com>
2025-07-06 13:33:27 -0600

    chore: temporarily disable cloud sync in usePreloadSettings
    Commented out the cloud sync logic to address potential issues during testing. This ensures the feature does not interfere with production while further testing and debugging are carried out.

--- src/hooks/usePreloadSettings.ts
+++ src/hooks/usePreloadSettings.ts
@@ -2,11 +2,7 @@ import { useEffect, useState } from "react";
 import loadSettings from "@/lib/loadSettings";
 import { useSettingsModalStore } from "@/store/SettingsModalStore";
 import { useTimerStore } from "@/store/timerStore";
-import { getAllCubes, getCubeById, saveBatchCubes } from '@/db/dbOperations';
-import { useSession } from "next-auth/react";
-import { getLastBackup } from '@/actions/actions';
-import { Cube } from '@/interfaces/Cube';
-import { useSyncBackup } from '@/hooks/useSyncBackup';
+import { getAllCubes, getCubeById } from '@/db/dbOperations';
 
 export function usePreloadSettings() {
   const setCubes = useTimerStore(store => store.setCubes);
@@ -14,9 +10,6 @@ export function usePreloadSettings() {
   const setNewScramble = useTimerStore(store => store.setNewScramble);
   const setSettings = useSettingsModalStore(store => store.setSettings);
   const [isMounted, setIsMounted] = useState(false);
-  const { data: session } = useSession();
-  const [synced, setSynced] = useState(false);
-  const { syncBackup } = useSyncBackup();
 
   useEffect(() => {
     const settings = loadSettings();
@@ -41,43 +34,45 @@ export function usePreloadSettings() {
     getAllCubes().then((res) => setCubes(res));
   }, [setCubes]);
 
-  useEffect(() => {
-    const checkCloudData = async () => {
-      if (session && session.user && session.user.email && !synced) {
-        setSynced(true);
-        try {
-          const localCubes = await getAllCubes();
-          const backup = await getLastBackup({ email: session.user.email });
-          if (!backup) {
-            return;
-          }
 
-          const parsedBackup = JSON.parse(backup);
-          const cloudBackup = JSON.parse(parsedBackup.data) as Cube[];
-
-          const localSolvesCount = localCubes.reduce((total, cube) => {
-            return total + cube.solves.all.length + cube.solves.session.length;
-          }, 0);
-
-          const cloudSolvesCount = cloudBackup.reduce((total, cube) => {
-            return total + cube.solves.all.length + cube.solves.session.length;
-          }, 0);
-
-          if (localSolvesCount === cloudSolvesCount) {
-            return;
-          }
-
-          const syncedCubes = await syncBackup(cloudBackup, localCubes);
-
-          setCubes(syncedCubes);
-        } catch (error) {
-          console.log(error);
-        }
-      }
-    };
-
-    checkCloudData();
-  }, [session, setCubes, syncBackup, synced]);
+  // TODO: Cloud sync functionality
+  // NEED MORE TIME FOR TESTING BEFORE RE-ENABLING IN PRODUCTION ENVIRONMENT
+  // useEffect(() => {
+  //   const checkCloudData = async () => {
+  //     if (session && session.user && session.user.email && !synced) {
+  //       setSynced(true);
+  //       try {
+  //         const localCubes = await getAllCubes();
+  //         const backup = await getLastBackup({ email: session.user.email });
+  //         if (!backup) {
+  //           return;
+  //         }
+  //
+  //         const parsedBackup = JSON.parse(backup);
+  //         const cloudBackup = JSON.parse(parsedBackup.data) as Cube[];
+  //
+  //         const syncedCubes = await syncBackup(cloudBackup, localCubes);
+  //
+  //         await saveBatchCubes(syncedCubes);
+  //
+  //         const selectedCube = useTimerStore.getState().selectedCube;
+  //         if (selectedCube) {
+  //           const syncedSelectedCube = syncedCubes.find(cube => cube.id === selectedCube.id);
+  //           if (syncedSelectedCube) {
+  //             useTimerStore.getState().setSelectedCube(syncedSelectedCube);
+  //             useTimerStore.getState().setNewScramble(syncedSelectedCube);
+  //           }
+  //         }
+  //
+  //         setCubes(syncedCubes);
+  //       } catch (error) {
+  //         console.log(error);
+  //       }
+  //     }
+  //   };
+  //
+  //   checkCloudData();
+  // }, [session, setCubes, syncBackup, synced]);
 
   useEffect(() => {
     setIsMounted(true);
