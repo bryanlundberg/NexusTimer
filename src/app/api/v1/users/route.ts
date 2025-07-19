d4644f451f08cb6bd6a54b3f13fd1d71aab57b46
Bryan Lundberg <bryanlundberg@outlook.com>
2025-07-19 14:40:48 -0600

    feat(api): add user management endpoints
    Introduce GET and POST endpoints for user data handling. GET retrieves paginated users, while POST creates a new user and sends a welcome email. Includes MongoDB integration and email sending using Resend.

---
+++ src/app/api/v1/users/route.ts
@@ -0,0 +1,58 @@
+import { NextRequest, NextResponse } from 'next/server';
+import connectDB from '@/db/mongodb';
+import User from '@/models/user';
+import { Resend } from 'resend';
+import Email from '@/components/email/email';
+
+const resend = new Resend(
+  process.env.RESEND_API_KEY || 'development-placeholder-no-email-sent'
+);
+
+export async function GET(request: NextRequest) {
+  try {
+    await connectDB();
+    const searchParams = request.nextUrl.searchParams;
+    const page = Math.max(0, Number(searchParams.get('page')) || 0);
+    const PER_PAGE = 50
+
+    const users = await User.find().limit(PER_PAGE).skip(page * PER_PAGE).sort({ createdAt: -1 })
+    const docsCount = await User.find().countDocuments()
+
+    return NextResponse.json({
+      events: users,
+      page: page,
+      pages: Math.max(0, (docsCount / PER_PAGE) - 1),
+    });
+  } catch (e) {
+    console.error(e)
+    return NextResponse.json({ error: 'Internal server error' }, { status: 500 });
+  }
+}
+
+export async function POST(request: NextRequest) {
+  try {
+    const { email, name, image } = await request.json();
+
+    if (!email || !name || !image) {
+      return NextResponse.json({ error: 'Incomplete fields' }, { status: 400 });
+    }
+
+    await connectDB();
+    let user = await User.findOne({ email });
+
+    if (!user) {
+      user = await User.create({ email, name, image });
+      await resend.emails.send({
+        from: 'NexusTimer <onboarding@nexustimer.com>',
+        to: [email],
+        subject: 'Welcome to NexusTimer – Let\'s Get Cubing!',
+        react: Email({ name }) as React.ReactElement,
+      });
+    }
+
+    return NextResponse.json(user);
+  } catch (error) {
+    console.error('Error creating user:', error);
+    return NextResponse.json({ error: 'Internal server error' }, { status: 500 });
+  }
+}
