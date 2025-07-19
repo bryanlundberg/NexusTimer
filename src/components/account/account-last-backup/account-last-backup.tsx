fdf7ccbdf5b06ed5937072228ccb50bbf368b793
Bryan Lundberg <bryanlundberg@outlook.com>
2025-07-19 06:27:35 -0600

    `feat: format last backup date with moment.js`
    Integrate moment.js to display the last backup date in a readable format. Dates are now formatted as "DD/MMMM/YYYY HH:mm:ss" when available, improving user experience.

--- src/components/account/account-last-backup/account-last-backup.tsx
+++ src/components/account/account-last-backup/account-last-backup.tsx
@@ -4,16 +4,17 @@ import { getLastBackupDate } from "@/actions/actions";
 import { Session } from "next-auth";
 import { useTranslations } from "next-intl";
 import { useEffect, useState } from "react";
+import moment from 'moment';
 
 export default function AccountLastBackup({ session }: { session: Session }) {
-  const t = useTranslations("Index");
+  const t = useTranslations('Index');
   const [lastBackupDate, setLastBackupDate] = useState<string | null>(null);
   const [isLoading, setIsLoading] = useState(true);
   useEffect(() => {
     const fetchLastBackupDate = async () => {
       if (!session || !session.user || !session.user.email) return;
       const date = await getLastBackupDate({ email: session.user.email });
-      setLastBackupDate(date ? date : "No backup found");
+      setLastBackupDate(date ? date : 'No backup found');
       setIsLoading(false);
     };
 
@@ -24,8 +25,12 @@ export default function AccountLastBackup({ session }: { session: Session }) {
     <>
       <div>
         {!isLoading
-          ? t("SettingsPage.last-backup") + " " + lastBackupDate
-          : t("SettingsPage.fetching-last-backup")}
+          ? t('SettingsPage.last-backup') +
+          ' ' +
+          (lastBackupDate && lastBackupDate !== 'No backup found'
+            ? moment(new Date(lastBackupDate)).format('DD/MMMM/YYYY HH:mm:ss')
+            : lastBackupDate)
+          : t('SettingsPage.fetching-last-backup')}
       </div>
     </>
   );
