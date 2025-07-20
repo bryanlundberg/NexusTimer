a66c34c63db334b7a133b9e50057c1ead2f243d8
Bryan Lundberg <bryanlundberg@outlook.com>
2025-07-20 08:51:10 -0600

    chore: update pnpm-lock.yaml with dependency upgrades
    Upgraded multiple dependencies in pnpm-lock.yaml to their latest versions, including @radix-ui, @babel, react, and others. These updates aim to improve security, performance, and compatibility with the latest features in the ecosystem.

--- src/hooks/api/useUsers.ts
+++ src/hooks/api/useUsers.ts
@@ -1,11 +1,11 @@
-import useSWR from 'swr';
 import { fetcher } from '@/utils/fetcher';
+import useSWR from 'swr';
 
 export const useUsers = () => {
   const { data, error, isLoading } = useSWR(`/api/v1/users`, fetcher)
 
   return {
-    user: data,
+    users: data,
     isLoading,
     isError: error
   }
