4685270cf7eda6a19672abfe3cf36dc0be795212
Bryan Lundberg <bryanlundberg@outlook.com>
2025-07-26 07:02:31 -0600

    feat: add GET endpoint to fetch user by ID
    Introduced a GET handler for retrieving user details by ID. Handles cases for missing ID, non-existent users, and server errors, ensuring proper error responses and logging. Filters out sensitive data before sending the response.

--- src/app/api/v1/users/[id]/route.ts
+++ src/app/api/v1/users/[id]/route.ts
@@ -39,3 +39,27 @@ export async function PATCH(request: NextRequest, { params }: { params: Promise<
     return NextResponse.json({ error: 'Internal server error' }, { status: 500 });
   }
 }
+
+export async function GET(request: NextRequest, { params }: { params: Promise<{ id: string }> }) {
+  try {
+    const userId = (await params).id;
+
+    if (!userId) {
+      return NextResponse.json({ error: 'ID is required' }, { status: 404 });
+    }
+
+    await connectDB();
+    const user = await User.findById(userId);
+
+    if (!user) {
+      return NextResponse.json({ error: 'User not found' }, { status: 404 });
+    }
+
+    const { email, createdAt, updatedAt, __v, ...rest } = user.toObject();
+
+    return NextResponse.json(rest);
+  } catch (error) {
+    console.error('Error fetching user:', error);
+    return NextResponse.json({ error: 'Internal server error' }, { status: 500 });
+  }
+}
