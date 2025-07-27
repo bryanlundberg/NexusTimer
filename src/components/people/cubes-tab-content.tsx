ee3524e5fff242b64f50625ec3ca6e1de3be649d
Bryan Lundberg <bryanlundberg@outlook.com>
2025-07-26 23:04:16 -0600

    `feat: add CubesTabContent component for displaying cube data`
    Introduced a new `CubesTabContent` component to display cube information in a grid layout. It includes cards showing cube details with images, creation dates, and solve counts. A fallback message is shown if no cubes are available.

---
+++ src/components/people/cubes-tab-content.tsx
@@ -0,0 +1,37 @@
+import { Card } from '@/components/ui/card';
+import Image from 'next/image';
+import { cubeCollection } from '@/lib/const/cubeCollection';
+import * as React from 'react';
+import { Cube } from '@/interfaces/Cube';
+import moment from 'moment';
+
+interface CubesTabContentProps {
+  cubes: Cube[]
+}
+
+export default function CubesTabContent({ cubes }: CubesTabContentProps) {
+  return (
+    <div className={'grid grid-cols-2 gap-4'}>
+      {cubes?.length ? cubes.map((cube) => (
+        <Card key={cube.id} className={'flex flex-1 h-auto gap-2 flex-row items-center px-3'}>
+          <Image
+            src={cubeCollection.find((item) => item.name === cube.category)?.src || ''}
+            alt={cube.name}
+            className={'size-20 object-scale-down rounded p-1'}
+            draggable={false}
+            width={64}
+            height={64}
+          />
+          <div className={'flex flex-col space-y-2 grow'}>
+            <div className={'text-lg font-semibold'}>{cube.name}</div>
+            <div className={'text-sm text-muted-foreground'}>Created
+              at: {moment(cube.createdAt).format('DD/MM/YYYY')}</div>
+            <div className={'text-sm text-muted-foreground'}>Solves: {cube.solves.session.length}</div>
+          </div>
+        </Card>
+      )) : (
+        <div className="text-center text-gray-500">No cubes found.</div>
+      )}
+    </div>
+  )
+}
