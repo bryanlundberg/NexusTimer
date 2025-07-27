71e73f7a61d04e2c253adca3472f1c81b0b2e9ea
Bryan Lundberg <bryanlundberg@outlook.com>
2025-07-26 21:56:45 -0600

    feat: add user-specific people page with tabs and backup parsing
    Introduce a new user-specific page under `/people/[userId]/` with tabs for Overview, Stats, Cubes, and Last Activity. Includes dynamic data fetching for user info and backup parsing to display user-related details such as solves and achievements.

---
+++ src/app/people/[userId]/page.tsx
@@ -0,0 +1,126 @@
+'use client';
+import { useParams } from 'next/navigation';
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbSeparator
+} from '@/components/ui/breadcrumb';
+import FadeIn from '@/components/fade-in/fade-in';
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
+import { useUser } from '@/hooks/api/useUser';
+import * as React from 'react';
+import { useEffect } from 'react';
+import Link from 'next/link';
+import { useBackup } from '@/hooks/api/useBackup';
+import { Badge } from '@/components/ui/badge';
+import { Card } from '@/components/ui/card';
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs'
+import { GlobeAmericasIcon } from '@heroicons/react/24/outline';
+import { PeopleTabs } from '@/enums/PeopleTabs';
+import { useQueryState } from 'nuqs';
+import { STATES } from '@/constants/states';
+import { Cube } from '@/interfaces/Cube';
+import { importNexusTimerData } from '@/lib/importDataFromFile';
+import SolvesTabContent from '@/components/people/solves-tab-content';
+import OverviewTabContent from '@/components/people/overview-tab-content';
+
+export default function Page() {
+  const params = useParams<{ userId: string; }>()
+  const userId = params.userId;
+  const { data: user, isLoading: isLoadingUser } = useUser(userId);
+  const { backup: backupString, isLoading: isLoadingBackup } = useBackup(userId);
+  const [tab, setTab] = useQueryState(STATES.PEOPLE_PAGE.TAB_MODE.KEY, { defaultValue: STATES.PEOPLE_PAGE.TAB_MODE.DEFAULT_VALUE });
+  const [backup, setBackup] = React.useState<Cube[]>([]);
+  const handleChangeTab = (value: PeopleTabs) => setTab(value);
+
+  useEffect(() => {
+    const parseBackup = () => {
+      if (backupString?.data) {
+        try {
+          const parsedBackup = importNexusTimerData(backupString?.data);
+          setBackup(parsedBackup);
+        } catch (error) {
+          console.error('Error parsing backup string:', error);
+          setBackup([]);
+        }
+      }
+    }
+    parseBackup();
+  }, [backupString,]);
+
+  if (isLoadingUser || isLoadingBackup) {
+    return (
+      <FadeIn className="flex flex-col grow items-center justify-center">
+        <div className="text-lg">Loading user data...</div>
+      </FadeIn>
+    );
+  }
+
+  return (
+    <FadeIn className="flex flex-col grow overflow-auto">
+      <div className="max-w-7xl mx-auto p-4 flex flex-col w-full min-h-full">
+        <Breadcrumb>
+          <BreadcrumbList>
+            <BreadcrumbItem>
+              <BreadcrumbLink asChild>
+                <Link href={'/people'}>People</Link>
+              </BreadcrumbLink>
+            </BreadcrumbItem>
+            <BreadcrumbSeparator/>
+            <BreadcrumbItem>
+              {user.name}
+            </BreadcrumbItem>
+          </BreadcrumbList>
+        </Breadcrumb>
+
+        <div className="flex p-5">
+          <div className="flex flex-col gap-2 items-center">
+            <Avatar className="size-40 mb-2 shadow-lg">
+              <AvatarImage className={'object-cover'} src={user.image} alt={user.name}/>
+              <AvatarFallback>{user.name.substring(0, 2).toUpperCase()}</AvatarFallback>
+            </Avatar>
+            <div className={'flex items-center gap-1'}><GlobeAmericasIcon className={'size-5'}/> Asia/ Jakarta</div>
+            <Badge variant={'secondary'}>Road Sub 10</Badge>
+            <Card className={'size-40'}>SECCION DE LOGROS</Card>
+          </div>
+
+          <div className="flex flex-col ml-4 grow">
+            <Tabs defaultValue={tab} className="w-full">
+              <TabsList>
+                <TabsTrigger
+                  onClick={() => handleChangeTab(PeopleTabs.OVERVIEW)}
+                  value={PeopleTabs.OVERVIEW}
+                >Overview</TabsTrigger>
+                <TabsTrigger
+                  onClick={() => handleChangeTab(PeopleTabs.STATS)}
+                  value={PeopleTabs.STATS}
+                >Stats</TabsTrigger>
+                <TabsTrigger
+                  onClick={() => handleChangeTab(PeopleTabs.CUBES)}
+                  value={PeopleTabs.CUBES}
+                >Cubes</TabsTrigger>
+                <TabsTrigger
+                  onClick={() => handleChangeTab(PeopleTabs.LAST_ACTIVITY)}
+                  value={PeopleTabs.LAST_ACTIVITY}
+                >Last activity</TabsTrigger>
+              </TabsList>
+              <TabsContent value={PeopleTabs.OVERVIEW}>
+                <SolvesTabContent cubes={backup}/>
+              </TabsContent>
+              <TabsContent value={PeopleTabs.STATS}>Change your password here.</TabsContent>
+              <TabsContent value={PeopleTabs.CUBES}>
+                <OverviewTabContent backup={backup}/>
+              </TabsContent>
+              <TabsContent value={PeopleTabs.LAST_ACTIVITY}>
+                <div className="p-4">Last activity content goes here.</div>
+              </TabsContent>
+            </Tabs>
+          </div>
+        </div>
+
+      </div>
+    </FadeIn>
+  )
+}
