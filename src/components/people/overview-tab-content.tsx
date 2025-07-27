d3993ff6b1e3f41db2ae0cdfa02ded72201b91c6
Bryan Lundberg <bryanlundberg@outlook.com>
2025-07-27 07:14:00 -0600

    **fix: handle empty state in OverviewTabContent**
    Added a fallback to render the `EmptyTabContent` component when `solvesByCategory` is empty. Also adjusted the grid layout breakpoint from `lg` to `xl` for better responsiveness.

--- src/components/people/overview-tab-content.tsx
+++ src/components/people/overview-tab-content.tsx
@@ -5,6 +5,7 @@ import _ from 'lodash';
 import moment from 'moment';
 import formatTime from '@/lib/formatTime';
 import { Categories } from '@/interfaces/Categories';
+import EmptyTabContent from '@/components/people/empty-tab-content';
 
 export default function OverviewTabContent({ cubes }: { cubes: Cube[] }) {
   const solvesByCategory = useMemo(() => {
@@ -18,8 +19,12 @@ export default function OverviewTabContent({ cubes }: { cubes: Cube[] }) {
     );
   }, [cubes]);
 
+  if (_.isEmpty(solvesByCategory)) {
+    return <EmptyTabContent/>
+  }
+
   return (
-    <div className={'grid grid-cols-1 lg:grid-cols-2 gap-4'}>
+    <div className={'grid grid-cols-1 xl:grid-cols-2 gap-4'}>
       {Object.entries(solvesByCategory).map(([category, solves]) => (
         <SolveCard
           key={solves[0].id}
