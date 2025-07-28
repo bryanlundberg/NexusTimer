2a7e4af3bcfcde2b1eaf2be59ec9d67fa85593e6
Bryan Lundberg <bryanlundberg@outlook.com>
2025-07-27 20:43:47 -0600

    `feat: add PeopleSkeleton component for loading states`
    Introduce a new `PeopleSkeleton` component to display a placeholder UI during the loading state. This component leverages the `Skeleton` and `FadeIn` components to ensure a visually consistent and smooth experience. It accommodates various layouts, including headers, avatars, and content grids.

---
+++ src/components/people/people-skeleton.tsx
@@ -0,0 +1,32 @@
+import FadeIn from "../fade-in/fade-in";
+import { Skeleton } from '@/components/ui/skeleton';
+
+export default function PeopleSkeleton() {
+  return (
+    <FadeIn className="flex flex-col grow overflow-auto">
+      <div className="max-w-7xl mx-auto p-4 flex flex-col w-full min-h-full">
+        <div className={"flex flex-row gap-2"}>
+          <Skeleton className={"w-20 h-8"}/>
+          <Skeleton className={"w-22 h-8"}/>
+          <Skeleton className={"w-26 h-8"}/>
+        </div>
+        <div className="flex flex-col md:flex-row p-5 relative">
+          <div className="flex flex-col items-center justify-center w-full md:w-1/4 h-64">
+            <Skeleton className="w-24 h-24 rounded-full mb-4"></Skeleton>
+            <Skeleton className="w-3/4 h-6  rounded mb-2"></Skeleton>
+            <Skeleton className="w-1/2 h-6  rounded"></Skeleton>
+          </div>
+          <div className="flex flex-col ml-4 grow">
+            <Skeleton className="w-full max-w-60 h-8 rounded mb-4"></Skeleton>
+            <div className="grid grid-cols-1 xl:grid-cols-2 gap-4">
+              <Skeleton className="h-64"></Skeleton>
+              <Skeleton className="h-64"></Skeleton>
+              <Skeleton className="h-64"></Skeleton>
+              <Skeleton className="h-64"></Skeleton>
+            </div>
+          </div>
+        </div>
+      </div>
+    </FadeIn>
+  );
+}
