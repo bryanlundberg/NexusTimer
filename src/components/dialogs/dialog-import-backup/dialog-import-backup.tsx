adec040133ab21aa0848e10b17894c11bd91932b
Bryan Lundberg <bryanlundberg@outlook.com>
2025-08-02 11:17:38 -0600

    **fix: improve backup import handling and update text consistency**
    Added an error toast for empty backup files to notify users clearly. Improved the user alert message after successful imports and standardized single quotes usage across the file.

--- src/components/dialogs/dialog-import-backup/dialog-import-backup.tsx
+++ src/components/dialogs/dialog-import-backup/dialog-import-backup.tsx
@@ -11,7 +11,7 @@ import { useNXData } from '@/hooks/useNXData';
 
 export default function DialogImportBackup() {
   const { getAllCubes, clearCubes, saveBatchCubes } = useNXData();
-  const t = useTranslations("Index.backup-modal");
+  const t = useTranslations('Index.backup-modal');
   const [isImporting, setIsImporting] = useState(false);
   const dataInputRef = useRef<HTMLInputElement>(null);
   const setSelectedCube = useTimerStore((state) => state.setSelectedCube);
@@ -23,6 +23,7 @@ export default function DialogImportBackup() {
       try {
         setIsImporting(true);
         const response = await importDataFromFile(e);
+        if (response && !response?.length) return toast.error('No valid data found in the backup file.');
         if (response) {
           await clearCubes();
           await saveBatchCubes(response);
@@ -32,7 +33,7 @@ export default function DialogImportBackup() {
           setCubes(cubesDB);
           router.push('/cubes');
           setSelectedCube(null);
-          alert('Backup imported successfully! You can now view your cubes in the Cubes section. Organize your cubes categories before!');
+          alert('Backup imported successfully! Organize your cubes categories before!');
         }
       } catch (error) {
         toast.error('Backup import failed. Please try again.');
@@ -48,9 +49,9 @@ export default function DialogImportBackup() {
     <DialogContent className="sm:max-w-[425px]">
       <DialogHeader>
         <div className="flex flex-auto items-center justify-center">
-          <DialogTitle>{t("title")}</DialogTitle>
+          <DialogTitle>{t('title')}</DialogTitle>
         </div>
-        <DialogDescription>{t("description")}</DialogDescription>
+        <DialogDescription>{t('description')}</DialogDescription>
       </DialogHeader>
       {!isImporting ? (
         <div className="relative border-1 border-dashed border-primary w-full h-20 text-md flex justify-center items-center hover:bg-primary/80 transition duration-200 text-bg-foreground cursor-pointer hover:border-none">
@@ -62,25 +63,25 @@ export default function DialogImportBackup() {
             className="absolute z-50 w-full h-full opacity-0 hover:cursor-pointer"
           />
           <div className="absolute z-40 text-center text-xs">
-            {t("drag-drop")}
+            {t('drag-drop')}
           </div>
         </div>
       ) : (
         <div className="text-center mx-auto flex-col items-center gap-2">
           <div className="flex justify-center mt-3">
-            <Loading />
+            <Loading/>
           </div>
-          <div className="mx-auto mt-2">{t("loading-part-1")}</div>
-          <div className="font-bold">{t("loading-part-2")}</div>
+          <div className="mx-auto mt-2">{t('loading-part-1')}</div>
+          <div className="font-bold">{t('loading-part-2')}</div>
         </div>
       )}
       <div className="flex flex-auto items-center justify-center">
-        <div className="font-medium mt-3">{t("welcome")}</div>
+        <div className="font-medium mt-3">{t('welcome')}</div>
       </div>
       <DialogFooter>
         <ul className="flex items-center justify-center flex-auto gap-2 ">
           <Image
-            src={"/timer-logos/nexustimer.jpg"}
+            src={'/timer-logos/nexustimer.jpg'}
             alt="nexustimer logo"
             width={64}
             height={64}
@@ -88,7 +89,7 @@ export default function DialogImportBackup() {
             draggable={false}
           />
           <Image
-            src={"/timer-logos/cstimer.jpg"}
+            src={'/timer-logos/cstimer.jpg'}
             alt="cstimer logo"
             width={64}
             height={64}
@@ -97,7 +98,7 @@ export default function DialogImportBackup() {
           />
 
           <Image
-            src={"/timer-logos/twistytimer.jpg"}
+            src={'/timer-logos/twistytimer.jpg'}
             alt="twistytimer logo"
             width={64}
             height={64}
@@ -106,7 +107,7 @@ export default function DialogImportBackup() {
           />
 
           <Image
-            src={"/timer-logos/cubedesk.jpg"}
+            src={'/timer-logos/cubedesk.jpg'}
             alt="cubedesk logo"
             width={64}
             height={64}
