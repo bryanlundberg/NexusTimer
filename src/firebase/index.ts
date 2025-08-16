d8ccfe695a8a313418e7313160a9338556fb4a82
Bryan Lundberg <bryanlundberg@outlook.com>
2025-08-15 22:29:02 -0600

    feat(firebase): add Firebase initialization and Firestore setup
    Introduce Firebase configuration and initialization using environment variables. This setup includes integration with Firestore, allowing database operations for the application.

---
+++ src/firebase/index.ts
@@ -0,0 +1,16 @@
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from '@firebase/firestore';
+// Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_API_KEY || '',
+  authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN || '',
+  projectId: process.env.NEXT_PUBLIC_PROJECT_ID || '',
+  storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET || '',
+  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID || '',
+  appId: process.env.NEXT_PUBLIC_APP_ID || '',
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app)
