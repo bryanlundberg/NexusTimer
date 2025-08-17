7c97f3f4954afc8a188eab7345ff0090ca1ea923
Bryan Lundberg <bryanlundberg@outlook.com>
2025-08-17 14:59:02 -0600

    feat: integrate countdown logic and room state in match UI
    Added countdown timer using `useCountdown` and integrated room state from `useClashManager` to dynamically display match finalization time. Improved overall functionality by replacing static time with a dynamic countdown.

--- src/components/clash/match-started/match-started.tsx
+++ src/components/clash/match-started/match-started.tsx
@@ -1,4 +1,5 @@
 'use client'
+import React from 'react';
 import { Hourglass } from 'lucide-react';
 import { cubeCollection } from '@/lib/const/cubeCollection';
 import Image from 'next/image';
@@ -10,6 +11,8 @@ import Sidebar from '@/components/clash/sidebar/sidebar';
 import Lobby from '@/components/clash/lobby/lobby';
 import { PlayerStatus } from '@/enums/PlayerStatus';
 import Chat from '@/components/clash/chat/chat';
+import { useClashManager } from '@/store/ClashManager';
+import { useCountdown } from '@/hooks/useCountdown';
 
 const mockedPlayers = [
   { id: '1', name: 'Rednaxela', avatarUrl: 'https://github.com/shadcn.png', status: PlayerStatus.PREPARING as const },
@@ -27,6 +30,8 @@ export default function MatchStarted() {
   const lobby = useClashWindows((s) => s.lobby);
   const setPosition = useClashWindows((s) => s.setPosition);
   const setSize = useClashWindows((s) => s.setSize);
+  const room = useClashManager(state => state.room);
+  const { mmss } = useCountdown(room?.matchFinalizationTime);
 
   return (
     <div className={'flex w-full min-h-dvh max-h-dvh overflow-hidden'}>
@@ -37,12 +42,12 @@ export default function MatchStarted() {
           <Image src={cubeCollection[0].src} alt={'Clash Icon'} width={20} height={20}/>
           <span>3x3</span>
           <span>| Clash Started |</span>
-          <span className={'flex items-center gap-1'}> <Hourglass size={14} fill={'#fff'}/> 00:15:23</span>
+          <span className={'flex items-center gap-1'}> <Hourglass size={14} fill={'#fff'}/> {mmss}</span>
         </div>
 
         <div className={'flex flex-col justify-start items-center h-full'}>
           <div className={'scroll-m-20 text-center text-4xl font-extrabold tracking-tight text-balance'}>Round 3/5</div>
-          <div className={'px-4 text-center md:text-2xl 2xl:text-3xl'}>D2 F' L U R2 F U F' U B2 L2 U L2 U F2 U' R2 D2 F2
+          <div className={'px-4 text-center md:text-2xl 2xl:text-3xl'}>D2 F&#39; L U R2 F U F&#39; U B2 L2 U L2 U F2 U&#39; R2 D2 F2
             D L
           </div>
           <div className={'text-4xl md:text-5xl lg:text-6xl xl:text-9xl grow flex items-center-safe justify-center w-full'}>9.91</div>
