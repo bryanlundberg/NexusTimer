25656bd51bebe99e883a5ce892fa1cd8e04e40a2
Bryan Lundberg <bryanlundberg@outlook.com>
2025-08-17 14:00:29 -0600

    feat(clash): integrate dynamic room state handling
    Added dynamic handling of room state by integrating Firestore and session data. Enhanced room-specific logic to display appropriate components based on the room's status and redirect if the room is invalid.

--- src/app/clash/[roomId]/page.tsx
+++ src/app/clash/[roomId]/page.tsx
@@ -1,16 +1,49 @@
 'use client'
 import FadeIn from '@/components/fade-in/fade-in';
-import { useParams } from 'next/navigation';
+import { useParams, useRouter } from 'next/navigation';
+import { useFirestoreCache } from '@/hooks/useFirebaseCache';
+import { FirestoreCollections } from '@/constants/FirestoreCollections';
+import { useEffect } from 'react';
+import { Room } from '@/interfaces/Room';
+import { RoomStatus } from '@/enums/RoomStatus';
+import AwaitingMatch from '@/components/clash/awaiting-match/awaiting-match';
 import MatchStarted from '@/components/clash/match-started/match-started';
+import MatchFinished from '@/components/clash/match-finished/match-finished';
+import { useSession } from 'next-auth/react';
+import { useClashManager } from '@/store/ClashManager';
+import _ from 'lodash';
 
 export default function Page() {
   const { roomId } = useParams()
-  console.log('Room ID:', roomId);
+  const { useDocument } = useFirestoreCache();
+  const { data: session } = useSession();
+  const router = useRouter();
+  const { data: loadedRoom, loading } = useDocument(`${FirestoreCollections.CLASH_ROOMS}/${roomId}`)
+  const room = useClashManager((state => state.room));
+  const setRoom = useClashManager((state => state.setRoom));
+
+  useEffect(() => {
+    if (!loading && !loadedRoom) {
+      router.push('/clash');
+    }
+  }, [loading, loadedRoom, router]);
+
+  useEffect(() => {
+    if (loading) return;
+    if (!loadedRoom) return;
+    if (room && _.isEqual(room, loadedRoom)) return;
+    setRoom(loadedRoom as Room);
+  }, [loading, loadedRoom, setRoom, room]);
+
+  if (loading) {
+    return null;
+  }
 
   return (
     <FadeIn className="flex flex-col grow overflow-auto">
-      {/*<AwaitingMatch/>*/}
-      <MatchStarted/>
+      {room?.status === RoomStatus.IDLE && (<AwaitingMatch/>)}
+      {room?.status === RoomStatus.IN_PROGRESS && (<MatchStarted/>)}
+      {room?.status === RoomStatus.FINISHED && (<MatchFinished/>)}
     </FadeIn>
   );
 }
