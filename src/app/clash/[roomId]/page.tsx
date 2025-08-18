90ecb1de2491a64d6244b6bc45c1f474b6066089
Bryan Lundberg <bryanlundberg@outlook.com>
2025-08-17 18:12:28 -0600

    feat: add real-time message loading for clash rooms
    Introduced functionality to load and display the latest messages for clash rooms by integrating Firestore's collection listener. Updated state management with a new `setMessages` method to handle and sync messages efficiently. Enhanced conditional checks to ensure optimal data loading and prevent unnecessary updates.

--- src/app/clash/[roomId]/page.tsx
+++ src/app/clash/[roomId]/page.tsx
@@ -9,33 +9,44 @@ import { RoomStatus } from '@/enums/RoomStatus';
 import AwaitingMatch from '@/components/clash/awaiting-match/awaiting-match';
 import MatchStarted from '@/components/clash/match-started/match-started';
 import MatchFinished from '@/components/clash/match-finished/match-finished';
-import { useSession } from 'next-auth/react';
 import { useClashManager } from '@/store/ClashManager';
 import _ from 'lodash';
 
 export default function Page() {
   const { roomId } = useParams()
-  const { useDocument } = useFirestoreCache();
-  const { data: session } = useSession();
+  const { useDocument, useCollection } = useFirestoreCache();
   const router = useRouter();
-  const { data: loadedRoom, loading } = useDocument(`${FirestoreCollections.CLASH_ROOMS}/${roomId}`)
-  const room = useClashManager((state => state.room));
+  const { data: loadedRoom, loading: loadingRoom } = useDocument(`${FirestoreCollections.CLASH_ROOMS}/${roomId}`)
+  const {
+    data: loadedMessages,
+    loading: loadingMessages
+  } = useCollection(`${FirestoreCollections.CLASH_ROOMS}/${roomId}/messages`, {
+    orderBy: [{ field: 'createdAt', direction: 'desc' }],
+    limit: 10
+  });
   const setRoom = useClashManager((state => state.setRoom));
+  const setMessages = useClashManager((state => state.setMessages));
+  const messages = useClashManager((state => state.messages));
+  const room = useClashManager((state => state.room));
 
   useEffect(() => {
-    if (!loading && !loadedRoom) {
+    if (!loadingRoom && !loadedRoom) {
       router.push('/clash');
     }
-  }, [loading, loadedRoom, router]);
+  }, [loadingRoom, loadedRoom, router]);
 
   useEffect(() => {
-    if (loading) return;
-    if (!loadedRoom) return;
-    if (room && _.isEqual(room, loadedRoom)) return;
+    if (loadingRoom || !loadedRoom || _.isEqual(room, loadedRoom)) return;
     setRoom(loadedRoom as Room);
-  }, [loading, loadedRoom, setRoom, room]);
+  }, [loadingRoom, loadedRoom, setRoom, room]);
 
-  if (loading) {
+  useEffect(() => {
+    if (loadingMessages || !loadedMessages || _.isEqual(messages, loadedMessages)) return;
+    setMessages(loadedMessages);
+  }, [loadedMessages, loadingMessages, messages, setMessages]);
+
+
+  if (loadingRoom) {
     return null;
   }
 
