0ae8bebca2a3cb908907694d73867879c460133e
Bryan Lundberg <bryanlundberg@outlook.com>
2025-08-17 18:11:46 -0600

    feat: add message management to ClashManager store
    Introduced support for managing room messages in the ClashManager store by adding a `messages` state and a `setMessages` method. This enhances the store's capabilities for handling room-related data.

--- src/store/ClashManager.ts
+++ src/store/ClashManager.ts
@@ -1,16 +1,19 @@
 import { create } from 'zustand';
 import { Room } from '@/interfaces/Room';
+import { RoomMessage } from '@/interfaces/RoomMessage';
 
 interface ClashManager {
   room: Room | null;
+  messages: RoomMessage[]
 
   setRoom: (room: Room) => void;
+  setMessages: (messages: RoomMessage[]) => void;
 }
 
 export const useClashManager = create<ClashManager>((set) => ({
   room: null,
+  messages: [],
 
-  setRoom: (room: Room) => {
-    set({ room });
-  },
+  setRoom: (room: Room) => {set({ room });},
+  setMessages: (messages: RoomMessage[]) => set({ messages: messages })
 }));
