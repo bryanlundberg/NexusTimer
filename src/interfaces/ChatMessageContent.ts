0e89934342b8da5a10e00447150ad6e52641bb09
Bryan Lundberg <bryanlundberg@outlook.com>
2025-08-23 07:19:22 -0600

    feat: add role property to ChatMessageContent interface
    Introduced a new role property to the ChatMessageContent interface to represent the player's role. This enhancement integrates the PlayerRole enum, ensuring better type safety and clarity in message structures.

--- src/interfaces/ChatMessageContent.ts
+++ src/interfaces/ChatMessageContent.ts
@@ -1,6 +1,9 @@
+import { PlayerRole } from '@/enums/PlayerRole';
+
 export interface ChatMessageContent {
   senderId: string;
   senderName: string;
   senderImage: string;
   message: string;
+  role: PlayerRole;
 }
