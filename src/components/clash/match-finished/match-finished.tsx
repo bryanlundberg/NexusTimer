090fb7c087daf535b9e486a83464331ab474f24e
Bryan Lundberg <bryanlundberg@outlook.com>
2025-08-24 10:13:34 -0600

    feat: integrate leave clash functionality in match results
    Added necessary hooks and session management to enable users to leave the clash room directly from the match results page. Adjusted the "Back to lobby" button to call the leave clash handler with appropriate parameters.

--- src/components/clash/match-finished/match-finished.tsx
+++ src/components/clash/match-finished/match-finished.tsx
@@ -1,5 +1,9 @@
+"use client";
 import ChartResults from '@/components/clash/chart-results/chart-results';
 import { Button } from '@/components/ui/button';
+import { useRoomUtils } from '@/hooks/useRoomUtils';
+import { useSession } from 'next-auth/react';
+import { useClashManager } from '@/store/ClashManager';
 
 // IDEAS, INCLUDE PERCENTAGE CLEAN SOLVES - 0 100%
 // ADD EACH INDIVIDUAL SOLVE
@@ -7,13 +11,17 @@ import { Button } from '@/components/ui/button';
 // HIGHTLIGHT CURRENT USER IN TABLE
 
 export default function MatchFinished() {
+  const { handleLeaveClash } = useRoomUtils();
+  const { data: session } = useSession();
+  const room = useClashManager((state) => state.room);
+
   return (
     <div className={'max-w-7xl mx-auto mt-5'}>
       <h1 className={'text-3xl font-bold mb-4 text-center'}>RESULTS</h1>
       <ChartResults/>
 
       <div className={"flex justify-center mt-6"}>
-        <Button>Back to lobby</Button>
+        <Button onClick={() => handleLeaveClash(room!, session!)}>Back to lobby</Button>
       </div>
     </div>
   )
