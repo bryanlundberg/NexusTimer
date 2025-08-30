fbd644a4444f73187a86c5643e12804942d91c31
Bryan Lundberg <bryanlundberg@outlook.com>
2025-08-30 06:04:54 -0600

    feat: add AlertProvider and useAlert hook for alert handling
    Introduced a reusable AlertProvider component and a useAlert hook to manage and display alert dialogs across the application. This implementation provides a flexible API for triggering confirmation dialogs with customizable options such as titles, subtitles, and button texts.

---
+++ src/hooks/useAlert.ts
@@ -0,0 +1,9 @@
+"use client";
+
+import type { AlertFunction } from "@/components/alert/AlertProvider";
+import { useAlertContext } from "@/components/alert/AlertProvider";
+
+export default function useAlert(): AlertFunction {
+  const { alert } = useAlertContext();
+  return alert;
+}
