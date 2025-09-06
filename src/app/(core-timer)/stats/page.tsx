0b2ba19ab668928ff4bb50b8c9b00b19e17744f8
Bryan Lundberg <bryanlundberg@outlook.com>
2025-09-06 11:27:22 -0600

    feat: add stats page with navigation and category statistics
    Introduced a new stats page under the core-timer module. This page includes navigation, category statistics, and animations using FadeIn and ScrollArea components for a better user experience.

---
+++ src/app/(core-timer)/stats/page.tsx
@@ -0,0 +1,19 @@
+'use client';
+import Navigation from '@/components/navigation/navigation';
+import CategoryStatistics from '@/components/stats/CategoryStatistics';
+import FadeIn from '@/components/fade-in/fade-in';
+import { ScrollArea } from '@/components/ui/scroll-area';
+
+export default function Page() {
+  return (
+    <ScrollArea className={'max-h-dvh overflow-auto'}>
+      <FadeIn>
+        <div className="px-2 pt-2 flex flex-col w-full min-h-full">
+          {/* header */}
+          <Navigation showMainCubeSelector/>
+          <CategoryStatistics/>
+        </div>
+      </FadeIn>
+    </ScrollArea>
+  );
+}
