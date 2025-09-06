1341f7b6d30f53e2772649fc1cb72ff07d72a214
Bryan Lundberg <bryanlundberg@outlook.com>
2025-09-06 11:28:50 -0600

    feat: add conditional rendering for LoaderProvider
    Introduced a loaderProvider prop to conditionally render the LoaderProvider component. This provides greater flexibility for cases where the LoaderProvider is not required. Default behavior remains unchanged for backward compatibility.

--- src/components/providers.tsx
+++ src/components/providers.tsx
@@ -8,20 +8,28 @@ import AlertProvider from '@/components/alert/AlertProvider';
 const PreloadSettings = dynamic(() => import('@/components/PreloadSettings'), { ssr: false });
 
 export default function Providers({
+  loaderProvider = true,
   children,
 }: {
   children: React.ReactNode;
+  loaderProvider?: boolean;
 }) {
   return (
     <>
       <PreloadSettings>
-        <LoaderProvider>
+        {loaderProvider ? (
+          <LoaderProvider>
+            <AlertProvider>
+              {children}
+            </AlertProvider>
+          </LoaderProvider>
+        ) : (
           <AlertProvider>
             {children}
           </AlertProvider>
-        </LoaderProvider>
+        )}
       </PreloadSettings>
-      <LoaderOverlay />
+      <LoaderOverlay/>
     </>
   );
 }
