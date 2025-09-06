eadddfc05da66cd7f124292b7abc680749dfb543
Bryan Lundberg <bryanlundberg@outlook.com>
2025-09-06 16:34:44 -0600

    refactor: remove commented-out cloud sync logic
    Removed outdated and unused cloud sync functionality that was commented out. This cleanup improves code readability and reduces unnecessary clutter in the file.

--- src/hooks/usePreloadSettings.ts
+++ src/hooks/usePreloadSettings.ts
@@ -58,45 +58,6 @@ export function usePreloadSettings() {
     return () => clearInterval(intervalId);
   }, [session]);
 
-  // TODO: Cloud sync functionality
-  // NEED MORE TIME FOR TESTING BEFORE RE-ENABLING IN PRODUCTION ENVIRONMENT
-  // useEffect(() => {
-  //   const checkCloudData = async () => {
-  //     if (session && session.user && session.user.email && !synced) {
-  //       setSynced(true);
-  //       try {
-  //         const localCubes = await getAllCubes();
-  //         const backup = await getLastBackup({ email: session.user.email });
-  //         if (!backup) {
-  //           return;
-  //         }
-  //
-  //         const parsedBackup = JSON.parse(backup);
-  //         const cloudBackup = JSON.parse(parsedBackup.data) as Cube[];
-  //
-  //         const syncedCubes = await syncBackup(cloudBackup, localCubes);
-  //
-  //         await saveBatchCubes(syncedCubes);
-  //
-  //         const selectedCube = useTimerStore.getState().selectedCube;
-  //         if (selectedCube) {
-  //           const syncedSelectedCube = syncedCubes.find(cube => cube.id === selectedCube.id);
-  //           if (syncedSelectedCube) {
-  //             useTimerStore.getState().setSelectedCube(syncedSelectedCube);
-  //             useTimerStore.getState().setNewScramble(syncedSelectedCube);
-  //           }
-  //         }
-  //
-  //         setCubes(syncedCubes);
-  //       } catch (error) {
-  //         console.log(error);
-  //       }
-  //     }
-  //   };
-  //
-  //   checkCloudData();
-  // }, [session, setCubes, syncBackup, synced]);
-
   useEffect(() => {
     setIsMounted(true);
   }, []);
