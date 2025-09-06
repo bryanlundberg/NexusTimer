95833b74ff023b9185cce56dbb7240804fb748c0
Bryan Lundberg <bryanlundberg@outlook.com>
2025-09-06 11:28:17 -0600

    refactor: simplify Home page component structure
    Replaced the existing Home page implementation with a minimal structure. Removed unused imports, components, and logic to streamline the codebase and improve maintainability.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,33 +1,3 @@
-"use client";
-import HeaderTimer from "@/components/timer/HeaderTimer";
-import TimerWidgets from "@/components/timer/TimerWidgets";
-import TimerContainer from "@/components/timer/TimerContainer";
-import { MainTimer } from "@/components/timer/MainTimer";
-import ScrambleModal from "@/components/timer/ScrambleModal";
-import FadeIn from "@/components/fade-in/fade-in";
-import { useEffect } from 'react';
-import { useTimerStore } from '@/store/timerStore';
-import { useTranslations } from 'next-intl';
-
-export default function Home() {
-  const resetTimerStore = useTimerStore((state) => state.reset);
-  const t = useTranslations("Metadata");
-
-  useEffect(() => {
-    resetTimerStore();
-  }, [resetTimerStore]);
-
-  return (
-    <>
-      <FadeIn className={"flex flex-col grow overflow-hidden"}>
-        <h1 className="sr-only">{t("description")}</h1>
-        <TimerContainer>
-          <HeaderTimer/>
-          <MainTimer/>
-          <TimerWidgets/>
-        </TimerContainer>
-        <ScrambleModal/>
-      </FadeIn>
-    </>
-  );
+export default function Page() {
+  return <div>Home</div>;
 }
