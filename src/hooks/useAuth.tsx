bc30cf1d8e3fc214bd7f484d7a8f338b07e72f71
Bryan Lundberg <bryanlundberg@outlook.com>
2025-09-06 16:22:45 -0600

    feat: add useAuth hook for authentication handling
    Introduce a custom hook, useAuth, to manage authentication logic. It includes handling account unlinking with session confirmation and error handling, leveraging next-auth, toast notifications, and custom alert dialogs.

---
+++ src/hooks/useAuth.tsx
@@ -0,0 +1,36 @@
+import { signOut, useSession } from 'next-auth/react';
+import { toast } from 'sonner';
+import { useNXData } from '@/hooks/useNXData';
+import { useTranslations } from 'next-intl';
+import useAlert from '@/hooks/useAlert';
+
+export default function useAuth() {
+  const t = useTranslations('Index');
+  const { clearCubes } = useNXData();
+  const { data: session } = useSession();
+  const alert = useAlert();
+
+  const handleResetDeviceData = async () => {
+    try {
+      const confirm = await alert({
+        title: t('SettingsPage.unlink-account'),
+        subtitle: t('SettingsPage.unlink-account-para1') + ' ' + (session?.user?.name || '') + '. ' + t('SettingsPage.unlink-account-para2'),
+        confirmText: t('Inputs.continue'),
+        cancelText: t('Inputs.cancel'),
+      })
+
+      if (!confirm) return
+
+      await clearCubes();
+      await signOut({ redirectTo: '/' });
+    } catch (error) {
+      console.error('Error resetting device data:', error);
+      toast.error('Error unlinking account');
+    }
+  };
+
+
+  return {
+    handleResetDeviceData
+  }
+}
