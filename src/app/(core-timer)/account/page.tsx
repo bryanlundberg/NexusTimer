e4e9a4421708c5209175c03d94d0b84e82cfe5b5
Bryan Lundberg <bryanlundberg@outlook.com>
2025-09-06 16:22:34 -0600

    refactor(account): simplify unlink account flow
    Replaced inline unlink account logic with a reusable hook to streamline functionality and reduce code duplication. Removed Dialog component and directly integrated the button for a more straightforward implementation.

--- src/app/(core-timer)/account/page.tsx
+++ src/app/(core-timer)/account/page.tsx
@@ -1,5 +1,5 @@
 'use client';
-import { signOut, useSession } from 'next-auth/react';
+import { useSession } from 'next-auth/react';
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
 import { Button } from '@/components/ui/button';
 import AccountHeader from '@/components/account/account-header/account-header';
@@ -7,41 +7,15 @@ import Link from 'next/link';
 import AccountNotAuth from '@/components/account/account-not-auth/account-not-auth';
 import AccountLastBackup from '@/components/account/account-last-backup/account-last-backup';
 import { useTranslations } from 'next-intl';
-import {
-  Dialog,
-  DialogClose,
-  DialogContent,
-  DialogDescription,
-  DialogFooter,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
-} from '@/components/ui/dialog';
-import { useNXData } from '@/hooks/useNXData';
 import uploadFile from '@/utils/uploadFile';
 import { toast } from 'sonner';
 import loader from '@/utils/loader';
+import useAuth from '@/hooks/useAuth';
 
 export default function Page() {
   const { data: session, update } = useSession();
-  const { getAllCubes, clearCubes } = useNXData();
   const t = useTranslations('Index');
-
-  const handleResetDeviceData = async () => {
-    try {
-      loader.start();
-      const cubes = await getAllCubes();
-      if (cubes) {
-        await clearCubes();
-      }
-
-      await signOut({ redirectTo: '/' });
-    } catch (error) {
-      console.error('Error resetting device data:', error);
-      toast.error('Error unlinking account');
-      loader.stop();
-    }
-  };
+  const { handleResetDeviceData } = useAuth();
 
   const handleUpdateAvatar = async (event: React.ChangeEvent<HTMLInputElement>) => {
     const file = event.target.files?.[0];
@@ -84,7 +58,7 @@ export default function Page() {
       <AccountHeader back="/app" label={t('SettingsPage.account')}/>
       <div className="flex flex-col gap-3 justify-center items-center">
         <Avatar className="size-20 relative group/item">
-          <AvatarImage className={"object-cover"} src={session.user?.image as string}/>
+          <AvatarImage className={'object-cover'} src={session.user?.image as string}/>
           <AvatarFallback>
             {session.user?.name?.slice(0, 2).toUpperCase()}
           </AvatarFallback>
@@ -98,7 +72,7 @@ export default function Page() {
 
         <div className="font-mono">{session.user?.email}</div>
 
-        <Link  href={`/people/${session.user?.id}`} className="w-full">
+        <Link href={`/people/${session.user?.id}`} className="w-full">
           <Button className="w-full">
             Open Profile
           </Button>
@@ -116,35 +90,10 @@ export default function Page() {
           </Button>
         </Link>
 
-        <Dialog>
-          <DialogTrigger className="w-full" asChild>
-            <Button className="w-full" variant={'destructive'}>
-              {t('SettingsPage.unlink-account')}
-            </Button>
-          </DialogTrigger>
-          <DialogContent>
-            <DialogHeader>
-              <DialogTitle>{t('SettingsPage.unlink-account')}</DialogTitle>
-              <DialogDescription>
-                {t('SettingsPage.unlink-account-para1')}{' '}
-                <span className="text-primary">{session.user?.name}</span>
-              </DialogDescription>
-              <DialogDescription className="text-yellow-600">
-                {t('SettingsPage.unlink-account-para2')}
-              </DialogDescription>
-            </DialogHeader>
-            <DialogFooter className="flex flex-col gap-2">
-              <DialogClose className="flex-1" asChild>
-                <Button variant={'secondary'} className="w-full">
-                  {t('Inputs.cancel')}
-                </Button>
-              </DialogClose>
-              <Button className="flex-1" onClick={handleResetDeviceData}>
-                {t('Inputs.continue')}
-              </Button>
-            </DialogFooter>
-          </DialogContent>
-        </Dialog>
+
+        <Button className="w-full" variant={'destructive'} onClick={handleResetDeviceData}>
+          {t('SettingsPage.unlink-account')}
+        </Button>
 
         <AccountLastBackup session={session}/>
       </div>
