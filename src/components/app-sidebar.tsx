8dcb00b6ce1d4977fc72ec24ea3a2c40ad3915ff
Bryan Lundberg <bryanlundberg@outlook.com>
2025-09-06 11:29:50 -0600

    feat: add AppSidebar component with navigation structure
    Introduce the AppSidebar component, integrating main, secondary, and user navigation. Includes reusable NavMain and Collapsible components to support hierarchical menus for better modularity and design scalability.

---
+++ src/components/app-sidebar.tsx
@@ -0,0 +1,114 @@
+'use client'
+
+import * as React from 'react'
+import { BookOpen, Bot, Command, GithubIcon, LandPlot, LayoutPanelLeft, LifeBuoy, UsersRound, } from 'lucide-react'
+
+import { NavMain } from '@/components/nav-main'
+import { NavSecondary } from '@/components/nav-secondary'
+import { NavUser } from '@/components/nav-user'
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from '@/components/ui/sidebar'
+import { DiscordLogoIcon } from '@radix-ui/react-icons'
+
+const data = {
+  user: {
+    name: 'shadcn',
+    email: 'm@example.com',
+    avatar: '/avatars/shadcn.jpg',
+  },
+  navMain: [
+    {
+      title: 'Application',
+      url: '/app',
+      icon: LayoutPanelLeft,
+      isActive: true,
+      items: [
+        {
+          title: 'Timer',
+          url: '/app',
+        },
+        {
+          title: 'Solves',
+          url: '/solves',
+        },
+        {
+          title: 'Statistics',
+          url: '/stats',
+        },
+        {
+          title: 'Cubes',
+          url: '/cubes',
+        },
+        {
+          title: 'Settings',
+          url: '/settings',
+        }
+      ],
+    },
+    {
+      title: 'Community',
+      url: '/people',
+      icon: UsersRound,
+    },
+    {
+      title: 'Multiplayer',
+      url: '/clash',
+      icon: LandPlot,
+    },
+  ],
+  navSecondary: [
+    {
+      title: 'Support',
+      url: 'https://github.com/bryanlundberg/NexusTimer/issues',
+      icon: LifeBuoy,
+    },
+    {
+      title: 'GitHub',
+      url: 'https://github.com/bryanlundberg/NexusTimer',
+      icon: GithubIcon,
+    },
+    {
+      title: 'Discord',
+      url: 'https://discord.gg/grenDQFw',
+      icon: DiscordLogoIcon,
+    }
+  ]
+}
+
+export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
+  return (
+    <Sidebar variant="inset" {...props}>
+      <SidebarHeader>
+        <SidebarMenu>
+          <SidebarMenuItem>
+            <SidebarMenuButton size="lg" asChild>
+              <a href="#">
+                <div className="flex aspect-square size-8 items-center justify-center rounded-lg bg-sidebar-primary text-sidebar-primary-foreground">
+                  <Command className="size-4"/>
+                </div>
+                <div className="grid flex-1 text-left text-sm leading-tight">
+                  <span className="truncate font-semibold">Nexus Timer</span>
+                  <span className="truncate text-xs">Welcome! </span>
+                </div>
+              </a>
+            </SidebarMenuButton>
+          </SidebarMenuItem>
+        </SidebarMenu>
+      </SidebarHeader>
+      <SidebarContent>
+        <NavMain items={data.navMain}/>
+        <NavSecondary items={data.navSecondary as any} className="mt-auto"/>
+      </SidebarContent>
+      <SidebarFooter>
+        <NavUser user={data.user}/>
+      </SidebarFooter>
+    </Sidebar>
+  )
+}
---
+++ src/components/nav-main.tsx
@@ -0,0 +1,80 @@
+"use client"
+
+import { type LucideIcon } from "lucide-react"
+
+import {
+  Collapsible,
+  CollapsibleContent,
+  CollapsibleTrigger,
+} from "@/components/ui/collapsible"
+import {
+  SidebarGroup,
+  SidebarGroupLabel,
+  SidebarMenu,
+  SidebarMenuAction,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  SidebarMenuSub,
+  SidebarMenuSubButton,
+  SidebarMenuSubItem,
+} from "@/components/ui/sidebar"
+import { ChevronRightIcon } from "@radix-ui/react-icons"
+import Link from 'next/link';
+
+export function NavMain({
+  items,
+}: {
+  items: {
+    title: string
+    url: string
+    icon: LucideIcon
+    isActive?: boolean
+    items?: {
+      title: string
+      url: string
+    }[]
+  }[]
+}) {
+  return (
+    <SidebarGroup>
+      <SidebarGroupLabel>Platform</SidebarGroupLabel>
+      <SidebarMenu>
+        {items.map((item) => (
+          <Collapsible key={item.title} asChild defaultOpen={item.isActive}>
+            <SidebarMenuItem>
+              <SidebarMenuButton asChild tooltip={item.title}>
+                <Link href={item.url}>
+                  <item.icon />
+                  <span>{item.title}</span>
+                </Link>
+              </SidebarMenuButton>
+              {item.items?.length ? (
+                <>
+                  <CollapsibleTrigger asChild>
+                    <SidebarMenuAction className="data-[state=open]:rotate-90">
+                      <ChevronRightIcon />
+                      <span className="sr-only">Toggle</span>
+                    </SidebarMenuAction>
+                  </CollapsibleTrigger>
+                  <CollapsibleContent>
+                    <SidebarMenuSub>
+                      {item.items?.map((subItem) => (
+                        <SidebarMenuSubItem key={subItem.title}>
+                          <SidebarMenuSubButton asChild>
+                            <Link href={subItem.url}>
+                              <span>{subItem.title}</span>
+                            </Link>
+                          </SidebarMenuSubButton>
+                        </SidebarMenuSubItem>
+                      ))}
+                    </SidebarMenuSub>
+                  </CollapsibleContent>
+                </>
+              ) : null}
+            </SidebarMenuItem>
+          </Collapsible>
+        ))}
+      </SidebarMenu>
+    </SidebarGroup>
+  )
+}
