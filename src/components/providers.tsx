deed0ae47d7d9ca58a66ddc4dc87dc126b465702
Bryan Lundberg <bryanlundberg@outlook.com>
2025-09-06 20:57:30 -0600

    refactor: simplify PreloadSettings import in providers
    Replaced the dynamic import of PreloadSettings with a static import. This change enhances readability and eliminates the need for server-side rendering configuration.

--- src/components/providers.tsx
+++ src/components/providers.tsx
@@ -2,9 +2,7 @@
 
 import { LoaderProvider } from '@/components/loader/loader-context';
 import LoaderOverlay from '@/components/loader/loader-overlay';
-import dynamic from 'next/dynamic';
-
-const PreloadSettings = dynamic(() => import('@/components/PreloadSettings'), { ssr: false });
+import PreloadSettings from '@/components/PreloadSettings';
 
 export default function Providers({
   loaderProvider = true,
