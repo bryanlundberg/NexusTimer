3d426464cb25f52787ce0aaf0bd9cd12f3d9a47d
Bryan Lundberg <bryanlundberg@outlook.com>
2025-09-07 08:17:00 -0600

    feat: apply user-defined color theme in Providers
    Introduced logic to apply a user-selected color theme using useWebsiteColors and settings from SettingsModalStore. Leveraged useEffect to update the theme dynamically based on user preferences.

--- src/components/providers.tsx
+++ src/components/providers.tsx
@@ -3,6 +3,9 @@
 import { LoaderProvider } from '@/components/loader/loader-context';
 import LoaderOverlay from '@/components/loader/loader-overlay';
 import PreloadSettings from '@/components/PreloadSettings';
+import { useEffect } from 'react';
+import { useSettingsModalStore } from '@/store/SettingsModalStore';
+import useWebsiteColors from '@/hooks/useWebsiteColors';
 
 export default function Providers({
   loaderProvider = true,
@@ -11,6 +14,14 @@ export default function Providers({
   children: React.ReactNode;
   loaderProvider?: boolean;
 }) {
+
+  const settings = useSettingsModalStore(store => store.settings);
+  const { applyColorTheme } = useWebsiteColors();
+
+  useEffect(() => {
+    applyColorTheme(settings.preferences.colorTheme);
+  }, [applyColorTheme, settings.preferences.colorTheme]);
+
   return (
     <>
       <PreloadSettings>
