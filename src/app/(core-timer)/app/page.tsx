6da1fa3c12eb5bf79dac63acbcb44947ea81fbe8
Bryan Lundberg <bryanlundberg@outlook.com>
2025-09-07 18:10:45 -0600

    feat(timer): add fireworks effect for new record solves
    Introduced a fireworks animation when a new solve record is achieved. This provides visual feedback by utilizing a FireworksBackground component and related logic to detect record conditions.

--- src/app/(core-timer)/app/page.tsx
+++ src/app/(core-timer)/app/page.tsx
@@ -1,17 +1,25 @@
-"use client";
-import HeaderTimer from "@/components/timer/HeaderTimer";
-import TimerWidgets from "@/components/timer/TimerWidgets";
-import TimerContainer from "@/components/timer/TimerContainer";
-import { MainTimer } from "@/components/timer/MainTimer";
-import ScrambleModal from "@/components/timer/ScrambleModal";
-import FadeIn from "@/components/fade-in/fade-in";
-import { useEffect } from 'react';
+'use client';
+import HeaderTimer from '@/components/timer/HeaderTimer';
+import TimerWidgets from '@/components/timer/TimerWidgets';
+import TimerContainer from '@/components/timer/TimerContainer';
+import { MainTimer } from '@/components/timer/MainTimer';
+import ScrambleModal from '@/components/timer/ScrambleModal';
+import FadeIn from '@/components/fade-in/fade-in';
+import { useEffect, useMemo } from 'react';
 import { useTimerStore } from '@/store/timerStore';
 import { useTranslations } from 'next-intl';
+import { FireworksBackground } from '@/components/ui/shadcn-io/fireworks-background';
 
 export default function Home() {
   const resetTimerStore = useTimerStore((state) => state.reset);
-  const t = useTranslations("Metadata");
+  const t = useTranslations('Metadata');
+  const timerStatistics = useTimerStore(store => store.timerStatistics);
+  const lastSolve = useTimerStore(store => store.lastSolve);
+  const isSolving = useTimerStore(store => store.isSolving);
+
+  const isRecord = useMemo(() => {
+    return timerStatistics.global.best === lastSolve?.time && !isSolving;
+  }, [isSolving, lastSolve, timerStatistics.global.best]);
 
   useEffect(() => {
     resetTimerStore();
@@ -19,14 +27,25 @@ export default function Home() {
 
   return (
     <>
-      <FadeIn className={"flex flex-col grow"}>
-        <h1 className="sr-only">{t("description")}</h1>
+      <FadeIn className={'flex flex-col grow relative'}>
+        <h1 className="sr-only">{t('description')}</h1>
         <TimerContainer>
           <HeaderTimer/>
           <MainTimer/>
           <TimerWidgets/>
         </TimerContainer>
         <ScrambleModal/>
+
+        {isRecord && (
+          <div className="absolute inset-0 z-1">
+            <FireworksBackground
+              fireworkSpeed={{ min: 8, max: 16 }}
+              fireworkSize={{ min: 4, max: 10 }}
+              particleSpeed={{ min: 4, max: 14 }}
+              particleSize={{ min: 2, max: 10 }}
+            />
+          </div>
+        )}
       </FadeIn>
     </>
   );
