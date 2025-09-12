78c55949b6f134b0b77a92b0a35e1fe97af21318
Bryan Lundberg <bryanlundberg@outlook.com>
2025-09-12 06:19:38 -0600

    refactor: replace ScrollArea with div in stats page
    Replaced the ScrollArea component with a standard div to simplify the layout and improve maintainability of the stats page. This change ensures consistent behavior without additional dependencies from the ScrollArea component.

--- src/app/(core-timer)/stats/page.tsx
+++ src/app/(core-timer)/stats/page.tsx
@@ -2,17 +2,16 @@
 import Navigation from '@/components/navigation/navigation';
 import CategoryStatistics from '@/components/stats/CategoryStatistics';
 import FadeIn from '@/components/fade-in/fade-in';
-import { ScrollArea } from '@/components/ui/scroll-area';
 
 export default function Page() {
   return (
-    <ScrollArea className={'max-h-dvh overflow-auto'}>
+    <div className={'max-h-dvh overflow-auto'}>
       <FadeIn>
         <div className="px-2 pt-2 flex flex-col w-full min-h-full">
           <Navigation showMainCubeSelector/>
           <CategoryStatistics/>
         </div>
       </FadeIn>
-    </ScrollArea>
+    </div>
   );
 }
