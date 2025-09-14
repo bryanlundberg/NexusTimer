003f3c31871228bfd9ab443a88cf3cf4d7b5cb03
Bryan Lundberg <bryanlundberg@outlook.com>
2025-09-14 07:57:33 -0600

    refactor: simplify MainTimer mode rendering logic
    Removed unnecessary dependency on settings from MainTimer. Updated rendering logic to directly align with timer modes for improved clarity and maintainability.

--- src/components/timer/MainTimer.tsx
+++ src/components/timer/MainTimer.tsx
@@ -1,4 +1,3 @@
-import { useSettingsModalStore } from "@/store/SettingsModalStore";
 import ManualMode from "./ManualMode";
 import Timer from "./Timer";
 import { useTimerStore } from "@/store/timerStore";
@@ -8,14 +7,16 @@ import TimerVirtual from '@/components/timer/TimerVirtual';
 
 export function MainTimer() {
   const timerMode = useTimerStore(store => store.timerMode);
-  const settings = useSettingsModalStore(store => store.settings);
-  const manualMode = settings.timer.manualMode;
   const event = useTimerStore(store => store.event);
 
   return (
     <>
       {timerMode === TimerMode.NORMAL && (
-        <>{manualMode ? <ManualMode /> : <Timer />}</>
+        <Timer />
+      )}
+
+      {timerMode === TimerMode.MANUAL && (
+        <ManualMode />
       )}
 
       {timerMode === TimerMode.STACKMAT && (
