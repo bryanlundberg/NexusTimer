b1849bb2e34c655c51c4b2c71b8e0dfe22b7792d
Bryan Lundberg <bryanlundberg@outlook.com>
2025-09-14 07:44:56 -0600

    feat: update ButtonSelectMode to handle cube-based restrictions
    Added logic to disable Virtual timer mode for non-3x3 cubes and ensured the button is disabled when no cube is selected. Adjusted import formatting and enhanced code consistency.

--- src/components/navigation/buttons/button-select-mode.tsx
+++ src/components/navigation/buttons/button-select-mode.tsx
@@ -1,5 +1,5 @@
-"use client";
-import { Button } from "@/components/ui/button";
+'use client';
+import { Button } from '@/components/ui/button';
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -8,27 +8,37 @@ import {
   DropdownMenuRadioItem,
   DropdownMenuSeparator,
   DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
-import { useTimerStore } from "@/store/timerStore";
-import { MixIcon } from "@radix-ui/react-icons";
-import { useTranslations } from "next-intl";
-import { TimerMode } from "@/enums/TimerMode";
+} from '@/components/ui/dropdown-menu';
+import { useTimerStore } from '@/store/timerStore';
+import { MixIcon } from '@radix-ui/react-icons';
+import { useTranslations } from 'next-intl';
+import { TimerMode } from '@/enums/TimerMode';
+import { useEffect } from 'react';
 
 export default function ButtonSelectMode() {
   const timerMode = useTimerStore((state) => state.timerMode);
   const setTimerMode = useTimerStore((state) => state.setTimerMode);
-  const t = useTranslations("Index");
+  const selectedCube = useTimerStore(state => state.selectedCube)
+  const t = useTranslations('Index');
+
+  useEffect(() => {
+    if (!selectedCube) return;
+    if (selectedCube.category !== '3x3' && timerMode === TimerMode.VIRTUAL) {
+      setTimerMode(TimerMode.NORMAL)
+    }
+  }, [selectedCube, setTimerMode, timerMode])
+
   return (
     <>
       <DropdownMenu>
         <DropdownMenuTrigger asChild>
-          <Button variant="ghost" className="py-0 px-3">
-            <MixIcon />
+          <Button variant="ghost" className="py-0 px-3" disabled={!selectedCube}>
+            <MixIcon/>
           </Button>
         </DropdownMenuTrigger>
         <DropdownMenuContent className="w-fit">
-          <DropdownMenuLabel>{t("HomePage.mode")}</DropdownMenuLabel>
-          <DropdownMenuSeparator />
+          <DropdownMenuLabel>{t('HomePage.mode')}</DropdownMenuLabel>
+          <DropdownMenuSeparator/>
           <DropdownMenuRadioGroup
             value={timerMode}
             onValueChange={(e: any) => setTimerMode(e)}
@@ -37,7 +47,7 @@ export default function ButtonSelectMode() {
             <DropdownMenuRadioItem value={TimerMode.STACKMAT}>
               Stackmat
             </DropdownMenuRadioItem>
-            <DropdownMenuRadioItem value={TimerMode.VIRTUAL}>
+            <DropdownMenuRadioItem value={TimerMode.VIRTUAL} disabled={selectedCube?.category !== '3x3'}>
               Virtual
             </DropdownMenuRadioItem>
             <DropdownMenuRadioItem value={TimerMode.SMART_CUBE} disabled>
