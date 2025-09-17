2517a775d2b68c32f58770faba67406a25b166c0
Bryan Lundberg <bryanlundberg@outlook.com>
2025-09-17 06:27:28 -0600

    feat(stats): enhance navigation with new header components
    Replaced the main cube selector in Navigation with a flexible header layout. Added SidebarTrigger, MainCubeSelector, and DateRangeSelect for improved user control and functionality. Disabled the default menu to

--- src/app/(core-timer)/stats/page.tsx
+++ src/app/(core-timer)/stats/page.tsx
@@ -2,13 +2,22 @@
 import Navigation from '@/components/navigation/navigation';
 import CategoryStatistics from '@/components/stats/CategoryStatistics';
 import FadeIn from '@/components/fade-in/fade-in';
+import { SidebarTrigger } from '@/components/ui/sidebar';
+import MainCubeSelector from '@/components/MainCubeSelector';
+import DateRangeSelect from '@/components/stats/DateRangeSelect';
 
 export default function Page() {
   return (
     <div className={'max-h-dvh overflow-auto'}>
       <FadeIn>
         <div className="px-2 pt-2 flex flex-col w-full min-h-full">
-          <Navigation showMainCubeSelector/>
+          <Navigation showMenu={false}>
+            <div className="flex justify-between items-center gap-2 w-full">
+              <SidebarTrigger/>
+              <MainCubeSelector/>
+              <DateRangeSelect/>
+            </div>
+          </Navigation>
           <CategoryStatistics/>
         </div>
       </FadeIn>
