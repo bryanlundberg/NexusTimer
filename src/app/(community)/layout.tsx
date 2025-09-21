dc971335f2e366f02a7b787af124b4e242ba6618
Bryan Lundberg <bryanlundberg@outlook.com>
2025-09-20 18:42:53 -0600

    feat: add conditional FloatButton in community layout
    Introduced a FloatButton that renders conditionally based on the number of users in the CompareUsers store. This enhances interactivity and improves the user experience in the community layout.

--- src/app/(community)/layout.tsx
+++ src/app/(community)/layout.tsx
@@ -1,19 +1,24 @@
+"use client";
 import Providers from '@/components/providers';
 import React from 'react';
 import { AppSidebar } from '@/components/app-sidebar';
 import { SidebarInset } from '@/components/ui/sidebar';
+import FloatButton from '@/components/people/FloatButton';
+import { useCompareUsersStore } from '@/store/CompareUsers';
 
 export default function Layout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const users = useCompareUsersStore(state => state.users);
   return (
     <>
       <AppSidebar/>
       <SidebarInset className={"overflow-hidden"} style={{ height: "calc(100dvh - 1rem)" }}>
         <Providers loaderProvider={false}>
           {children}
+          {users.length > 0 && <FloatButton/>}
         </Providers>
       </SidebarInset>
     </>
