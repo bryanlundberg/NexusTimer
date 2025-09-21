a4442aa2cbb35cc2ee123e9052ef84f6a9b4cdea
Bryan Lundberg <bryanlundberg@outlook.com>
2025-09-21 11:34:29 -0600

    feat(compare-users): enhance modal with user stats and categories
    Added detailed user statistics including total solves, cube breakdowns, and individual categories comparisons. Implemented dynamic category handling, data fetching from backups, and improved styling for better usability.

--- src/components/people/compare-users-modal.tsx
+++ src/components/people/compare-users-modal.tsx
@@ -2,83 +2,241 @@ import Image from 'next/image';
 import { XIcon } from 'lucide-react';
 import { useCompareUsersStore } from '@/store/CompareUsers';
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
-import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from '@/components/ui/table';
 import RecordsRadar from '@/components/charts/RecordsRadar';
+import { cn } from '@/lib/utils';
+import { Categories } from '@/interfaces/Categories';
+import { Cube } from '@/interfaces/Cube';
+import { decompressSync, strFromU8 } from 'fflate';
+import { useEffect, useMemo, useState } from 'react';
+import calcBestTime from '@/lib/calcBestTime';
+import calcAverageStatistics from '@/lib/calcAverageStatistics';
+import calcTotalSolvesStatistics from '@/lib/calcTotalSolvesStatistics';
+import _ from 'lodash';
+import formatTime from '@/lib/formatTime';
 
 export default function CompareUsersModal() {
   const closeOverlay = useCompareUsersStore(state => state.closeOverlay);
   const users = useCompareUsersStore(state => state.users);
+  const [userCubes, setUserCubes] = useState<any>({});
+
+  useEffect(() => {
+    let isMounted = true;
+
+    async function loadBackups() {
+      const entries = await Promise.all(users.map(async (user) => {
+        try {
+          if (user.backup?.url) {
+            const response = await fetch(user.backup.url);
+            if (!response.ok) throw new Error('Network response was not ok');
+            const compressed = new Uint8Array(await response.arrayBuffer());
+            const decompressed = decompressSync(compressed);
+            const data = strFromU8(decompressed);
+            const cubeData = (JSON.parse(data) as Cube[]);
+            const merged = _.groupBy(cubeData, 'category');
+
+            return [user._id, merged] as const;
+          }
+        } catch (e) {
+          console.error('Error loading backup for user', user._id, e);
+        }
+        return [user._id, null] as const;
+      }));
+      if (!isMounted) return;
+
+      setUserCubes(Object.fromEntries(entries));
+    }
+
+    if (users.length) loadBackups();
+    return () => {
+      isMounted = false;
+    };
+  }, [users]);
+
+  const usersStats: CompareUser[] = useMemo(() => {
+    return users.map(user => {
+      const cubesDB = userCubes[user._id] || {};
+      const byCategory = Object.create(null) as CompareUser;
+      (CATEGORIES as Categories[]).forEach((category) => {
+        const cubeData = cubesDB[category] ?? [];
+        const cubeName = '';
+
+        const single = calcBestTime({ cubesDB: cubeData, category, cubeName }).global;
+        const average = calcAverageStatistics({ cubesDB: cubeData, category, cubeName }).global;
+        const count = calcTotalSolvesStatistics({ cubesDB: cubeData, category, cubeName }).global;
+
+        (byCategory as any)[category] = { single, average, count };
+      });
+      return { _id: user._id, ...(byCategory as any) } as CompareUser;
+    });
+  }, [users, userCubes]);
 
   return (
-    <div className={'bg-background w-full h-full flex flex-col p-4 fixed top-0 left-0 z-50'}>
-      <header className={'flex items-center justify-between'}>
+    <div className={'bg-background w-full h-full flex flex-col fixed top-0 left-0 z-50 overflow-y-auto'}>
+      <header className={'flex items-center justify-between p-4'}>
         <div className="flex items-center gap-3">
           <div className="h-8 w-8 rounded-md bg-white/10 border border-white/15 flex items-center justify-center">
             <Image src={'/logo.png'} alt={'logo'} width={20} height={20} className={'invert-100'}/>
           </div>
           <span className="text-sm font-semibold tracking-wide text-white/90 hidden sm:block">NexusTimer</span>
         </div>
-        <h1 className="text-lg font-semibold text-background-foreground">Comparative</h1>
+        <h2 className="scroll-m-20 border-b pb-2 text-xl font-semibold tracking-tight first:mt-0 text-background-foreground">Comparative</h2>
         <XIcon onClick={closeOverlay}/>
       </header>
 
-      <div>Technical Sheet</div>
+      <div id={'table'}>
 
-      <div>Basics</div>
+        <TableRow title={''} className={'sticky top-0 right-0 bg-background z-10 border-b border-b-white/10'}>
+          {users.map((user) => {
+            return (
+              <div key={user._id} className={'w-52 py-3'}>
+                <div className={'flex flex-col items-center gap-2'}>
+                  <Avatar className={'size-24'}>
+                    <AvatarImage className={'object-cover'} src={user.image}/>
+                    <AvatarFallback>CN</AvatarFallback>
+                  </Avatar>
+                  <span className={"font-semibold"}>{user.name}</span>
+                </div>
+              </div>
+            )
+          })}
+        </TableRow>
 
-      <Table>
-        <TableHeader>
-          <TableRow>
-            <TableHead className="w-[150px]"></TableHead>
-            {users.map((user) => {
-              return (
-                <TableHead key={user._id} className={"w-[100px]"} >
-                  <div  className={"flex flex-col items-center gap-2"}>
-                    <Avatar className={'size-24'}>
-                      <AvatarImage className={'object-cover'} src={user.image}/>
-                      <AvatarFallback>CN</AvatarFallback>
-                    </Avatar>
-                    <span>{user.name}</span>
-                  </div>
-                </TableHead>
-              )
-            })}
-          </TableRow>
-        </TableHeader>
-      </Table>
+        <TableRow title={'Time Zone'}>
+          {users.map((user) => {
+            return (
+              <div key={user._id} className={'w-52 text-center'}>
+                Mexico City
+              </div>
+            )
+          })}
+        </TableRow>
 
-      <Table>
-        <TableHeader>
-          <TableRow>
-            <TableHead className="w-[150px]">3x3</TableHead>
-            <TableHead className="w-[100px]"></TableHead>
-            <TableHead className="w-[100px]"></TableHead>
-          </TableRow>
-        </TableHeader>
-        <TableBody>
-          <TableRow>
-            <TableCell className={"w-[150px]"}>Best</TableCell>
-            <TableCell className={"w-[100px] text-center"}>2.5s</TableCell>
-            <TableCell className={"w-[100px] text-center"}>3.2s</TableCell>
-          </TableRow>
+        <TableRow title={'First solve'}>
+          {users.map((user) => {
+            return (
+              <div key={user._id} className={'w-52 text-center'}>
+                {new Date(user.createdAt).toLocaleDateString()}
+              </div>
+            )
+          })}
+        </TableRow>
 
-          <TableRow>
-            <TableCell className={"w-[150px]"}>Average</TableCell>
-            <TableCell className={"w-[100px] text-center"}>2.5s</TableCell>
-            <TableCell className={"w-[100px] text-center"}>3.2s</TableCell>
-          </TableRow>
+        <TableRow title={'Total solves'}>
+          {users.map((user) => {
+            return (
+              <div key={user._id} className={'w-52 text-center'}>
+                12,687
+              </div>
+            )
+          })}
+        </TableRow>
 
-          <TableRow>
-            <TableCell className={"w-[150px]"}>Total</TableCell>
-            <TableCell className={"w-[100px] text-center"}>1235</TableCell>
-            <TableCell className={"w-[100px] text-center"}>9512</TableCell>
-          </TableRow>
-        </TableBody>
-      </Table>
+        <TableRow title={'Total Cubes'}>
+          {users.map((user) => {
+            return (
+              <div key={user._id} className={'w-52 text-center'}>
+                196
+              </div>
+            )
+          })}
+        </TableRow>
 
-      <div className="mt-6">
-        <RecordsRadar users={users} />
+        <TableRow title={''}>
+          {users.map((user) => {
+            return (
+              <div key={user._id} className={'w-52 text-center'}>
+                <div className="h-px bg-primary w-full my-2"/>
+              </div>
+            )
+          })}
+        </TableRow>
+
+        {CATEGORIES.map((category) => (
+          <div key={category}>
+            <CategoryBlock category={category} users={usersStats} />
+            <TableRow title={''}>
+              {users.map((user) => (
+                <div key={user._id} className={'w-52 text-center'}>
+                  <div className="h-px bg-white/10 w-full my-1"/>
+                </div>
+              ))}
+            </TableRow>
+          </div>
+        ))}
+
+        <div className="mt-6">
+          <RecordsRadar users={users}/>
+        </div>
       </div>
     </div>
+  )
+}
+
+const TableRow = ({ title, children, className }: { title: string, children: React.ReactNode, className?: string }) => {
+  return (
+    <div className={cn('flex gap-3', className)}>
+      <div className={'w-40 pl-4 opacity-80 text-sm'}>{title}</div>
+      {children}
+    </div>
+  )
+}
+
+const CATEGORIES: Categories[] = [
+  '2x2',
+  '3x3',
+  '3x3 OH',
+  '4x4',
+  '5x5',
+  '6x6',
+  '7x7',
+  'SQ1',
+  'Skewb',
+  'Pyraminx',
+  'Megaminx',
+  'Clock',
+];
+
+type CompareUser = {
+  [key in Categories]: { single: number; average: number; count: number; };
+} & {
+  _id: string;
+};
+
+const CategoryBlock = ({ category, users }: { category: Categories, users: CompareUser[] }) => {
+  return (
+    <>
+      <TableRow title={`${category} Single`}>
+        {users.map((user) => {
+          const val = user[category]?.single;
+          return (
+            <div key={user._id} className={'w-52 text-center'}>
+              {val ? formatTime(val) : '—'}
+            </div>
+          )
+        })}
+      </TableRow>
+
+      <TableRow title={`${category} Average`}>
+        {users.map((user) => {
+          const val = user[category]?.average;
+          return (
+            <div key={user._id} className={'w-52 text-center'}>
+              {val ? formatTime(val) : '—'}
+            </div>
+          )
+        })}
+      </TableRow>
+
+      <TableRow title={`${category} Count`}>
+        {users.map((user) => {
+          const val = user[category]?.count;
+          return (
+            <div key={user._id} className={'w-52 text-center'}>
+              {val ? formatTime(val) : '—'}
+            </div>
+          )
+        })}
+      </TableRow>
+    </>
   )
 }
