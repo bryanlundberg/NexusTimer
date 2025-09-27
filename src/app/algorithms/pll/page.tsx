cbff10f2d6435167eed18ce878c105a16fe2de2d
Bryan Lundberg <bryanlundberg@outlook.com>
2025-09-27 14:25:05 -0600

    feat: integrate PLL algorithms into dedicated page
    Replaced placeholder content with the AlgorithmsPage component, displaying PLL algorithms dynamically. Added support for virtualization with experimental stickering specific to PLL using TwistyPlayer configurations. Enhances functionality and user experience for the PLL page.

--- src/app/algorithms/pll/page.tsx
+++ src/app/algorithms/pll/page.tsx
@@ -1,5 +1,15 @@
+import { AlgorithmsPage } from '@/components/algorithms/algorithms-page/algorithms-page';
+import { PLL_ALGS } from '@/algs/pll';
+import { TwistyPlayer } from 'cubing/twisty';
+
 export default function Page() {
   return (
-    <div>pll</div>
+    <AlgorithmsPage
+      algorithms={PLL_ALGS}
+      title={'PLL - Permutation of the Last Layer'}
+      virtualization={{
+        experimentalStickering: 'PLL',
+      } as unknown as TwistyPlayer}
+    />
   )
 }
