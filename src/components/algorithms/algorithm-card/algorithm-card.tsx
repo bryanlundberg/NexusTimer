8af2927a1f1d5bad13a4136604dbf84a2a2790f8
Bryan Lundberg <bryanlundberg@outlook.com>
2025-09-27 14:24:50 -0600

    feat: add AlgorithmCard component for OLL visualization
    Introduce the AlgorithmCard component for rendering OLL algorithms with customizable visualization via TwistyPlayer. This includes displaying algorithm details, handling alternative algorithms, and providing a click interaction for further actions.

---
+++ src/components/algorithms/algorithm-card/algorithm-card.tsx
@@ -0,0 +1,57 @@
+import AlgorithmRender from '@/components/twisty/AlgorithmRender';
+import { Card } from '@/components/ui/card';
+import { Label } from '@/components/ui/label';
+import * as React from 'react';
+import { AlgorithmCollection } from '@/interfaces/AlgorithmCollection';
+import { cn } from '@/lib/utils';
+import { TwistyPlayer } from 'cubing/twisty';
+import _ from 'lodash';
+
+interface AlgorithmCardProps extends React.HTMLAttributes<HTMLDivElement> {
+  onAlgorithmClick?: () => void
+  algorithm: AlgorithmCollection
+  virtualization?: TwistyPlayer
+}
+
+export default function AlgorithmCard({
+  algorithm,
+  onAlgorithmClick,
+  virtualization,
+  ...rest
+}: AlgorithmCardProps) {
+
+  const defaults = _.merge({
+    visualization: 'experimental-2D-LL',
+    background: 'none',
+    controlPanel: 'none',
+    alg: algorithm.alg[0],
+    experimentalStickering: 'OLL',
+    experimentalSetupAnchor: 'end',
+  }, virtualization)
+
+  return (
+    <Card className={cn('p-3 mb-3 h-auto bg-background break-inside-avoid-column', rest.className)} {...rest}>
+      <h1>{`OLL-${algorithm.group}-${algorithm.name}`}</h1>
+      <div className={'flex flex-col md:flex-row items-start gap-3'}>
+        <AlgorithmRender
+          config={defaults}
+          width={120}
+          height={120}
+        />
+        <div className={'flex flex-col gap-2 justify-between text-sm grow w-full'}>
+          {algorithm.alg.map((alg, index) => (
+            <Card
+              className={'p-3 flex items-center justify-center flex-row'} onClick={onAlgorithmClick}
+              key={`OLL-${algorithm.group}-${algorithm.name}-alg-${index}`}
+            >
+              <div className={'grow space-y-2'}>
+                <Label className={'ml-2'}>Alternative #{index + 1}:</Label>
+                <span className={'text-lg'}>{alg}</span>
+              </div>
+            </Card>
+          ))}
+        </div>
+      </div>
+    </Card>
+  )
+}
