94a64e02f46fc4f8a586105d75a99011d6c80f26
Bryan Lundberg <bryanlundberg@outlook.com>
2025-09-28 16:24:13 -0600

    feat: add PARITY_555_ALGS collection for edge cases
    Introduced a new algorithm collection for the PARITY 555 group, targeting edge cases. This adds one predefined algorithm to expand functionality and improve modularity.

---
+++ src/algs/parity-555.ts
@@ -0,0 +1,9 @@
+import { AlgorithmCollection } from '@/interfaces/AlgorithmCollection';
+
+export const PARITY_555_ALGS: AlgorithmCollection[] = [
+  {
+    name: "Case 1",
+    alg: ["Rw U2 x Rw U2 Rw U2 Rw' U2 Lw U2 3Rw' U2 Rw U2 Rw' U2 Rw'"],
+    group: "Edge"
+  }
+];
