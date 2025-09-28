f05b08dbe971cea4358bef205d594c043edd6fd2
Bryan Lundberg <bryanlundberg@outlook.com>
2025-09-28 06:57:27 -0600

    feat: enhance algorithms page with scroll area and suggestions
    Replaced the main container with a scrollable area for better UX and added a Suggestions component to improve content engagement. Updated styling and structure for increased clarity and visual appeal.

--- src/app/algorithms/page.tsx
+++ src/app/algorithms/page.tsx
@@ -2,19 +2,26 @@ import MoreCategories from '@/components/algorithms/more-categories/more-categor
 import { ALGORITHM_SETS } from '@/constants/algorithms-sets';
 import AlgorithmsCollection from '@/components/algorithms/algorithms-collection/algorithms-collection';
 import BreadcrumbNav from '@/components/algorithms/breadcrumb-nav/breadcrumb-nav';
+import { ScrollArea } from '@/components/ui/scroll-area';
+import Suggestions from '@/components/algorithms/suggestions/suggestions';
 
 export default function Page() {
   return (
-    <div className="p-4">
+    <ScrollArea className="p-4 max-h-dvh overflow-auto">
       <BreadcrumbNav hideCollectionsSegment/>
 
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
+      <h1 className="scroll-m-20 text-center text-4xl font-extrabold tracking-tight text-balance">
+        Master Your Cubing Journey with Algorithm Training
+      </h1>
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 py-10">
         {ALGORITHM_SETS.map((set) => (
           <AlgorithmsCollection key={set.slug} set={set}/>
         ))}
 
         <MoreCategories/>
       </div>
-    </div>
+
+      <Suggestions/>
+    </ScrollArea>
   )
 }
