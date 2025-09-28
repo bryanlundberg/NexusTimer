12c6bc7faa496e06bd6c79498f35f0543ea76ded
Bryan Lundberg <bryanlundberg@outlook.com>
2025-09-28 13:05:36 -0600

    feat: add WV algorithms page with 3D visualization integration
    Implement a dedicated page for WV (Winter Variation) algorithms, enabling 3D visualization of cases using TwistyPlayer. This enhances user interaction and provides detailed information on orienting last layer corners during the F2L pair insertion.

---
+++ src/app/algorithms/wv/page.tsx
@@ -0,0 +1,20 @@
+import { AlgorithmsPage } from '@/components/algorithms/algorithms-page/algorithms-page';
+import { TwistyPlayer } from 'cubing/twisty';
+import { WV_ALGS } from '@/algs/vw';
+
+export default function Page() {
+  return (
+    <AlgorithmsPage
+      algorithms={WV_ALGS}
+      title={'WV - Algorithms'}
+      description={'WV (Winter Variation) algorithms are used to orient the corners of your last layer whilst you insert your final F2L pair, in the case where the pair is connected and where all of the last layer edges are already oriented.'}
+      virtualization={{
+        experimentalStickering: 'auto',
+        visualization: '3D',
+        experimentalDragInput: 'none',
+      } as unknown as TwistyPlayer}
+      fileCollectionName={'wv.ts'}
+      puzzle={'3x3x3'}
+    />
+  );
+}
