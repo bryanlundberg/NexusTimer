d9cdb083cd15246a485a85057c83c3a45169e3d3
Bryan Lundberg <bryanlundberg@outlook.com>
2025-09-28 08:07:12 -0600

    feat: add VLS algorithms page
    Introduce a new page for VLS (Valk Last Slot) algorithms. The page provides a description, 3D visualization, and a curated selection of VLS cases to assist users in learning efficiently. This enhances the algorithms section with comprehensive VLS resources.

---
+++ src/app/algorithms/vls/page.tsx
@@ -0,0 +1,20 @@
+import { AlgorithmsPage } from '@/components/algorithms/algorithms-page/algorithms-page';
+import { TwistyPlayer } from 'cubing/twisty';
+import { BLE_ALGS } from '@/algs/ble';
+import { VLS_ALGS } from '@/algs/vls';
+
+export default function Page() {
+  return (
+    <AlgorithmsPage
+      algorithms={VLS_ALGS}
+      title={'VLS - Algorithms'}
+      description={'VLS (Valk Last Slot) algorithms solve the final F2L slot and OLL at the same time, in the cases where you have a connected F2L pair. This selection of algorithms serves as an introduction to the full VLS algorithm set, and aims to highlight the most useful cases to know.'}
+      virtualization={{
+        experimentalStickering: 'OLL',
+        visualization: '3D',
+        experimentalDragInput: 'none'
+      } as unknown as TwistyPlayer}
+      fileCollectionName={'vls.ts'}
+    />
+  );
+}
