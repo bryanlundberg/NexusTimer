5468a614066941bbe5be37ab500d9f04e9c024d5
Bryan Lundberg <bryanlundberg@outlook.com>
2025-09-28 05:56:29 -0600

    refactor: add optional hideCollectionsSegment prop to BreadcrumbNav
    Introduced the hideCollectionsSegment prop to conditionally remove the collections segment from the breadcrumb navigation. This improves component flexibility and usage in various contexts without unnecessary UI elements.

--- src/components/algorithms/breadcrumb-nav/breadcrumb-nav.tsx
+++ src/components/algorithms/breadcrumb-nav/breadcrumb-nav.tsx
@@ -1,18 +1,19 @@
-import {SidebarTrigger} from "@/components/ui/sidebar";
-import {Separator} from "@/components/ui/separator";
-import {Breadcrumb, BreadcrumbItem, BreadcrumbLink, BreadcrumbList} from "@/components/ui/breadcrumb";
-import Link from "next/link";
-import {Select, SelectContent, SelectItem, SelectTrigger, SelectValue} from "@/components/ui/select";
-import * as React from "react";
-import {usePathname, useRouter} from "next/navigation";
+'use client'
 
-export default function BreadcrumbNav() {
+import { SidebarTrigger } from '@/components/ui/sidebar';
+import { Separator } from '@/components/ui/separator';
+import { Breadcrumb, BreadcrumbItem, BreadcrumbLink, BreadcrumbList } from '@/components/ui/breadcrumb';
+import Link from 'next/link';
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select';
+import { usePathname, useRouter } from 'next/navigation';
+
+export default function BreadcrumbNav({ hideCollectionsSegment = false }: { hideCollectionsSegment?: boolean }) {
   const router = useRouter();
   const pathname = usePathname()
   const segment = pathname.split('/')[2];
 
   const handleNavigate = (value: string) => {
-    router.push("/algorithms/" + value);
+    router.push('/algorithms/' + value);
   }
 
   return (
@@ -26,19 +27,23 @@ export default function BreadcrumbNav() {
               <Link href={'/algorithms'}>Algorithms</Link>
             </BreadcrumbLink>
           </BreadcrumbItem>
-          <Separator orientation="vertical" className="mr-2 data-[orientation=vertical]:h-4"/>
-          <BreadcrumbItem>
-            <Select onValueChange={(e) => handleNavigate(e)} defaultValue={segment}>
-              <SelectTrigger className="w-[100px]">
-                <SelectValue/>
-              </SelectTrigger>
-              <SelectContent>
-                <SelectItem value="oll">OLL</SelectItem>
-                <SelectItem value="pll">PLL</SelectItem>
-                <SelectItem value="coll">COLL</SelectItem>
-              </SelectContent>
-            </Select>
-          </BreadcrumbItem>
+          {!hideCollectionsSegment && (
+            <>
+              <Separator orientation="vertical" className="mr-2 data-[orientation=vertical]:h-4"/>
+              <BreadcrumbItem>
+                <Select onValueChange={(e) => handleNavigate(e)} defaultValue={segment}>
+                  <SelectTrigger className="w-[100px]">
+                    <SelectValue/>
+                  </SelectTrigger>
+                  <SelectContent>
+                    <SelectItem value="oll">OLL</SelectItem>
+                    <SelectItem value="pll">PLL</SelectItem>
+                    <SelectItem value="coll">COLL</SelectItem>
+                  </SelectContent>
+                </Select>
+              </BreadcrumbItem>
+            </>
+          )}
         </BreadcrumbList>
       </Breadcrumb>
     </div>
