0bab89cf525ca4dbba03ddeb14f6537d1a0b617c
Bryan Lundberg <bryanlundberg@outlook.com>
2025-09-27 22:32:52 -0600

    feat: add constants for algorithm sets with metadata
    Introduced a new constants file defining algorithm sets like OLL, PLL, and COLL, including their metadata such as difficulty, total cases, and icons. This will serve as a reference for organizing and displaying algorithm-related data.

---
+++ src/constants/algorithms-sets.ts
@@ -0,0 +1,33 @@
+import { Grid3x3, Layers } from 'lucide-react';
+
+export const ALGORITHM_SETS = [
+  {
+    slug: '/algorithms/oll',
+    title: 'OLL',
+    description: 'Orientation of the Last Layer',
+    cube: '3x3',
+    total: 57,
+    Icon: Layers,
+    difficulty: 1,
+  },
+  {
+    slug: '/algorithms/pll',
+    title: 'PLL',
+    description: 'Permutation of the Last Layer',
+    cube: '3x3',
+    total: 21,
+    Icon: Grid3x3,
+    difficulty: 1,
+  },
+  {
+    slug: '/algorithms/coll',
+    title: 'COLL',
+    description: 'Corners of the Last Layer',
+    cube: '3x3',
+    total: 9,
+    Icon: Grid3x3,
+    difficulty: 2,
+  }
+] as const;
+
+export type ALGORITHM_SET = (typeof ALGORITHM_SETS)[number];
