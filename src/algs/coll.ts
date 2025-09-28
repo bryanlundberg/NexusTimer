401bd2a0d208ade87ee3eef52330db5dd4389b3b
Bryan Lundberg <bryanlundberg@outlook.com>
2025-09-27 22:32:58 -0600

    refactor: simplify and group COLL_ALGS array data structure
    Reduced redundancy in COLL_ALGS definitions by minimizing unnecessary list formatting and added consistency across all cases. Group names and algorithms are clarified for better readability and maintainability.

--- src/algs/coll.ts
+++ src/algs/coll.ts
@@ -1,281 +1,217 @@
 import { AlgorithmCollection } from '@/interfaces/AlgorithmCollection';
 
 export const COLL_ALGS: AlgorithmCollection[] = [
-  // Sune cases
+  // Sune cases (6 algorithms)
   {
     name: "Sune 1",
-    alg: [
-      "R U R' U R U2' R'"
-    ],
-    group: "Sune cases",
-    prob: null
+    alg: ["R U R' U R U2' R'"],
+    group: "Sune"
   },
   {
     name: "Sune 2",
-    alg: [
-      "F' (R U2' R' U2) R' F2 (R U R U') R' F'"
-    ],
-    group: "Sune cases",
-    prob: null
+    alg: ["F' (R U2' R' U2) R' F2 (R U R U') R' F'"],
+    group: "Sune"
   },
   {
     name: "Sune 3",
-    alg: [
-      "R U' L' U R' U' L L' (R U R' U') L (U2 R U2' R')"
-    ],
-    group: "Sune cases",
-    prob: null
+    alg: ["R U' L' U R' U' L"],
+    group: "Sune"
   },
   {
     name: "Sune 4",
-    alg: [
-      "(L' U2 L U2') R (U' L' U L) R'"
-    ],
-    group: "Sune cases",
-    prob: null
+    alg: ["L' (R U R' U') L (U2 R U2' R')"],
+    group: "Sune"
   },
   {
     name: "Sune 5",
-    alg: [
-      "y' (R U R' U) (R U' R D) (R' U' R D') R2'"
-    ],
-    group: "Sune cases",
-    prob: null
+    alg: ["(L' U2 L U2') R (U' L' U L) R'"],
+    group: "Sune"
+  },
+  {
+    name: "Sune 6",
+    alg: ["y' (R U R' U) (R U' R D) (R' U' R D') R2'"],
+    group: "Sune"
   },
 
-  // Anti-Sune cases
+  // Anti-Sune cases (6 algorithms)
   {
     name: "Anti-Sune 1",
-    alg: [
-      "y R U2' R' U' R U' R'"
-    ],
-    group: "Anti-Sune cases",
-    prob: null
+    alg: ["y R U2' R' U' R U' R'"],
+    group: "Anti-Sune"
   },
   {
     name: "Anti-Sune 2",
-    alg: [
-      "(R U' R' U2) (R U' R' U2) (R' D' R) U (R' D R)"
-    ],
-    group: "Anti-Sune cases",
-    prob: null
+    alg: ["(R U' R' U2) (R U' R' U2) (R' D' R) U (R' D R)"],
+    group: "Anti-Sune"
   },
   {
     name: "Anti-Sune 3",
-    alg: [
-      "y2 L' U R U' L U R' y2 R (L' U' L U) R' (U2' L' U2 L)"
-    ],
-    group: "Anti-Sune cases",
-    prob: null
+    alg: ["y2 L' U R U' L U R'"],
+    group: "Anti-Sune"
   },
   {
     name: "Anti-Sune 4",
-    alg: [
-      "y2 (R U2 R' U2') L' (U R U' R') L"
-    ],
-    group: "Anti-Sune cases",
-    prob: null
+    alg: ["y2 R (L' U' L U) R' (U2' L' U2 L)"],
+    group: "Anti-Sune"
   },
   {
     name: "Anti-Sune 5",
-    alg: [
-      "y (R' U' R U') (R' U R' D') (R U R' D) R2"
-    ],
-    group: "Anti-Sune cases",
-    prob: null
+    alg: ["y2 (R U2 R' U2') L' (U R U' R') L "],
+    group: "Anti-Sune"
+  },
+  {
+    name: "Anti-Sune 6",
+    alg: ["y (R' U' R U') (R' U R' D') (R U R' D) R2"],
+    group: "Anti-Sune"
   },
 
-  // L cases
+  // L cases (6 algorithms)
   {
     name: "L 1",
-    alg: [
-      "y (R U R' U) (R U' R' U) (R U' R' U) R U2' R'"
-    ],
-    group: "L cases",
-    prob: null
+    alg: ["y (R U R' U) (R U' R' U) (R U' R' U) R U2' R'"],
+    group: "L"
   },
   {
     name: "L 2",
-    alg: [
-      "y' r U2' (R2' F R F') R U2' r'"
-    ],
-    group: "L cases",
-    prob: null
+    alg: ["y' r U2' (R2' F R F') R U2' r'"],
+    group: "L"
   },
   {
     name: "L 3",
-    alg: [
-      "y' (R U2 R D) (R' U2 R D') R2' y2 (R' U2 R' D') (R U2 R' D) R2"
-    ],
-    group: "L cases",
-    prob: null
+    alg: ["y' (R U2 R D) (R' U2 R D') R2'"],
+    group: "L"
   },
   {
     name: "L 4",
-    alg: [
-      "y' (F R' F' r) (U R U' r') F' (r U R' U') (r' F R)"
-    ],
-    group: "L cases",
-    prob: null
+    alg: ["y2 (R' U2 R' D') (R U2 R' D) R2"],
+    group: "L"
+  },
+  {
+    name: "L 5",
+    alg: ["y' (F R' F' r) (U R U' r')"],
+    group: "L"
+  },
+  {
+    name: "L 6",
+    alg: ["F' (r U R' U') (r' F R)"],
+    group: "L"
   },
 
-  // T cases
+  // T cases (6 algorithms)
   {
     name: "T 1",
-    alg: [
-      "(R U2' R' U' R U' R2') (U2' R U R' U R)"
-    ],
-    group: "T cases",
-    prob: null
+    alg: ["(R U2' R' U' R U' R2') (U2' R U R' U R)"],
+    group: "T"
   },
   {
     name: "T 2",
-    alg: [
-      "y2 F (R U R' U') (R U' R' U') (R U R' F')"
-    ],
-    group: "T cases",
-    prob: null
+    alg: ["y2 F (R U R' U') (R U' R' U') (R U R' F')"],
+    group: "T"
   },
   {
     name: "T 3",
-    alg: [
-      "(R' U R) U2' L' (R' U R U') L (R' U R2 D) (r' U2 r) (D' R2' U' R)"
-    ],
-    group: "T cases",
-    prob: null
+    alg: ["(R' U R) U2' L' (R' U R U') L"],
+    group: "T"
   },
   {
     name: "T 4",
-    alg: [
-      "y (l' U' L U) (R U' r' F) y' (r U R' U') (r' F R F')"
-    ],
-    group: "T cases",
-    prob: null
+    alg: ["(R' U R2 D) (r' U2 r) (D' R2' U' R)"],
+    group: "T"
+  },
+  {
+    name: "T 5",
+    alg: ["y (l' U' L U) (R U' r' F)"],
+    group: "T"
+  },
+  {
+    name: "T 6",
+    alg: ["y' (r U R' U') (r' F R F')"],
+    group: "T"
   },
 
-  // U cases
+  // U cases (6 algorithms)
   {
     name: "U 1",
-    alg: [
-      "y2 (R U R' U R U2' R2') (U' R U' R' U2 R)"
-    ],
-    group: "U cases",
-    prob: null
+    alg: ["y2 (R U R' U R U2' R2') (U' R U' R' U2 R)"],
+    group: "U"
   },
   {
     name: "U 2",
-    alg: [
-      "F (R U' R' U) (R U R' U) (R U' R' F')"
-    ],
-    group: "U cases",
-    prob: null
+    alg: ["F (R U' R' U) (R U R' U) (R U' R' F')"],
+    group: "U"
   },
   {
     name: "U 3",
-    alg: [
-      "y2 R2 D (R' U2 R) D' (R' U2 R') R2' D' (R U2 R') D (R U2 R)"
-    ],
-    group: "U cases",
-    prob: null
+    alg: ["y2 R2 D (R' U2 R) D' (R' U2 R')"],
+    group: "U"
   },
   {
     name: "U 4",
-    alg: [
-      "R' F (R U' R' U') (R U R' F') (R U R' U') (R' F R F' R)"
-    ],
-    group: "U cases",
-    prob: null
+    alg: ["R2' D' (R U2 R') D (R U2 R)"],
+    group: "U"
   },
   {
     name: "U 5",
-    alg: [
-      "(R' U2 R) F (U' R' U' R) U F'"
-    ],
-    group: "U cases",
-    prob: null
+    alg: ["R' F (R U' R' U') (R U R' F') (R U R' U') (R' F R F' R)"],
+    group: "U"
+  },
+  {
+    name: "U 6",
+    alg: ["(R' U2 R) F (U' R' U' R) U F'"],
+    group: "U"
   },
 
-  // Pi cases
+  // Pi cases (6 algorithms)
   {
     name: "Pi 1",
-    alg: [
-      "R U2' R2' U' R2 U' R2' U2' R"
-    ],
-    group: "Pi cases",
-    prob: null
+    alg: ["R U2' R2' U' R2 U' R2' U2' R"],
+    group: "Pi"
   },
   {
     name: "Pi 2",
-    alg: [
-      "(R U D') (R U R' D) (R2 U' R' U') R2' U2' R"
-    ],
-    group: "Pi cases",
-    prob: null
+    alg: ["(R U D') (R U R' D) (R2 U' R' U') R2' U2' R"],
+    group: "Pi"
   },
   {
     name: "Pi 3",
-    alg: [
-      "y F (U R U' R') (U R U' R2') F' R (U R U' R')"
-    ],
-    group: "Pi cases",
-    prob: null
+    alg: ["y F (U R U' R') (U R U' R2') F' R (U R U' R')"],
+    group: "Pi"
   },
   {
     name: "Pi 4",
-    alg: [
-      "(R U R' U') R' F (R2 U R' U') (R U R' U') F'"
-    ],
-    group: "Pi cases",
-    prob: null
+    alg: ["(R U R' U') R' F (R2 U R' U') (R U R' U') F'"],
+    group: "Pi"
   },
   {
     name: "Pi 5",
-    alg: [
-      "y' (R U R' U) F' (R U2' R' U2') (R' F R)"
-    ],
-    group: "Pi cases",
-    prob: null
+    alg: ["y' (R U R' U) F' (R U2' R' U2') (R' F R)"],
+    group: "Pi"
   },
   {
     name: "Pi 6",
-    alg: [
-      "y F (U R U' R') (U R U2' R') (U' R U R') F'"
-    ],
-    group: "Pi cases",
-    prob: null
+    alg: ["y F (U R U' R') (U R U2' R') (U' R U R') F'"],
+    group: "Pi"
   },
 
-  // H cases
+  // H cases (4 algorithms)
   {
     name: "H 1",
-    alg: [
-      "(R U R' U) (R U' R' U) R U2' R'"
-    ],
-    group: "H cases",
-    prob: null
+    alg: ["(R U R' U) (R U' R' U) R U2' R'"],
+    group: "H"
   },
   {
     name: "H 2",
-    alg: [
-      "y F (R U R' U') (R U R' U') (R U R' U') F'"
-    ],
-    group: "H cases",
-    prob: null
+    alg: ["y F (R U R' U') (R U R' U') (R U R' U') F'"],
+    group: "H"
   },
   {
     name: "H 3",
-    alg: [
-      "F (R U' R' U) (R U2 R' U') (R U R' U') F'"
-    ],
-    group: "H cases",
-    prob: null
+    alg: ["F (R U' R' U) (R U2 R' U') (R U R' U') F'"],
+    group: "H"
   },
   {
     name: "H 4",
-    alg: [
-      "(R U R' U) (R U L' U) R' U' L"
-    ],
-    group: "H cases",
-    prob: null
+    alg: ["(R U R' U) (R U L' U) R' U' L"],
+    group: "H"
   }
 ];
