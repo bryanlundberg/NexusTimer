bbf08ecce18d757fa1f25861fdfa4788fd200cd7
Bryan Lundberg <bryanlundberg@outlook.com>
2025-09-28 09:34:14 -0600

    feat: add puzzle type property to COLL page component
    Include the puzzle type '3x3x3' as a new property in the COLL page component to enhance configuration options. This update ensures better clarity and flexibility for the component behavior.

--- src/app/algorithms/coll/page.tsx
+++ src/app/algorithms/coll/page.tsx
@@ -12,6 +12,7 @@ export default function Page() {
         experimentalStickering: 'PLL',
       } as unknown as TwistyPlayer}
       fileCollectionName={'coll.ts'}
+      puzzle={'3x3x3'}
     />
   )
 }
