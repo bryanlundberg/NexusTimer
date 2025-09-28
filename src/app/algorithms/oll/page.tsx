72d076a6e82d4eef61bb543370b2f1941c05d5e5
Bryan Lundberg <bryanlundberg@outlook.com>
2025-09-27 20:41:49 -0600

    refactor: update OLL page structure and add description
    Enhanced the OLL page by adding a detailed description about the OLL algorithms, explaining their purpose in the CFOP method. Improved readability of the component structure for better maintainability.

--- src/app/algorithms/oll/page.tsx
+++ src/app/algorithms/oll/page.tsx
@@ -4,7 +4,14 @@ import { OLL_ALGS } from '@/algs/oll';
 import { TwistyPlayer } from 'cubing/twisty';
 
 export default function Page() {
-  return <AlgorithmsPage algorithms={OLL_ALGS} title={'OLL - Orientation of the Last Layer'} virtualization={{
-    experimentalStickering: 'OLL',
-  } as unknown as TwistyPlayer}/>;
+  return (
+    <AlgorithmsPage
+      algorithms={OLL_ALGS}
+      title={'OLL - Algorithms'}
+      description={'The OLL (Orientation of Last Layer) algorithms for solving the Rubik\'s cube with the CFOP method. These algorithms are used to orient all of the pieces on the last layer, once the F2L is complete.'}
+      virtualization={{
+        experimentalStickering: 'OLL',
+      } as unknown as TwistyPlayer}
+    />
+  );
 }
