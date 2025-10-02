e924bd3e31df5bbc97d97ac43a70d75ed1310a57
Bryan Lundberg <bryanlundberg@outlook.com>
2025-10-02 06:44:20 -0600

    feat: add EG-1 algorithm collection for algorithm handling
    Introduce a comprehensive EG-1 algorithm collection to the project, categorized by groups such as Antisune, Ortega, Pi, Sune, T, U, and H. This enhances algorithm management and provides structured

---
+++ src/algs/eg-1.ts
@@ -0,0 +1,262 @@
+import { AlgorithmCollection } from "@/interfaces/AlgorithmCollection";
+
+export const EG_1_ALGS: AlgorithmCollection[] = [
+  {
+    name: "AS1",
+    alg: ["R' F R2 U R' F' U' R U' R'"],
+    group: "Antisune",
+    prob: null
+  },
+  {
+    name: "AS2",
+    alg: ["R U' R' F' U' F2 R U' R'"],
+    group: "Antisune",
+    prob: null
+  },
+  {
+    name: "AS3",
+    alg: ["F L F' L2 U' L U F' L F L'"],
+    group: "Antisune",
+    prob: null
+  },
+  {
+    name: "AS4",
+    alg: ["F' R U R' U' R U R2 F' R"],
+    group: "Antisune",
+    prob: null
+  },
+  {
+    name: "AS5",
+    alg: ["R U' R2 F R U' R' F R F'"],
+    group: "Antisune",
+    prob: null
+  },
+  {
+    name: "AS6",
+    alg: ["L' U L U' L' U L U L F L' F'"],
+    group: "Antisune",
+    prob: null
+  },
+  {
+    name: "H1",
+    alg: ["R' F R2 U' R' F R U R' F'"],
+    group: "H",
+    prob: null
+  },
+  {
+    name: "H2",
+    alg: ["F' U R U' R2 F2 R U' F"],
+    group: "H",
+    prob: null
+  },
+  {
+    name: "H3",
+    alg: ["R U R' F' R U R' U' R U R'"],
+    group: "H",
+    prob: null
+  },
+  {
+    name: "H4",
+    alg: ["R' F R F' U2 F R U2 R' F"],
+    group: "H",
+    prob: null
+  },
+  {
+    name: "L1",
+    alg: ["R U' R' U R U' R2 F' R F"],
+    group: "L",
+    prob: null
+  },
+  {
+    name: "L2",
+    alg: ["L' U L U' L' U L2 F L' F'"],
+    group: "L",
+    prob: null
+  },
+  {
+    name: "L3",
+    alg: ["L' U' L F L' U' L U F' L F L'"],
+    group: "L",
+    prob: null
+  },
+  {
+    name: "L4",
+    alg: ["R U R' F' R U R' U' F R' F' R"],
+    group: "L",
+    prob: null
+  },
+  {
+    name: "L5",
+    alg: ["R' U R2 U' R2 U' F R2 U' R'"],
+    group: "L",
+    prob: null
+  },
+  {
+    name: "L6",
+    alg: ["R' F R2 U R' F' R U2 R'"],
+    group: "L",
+    prob: null
+  },
+  {
+    name: "Adjacent",
+    alg: ["R2 U' R2 U2 F2 U' R2"],
+    group: "Ortega",
+    prob: null
+  },
+  {
+    name: "Diagonal",
+    alg: ["R U' R' U' R' F2 U' R U R"],
+    group: "Ortega",
+    prob: null
+  },
+  {
+    name: "Solved",
+    alg: ["R' U R' U' R' F R2 U' R' U' R U R' F' R2"],
+    group: "Ortega",
+    prob: null
+  },
+  {
+    name: "P1",
+    alg: ["F U' R' F R U' F2 R U R'"],
+    group: "Pi",
+    prob: null
+  },
+  {
+    name: "P2",
+    alg: ["R' F R2 U' R2 F R"],
+    group: "Pi",
+    prob: null
+  },
+  {
+    name: "P3",
+    alg: ["F R' F U' F2 R U R"],
+    group: "Pi",
+    prob: null
+  },
+  {
+    name: "P4",
+    alg: ["R U' R' U R U' R' F R U' R'"],
+    group: "Pi",
+    prob: null
+  },
+  {
+    name: "P5",
+    alg: ["L F' L' F L F' L2 U' L U L' U L"],
+    group: "Pi",
+    prob: null
+  },
+  {
+    name: "P6",
+    alg: ["R' F R F' R' F R2 U R' U' R U' R'"],
+    group: "Pi",
+    prob: null
+  },
+  {
+    name: "S1",
+    alg: ["L F' L2 U' L F U L' U L"],
+    group: "Sune",
+    prob: null
+  },
+  {
+    name: "S2",
+    alg: ["L' U L F U F2 L' U L"],
+    group: "Sune",
+    prob: null
+  },
+  {
+    name: "S3",
+    alg: ["F' R' F R2 U R' U' F R' F' R"],
+    group: "Sune",
+    prob: null
+  },
+  {
+    name: "S4",
+    alg: ["F L' U' L U L' U' L2 F L'"],
+    group: "Sune",
+    prob: null
+  },
+  {
+    name: "S5",
+    alg: ["L' U L2 F' L' U L F' L' F"],
+    group: "Sune",
+    prob: null
+  },
+  {
+    name: "S6",
+    alg: ["R U' R' U R U' R' U' R' F' R F"],
+    group: "Sune",
+    prob: null
+  },
+  {
+    name: "T1",
+    alg: ["R2 B2 U' R' U' R U' R' U R'"],
+    group: "T",
+    prob: null
+  },
+  {
+    name: "T2",
+    alg: ["L' U L2 F' L' U' L' U2 L"],
+    group: "T",
+    prob: null
+  },
+  {
+    name: "T3",
+    alg: ["R' F' R2 U R' F' R U R'"],
+    group: "T",
+    prob: null
+  },
+  {
+    name: "T4",
+    alg: ["R U R' F R U R' F U' R U' R'"],
+    group: "T",
+    prob: null
+  },
+  {
+    name: "T5",
+    alg: ["F L F' L2 U' L U L' U' L"],
+    group: "T",
+    prob: null
+  },
+  {
+    name: "T6",
+    alg: ["F' R' F R2 U R' U' R U R'"],
+    group: "T",
+    prob: null
+  },
+  {
+    name: "U1",
+    alg: ["F R U' R' F x F' L' U' L' U x'"],
+    group: "U",
+    prob: null
+  },
+  {
+    name: "U2",
+    alg: ["R U' R2 F R2 U R' U' R U' R'"],
+    group: "U",
+    prob: null
+  },
+  {
+    name: "U3",
+    alg: ["L F' L' F L F' L2 U L"],
+    group: "U",
+    prob: null
+  },
+  {
+    name: "U4",
+    alg: ["F' U2 R U2 R' U2 F"],
+    group: "U",
+    prob: null
+  },
+  {
+    name: "U5",
+    alg: ["F' U L F2 L' F U2 F"],
+    group: "U",
+    prob: null
+  },
+  {
+    name: "U6",
+    alg: ["F U' R' F2 R F' U2 F'"],
+    group: "U",
+    prob: null
+  }
+];
