5085a0451e7a2ea8d9d8ae75129bee81d24fcc5c
Bryan Lundberg <bryanlundberg@outlook.com>
2025-10-04 06:49:12 -0600

    refactor: update OCLL algorithms structure and add variations
    Adjusted the structure of the OCLL algorithms for better organization and clarity. Added multiple algorithm variations for each case, ensuring comprehensive coverage and consistency across the dataset.

--- src/algs/ocll.ts
+++ src/algs/ocll.ts
@@ -2,45 +2,80 @@ import { AlgorithmCollection } from '@/interfaces/AlgorithmCollection';
 
 export const OCLL_ALGS: AlgorithmCollection[] = [
   {
-    name: "Sune",
-    alg: ["R U R' U R U2' R'", "U' R' U2' R U R' U R"],
-    group: "Sune",
-    prob: 4/27
+    name: 'Sune',
+    group: 'OCLL',
+    setup: '',
+    algs: [
+      "R U R' U R U2 R'",
+      "R U R2 U' R2 U R",
+      "R U' L' U R' U' L",
+      "y' R' U2 R U R' U R"
+    ]
   },
   {
-    name: "Headlights",
-    alg: ["F (R U R' U') F'", "U2 F (U R U' R') F'"],
-    group: "U",
-    prob: 4/27
+    name: 'Anti Sune',
+    group: 'OCLL',
+    setup: '',
+    algs: [
+      "R U2 R' U' R U' R'",
+      "y L' U' L U' L' U2 L",
+      "y' R' U' R U' R' U2 R",
+      "R' F R F' R U R'"
+    ]
   },
   {
-    name: "Anti-Sune",
-    alg: ["R' U' R U' R' U2 R", "U R U2' R' U' R U' R'"],
-    group: "Anti-Sune",
-    prob: 4/27
+    name: 'Pi',
+    group: 'OCLL',
+    setup: '',
+    algs: [
+      "F R U R' U' R U R' U' F'",
+      "R U' R2 U R2 U R2 U' R",
+      "R U2 R2 U' R2 U' R2 U2 R",
+      "y' R' F R2 U' R2 F R"
+    ]
   },
   {
-    name: "Bruno",
-    alg: ["F (R U R' U') (R U R' U') F'", "U2 F (U R U' R') (U R U' R') F'", "U' R' F R2 U' R2' F R'"],
-    group: "Pi",
-    prob: 4/27
+    name: 'P',
+    group: 'OCLL',
+    setup: '',
+    algs: [
+      "F R U R' U' F'",
+      "y2 F U R U' R' F'",
+      "y' R2 D R' U2 R D' R' U2 R'",
+      "y R2 D' R U2 R' D R U2 R"
+    ]
   },
   {
-    name: "Chameleon",
-    alg: ["(R U R' U') (R' F R F')", "U2 (L' U' L U) (L F' L' F)"],
-    group: "T",
-    prob: 4/27
+    name: 'L',
+    group: 'OCLL',
+    setup: '',
+    algs: [
+      "y F' R U R' U' R' F R",
+      "F R U' R' U' R U R' F'",
+      "F R' F' R U R U' R'",
+      "F' R U R' U' R' F R"
+    ]
   },
   {
-    name: "Double Anti-Sune",
-    alg: ["R2 U2' R' U2' R2"],
-    group: "H",
-    prob: 2/27
+    name: 'T',
+    group: 'OCLL',
+    setup: '',
+    algs: [
+      "R U R' U' R' F R F'",
+      "U R U R' U' F' U' F",
+      "y2 x U' L' U R' U' L U R",
+      "x L U R' U' L' U R U'"
+    ]
   },
   {
-    name: "Bowtie",
-    alg: ["F R U' (R' U' R U) R' F'", "U F' (R U R' U') R' F R"],
-    group: "L",
-    prob: 4/27
-  },
-];
+    name: 'H',
+    group: 'OCLL',
+    setup: '',
+    algs: [
+      "R2 U2 R' U2 R2",
+      'R2 U2 R U2 R2',
+      "y R U R' U R U' R' U R U2 R'",
+      "F R U R' U' R U R' U' R U R' U' F'"
+    ]
+  }
+]
