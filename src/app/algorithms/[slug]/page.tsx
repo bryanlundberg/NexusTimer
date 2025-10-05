a448c94db24a0838ed27481c51ddd85e149999f1
Bryan Lundberg <bryanlundberg@outlook.com>
2025-10-05 16:53:49 -0600

    feat: add dynamic metadata generation for algorithms page
    Implemented dynamic metadata generation based on the algorithm set slug. This ensures each page displays relevant titles and descriptions, improving SEO and user experience. Added type definitions to enhance type safety and readability.

--- src/app/algorithms/[slug]/page.tsx
+++ src/app/algorithms/[slug]/page.tsx
@@ -2,6 +2,26 @@ import { TwistyPlayer } from 'cubing/twisty';
 import { notFound } from 'next/navigation';
 import { AlgorithmsPage } from '@/components/algorithms/algorithms-page/algorithms-page';
 import { ALGORITHM_SETS } from '@/constants/algorithms-sets';
+import type { Metadata, ResolvingMetadata } from 'next'
+
+type Props = { params: Promise<{ slug: string }> }
+
+export async function generateMetadata({ params }: Props, parent: ResolvingMetadata): Promise<Metadata> {
+  const slug = (await params).slug
+
+  const collection = ALGORITHM_SETS.find((set) => set.slug === slug)
+  if (collection) {
+    return {
+      title: collection.title + ' - Algorithms',
+      description: collection.description,
+    }
+  }
+
+  return {
+    title: 'Algorithms',
+    description: 'Explore a wide range of algorithm sets for various puzzles, complete with interactive 3D visualizations to enhance your learning experience.',
+  }
+}
 
 export async function generateStaticParams() {
   return ALGORITHM_SETS.map((set) => {
@@ -9,7 +29,7 @@ export async function generateStaticParams() {
   });
 }
 
-export default async function Page({ params }: { params: Promise<{ slug: string }> }) {
+export default async function Page({ params }: Props) {
   const { slug } = await params
   const collection = ALGORITHM_SETS.find((set: { slug: string; }) => set.slug === slug);
 
