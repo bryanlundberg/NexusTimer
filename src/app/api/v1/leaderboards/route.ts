e5eb27e8ceea518e342e92b4ba833de1e3ab1278
Bryan Lundberg <bryanlundberg@outlook.com>
2025-10-05 06:12:19 -0600

    fix: increase leaderboard limit to 100 results
    Updated the leaderboard query to return up to 100 results instead of 10. This change ensures a broader range of data is displayed, improving visibility for users.

--- src/app/api/v1/leaderboards/route.ts
+++ src/app/api/v1/leaderboards/route.ts
@@ -5,6 +5,6 @@ import { NextResponse } from 'next/server';
 export async function GET() {
   await connectDB();
 
-  const leaderboards = await Solve.find().sort({ time: 1 }).limit(10).populate('user');
+  const leaderboards = await Solve.find().sort({ time: 1 }).limit(100).populate('user');
   return NextResponse.json(leaderboards)
 }
