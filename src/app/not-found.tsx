886a17f62941303a55ebfbb27814880e7417a61f
Bryan Lundberg <bryanlundberg@outlook.com>
2025-10-05 16:01:35 -0600

    refactor: adjust not-found page layout and button styling
    Updated image dimensions and padding to improve responsiveness. Changed button variant and adjusted breakpoint for improved design consistency on larger screens.

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -11,12 +11,12 @@ export default function NotFound() {
         alt=""
         priority
         className="blur-sm object-cover absolute inset-0 w-full h-full"
-        width={1920}
-        height={1080}
+        width={480}
+        height={480}
       />
 
-      <div className="flex flex-1 items-center justify-center p-16 relative z-10">
-        <div className="bg-gradient-to-br from-gray-50 to-gray-200 p-16 flex flex-col gap-6 rounded-2xl shadow-xl max-w-lg">
+      <div className="flex flex-1 items-center justify-center p-8 relative z-10">
+        <div className="bg-gradient-to-br from-gray-50 to-gray-200 p-8 flex flex-col gap-6 rounded-2xl shadow-xl max-w-lg">
           <h1 className="text-4xl font-extrabold text-gray-800">
             Lost in the void?
           </h1>
@@ -25,14 +25,14 @@ export default function NotFound() {
             dimension.
           </p>
           <Link href="/app">
-            <Button>
+            <Button variant={"secondary"}>
               <ArrowLeftIcon className="w-5 h-5" /> Back to Home
             </Button>
           </Link>
         </div>
       </div>
 
-      <div className="w-1/2 relative items-center justify-center p-8 z-10 hidden md:flex">
+      <div className="w-1/2 relative items-center justify-center p-8 z-10 hidden lg:flex">
         <Image
           src="/utils/not_found.jpg"
           alt="Not found illustration"
