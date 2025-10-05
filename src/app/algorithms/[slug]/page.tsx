817a80a7e17671689e0e261e8bec0bbb0574b827
Bryan Lundberg <bryanlundberg@outlook.com>
2025-10-05 16:45:09 -0600

    feat: add dynamic page generation for algorithm sets
    Introduce a new dynamic page for algorithm sets using Next.js. Implements static param generation and proper handling of nonexistent pages. Additionally, integrates the AlgorithmsPage component with relevant collection data.

---
+++ src/app/algorithms/[slug]/page.tsx
@@ -0,0 +1,30 @@
+import { TwistyPlayer } from 'cubing/twisty';
+import { notFound } from 'next/navigation';
+import { AlgorithmsPage } from '@/components/algorithms/algorithms-page/algorithms-page';
+import { ALGORITHM_SETS } from '@/constants/algorithms-sets';
+
+export async function generateStaticParams() {
+  return ALGORITHM_SETS.map((set) => {
+    return { slug: set.slug };
+  });
+}
+
+export default async function Page({ params }: { params: Promise<{ slug: string }> }) {
+  const { slug } = await params
+  const collection = ALGORITHM_SETS.find((set: { slug: string; }) => set.slug === slug);
+
+  if (!collection) {
+    notFound();
+  }
+
+  return (
+    <AlgorithmsPage
+      algorithms={collection.algorithms}
+      title={`${collection.title} - Algorithms`}
+      description={collection.description}
+      virtualization={collection.virtualization as unknown as TwistyPlayer}
+      fileCollectionName={collection.file}
+      puzzle={collection.puzzle}
+    />
+  )
+}
