6f78bd4d97df6059e416d71e8cd5471d3d80602c
Bryan Lundberg <bryanlundberg@outlook.com>
2025-10-09 06:07:03 -0600

    refactor: add container class to Table component for overflow handling

--- src/components/ui/table.tsx
+++ src/components/ui/table.tsx
@@ -4,11 +4,15 @@ import * as React from "react"
 
 import { cn } from "@/lib/utils"
 
-function Table({ className, ...props }: React.ComponentProps<"table">) {
+interface TableProps extends React.ComponentProps<"table"> {
+  containerClassName?: string
+}
+
+function Table({ className, containerClassName, ...props }: TableProps) {
   return (
     <div
       data-slot="table-container"
-      className="relative w-full overflow-x-auto"
+      className={cn("relative w-full overflow-x-auto", containerClassName)}
     >
       <table
         data-slot="table"
