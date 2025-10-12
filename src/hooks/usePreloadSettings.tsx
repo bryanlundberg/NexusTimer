c3c355d975e3ec01b7c1d98840aa8a23927c1aa5
Bryan Lundberg <bryanlundberg@outlook.com>
2025-10-12 12:49:59 -0600

    fix: remove unnecessary optional chaining in usePreloadSettings
    Cleaned up optional chaining for `settings.sync` properties in `usePreloadSettings`. This improves readability and aligns with the assumption that `settings.sync` is always defined.

--- src/hooks/usePreloadSettings.tsx
+++ src/hooks/usePreloadSettings.tsx
@@ -87,14 +87,14 @@ export function usePreloadSettings() {
 
     if (isOffline) return;
 
-    const hasReachedBackupInterval = Number(settings?.sync?.backupInterval) <= Number(settings?.sync?.totalSolves);
-    const isAutoSaveEnabled = settings?.sync?.autoSaveEnabled;
+    const hasReachedBackupInterval = Number(settings.sync.backupInterval) <= Number(settings.sync.totalSolves);
+    const isAutoSaveEnabled = settings.sync.autoSaveEnabled;
     const solvesIntervalReached = hasReachedBackupInterval && isAutoSaveEnabled;
-    const shouldShowToast = (!firstLoaded && settings?.sync?.autoLoadEnabled && user && cubes?.length && (cubes.some(cube => cube.solves.all.length > 0) || cubes.some(cube => cube.solves.session.length > 0))) || solvesIntervalReached;
+    const shouldShowToast = (!firstLoaded && settings.sync.autoLoadEnabled && user && cubes?.length && (cubes.some(cube => cube.solves.all.length > 0) || cubes.some(cube => cube.solves.session.length > 0))) || solvesIntervalReached;
 
     if (!shouldShowToast) return;
 
-    if (!moment(settings?.sync?.lastSync || 0).isBefore(moment().subtract(5, 'minutes'))) {
+    if (!moment(settings.sync.lastSync || 0).isBefore(moment().subtract(5, 'minutes'))) {
       setFirstLoaded(true);
       return;
     }
@@ -127,16 +127,16 @@ export function usePreloadSettings() {
     firstLoaded,
     handleSync,
     session?.user?.id,
-    settings.sync?.autoLoadEnabled,
+    settings.sync.autoLoadEnabled,
     user,
     SYNC_TOAST_ID,
     setFirstLoaded,
     cubes,
-    settings?.sync?.lastSync,
-    settings?.sync?.backupInterval,
-    settings?.sync?.totalSolves,
+    settings?.sync.lastSync,
+    settings?.sync.backupInterval,
+    settings?.sync.totalSolves,
     isOffline,
-    settings?.sync?.autoSaveEnabled
+    settings?.sync.autoSaveEnabled
   ]);
 
   return { isMounted };
