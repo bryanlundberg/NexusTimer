030ca293bf5616237b2ae6e762a3596a7217f64f
Bryan Lundberg <bryanlundberg@outlook.com>
2025-10-13 05:51:25 -0600

    feat: add manual solve mode to useSolveData hook
    Introduced a new `saveSolveManualMode` function to support saving manual solves in the `useSolveData` hook. Added state management for manual time value and implemented validation and conversion logic for user-inputted times. This enhances flexibility by

--- src/hooks/useSolveData.ts
+++ src/hooks/useSolveData.ts
@@ -3,6 +3,8 @@ import { Solve } from "@/interfaces/Solve";
 import genId from "@/lib/genId";
 import { useNXData } from '@/hooks/useNXData';
 import { useSettingsModalStore } from '@/store/SettingsModalStore';
+import convertToMs from '@/lib/convertToMs';
+import { useState } from 'react';
 
 export default function useSolveData() {
   const { saveCube } = useNXData();
@@ -14,6 +16,7 @@ export default function useSolveData() {
   const setNewScramble = useTimerStore(store => store.setNewScramble);
   const updateSetting = useSettingsModalStore(state => state.updateSetting)
   const solvesSinceLastSync = useSettingsModalStore(state => state.settings.sync.totalSolves)
+  const [value, setValue] = useState<string>("");
 
   const saveSolveMainTimer = async () => {
     if (selectedCube && scramble) {
@@ -49,7 +52,48 @@ export default function useSolveData() {
     setNewScramble(selectedCube);
   };
 
+  const saveSolveManualMode = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    if (!selectedCube) return;
+    if (!scramble) return;
+    if (parseInt(value) === 0 || value === "") return;
+
+    const msTime = convertToMs(value);
+    const now = Date.now();
+
+    const newSolve: Solve = {
+      id: genId(),
+      startTime: now - msTime,
+      endTime: Date.now(),
+      scramble: scramble,
+      bookmark: false,
+      time: msTime,
+      dnf: false,
+      plus2: false,
+      rating: Math.floor(Math.random() * 20) + scramble.length,
+      cubeId: selectedCube.id,
+      comment: "",
+    };
+
+    const updatedCube = {
+      ...selectedCube,
+      solves: {
+        ...selectedCube.solves,
+        session: [newSolve, ...selectedCube.solves.session],
+      },
+    }
+
+    saveCube(updatedCube);
+    setSelectedCube(updatedCube);
+    setLastSolve({ ...newSolve });
+    setNewScramble(selectedCube);
+    setValue("");
+  }
+
   return {
     saveSolveMainTimer,
+    value,
+    setValue,
+    saveSolveManualMode
   };
 }
