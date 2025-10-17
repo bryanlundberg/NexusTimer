bc81669e4524ec102797336dd43c6d25c8c041e2
Bryan Lundberg <bryanlundberg@outlook.com>
2025-10-17 08:56:47 -0600

    feat(clash): parse numeric fields for room configuration
    Added parsing of `maxRoundTime` and `totalRounds` to ensure numeric values when creating a Clash room. This prevents potential type issues during calculations and enhances data integrity.

--- src/components/clash/create-room-modal/create-room-modal.tsx
+++ src/components/clash/create-room-modal/create-room-modal.tsx
@@ -1,23 +1,28 @@
-import { DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle } from '@/components/ui/dialog';
-import { Globe2, Lock } from 'lucide-react';
-import { Button } from '@/components/ui/button';
-import { RoomType } from '@/enums/RoomType';
-import { Label } from '@/components/ui/label';
-import { Input } from '@/components/ui/input';
-import { Controller, useForm } from 'react-hook-form';
-import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue, } from '@/components/ui/select'
-import { FirestoreCollections } from '@/constants/FirestoreCollections';
-import { RoomStatus } from '@/enums/RoomStatus';
-import moment from 'moment';
-import { useFirestoreCache } from '@/hooks/useFirebaseCache';
-import { useRouter } from 'next/navigation';
-import { useSession } from 'next-auth/react';
+import { DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle } from '@/components/ui/dialog'
+import { Globe2, Lock } from 'lucide-react'
+import { Button } from '@/components/ui/button'
+import { RoomType } from '@/enums/RoomType'
+import { Label } from '@/components/ui/label'
+import { Input } from '@/components/ui/input'
+import { Controller, useForm } from 'react-hook-form'
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select'
+import { FirestoreCollections } from '@/constants/FirestoreCollections'
+import { RoomStatus } from '@/enums/RoomStatus'
+import moment from 'moment'
+import { useFirestoreCache } from '@/hooks/useFirebaseCache'
+import { useRouter } from 'next/navigation'
+import { useSession } from 'next-auth/react'
 
-export function CreateRoomModalContent({ mode }: { mode: RoomType; }) {
-  const { addDocument, updateDocument } = useFirestoreCache();
-  const { data: session } = useSession();
-  const router = useRouter();
-  const { handleSubmit, control, formState: { isSubmitting, errors }, register } = useForm({
+export function CreateRoomModalContent({ mode }: { mode: RoomType }) {
+  const { addDocument, updateDocument } = useFirestoreCache()
+  const { data: session } = useSession()
+  const router = useRouter()
+  const {
+    handleSubmit,
+    control,
+    formState: { isSubmitting, errors },
+    register
+  } = useForm({
     defaultValues: {
       name: '',
       event: '3x3',
@@ -30,11 +35,15 @@ export function CreateRoomModalContent({ mode }: { mode: RoomType; }) {
   })
 
   const submitForm = async (data: any) => {
-    const now = Date.now();
+    const now = Date.now()
     const roomDefaults = await addDocument(FirestoreCollections.CLASH_ROOMS, {
       ...data,
+      maxRoundTime: parseInt(data.maxRoundTime, 10),
+      totalRounds: parseInt(data.totalRounds, 10),
       preparationFinalizationTime: moment(now).add(2, 'minutes').valueOf(),
-      matchFinalizationTime: moment(now).add(parseInt(data.totalRounds, 10) * parseInt(data.maxRoundTime, 10) + 2, 'minutes').valueOf(),
+      matchFinalizationTime: moment(now)
+        .add(parseInt(data.totalRounds, 10) * parseInt(data.maxRoundTime, 10) + 2, 'minutes')
+        .valueOf(),
       createdAt: Date.now(),
       createdBy: session?.user?.id || '',
 
@@ -42,10 +51,10 @@ export function CreateRoomModalContent({ mode }: { mode: RoomType; }) {
         leaderId: session?.user?.id || '',
         term: 1
       }
-    });
+    })
 
     await updateDocument(`${FirestoreCollections.CLASH_ROOMS}/${roomDefaults.id}`, roomDefaults)
-    router.push(`/clash/${roomDefaults.id}`);
+    router.push(`/clash/${roomDefaults.id}`)
   }
 
   return (
@@ -53,9 +62,13 @@ export function CreateRoomModalContent({ mode }: { mode: RoomType; }) {
       <DialogHeader>
         <DialogTitle>
           {mode === RoomType.PUBLIC ? (
-            <span className="inline-flex items-center gap-2"><Globe2 className="size-4"/> New public room</span>
+            <span className="inline-flex items-center gap-2">
+              <Globe2 className="size-4" /> New public room
+            </span>
           ) : (
-            <span className="inline-flex items-center gap-2"><Lock className="size-4"/> New private room</span>
+            <span className="inline-flex items-center gap-2">
+              <Lock className="size-4" /> New private room
+            </span>
           )}
         </DialogTitle>
         <DialogDescription>
@@ -65,7 +78,9 @@ export function CreateRoomModalContent({ mode }: { mode: RoomType; }) {
 
       <div className="space-y-2">
         <div className="grid gap-2">
-          <Label htmlFor="room-name" className="text-sm font-medium">Room name</Label>
+          <Label htmlFor="room-name" className="text-sm font-medium">
+            Room name
+          </Label>
           <Input
             autoComplete={'off'}
             {...register('name', { required: 'Room name is required' })}
@@ -76,14 +91,16 @@ export function CreateRoomModalContent({ mode }: { mode: RoomType; }) {
 
         <div className="grid grid-cols-2 gap-2">
           <div>
-            <Label htmlFor="room-event" className="text-sm font-medium">Event</Label>
+            <Label htmlFor="room-event" className="text-sm font-medium">
+              Event
+            </Label>
             <Controller
               name={'event'}
               control={control}
               render={({ field: { onChange, value } }) => (
                 <Select value={value} onValueChange={onChange}>
                   <SelectTrigger className="w-full">
-                    <SelectValue/>
+                    <SelectValue />
                   </SelectTrigger>
                   <SelectContent>
                     <SelectItem value="2x2">2x2</SelectItem>
@@ -105,14 +122,16 @@ export function CreateRoomModalContent({ mode }: { mode: RoomType; }) {
           </div>
 
           <div>
-            <Label htmlFor="rounds" className="text-sm font-medium">Rounds</Label>
+            <Label htmlFor="rounds" className="text-sm font-medium">
+              Rounds
+            </Label>
             <Controller
               name={'totalRounds'}
               control={control}
               render={({ field: { onChange, value } }) => (
                 <Select value={value} onValueChange={onChange}>
                   <SelectTrigger className="w-full">
-                    <SelectValue/>
+                    <SelectValue />
                   </SelectTrigger>
                   <SelectContent>
                     <SelectItem value="5">Normal (5)</SelectItem>
@@ -126,28 +145,28 @@ export function CreateRoomModalContent({ mode }: { mode: RoomType; }) {
 
         {mode === RoomType.PRIVATE && (
           <div className="grid gap-2">
-            <Label htmlFor="room-pass" className="text-sm font-medium">Password</Label>
+            <Label htmlFor="room-pass" className="text-sm font-medium">
+              Password
+            </Label>
             <Input
               id="room-pass"
               autoComplete="off"
               {...register('password', {
                 validate: (value) => {
                   if (mode === RoomType.PRIVATE) {
-                    if (!value || value.trim().length === 0) return 'Password is required';
-                    const len = value.trim().length;
-                    if (len < 4) return 'Minimum password length is 4 characters';
-                    if (len > 10) return 'Maximum password length is 10 characters';
+                    if (!value || value.trim().length === 0) return 'Password is required'
+                    const len = value.trim().length
+                    if (len < 4) return 'Minimum password length is 4 characters'
+                    if (len > 10) return 'Maximum password length is 10 characters'
                   }
-                  return true;
-                },
+                  return true
+                }
               })}
               type="password"
               placeholder="••••••"
               aria-invalid={errors?.password ? 'true' : 'false'}
             />
-            {errors?.password && (
-              <p className="text-xs text-red-500">{String(errors.password.message)}</p>
-            )}
+            {errors?.password && <p className="text-xs text-red-500">{String(errors.password.message)}</p>}
           </div>
         )}
 
@@ -161,7 +180,7 @@ export function CreateRoomModalContent({ mode }: { mode: RoomType; }) {
             render={({ field: { onChange, value } }) => (
               <Select value={value} onValueChange={onChange}>
                 <SelectTrigger className={'w-full'}>
-                  <SelectValue/>
+                  <SelectValue />
                 </SelectTrigger>
                 <SelectContent>
                   <SelectItem value="10">10 sec</SelectItem>
@@ -197,15 +216,15 @@ export function CreateRoomModalContent({ mode }: { mode: RoomType; }) {
             )}
           />
 
-          <p className="text-xs text-muted-foreground">
-            Time limit for scrambling + inspection + solving (per solve)
-          </p>
+          <p className="text-xs text-muted-foreground">Time limit for scrambling + inspection + solving (per solve)</p>
         </div>
       </div>
 
       <DialogFooter className="mt-2">
-        <Button onClick={handleSubmit(submitForm)} disabled={isSubmitting}>Continue</Button>
+        <Button onClick={handleSubmit(submitForm)} disabled={isSubmitting}>
+          Continue
+        </Button>
       </DialogFooter>
     </DialogContent>
-  );
+  )
 }
