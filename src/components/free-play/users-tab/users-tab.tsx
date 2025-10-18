019e0a8ef9cd935b211dd2a537b598fa3d0910d5
Bryan Lundberg <bryanlundberg@outlook.com>
2025-10-18 08:28:45 -0600

    feat(free-play): integrate dynamic user presence in UsersTab
    Refactored UsersTab to utilize session-based user presence through `useFreeMode` and `useParams`. Replaced static player data with dynamic real-time updates, enhancing interactivity and relevancy in free play rooms.

--- src/components/free-play/users-tab/users-tab.tsx
+++ src/components/free-play/users-tab/users-tab.tsx
@@ -1,65 +1,14 @@
 import PlayerMiniCard from '@/components/clash/player-mini-card/player-mini-card'
+import useFreeMode from '@/hooks/useFreeMode'
+import { useParams } from 'next/navigation'
 
-export default function UsersTab({
-  players
-}: {
-  players: Array<{ id: string; name: string; image: string; status: string }>
-}) {
-  const playerss = [
-    {
-      id: '1',
-      name: 'Alice',
-      image:
-        'https://cdn.jsdelivr.net/gh/itspi3141/discord-fake-avatar-decorations@main/public/avatars/trick_or_treat.png',
-      status: 'online'
-    },
-    {
-      id: '2',
-      name: 'Bob',
-      image:
-        'https://cdn.jsdelivr.net/gh/itspi3141/discord-fake-avatar-decorations@main/public/avatars/trick_or_treat.png',
-      status: 'offline'
-    },
-    {
-      id: '3',
-      name: 'Charlie',
-      image:
-        'https://cdn.jsdelivr.net/gh/itspi3141/discord-fake-avatar-decorations@main/public/avatars/trick_or_treat.png',
-      status: 'online'
-    },
-    {
-      id: '4',
-      name: 'Diana',
-      image:
-        'https://cdn.jsdelivr.net/gh/itspi3141/discord-fake-avatar-decorations@main/public/avatars/trick_or_treat.png',
-      status: 'away'
-    },
-    {
-      id: '5',
-      name: 'Eve',
-      image:
-        'https://cdn.jsdelivr.net/gh/itspi3141/discord-fake-avatar-decorations@main/public/avatars/trick_or_treat.png',
-      status: 'online'
-    },
-    {
-      id: '6',
-      name: 'Frank',
-      image:
-        'https://cdn.jsdelivr.net/gh/itspi3141/discord-fake-avatar-decorations@main/public/avatars/trick_or_treat.png',
-      status: 'offline'
-    },
-    {
-      id: '7',
-      name: 'Grace',
-      image:
-        'https://cdn.jsdelivr.net/gh/itspi3141/discord-fake-avatar-decorations@main/public/avatars/trick_or_treat.png',
-      status: 'online'
-    }
-  ]
-
+export default function UsersTab() {
+  const { roomId } = useParams()
+  const { useUsersPresence } = useFreeMode()
+  const cubersOnline = useUsersPresence(roomId?.toString() || '')
   return (
     <div className={'flex flex-wrap gap-2'}>
-      {playerss.map((player) => (
+      {cubersOnline?.map((player) => (
         <PlayerMiniCard key={player.id} name={player.name} avatarUrl={player.image} />
       ))}
     </div>
