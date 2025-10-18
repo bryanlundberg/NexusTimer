efa9925b52cba64539f07cd9b754c5ed1c6c2b58
Bryan Lundberg <bryanlundberg@outlook.com>
2025-10-18 08:28:11 -0600

    feat(free-play): enhance TimerTab with session-based logic
    Added session-based user presence updates and solving state synchronization within the TimerTab component. Integrated hooks like `useFreeMode`, `useDeviceMatch`, and `useTimer` for improved functionality and reactivity. Enhanced the UI with real-time updates, scramble display, and time tracking.

--- src/components/free-play/timer-tab/timer-tab.tsx
+++ src/components/free-play/timer-tab/timer-tab.tsx
@@ -1,10 +1,76 @@
 import * as React from 'react'
+import { useEffect } from 'react'
+import useTimer from '@/hooks/useTimer'
+import { useTimerStore } from '@/store/timerStore'
+import { useSettingsModalStore } from '@/store/SettingsModalStore'
+import useDeviceMatch from '@/hooks/useDeviceMatch'
+import DisplayTime from '@/components/timer/display/display-time'
+import { Cube } from '@/interfaces/Cube'
+import useFreeMode from '@/hooks/useFreeMode'
+import { useSession } from 'next-auth/react'
+import { useParams } from 'next/navigation'
+import { TimerStatus } from '@/enums/TimerStatus'
 
 export default function TimerTab() {
+  const { roomId } = useParams()
+  const { updateUserPresenceStatus, addUserSolve, useRoomScramble } = useFreeMode()
+  const scramble = useRoomScramble(roomId?.toString() || '')
+
+  const { data: session } = useSession()
+  const settings = useSettingsModalStore((store) => store.settings)
+  const isSolving = useTimerStore((store) => store.isSolving)
+  const lastSolve = useTimerStore((store) => store.lastSolve)
+  const timerStatus = useTimerStore((store) => store.timerStatus)
+  const solvingTime = useTimerStore((store) => store.solvingTime)
+  const setTimerStatus = useTimerStore((store) => store.setTimerStatus)
+  const setIsSolving = useTimerStore((store) => store.setIsSolving)
+  const setSolvingTime = useTimerStore((store) => store.setSolvingTime)
+  const timerMode = useTimerStore((store) => store.timerMode)
+
+  const { device } = useDeviceMatch()
+
+  const { inspectionTime } = useTimer({
+    onFinishSolve: () =>
+      addUserSolve(roomId?.toString() || '', session?.user?.id || '', {
+        time: solvingTime,
+        date: Date.now(),
+        dnf: false,
+        plus2: false
+      }),
+    isSolving,
+    setTimerStatus,
+    selectedCube: {} as Cube,
+    inspectionRequired: false,
+    setIsSolving,
+    setSolvingTime,
+    timerMode,
+    settings: { timer: { startCue: false, holdToStart: false, inspectionTime: 15000 } }
+  })
+
+  useEffect(() => {
+    if (!session?.user?.id) return
+    if (!roomId) return
+    updateUserPresenceStatus(roomId.toString(), session.user.id, {
+      status: isSolving ? TimerStatus.SOLVING : TimerStatus.IDLE
+    })
+  }, [isSolving, session?.user?.id, roomId])
+
   return (
-    <div>
-      <div className={'text-center text-2xl mb-10'}>F (R U R' U') F' f (R U R' U') f'</div>
-      <div className={'text-9xl text-center'}>14.54</div>
+    <div className={'flex flex-col items-center justify-center h-max-content max-h-full flex-1'}>
+      <div className={'text-center text-2xl mb-20'}>{scramble}</div>
+
+      <div className={'grow my-auto'}>
+        <DisplayTime
+          isSolving={isSolving}
+          timerStatus={timerStatus}
+          lastSolve={lastSolve}
+          solvingTime={solvingTime}
+          device={device}
+          inspectionTime={inspectionTime}
+          hideWhileSolving={settings.features.hideWhileSolving}
+          className={'text-center'}
+        />
+      </div>
     </div>
   )
 }
