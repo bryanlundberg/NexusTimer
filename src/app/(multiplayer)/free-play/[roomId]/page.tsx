bd1900e2a1df3d939ba75811c36ff1c6f40a5d50
Bryan Lundberg <bryanlundberg@outlook.com>
2025-10-18 06:59:26 -0600

    feat(free-play): add room-specific page with tabs
    Introduced a page for individual rooms in free play mode, featuring tabs for timer, results, and user details. Integrated breadcrumb navigation, real-time room data via `useFreeMode`, and a responsive layout for better user experience.

---
+++ src/app/(multiplayer)/free-play/[roomId]/page.tsx
@@ -0,0 +1,65 @@
+'use client'
+import { useParams } from 'next/navigation'
+import useFreeMode from '@/hooks/useFreeMode'
+import { Tabs, TabsContent, TabsContents, TabsList, TabsTrigger } from '@/components/ui/shadcn-io/tabs'
+import { SidebarTrigger } from '@/components/ui/sidebar'
+import { Separator } from '@/components/ui/separator'
+import { Breadcrumb, BreadcrumbItem, BreadcrumbLink, BreadcrumbList } from '@/components/ui/breadcrumb'
+import Link from 'next/link'
+import * as React from 'react'
+import TimerTab from '@/components/free-play/timer-tab/timer-tab'
+import UsersTab from '@/components/free-play/users-tab/users-tab'
+import ResultsTab from '@/components/free-play/results-tab/results-tab'
+
+export default function Page() {
+  const { roomId } = useParams()
+  const { useRoom } = useFreeMode()
+  const room = useRoom(roomId?.toString() || '')
+
+  console.log(roomId)
+  return (
+    <div className="pt-4 px-4 md:pb-4 h-full flex flex-col">
+      <div className={'flex justify-between items-start h-fit'}>
+        <div className="flex items-center gap-2 mb-6">
+          <SidebarTrigger className="-ml-1" />
+          <Separator orientation="vertical" className="mr-2 data-[orientation=vertical]:h-4" />
+          <Breadcrumb>
+            <BreadcrumbList>
+              <BreadcrumbItem>
+                <BreadcrumbLink asChild>
+                  <Link href={'/free-play'}>Free Play</Link>
+                </BreadcrumbLink>
+              </BreadcrumbItem>
+              <Separator orientation="vertical" className="mr-2 data-[orientation=vertical]:h-4" />
+              <BreadcrumbItem>{roomId}</BreadcrumbItem>
+            </BreadcrumbList>
+          </Breadcrumb>
+        </div>
+
+        <div>online: 2</div>
+      </div>
+
+      <div>
+        <div className={'text-center text-xs mb-4'}>Next round starts in: 13:00</div>
+      </div>
+      <Tabs defaultValue="timer" className="bg-muted rounded-lg flex-1 px-2 pt-2 pb-2">
+        <TabsContents className="rounded-sm bg-background h-full">
+          <TabsContent value="timer" className="space-y-6 p-6">
+            <TimerTab />
+          </TabsContent>
+          <TabsContent value="results" className="space-y-6 p-6">
+            <ResultsTab />
+          </TabsContent>
+          <TabsContent value="people" className="space-y-6 p-6">
+            <UsersTab />
+          </TabsContent>
+        </TabsContents>
+        <TabsList className="grid w-full grid-cols-3">
+          <TabsTrigger value="timer">Timer</TabsTrigger>
+          <TabsTrigger value="results">Results</TabsTrigger>
+          <TabsTrigger value={'people'}>People</TabsTrigger>
+        </TabsList>
+      </Tabs>
+    </div>
+  )
+}
