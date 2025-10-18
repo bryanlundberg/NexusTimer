5fc7173a802e9d10eaa9754e743c59e50bf1d403
Bryan Lundberg <bryanlundberg@outlook.com>
2025-10-18 06:58:34 -0600

    feat(free-play): add TimerTab component for time display
    Introduced the `TimerTab` component to display a time and formula layout for the free play mode. This enhances the UI by providing a dedicated tab for timing-related functionality.

---
+++ src/components/free-play/timer-tab/timer-tab.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react'
+
+export default function TimerTab() {
+  return (
+    <div>
+      <div className={'text-center text-2xl mb-10'}>F (R U R' U') F' f (R U R' U') f'</div>
+      <div className={'text-9xl text-center'}>14.54</div>
+    </div>
+  )
+}
