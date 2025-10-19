c86d2a4bf3f9b2f67f7eb1261147fd175feb0359
Bryan Lundberg <bryanlundberg@outlook.com>
2025-10-19 07:32:17 -0600

    refactor(free-play): add padding to users-tab container
    Added padding to the users-tab container for improved spacing and layout. This ensures consistency with other components and enhances the overall user interface design.

--- src/components/free-play/users-tab/users-tab.tsx
+++ src/components/free-play/users-tab/users-tab.tsx
@@ -7,7 +7,7 @@ export default function UsersTab() {
   const { useUsersPresence } = useFreeMode()
   const cubersOnline = useUsersPresence(roomId?.toString() || '')
   return (
-    <div className={'grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4'}>
+    <div className={'grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4 p-4'}>
       {cubersOnline?.length &&
         cubersOnline?.map((player) => (
           <PlayerMiniCard
