35902b68307648bfbf4e338e09199abb518182f4
Bryan Lundberg <bryanlundberg@outlook.com>
2025-10-19 19:57:27 -0600

    refactor(worker): update type assertion for postMessage
    Replaced the type assertion from `as DedicatedWorkerGlobalScope` to `as unknown as DedicatedWorkerGlobalScope`. This refactor ensures stricter type safety and resolves potential TypeScript compatibility issues.

--- src/worker/deep-statistics.worker.ts
+++ src/worker/deep-statistics.worker.ts
@@ -1,68 +1,68 @@
 // <reference lib="webworker" />
-import { Cube } from '@/interfaces/Cube';
-import calcAverageStatistics from '@/lib/calcAverageStatistics';
-import calcTimeSpentStatistics from '@/lib/calcTimeSpentStatistics';
-import calcTotalSolvesStatistics from '@/lib/calcTotalSolvesStatistics';
-import calcAoStatistics from '@/lib/calcAoStatistics';
-import calcDeviation from '@/lib/calcDeviation';
-import calcSuccessRate from '@/lib/calcSuccessRate';
-import calcBestTime from '@/lib/calcBestTime';
-import getSolvesMetrics from '@/lib/getSolvesMetrics';
+import { Cube } from '@/interfaces/Cube'
+import calcAverageStatistics from '@/lib/calcAverageStatistics'
+import calcTimeSpentStatistics from '@/lib/calcTimeSpentStatistics'
+import calcTotalSolvesStatistics from '@/lib/calcTotalSolvesStatistics'
+import calcAoStatistics from '@/lib/calcAoStatistics'
+import calcDeviation from '@/lib/calcDeviation'
+import calcSuccessRate from '@/lib/calcSuccessRate'
+import calcBestTime from '@/lib/calcBestTime'
+import getSolvesMetrics from '@/lib/getSolvesMetrics'
 
 type InMsg = {
-  command: 'start'; data: {
-    cubes: Cube[],
+  command: 'start'
+  data: {
+    cubes: Cube[]
     selectedCube: Cube
   }
-};
+}
 
 self.onmessage = (event: MessageEvent<InMsg>) => {
-  const { command, data } = event.data;
+  const { command, data } = event.data
   if (command === 'start') {
-
     if (data.selectedCube) {
       const calculatedAverage = calcAverageStatistics({
         cubesDB: data.cubes,
         category: data.selectedCube.category,
-        cubeName: data.selectedCube.name,
-      });
+        cubeName: data.selectedCube.name
+      })
       const calculatedTimeSpent = calcTimeSpentStatistics({
         cubesDB: data.cubes,
         category: data.selectedCube.category,
-        cubeName: data.selectedCube.name,
-      });
+        cubeName: data.selectedCube.name
+      })
       const calculatedCounter = calcTotalSolvesStatistics({
         cubesDB: data.cubes,
         category: data.selectedCube.category,
-        cubeName: data.selectedCube.name,
-      });
+        cubeName: data.selectedCube.name
+      })
       const calculatedStats = calcAoStatistics({
         cubesDB: data.cubes,
         category: data.selectedCube.category,
-        cubeName: data.selectedCube.name,
-      });
+        cubeName: data.selectedCube.name
+      })
       const calculatedDeviation = calcDeviation({
         cubesDB: data.cubes,
         category: data.selectedCube.category,
-        cubeName: data.selectedCube.name,
-      });
+        cubeName: data.selectedCube.name
+      })
       const calculatedSuccessRate = calcSuccessRate({
         cubesDB: data.cubes,
         category: data.selectedCube.category,
-        cubeName: data.selectedCube.name,
-      });
+        cubeName: data.selectedCube.name
+      })
       const calculatedBest = calcBestTime({
         cubesDB: data.cubes,
         category: data.selectedCube.category,
-        cubeName: data.selectedCube.name,
-      });
+        cubeName: data.selectedCube.name
+      })
       const calculatedData = getSolvesMetrics({
         cubesDB: data.cubes,
         category: data.selectedCube.category,
-        cubeName: data.selectedCube.name,
-      });
+        cubeName: data.selectedCube.name
+      })
 
-      (self as DedicatedWorkerGlobalScope).postMessage({
+      ;(self as unknown as DedicatedWorkerGlobalScope).postMessage({
         result: {
           average: calculatedAverage,
           timeSpent: calculatedTimeSpent,
@@ -71,9 +71,9 @@ self.onmessage = (event: MessageEvent<InMsg>) => {
           deviation: calculatedDeviation,
           successRate: calculatedSuccessRate,
           best: calculatedBest,
-          data: calculatedData,
+          data: calculatedData
         }
-      });
+      })
     }
   }
-};
+}
