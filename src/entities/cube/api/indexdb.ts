a3e7bfdc04c92336e19723fa3cda017e68a2e52a
Bryan Lundberg <bryanlundberg@outlook.com>
2025-11-08 17:53:08 -0600

    feat(api): add cubesDB for IndexedDB cube management
    Introduced `cubesDB` to centralize cube operations with IndexedDB, including methods for retrieving, adding, updating, and fetching cubes by ID. This simplifies data handling and improves the scalability of the cube management API.

---
+++ src/entities/cube/api/indexdb.ts
@@ -0,0 +1,23 @@
+import { database } from '@/db/indexdb'
+import { Cube } from '@/interfaces/Cube'
+
+const STORE_NAME = 'nx-data'
+const Cubes = database.create(STORE_NAME)
+
+export const cubesDB = {
+  async getAll(): Promise<Cube[]> {
+    return await Cubes.find().get()
+  },
+
+  async getById(id: string): Promise<Cube | null> {
+    return await Cubes.get(id)
+  },
+
+  async add(cube: Cube) {
+    return await Cubes.add(cube)
+  },
+
+  async update(cube: Cube) {
+    return await Cubes.put(cube)
+  }
+}
