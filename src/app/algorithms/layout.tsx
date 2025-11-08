4c47ec15eb5d66623377b2698c960b4b8cef76fa
Bryan Lundberg <bryanlundberg@outlook.com>
2025-11-07 19:01:17 -0600

    chore(providers): remove unused `loaderProvider` prop
    Removed the redundant `loaderProvider` prop from the `Providers` component. This cleanup simplifies the JSX structure and ensures more maintainable and readable code.

--- src/app/algorithms/layout.tsx
+++ src/app/algorithms/layout.tsx
@@ -1,22 +1,16 @@
-'use client';
-import Providers from '@/components/providers';
-import React from 'react';
-import { AppSidebar } from '@/components/app-sidebar';
-import { SidebarInset } from '@/components/ui/sidebar';
+'use client'
+import Providers from '@/components/providers'
+import React from 'react'
+import { AppSidebar } from '@/components/app-sidebar'
+import { SidebarInset } from '@/components/ui/sidebar'
 
-export default function Layout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <>
-      <AppSidebar/>
+      <AppSidebar />
       <SidebarInset className={'overflow-hidden'} style={{ height: 'calc(100dvh - 1rem)' }}>
-        <Providers loaderProvider={false}>
-          {children}
-        </Providers>
+        <Providers>{children}</Providers>
       </SidebarInset>
     </>
-  );
+  )
 }
