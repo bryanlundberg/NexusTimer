98fcb246715c5e735479a22ee134d223481d1ff9
Bryan Lundberg <bryanlundberg@outlook.com>
2025-11-07 19:01:59 -0600

    chore(providers): remove unused LoaderOverlay component
    Deleted the unused `LoaderOverlay` component from the `Providers` component as it is no longer required. This cleanup reduces redundancy, simplifies the JSX structure, and ensures a more maintainable codebase.

--- src/components/providers.tsx
+++ src/components/providers.tsx
@@ -1,35 +1,24 @@
-'use client';
+'use client'
 
-import LoaderOverlay from '@/components/loader/loader-overlay';
-import PreloadSettings from '@/components/PreloadSettings';
-import { useEffect } from 'react';
-import { useSettingsModalStore } from '@/store/SettingsModalStore';
-import useWebsiteColors from '@/hooks/useWebsiteColors';
-import HeavyWebWorkers from '@/components/HeavyWebWorkers';
+import PreloadSettings from '@/components/PreloadSettings'
+import { useEffect } from 'react'
+import { useSettingsModalStore } from '@/store/SettingsModalStore'
+import useWebsiteColors from '@/hooks/useWebsiteColors'
+import HeavyWebWorkers from '@/components/HeavyWebWorkers'
 
-export default function Providers({
-  loaderProvider = true,
-  children,
-}: {
-  children: React.ReactNode;
-  loaderProvider?: boolean;
-}) {
-
-  const settings = useSettingsModalStore(store => store.settings);
-  const { applyColorTheme } = useWebsiteColors();
+export default function Providers({ children }: { children: React.ReactNode; loaderProvider?: boolean }) {
+  const settings = useSettingsModalStore((store) => store.settings)
+  const { applyColorTheme } = useWebsiteColors()
 
   useEffect(() => {
-    applyColorTheme(settings.preferences.colorTheme);
-  }, [applyColorTheme, settings.preferences.colorTheme]);
+    applyColorTheme(settings.preferences.colorTheme)
+  }, [applyColorTheme, settings.preferences.colorTheme])
 
   return (
     <>
       <PreloadSettings>
-        <HeavyWebWorkers>
-          {children}
-        </HeavyWebWorkers>
+        <HeavyWebWorkers>{children}</HeavyWebWorkers>
       </PreloadSettings>
-      <LoaderOverlay/>
     </>
-  );
+  )
 }
