90b1c0caec95579cda9842a44b0da0b4581c046b
Bryan Lundberg <bryanlundberg@outlook.com>
2025-11-08 17:54:33 -0600

    feat(types): add Cube-related TypeScript types for entities
    Introduced `Cube`, `CreateCubeDTO`, `UpdateCubeDTO`, and `DeleteCubeDTO` types to define the structure of cube entities and their data transfer objects. These additions improve type safety and clarity in cube entity management and related operations.

---
+++ src/entities/cube/model/types.ts
@@ -0,0 +1,29 @@
+import { CubeCategory } from '@/shared/config/cube-categories'
+import { Categories } from '@/interfaces/Categories'
+import { Solves } from '@/interfaces/Solves'
+
+export type Cube = {
+  id: string
+  name: string
+  category: Categories
+  solves: Solves
+  createdAt: number
+  favorite: boolean
+  updatedAt?: number
+  isDeleted?: boolean
+}
+
+export type CreateCubeDTO = {
+  name: string
+  category: CubeCategory
+}
+
+export type UpdateCubeDTO = {
+  id: string
+  name: string
+  category: CubeCategory
+}
+
+export type DeleteCubeDTO = {
+  id: string
+}
