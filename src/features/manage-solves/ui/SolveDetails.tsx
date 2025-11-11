01a47c1050975b5662d3ea6d738ded421d286391
Bryan Lundberg <bryanlundberg@outlook.com>
2025-11-11 06:23:55 -0600

    refactor: migrate SheetSolveDetails to SolveDetails

--- src/features/manage-solves/ui/SolveDetails.tsx
+++ src/features/manage-solves/ui/SolveDetails.tsx
@@ -1,55 +1,53 @@
-import MenuSolveOptions from '@/components/menu-solve-options/menu-solve-options'
-import { ScrambleDisplay } from '@/components/scramble-display'
 import formatTime from '@/shared/lib/formatTime'
-import { useDialogSolve } from '@/store/DialogSolve'
 import { useTimerStore } from '@/store/timerStore'
 import { CalendarIcon, ClockIcon } from '@radix-ui/react-icons'
 import { DateTime } from 'luxon'
 import { useLocale } from 'next-intl'
 import { DialogContent, DialogDescription, DialogHeader, DialogTitle } from '@/components/ui/dialog'
 import { Badge } from '@/components/ui/badge'
+import ScrambleDisplay from '@/shared/ui/scramble-display/ui/ScrambleDisplay'
+import { useOverlayStore } from '@/shared/model/overlay-store/useOverlayStore'
+import { Solve } from '@/entities/solve/model/types'
+import QuickActions from '@/features/manage-solves/ui/QuickActions'
 
-export default function SheetSolveDetails() {
-  const { handleCloseDialogSolve } = useDialogSolve()
-  const { solve } = useDialogSolve()
+export default function SolveDetails() {
+  const overlayStore = useOverlayStore()
   const selectedCube = useTimerStore((state) => state.selectedCube)
   const locale = useLocale()
+  const { activeOverlay } = overlayStore
 
   return (
     <DialogContent showCloseButton={false}>
       <DialogHeader>
         <DialogTitle className={'text-sm flex justify-between'}>
           <div className="flex gap-1 items-center">
-            <span className={'text-2xl'}>{formatTime(solve?.time || 0)}</span>
+            <span className={'text-2xl'}>{formatTime(activeOverlay?.metadata?.time || 0)}</span>
             <Badge className={'text-xs h-fit'}>{selectedCube?.category || 'Unknown'}</Badge>
           </div>
           <div className={'flex flex-col items-end text-xs font-normal'}>
             <p className="flex items-center justify-center gap-1">
               <CalendarIcon />
-              {DateTime.fromMillis(solve?.endTime || 0)
+              {DateTime.fromMillis(activeOverlay?.metadata?.endTime || 0)
                 .setLocale(locale)
                 .toFormat('DDDD')}
             </p>
             <p className="flex items-center justify-center gap-1">
               <ClockIcon />
-              {DateTime.fromMillis(solve?.endTime || 0)
+              {DateTime.fromMillis(activeOverlay?.metadata?.endTime || 0)
                 .setLocale(locale)
                 .toFormat('HH:mm:ss')}
             </p>
           </div>
         </DialogTitle>
-
-        <DialogDescription className="text-md text-start">{solve?.scramble}</DialogDescription>
-
+        <DialogDescription className="text-md text-start">{activeOverlay?.metadata?.scramble}</DialogDescription>
         <ScrambleDisplay
           show={true}
-          scramble={solve?.scramble || ''}
+          scramble={activeOverlay?.metadata?.scramble || ''}
           event={selectedCube?.category || '3x3'}
           className="h-20 md:h-24 lg:h-28 xl:h-32 2xl:h-36"
           visualization="2D"
         />
-
-        {solve && <MenuSolveOptions solve={solve} onDeleteSolve={handleCloseDialogSolve} caseOfUse="modal-solve" />}
+        {activeOverlay?.metadata && <QuickActions solve={activeOverlay?.metadata as Solve} />}
       </DialogHeader>
     </DialogContent>
   )
