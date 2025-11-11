302170147cb547b1161383c5206d9f4b46e73e24
Bryan Lundberg <bryanlundberg@outlook.com>
2025-11-11 06:27:15 -0600

    refactor: replace MenuSolveOptions with QuickActions

--- src/components/timer/Timer.tsx
+++ src/components/timer/Timer.tsx
@@ -1,16 +1,14 @@
 import { useTimerStore } from '@/store/timerStore'
 import { useSettingsModalStore } from '@/store/SettingsModalStore'
 import useTimer from '@/hooks/useTimer'
-import Confetti from 'react-dom-confetti'
 import useDeviceMatch from '@/hooks/useDeviceMatch'
-import { confettiConfig } from '@/lib/const/confettiConfig'
-import MenuSolveOptions from '../menu-solve-options/menu-solve-options'
 import DisplayContainer from './display/display-container'
 import DisplayTime from './display/display-time'
 import { ReactNode } from 'react'
 import { TimerStatus } from '@/enums/TimerStatus'
 import useSolveData from '@/hooks/useSolveData'
 import { useAudioTrigger } from '@/hooks/useAudioTrigger'
+import QuickActions from '@/features/manage-solves/ui/QuickActions'
 
 export default function Timer({ children }: { children?: ReactNode }) {
   const settings = useSettingsModalStore((store) => store.settings)
@@ -64,7 +62,7 @@ export default function Timer({ children }: { children?: ReactNode }) {
         />
       )}
       {lastSolve && settings.features.quickActionButtons && timerStatus === TimerStatus.IDLE && (
-        <MenuSolveOptions
+        <QuickActions
           solve={lastSolve}
           onDeleteSolve={() => setLastSolve(null)}
           caseOfUse="last-solve"
