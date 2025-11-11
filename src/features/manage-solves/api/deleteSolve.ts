b72011adbf95f5c11a8855cd93680374d593c1ff
Bryan Lundberg <bryanlundberg@outlook.com>
2025-11-11 06:29:12 -0600

    Remove unused deleteSolve API function

--- src/features/manage-solves/api/deleteSolve.ts
+++
@@ -1,7 +0,0 @@
-import { DeleteSolveDTO } from '@/features/manage-solves/model/types'
-import { solvesDB } from '@/entities/solve/api/indexdb'
-
-export default async function deleteSolve(dto: DeleteSolveDTO) {
-  const { cubeId, solveId, solveTab } = dto
-  return await solvesDB.deleteById(cubeId, solveId, solveTab)
-}
