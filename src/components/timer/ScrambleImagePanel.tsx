ab7f6dfe37c0c9da88338383833b984d033ac010
Bryan Lundberg <bryanlundberg@outlook.com>
2025-11-11 06:23:06 -0600

    Update ScrambleDisplay import path

--- src/components/timer/ScrambleImagePanel.tsx
+++ src/components/timer/ScrambleImagePanel.tsx
@@ -1,19 +1,19 @@
-import { ScrambleDisplay } from "../scramble-display";
-import { useTimerStore } from "@/store/timerStore";
-import { useSettingsModalStore } from "@/store/SettingsModalStore";
-import { motion, AnimatePresence } from "framer-motion";
-import { cn } from '@/lib/utils';
-import { useWindowSize } from 'react-use-size';
-import { SCRAMBLE_HEIGHT } from '@/constants/scramble-height';
+import { useTimerStore } from '@/store/timerStore'
+import { useSettingsModalStore } from '@/store/SettingsModalStore'
+import { motion, AnimatePresence } from 'framer-motion'
+import { cn } from '@/lib/utils'
+import { useWindowSize } from 'react-use-size'
+import { SCRAMBLE_HEIGHT } from '@/constants/scramble-height'
+import ScrambleDisplay from '@/shared/ui/scramble-display/ui/ScrambleDisplay'
 
 export default function ScrambleImagePanel() {
-  const settings = useSettingsModalStore(store => store.settings);
-  const scramble = useTimerStore(store => store.scramble);
-  const selectedCube = useTimerStore(store => store.selectedCube);
-  const setZoomInScramble = useTimerStore(store => store.setZoomInScramble);
-  const zoomInScramble = useTimerStore(store => store.zoomInScramble);
-  const isSolving = useTimerStore(store => store.isSolving);
-  const { height } = useWindowSize();
+  const settings = useSettingsModalStore((store) => store.settings)
+  const scramble = useTimerStore((store) => store.scramble)
+  const selectedCube = useTimerStore((store) => store.selectedCube)
+  const setZoomInScramble = useTimerStore((store) => store.setZoomInScramble)
+  const zoomInScramble = useTimerStore((store) => store.zoomInScramble)
+  const isSolving = useTimerStore((store) => store.isSolving)
+  const { height } = useWindowSize()
   return (
     <AnimatePresence>
       {(!zoomInScramble || !isSolving) && (
@@ -22,56 +22,55 @@ export default function ScrambleImagePanel() {
           initial={{ opacity: 0.8, y: 100 }}
           animate={{ opacity: 1, y: 0 }}
           exit={{ opacity: 0.8, y: 100 }}
-          transition={{ duration: 0.6, ease: "easeOut" }}
+          transition={{ duration: 0.6, ease: 'easeOut' }}
         >
           <div
-            className={"w-fit mx-auto"}
+            className={'w-fit mx-auto'}
             onPointerDownCapture={(e) => {
-              e.preventDefault();
-              e.stopPropagation();
-              setZoomInScramble(true);
+              e.preventDefault()
+              e.stopPropagation()
+              setZoomInScramble(true)
             }}
             onPointerUpCapture={(e) => {
-              e.preventDefault();
-              e.stopPropagation();
+              e.preventDefault()
+              e.stopPropagation()
             }}
             onClickCapture={(e) => {
-              e.preventDefault();
-              e.stopPropagation();
+              e.preventDefault()
+              e.stopPropagation()
             }}
             onTouchStartCapture={(e) => {
-              e.preventDefault();
-              e.stopPropagation();
-              setZoomInScramble(true);
+              e.preventDefault()
+              e.stopPropagation()
+              setZoomInScramble(true)
             }}
             onTouchEndCapture={(e) => {
-              e.preventDefault();
-              e.stopPropagation();
+              e.preventDefault()
+              e.stopPropagation()
             }}
             onMouseDownCapture={(e) => {
-              e.preventDefault();
-              e.stopPropagation();
-              setZoomInScramble(true);
+              e.preventDefault()
+              e.stopPropagation()
+              setZoomInScramble(true)
             }}
             onMouseUpCapture={(e) => {
-              e.preventDefault();
-              e.stopPropagation();
+              e.preventDefault()
+              e.stopPropagation()
             }}
             onContextMenu={(e) => {
-              e.preventDefault();
-              e.stopPropagation();
+              e.preventDefault()
+              e.stopPropagation()
             }}
           >
             <ScrambleDisplay
-              className={cn("w-20 h-20 md:w-32 mx-auto cursor-pointer", height <= SCRAMBLE_HEIGHT && "h-18 max-w-20")}
+              className={cn('w-20 h-20 md:w-32 mx-auto cursor-pointer', height <= SCRAMBLE_HEIGHT && 'h-18 max-w-20')}
               show={settings.features.scrambleImage}
               scramble={scramble}
-              event={selectedCube?.category || "3x3"}
+              event={selectedCube?.category || '3x3'}
             />
           </div>
-
         </motion.div>
       )}
     </AnimatePresence>
-  );
+  )
 }
