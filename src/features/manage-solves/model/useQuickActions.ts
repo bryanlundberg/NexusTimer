ca336ce298cad9267394b36c366316fa6620a59b
Bryan Lundberg <bryanlundberg@outlook.com>
2025-11-12 04:28:20 -0600

    Refactor: Replace solvesDB methods with dedicated APIs

--- src/features/manage-solves/model/useQuickActions.ts
+++ src/features/manage-solves/model/useQuickActions.ts
@@ -1,33 +1,49 @@
 import { Solve } from '@/entities/solve/model/types'
-import { solvesDB } from '@/entities/solve/api/indexdb'
 import { SolveTab } from '@/enums/SolveTab'
 import formatTime from '@/shared/lib/formatTime'
 import { toast } from 'sonner'
 import { useRouter } from 'next/navigation'
 import { useTimerStore } from '@/store/timerStore'
+import { deleteSolve } from '@/features/manage-solves/api/deleteSolve'
+import { toggleDNF } from '@/features/manage-solves/api/toggleDNF'
+import { togglePlus2 } from '@/features/manage-solves/api/togglePlus2'
+import { toggleBookmark } from '@/features/manage-solves/api/toggleBookmark'
+import { cubesDB } from '@/entities/cube/api/indexdb'
+import { useOverlayStore } from '@/shared/model/overlay-store/useOverlayStore'
 
 export default function useQuickActions(solve: Solve) {
   const router = useRouter()
   const selectedCube = useTimerStore((store) => store.selectedCube)
+  const setSelectedCube = useTimerStore((store) => store.setSelectedCube)
+  const setCubes = useTimerStore((store) => store.setCubes)
+  const cubes = useTimerStore((store) => store.cubes)
+  const { open, close, activeOverlay } = useOverlayStore()
 
   const handleToggleBookmark = async (solveTab: SolveTab) => {
+    if (!selectedCube) return
     const { cubeId, id: solveId } = solve
-    return await solvesDB.toggleBookmarkById(cubeId, solveId, !solve.bookmark, solveTab)
+    await toggleBookmark({ cubeId, solveId, bookmark: !solve.bookmark, solveTab })
+    syncUI(solveTab)
   }
 
   const handleToggleDNF = async (solveTab: SolveTab) => {
     const { cubeId, id: solveId } = solve
-    return await solvesDB.toggleDNFById(cubeId, solveId, !solve.dnf, solveTab)
+    await toggleDNF({ cubeId, solveId, dnf: !solve.dnf, solveTab })
+    syncUI(solveTab)
   }
 
   const handleTogglePlus2 = async (solveTab: SolveTab) => {
     const { cubeId, id: solveId } = solve
-    return await solvesDB.togglePlus2ById(cubeId, solveId, !solve.plus2, solveTab)
+    await togglePlus2({ cubeId, solveId, plus2: !solve.plus2, solveTab })
+    syncUI(solveTab)
   }
 
   const handleDeleteSolve = async (solveTab: SolveTab) => {
     const { cubeId, id: solveId } = solve
-    return await solvesDB.deleteById(cubeId, solveId, solveTab)
+    await deleteSolve({ cubeId, solveId, solveTab })
+    syncUI(solveTab)
+
+    toast.success(`Solve ${formatTime(solve.time)} deleted`, { duration: 1500 })
   }
 
   const handleClipboard = (customMessage?: { title?: string; description?: string }) => {
@@ -43,12 +59,33 @@ export default function useQuickActions(solve: Solve) {
 
   const handleTransferCollection = () => {
     if (!selectedCube) return
-    router.push(`/transfer-solves?source-collection=${selectedCube.id}`)
+    router.push(`/transfer-solves?source-collection=${selectedCube.id}`) // TODO: change source-collection to constant
   }
 
   const handleMoveToHistorial = async () => {
     const { cubeId, id: solveId } = solve
     // TODO: implement move to history
+  }
+
+  const syncUI = async (solveTab: SolveTab) => {
+    const updatedCube = await cubesDB.getById(selectedCube?.id || '')
+    setSelectedCube(updatedCube || null)
+    setCubes(cubes ? cubes.map((cube) => (cube.id === updatedCube?.id ? updatedCube : cube)) : [])
+
+    const list =
+      solveTab.toLowerCase() === SolveTab.SESSION.toLowerCase() ? updatedCube?.solves.session : updatedCube?.solves.all
+    const updatedSolve = list?.find((s) => s.id === solve.id && !s.isDeleted)
+
+    if (!updatedSolve) {
+      close()
+      return
+    }
+
+    open({
+      id: activeOverlay?.id || '',
+      metadata: updatedSolve,
+      component: activeOverlay?.component || null
+    })
   }
 
   return {
