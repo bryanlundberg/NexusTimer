c1bd054e8dbace112fdb5e70f639e8bddadae9fc
Bryan Lundberg <bryanlundberg@outlook.com>
2025-11-12 04:45:37 -0600

    Move logo component to shared/ui directory

--- src/shared/ui/logo/logo.tsx
+++ src/shared/ui/logo/logo.tsx
@@ -1,33 +1,27 @@
-import { cn } from "@/lib/utils";
-import Image from "next/image";
-import React from "react";
+import { cn } from '@/lib/utils'
+import Image from 'next/image'
+import React from 'react'
 
 interface LogoProps extends React.HTMLAttributes<HTMLDivElement> {
-  className?: string;
+  className?: string
 }
 
 export default function Logo({ className, ...rest }: LogoProps) {
   return (
     <>
-      <div
-        {...rest}
-        className={cn(
-          "flex gap-2 justify-center items-center mx-auto",
-          className
-        )}
-      >
+      <div {...rest} className={cn('flex gap-2 justify-center items-center mx-auto', className)}>
         <p className="text-xl font-bold">Nexus</p>
         <Image
-          src={"/logo.png"}
+          src={'/logo.png'}
           alt="logo"
           width={320}
           height={100}
-          className={"size-5 dark:invert invert-0"}
+          className={'size-5 dark:invert invert-0'}
           draggable={false}
         />
 
         <p className="text-xl font-bold">Timer ™</p>
       </div>
     </>
-  );
+  )
 }
