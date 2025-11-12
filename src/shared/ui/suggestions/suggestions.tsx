41747dd25242675cb6f512f20df5a8d0f159ee02
Bryan Lundberg <bryanlundberg@outlook.com>
2025-11-12 06:43:39 -0600

    Refactor: move suggestions component to shared/ui

--- src/shared/ui/suggestions/suggestions.tsx
+++ src/shared/ui/suggestions/suggestions.tsx
@@ -1,11 +1,8 @@
-import { ALGORITHMS_GITHUB_URL } from '@/constants/algorithms-github-url';
-import { SquarePen } from 'lucide-react';
-import Link from 'next/link';
+import { ALGORITHMS_GITHUB_URL } from '@/constants/algorithms-github-url'
+import { SquarePen } from 'lucide-react'
+import Link from 'next/link'
 
-export default function Suggestions({
-  message = '',
-  link = ALGORITHMS_GITHUB_URL,
-}) {
+export default function Suggestions({ message = '', link = ALGORITHMS_GITHUB_URL }) {
   return (
     <Link
       href={link}
@@ -13,7 +10,8 @@ export default function Suggestions({
       rel="noopener noreferrer"
       className="leading-7 [&:not(:first-child)]:mt-6 py-10 text-background-foreground hover:underline pb-5 flex items-center justify-center gap-2 text-sm w-fit"
     >
-      <SquarePen size={16}/>{message}
+      <SquarePen size={16} />
+      {message}
     </Link>
   )
 }
