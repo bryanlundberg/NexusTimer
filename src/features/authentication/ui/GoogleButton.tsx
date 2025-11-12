d7a57b0fb11ccf2ed4d3854d38463b8decd95a1a
Bryan Lundberg <bryanlundberg@outlook.com>
2025-11-12 04:45:30 -0600

    rename: Update `ButtonGoogle` to `GoogleButton`

---
+++ src/features/authentication/ui/GoogleButton.tsx
@@ -0,0 +1,22 @@
+'use client'
+import { Button } from '@/components/ui/button'
+import Image from 'next/image'
+import { signIn, useSession } from 'next-auth/react'
+import { useTranslations } from 'next-intl'
+
+export default function GoogleButton() {
+  const t = useTranslations('Index')
+  const { data: session } = useSession()
+  return (
+    <>
+      {!session && (
+        <>
+          <Button className="flex gap-2 items-center w-full" onClick={() => signIn('google', { redirectTo: '/app' })}>
+            <Image src={'/timer-logos/google.svg'} alt="google logo" width={20} height={20} />
+            <p className={'overflow-hidden text-ellipsis'}>{t('Inputs.sign-in-google')}</p>
+          </Button>
+        </>
+      )}
+    </>
+  )
+}
