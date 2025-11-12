52acf9ac36d2db571afb96c8f626e98771121ca0
Bryan Lundberg <bryanlundberg@outlook.com>
2025-11-12 05:30:22 -0600

    Update loading component to use Spinner

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,11 +1,11 @@
-import Loading from "@/components/Loading";
+import { Spinner } from '@/components/ui/spinner'
 
 export default function Page() {
   return (
     <>
       <div className="min-h-dvh max-h-dvh w-full overflow-hidden flex items-center justify-center bg-background">
-        <Loading />
+        <Spinner />
       </div>
     </>
-  );
+  )
 }
