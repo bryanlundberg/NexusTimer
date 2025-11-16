4821b2397790d2a3f150a6fe499f2e1baa578d74
Bryan Lundberg <bryanlundberg@outlook.com>
2025-11-15 20:17:24 -0600

    add SolveServer type and schema to solve/model

--- src/entities/solve/model/types.ts
+++ src/entities/solve/model/types.ts
@@ -1,3 +1,6 @@
+import { z } from 'zod'
+import { CUBE_CATEGORIES } from '@/shared/config/cube-categories'
+
 export type Solves = {
   session: Array<Solve>
   all: Array<Solve>
@@ -18,3 +21,17 @@ export type Solve = {
   isDeleted?: boolean
   updatedAt?: number
 }
+
+const SolveServerSchema = z.object({
+  _id: z.string(),
+  userId: z.any(),
+  time: z.number(),
+  scramble: z.string(),
+  solution: z.string().optional(),
+  puzzle: z.enum(CUBE_CATEGORIES),
+  createdAt: z.string(),
+  updatedAt: z.string().optional(),
+  smart: z.boolean().optional()
+})
+
+export type SolveServer = z.infer<typeof SolveServerSchema>
