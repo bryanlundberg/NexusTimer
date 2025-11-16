c45a2ebd7e114f10cbff6560c912f17d1010b425
Bryan Lundberg <bryanlundberg@outlook.com>
2025-11-15 20:40:33 -0600

    add SelectLanguage component to widgets/ui

---
+++ src/widgets/select-language/ui/SelectLanguage.tsx
@@ -0,0 +1,24 @@
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select'
+import { languages } from '@/lib/const/languages'
+import { syncTranslations } from '@/actions/language'
+import { useLocale } from 'next-intl'
+
+export default function SelectLanguage() {
+  const locale = useLocale()
+  return (
+    <Select defaultValue={locale} onValueChange={syncTranslations}>
+      <SelectTrigger className="w-[180px] bg-background">
+        <SelectValue />
+      </SelectTrigger>
+      <SelectContent>
+        {languages.map((item) => {
+          return (
+            <SelectItem value={item.code} key={item.code}>
+              {item.name}
+            </SelectItem>
+          )
+        })}
+      </SelectContent>
+    </Select>
+  )
+}
