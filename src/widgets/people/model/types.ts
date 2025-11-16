c1975e61e4890d801d3064f63e813416d1aee73a
Bryan Lundberg <bryanlundberg@outlook.com>
2025-11-16 07:51:19 -0600

    add PeopleTabs type to widgets/people/model

---
+++ src/widgets/people/model/types.ts
@@ -0,0 +1,8 @@
+export enum PeopleTabs {
+  OVERVIEW = 'overview',
+  CUBES = 'cubes',
+  LAST_ACTIVITY = 'last_activity'
+}
+
+export const PEOPLE_TAB_QS_KEY = 'tab'
+export const PEOPLE_TAB_DEFAULT = PeopleTabs.OVERVIEW
