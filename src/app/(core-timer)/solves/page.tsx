c6b9deb0703988ed476d89e3254dbc031b6815b6
Bryan Lundberg <bryanlundberg@outlook.com>
2025-11-22 15:42:46 -0600

    Refactor: update SolvesPage with new UI and logic

--- src/app/(core-timer)/solves/page.tsx
+++ src/app/(core-timer)/solves/page.tsx
@@ -1,1 +1,33 @@
-export { default } from '@/pages/solves/ui/SolvesPage'
+'use client'
+import { useTimerStore } from '@/shared/model/timer/useTimerStore'
+import { useQueryState } from 'nuqs'
+import { useDebouncedCallback } from 'use-debounce'
+import { useMemo } from 'react'
+import SolvesPageHeader from '@/widgets/navigation-header/ui/SolvesPageHeader'
+import SolvesGrid from '@/features/solves-grid/ui/SolvesGrid'
+import { STATES } from '@/shared/const/states'
+import { SolveTab } from '@/shared/types/enums'
+
+export default function SolvesPage() {
+  const selectedCube = useTimerStore((state) => state.selectedCube)
+  const [tabMode] = useQueryState(STATES.SOLVES_PAGE.TAB_MODE.KEY, {
+    defaultValue: STATES.SOLVES_PAGE.TAB_MODE.DEFAULT_VALUE
+  })
+  const [, setQuery] = useQueryState(STATES.SOLVES_PAGE.QUERY.KEY, {
+    defaultValue: STATES.SOLVES_PAGE.QUERY.DEFAULT_VALUE
+  })
+
+  const handleSearch = useDebouncedCallback((value) => setQuery(value), 1000)
+
+  const displaySolves = useMemo(() => {
+    if (!selectedCube) return []
+    return tabMode === SolveTab.SESSION ? selectedCube.solves.session : selectedCube.solves.all
+  }, [selectedCube, tabMode])
+
+  return (
+    <div className={'h-dvh flex flex-col'}>
+      <SolvesPageHeader handleSearch={handleSearch} />
+      <SolvesGrid solves={displaySolves} />
+    </div>
+  )
+}
