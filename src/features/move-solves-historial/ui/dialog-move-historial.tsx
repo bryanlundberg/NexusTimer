55e6b2e755c97165a4d61e5948162d98f21f0f09
Bryan Lundberg <bryanlundberg@outlook.com>
2025-11-22 05:14:34 -0600

    refactor: migrate solves modal to overlay store

---
+++ src/features/move-solves-historial/ui/dialog-move-historial.tsx
@@ -0,0 +1,52 @@
+import { Button } from '@/components/ui/button'
+import { DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle } from '@/components/ui/dialog'
+import { useTimerStore } from '@/store/timerStore'
+import { useTranslations } from 'next-intl'
+import { toast } from 'sonner'
+import { useNXData } from '@/hooks/useNXData'
+import { useOverlayStore } from '@/shared/model/overlay-store/useOverlayStore'
+
+export default function DialogMoveHistorial() {
+  const overlayStore = useOverlayStore()
+  const { getAllCubes, getCubeById, finishSession } = useNXData()
+  const t = useTranslations('Index')
+  const selectedCube = useTimerStore((state) => state.selectedCube)
+  const setCubes = useTimerStore((state) => state.setCubes)
+  const setSelectedCube = useTimerStore((state) => state.setSelectedCube)
+
+  const handleMoveSessionToHistorial = async () => {
+    if (selectedCube) {
+      await finishSession(selectedCube)
+      const cubesDB = await getAllCubes()
+      setCubes(cubesDB)
+      const currentCube = await getCubeById(selectedCube.id)
+      setSelectedCube(currentCube)
+      return
+    }
+
+    toast(t('SolvesPage.toast.unable-action'), {
+      description: t('SolvesPage.toast.warning-select-cube')
+    })
+  }
+
+  return (
+    <>
+      <DialogContent className="max-w-96 rounded-md">
+        <DialogHeader>
+          <DialogTitle>{t('SolvesPage.dialogs.move-to-history')}</DialogTitle>
+          <DialogDescription>{t('SolvesPage.dialogs.move-to-history-para')}</DialogDescription>
+          <DialogFooter>
+            <div className="flex justify-end gap-1 mt-5">
+              <Button variant={'ghost'} onClick={overlayStore.close}>
+                {t('Inputs.cancel')}
+              </Button>
+              <Button variant={'ghost'} onClick={handleMoveSessionToHistorial}>
+                {t('Inputs.move')}
+              </Button>
+            </div>
+          </DialogFooter>
+        </DialogHeader>
+      </DialogContent>
+    </>
+  )
+}
--- src/features/navigation/ui/button-move-solves.tsx
+++ src/features/navigation/ui/button-move-solves.tsx
@@ -1,23 +1,28 @@
 import { Button } from '@/components/ui/button'
 import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from '@/components/ui/tooltip'
-import { useSolveFiltersStore } from '@/store/SolvesFilters'
 import { EnterIcon } from '@radix-ui/react-icons'
 import { useTranslations } from 'next-intl'
+import { useOverlayStore } from '@/shared/model/overlay-store/useOverlayStore'
+import DialogMoveHistorial from '@/features/move-solves-historial/ui/dialog-move-historial'
 
 export default function ButtonMoveSolves() {
-  const { handleChangeIsOpenMoveSolvesDialog } = useSolveFiltersStore()
   const t = useTranslations('Index')
+  const { open } = useOverlayStore()
+
+  const handleMoveSolvesToHistory = () => {
+    open({
+      id: 'move-solves-historial',
+      component: <DialogMoveHistorial />,
+      metadata: {}
+    })
+  }
+
   return (
     <>
       <TooltipProvider delayDuration={100}>
         <Tooltip>
           <TooltipTrigger asChild>
-            <Button
-              size={'icon'}
-              variant={'ghost'}
-              className={'size-7 m-auto'}
-              onClick={() => handleChangeIsOpenMoveSolvesDialog()}
-            >
+            <Button size={'icon'} variant={'ghost'} className={'size-7 m-auto'} onClick={handleMoveSolvesToHistory}>
               <EnterIcon />
             </Button>
           </TooltipTrigger>
