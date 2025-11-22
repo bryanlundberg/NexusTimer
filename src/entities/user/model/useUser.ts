1bbdb011d64eb23e54cc66ae37af953c212038d2
Bryan Lundberg <bryanlundberg@outlook.com>
2025-11-22 16:35:43 -0600

    Refactor: handle undefined userId in useUser hook

--- src/entities/user/model/useUser.ts
+++ src/entities/user/model/useUser.ts
@@ -2,7 +2,7 @@ import useSWR from 'swr'
 import { fetcher } from '@/shared/lib/fetcher'
 
 export const useUser = (userId: string) => {
-  const { data, error, isLoading, mutate } = useSWR(`/api/v1/users/${userId}`, fetcher)
+  const { data, error, isLoading, mutate } = useSWR(`/api/v1/users/${userId ? userId : ''}`, fetcher)
 
   return {
     data,
