5a468a50ecc346278f4a7478d9f1bd6650ec1df6
Bryan Lundberg <bryanlundberg@outlook.com>
2025-11-22 15:41:51 -0600

    Refactor: update PeopleDetailsPage with new UI and logic

--- src/app/(community)/people/[userId]/page.tsx
+++ src/app/(community)/people/[userId]/page.tsx
@@ -1,1 +1,32 @@
-export { default } from '@/pages/people-details/ui/PeopleDetailsPage'
+'use client'
+import { useParams } from 'next/navigation'
+import { ScrollArea } from '@/components/ui/scroll-area'
+import PeopleSkeleton from '@/shared/ui/skeletons/people-skeleton'
+import { useBackup } from '@/entities/backup/model/useBackup'
+import { useUser } from '@/entities/user/model/useUser'
+import { filterCubes } from '@/entities/cube/lib/filterCubes'
+import FadeIn from '@/shared/ui/fade-in/fade-in'
+import { UserHeader } from '@/widgets/people/ui/UserHeader'
+import { PeopleTabs } from '@/widgets/people/ui/PeopleTabs'
+
+export default function PeopleDetailsPage() {
+  const { userId } = useParams<{ userId: string }>() ?? { userId: '' }
+
+  const { data: user, isLoading: isLoadingUser } = useUser(userId)
+  const { backup, isLoading: isLoadingBackup } = useBackup(user?.backup?.url)
+
+  if (isLoadingUser || isLoadingBackup) {
+    return <PeopleSkeleton />
+  }
+
+  const cubes = filterCubes(backup)
+
+  return (
+    <ScrollArea className={'max-h-dvh overflow-auto'}>
+      <FadeIn>
+        <UserHeader user={user} />
+        <PeopleTabs user={user} cubes={cubes} />
+      </FadeIn>
+    </ScrollArea>
+  )
+}
