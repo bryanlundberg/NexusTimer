006a1fac5251d8cf5cb1027e5a8700fe78a0c1fb
Bryan Lundberg <bryanlundberg@outlook.com>
2025-11-26 05:24:03 -0600

    Refactor: update create-collection spec to use new fixture

--- e2e/create-new-collection.spec.ts
+++ e2e/create-new-collection.spec.ts
@@ -1,35 +1,9 @@
-import { test, expect } from '@playwright/test'
+import { test } from './fixtures/create-cube-fixture'
+import { expect } from '@playwright/test'
 
-test('should create a new cube collection', async ({ page }) => {
-  await page.goto('http://localhost:3000/cubes')
-  await expect(page.getByRole('heading', { name: 'No cubes for display.' })).toBeVisible()
-  await page.getByTestId('empty-cubes-container').getByRole('button', { name: 'New collection' }).click()
-  await page.getByTestId('drawer-input-name').click()
-  await page.getByTestId('drawer-input-name').press('CapsLock')
-  await page.getByTestId('drawer-input-name').fill('T')
-  await page.getByTestId('drawer-input-name').press('CapsLock')
-  await page.getByTestId('drawer-input-name').fill('Test cube')
-  await expect(page.getByTestId('checkbox-category-2x2')).toBeVisible()
-  await expect(page.getByTestId('checkbox-category-3x3')).toBeVisible()
-  await expect(page.getByTestId('checkbox-category-3x3 OH')).toBeVisible()
-  await expect(page.getByTestId('checkbox-category-4x4')).toBeVisible()
-  await expect(page.getByTestId('checkbox-category-5x5')).toBeVisible()
-  await expect(page.getByTestId('checkbox-category-6x6')).toBeVisible()
-  await expect(page.getByTestId('checkbox-category-7x7')).toBeVisible()
-  await expect(page.getByTestId('checkbox-category-SQ1')).toBeVisible()
-  await expect(page.getByTestId('checkbox-category-Skewb')).toBeVisible()
-  await expect(page.getByTestId('checkbox-category-Pyraminx')).toBeVisible()
-  await expect(page.getByTestId('checkbox-category-Megaminx')).toBeVisible()
-  await expect(page.getByTestId('checkbox-category-Clock')).toBeVisible()
-  await page.getByTestId('checkbox-category-4x4').click()
-  await expect(page.getByText('Current selection: 4x4')).toBeVisible()
-  await page.getByTestId('checkbox-category-SQ1').click()
-  await expect(page.getByText('Current selection: SQ1')).toBeVisible()
-  await page.getByTestId('checkbox-category-Clock').click()
-  await expect(page.getByText('Current selection: Clock')).toBeVisible()
-  await page.getByTestId('drawer-accept-button').click()
+test('should create a new cube collection and validate IndexedDB data', async ({ cubeCreated }) => {
   await expect(
-    page
+    cubeCreated
       .locator('div')
       .filter({ hasText: /^Test cube$/ })
       .first()
