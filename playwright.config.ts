f16d0f34cc433a5bc30b334dd333c7f08b833d71
Bryan Lundberg <bryanlundberg@outlook.com>
2025-11-29 11:08:02 -0600

    Refactor: update Playwright config with webServer setup

--- playwright.config.ts
+++ playwright.config.ts
@@ -72,12 +72,12 @@ export default defineConfig({
     //   name: 'Google Chrome',
     //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
     // },
-  ]
+  ],
 
   /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://127.0.0.1:3000',
-  //   reuseExistingServer: !process.env.CI,
-  // },
+  webServer: {
+    command: 'npm run dev',
+    url: 'http://localhost:3000',
+    reuseExistingServer: !process.env.CI
+  }
 })
