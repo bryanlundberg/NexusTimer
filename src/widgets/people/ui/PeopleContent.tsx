57b80faf736f462a2ea702780d03010bfde23d8a
Bryan Lundberg <bryanlundberg@outlook.com>
2025-12-05 22:19:51 -0600

    refactor: remove unused usepeopletab hook

--- src/widgets/people/ui/PeopleContent.tsx
+++ src/widgets/people/ui/PeopleContent.tsx
@@ -1,6 +1,5 @@
 import { TabsContent } from '@/components/ui/tabs'
 import { PeopleTabs as PTabs } from '@/widgets/people/model/types'
-import { usePeopleTab } from '@/features/people-tab/model/usePeopleTab'
 import OverviewTabContent from '@/widgets/people/ui/overview-tab-content'
 import CubesTabContent from '@/widgets/people/ui/cubes-tab-content'
 import LastActivityTabContent from '@/widgets/people/ui/last-activity-tab-content'
@@ -10,8 +9,6 @@ interface Props {
 }
 
 export function PeopleContent({ cubes }: Props) {
-  const { value } = usePeopleTab()
-
   return (
     <div className="flex flex-col w-full">
       <div className="flex flex-col md:flex-row p-5 relative">
