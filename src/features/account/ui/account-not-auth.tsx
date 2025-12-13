1280c739340525a93166c4d3de52f055913383e1
Bryan Lundberg <bryanlundberg@outlook.com>
2025-12-13 05:53:39 -0600

    feat(account): add discord button and update header link

--- src/features/account/ui/account-not-auth.tsx
+++ src/features/account/ui/account-not-auth.tsx
@@ -3,16 +3,18 @@
 import { useTranslations } from 'next-intl'
 import GoogleButton from '@/features/authentication/ui/GoogleButton'
 import AccountHeader from '@/features/account/ui/account-header'
+import DiscordButton from '@/features/authentication/ui/DiscordButton';
 
 export default function AccountNotAuth() {
   const t = useTranslations('Index')
   return (
     <>
       <div className="max-w-md mx-auto bg-background/90 backdrop-blur-lg pt-2">
-        <AccountHeader back="/settings" label={t('SettingsPage.account')} />
+        <AccountHeader back="/app" label={t('SettingsPage.account')} />
 
         <div className="space-y-2">
           <GoogleButton />
+          <DiscordButton/>
           <p className="text-center text-xs">{t('SettingsPage.not-authenticated')}</p>
         </div>
       </div>
