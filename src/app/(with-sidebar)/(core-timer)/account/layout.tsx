628523bd488a74be13d1751f4b51772f9e6b038b
Bryan Lundberg <bryanlundberg@outlook.com>
2025-12-13 05:53:51 -0600

    feat(account): add auth check and scroll area

--- src/app/(with-sidebar)/(core-timer)/account/layout.tsx
+++ src/app/(with-sidebar)/(core-timer)/account/layout.tsx
@@ -1,13 +1,20 @@
+import { ScrollArea } from '@/components/ui/scroll-area';
+import { auth } from '@/shared/config/auth/auth';
+import AccountNotAuth from '@/features/account/ui/account-not-auth';
+
 export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const session = await auth()
+  if (!session?.user.id) return <AccountNotAuth/>
+
   return (
-    <div className="overflow-y-auto bg-background min-h-dvh">
+    <ScrollArea className={'max-h-dvh overflow-auto'}>
       <div className="max-w-md mx-auto bg-background/90 backdrop-blur-lg pt-2">
         <div className="p-2">{children}</div>
       </div>
-    </div>
+    </ScrollArea>
   )
 }
