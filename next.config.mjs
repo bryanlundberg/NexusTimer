021f552d58e36ff973895f23002aed3067382ee7
Bryan Lundberg <bryanlundberg@outlook.com>
2025-12-14 07:10:00 -0600

    fix(next): update config casing and scripts flags

--- next.config.mjs
+++ next.config.mjs
@@ -6,7 +6,7 @@ const withNextIntl = createNextIntlPlugin('./src/shared/config/i18n/request.ts')
 const withSerwist = withSerwistInit({
   swSrc: "/src/app/sw.ts",
   swDest: "public/sw.js",
-  disable: process.env.NODE_ENV !== "PRODUCTION"
+  disable: process.env.NODE_ENV !== "production"
 });
 
 /** @type {import('next').NextConfig} */
