ffc7529281e85471490dbb55c82a3e981193eb7e
Bryan Lundberg <bryanlundberg@outlook.com>
2025-12-15 06:40:27 -0600

    fix(ui): remove redundant spaces in jsx attributes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,22 +3,12 @@ import Link from 'next/link'
 import Dither from '@/components/ui/shadcn-io/dither'
 import { signIn } from 'next-auth/react'
 import { Button } from '@/components/ui/button'
-import {
-  AudioWaveform,
-  BarChart3,
-  Check,
-  DatabaseZap,
-  Globe,
-  StarIcon,
-  Timer,
-  TriangleAlert,
-  Users,
-  XIcon
-} from 'lucide-react'
+import { AudioWaveform, BarChart3, DatabaseZap, Globe, StarIcon, Timer, Users } from 'lucide-react'
 import Image from 'next/image'
 import { TextGenerateEffect } from '@/components/ui/shadcn-io/text-generate-effect'
 import { ScrollArea } from '@/components/ui/scroll-area'
 import { cn } from '@/shared/lib/utils'
+import RatedIcon from '@/shared/ui/rate-icon/RateIcon'
 
 export default function Page() {
   return (
@@ -762,19 +752,6 @@ function FeatureTable() {
         ))}
       </div>
     </section>
-  )
-}
-
-function RatedIcon({ type }: { type: 'check' | 'cross' | 'partial' }) {
-  const backgroundClass =
-    type === 'partial' ? 'bg-yellow-400/10' : type === 'check' ? 'bg-green-400/10' : 'bg-red-400/10'
-
-  return (
-    <div className={`size-10 rounded-full flex items-center justify-center mx-auto ${backgroundClass}`}>
-      {type === 'check' && <Check className={'text-green-400 size-6'} />}
-      {type === 'cross' && <XIcon className={'text-red-400 size-6'} />}
-      {type === 'partial' && <TriangleAlert className={'size-6 text-yellow-400'} />}
-    </div>
   )
 }
 
