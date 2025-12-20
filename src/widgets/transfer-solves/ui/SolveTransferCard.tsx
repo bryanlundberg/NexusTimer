3c09ae95125fc5b4828effca7d591e212fed3498
Bryan Lundberg <bryanlundberg@outlook.com>
2025-12-20 07:07:34 -0600

    refactor(ui): replace card with button in solve transfer card

--- src/widgets/transfer-solves/ui/SolveTransferCard.tsx
+++ src/widgets/transfer-solves/ui/SolveTransferCard.tsx
@@ -1,7 +1,8 @@
 import { Card } from '@/components/ui/card'
 import formatTime from '@/shared/lib/formatTime'
 import { Solve } from '@/entities/solve/model/types'
-import { format } from 'date-fns';
+import { format } from 'date-fns'
+import { Button } from '@/components/ui/button'
 
 interface SolveTransferCardProps {
   solve: Solve
@@ -11,11 +12,12 @@ interface SolveTransferCardProps {
 
 export default function SolveTransferCard({ solve, isSelected, onToggle }: SolveTransferCardProps) {
   return (
-    <Card
+    <Button
+      variant={'outline'}
       id={solve.id}
       data-testid={`solve-card-${solve.id}`}
       onClick={onToggle}
-      className={`relative grow flex items-center justify-center w-auto font-medium text-center transition duration-200 rounded-md cursor-pointer h-full bg-secondary text-secondary-foreground hover:opacity-70 ${isSelected ? 'ring-3 ring-primary' : ''}`}
+      className={`relative grow flex items-center justify-center w-auto font-medium text-center transition duration-200 rounded-md cursor-pointer h-full bg-secondary text-secondary-foreground ${isSelected ? 'ring-3 ring-primary' : ''}`}
     >
       <div className="tracking-wider pt-2">
         <span className="text-md">{formatTime(solve.time).split('.')[0]}</span>
@@ -26,6 +28,6 @@ export default function SolveTransferCard({ solve, isSelected, onToggle }: Solve
         {solve.dnf && <span className="text-red-500 font-bold ml-1">DNF</span>}
         {solve.plus2 && <span className="text-yellow-500 font-bold ml-1">+2</span>}
       </div>
-    </Card>
+    </Button>
   )
 }
