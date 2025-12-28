14f6c0475554b7c8d7dc63b7d6f336b8050147d2
Bryan Lundberg <bryanlundberg@outlook.com>
2025-12-28 14:35:52 -0600

    feat(ui): add download link for nexus connect app

--- src/features/nexus-connect/ui/ConnectQR.tsx
+++ src/features/nexus-connect/ui/ConnectQR.tsx
@@ -1,6 +1,8 @@
 import QRCode from 'react-qr-code'
 import { DialogContent, DialogDescription, DialogHeader, DialogTitle } from '@/components/ui/dialog'
 import { useNexusConnectStore } from '../model/useNexusConnectStore'
+import Link from 'next/link'
+import Image from 'next/image'
 
 export default function ConnectQR() {
   const nexusConnectId = useNexusConnectStore((state) => state.nexusConnectId)
@@ -13,7 +15,9 @@ export default function ConnectQR() {
     <DialogContent className="sm:max-w-xl">
       <DialogHeader>
         <DialogTitle>Connect Your Device</DialogTitle>
-        <DialogDescription>Scan the QR code below with your Nexus Connect app to pair your device!</DialogDescription>
+        <DialogDescription className={'mb-3'}>
+          Scan the QR code below with your Nexus Connect app to pair your device!
+        </DialogDescription>
 
         <div
           style={{
@@ -33,6 +37,22 @@ export default function ConnectQR() {
           />
         </div>
       </DialogHeader>
+      <DialogDescription>
+        <Link
+          href={'https://c707vqzanp.ufs.sh/f/7SKMBzVlOB2HwGJzdKMHr2JoUKbvhputIcT4jNE5ldLg96OB'}
+          target={'_blank'}
+          rel={'noreferrer noopener'}
+        >
+          <Image
+            src={'/utils/android-apk.webp'}
+            alt={'Download Nexus Connect App'}
+            width={200}
+            height={60}
+            className={'mx-auto'}
+            unoptimized
+          />
+        </Link>
+      </DialogDescription>
     </DialogContent>
   )
 }
