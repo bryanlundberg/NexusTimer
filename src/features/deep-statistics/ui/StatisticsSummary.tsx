1c2469eb60f6f6e372fcc8d6cf995db05a52780a
Bryan Lundberg <bryanlundberg@outlook.com>
2026-01-07 18:38:59 -0600

    feat(statistics): add localization support

--- src/features/deep-statistics/ui/StatisticsSummary.tsx
+++ src/features/deep-statistics/ui/StatisticsSummary.tsx
@@ -3,12 +3,14 @@ import { Card } from '@/components/ui/card'
 import { GaugeIcon, PercentIcon, TimerIcon, TrophyIcon } from 'lucide-react'
 import formatTime from '@/shared/lib/formatTime'
 import { DeepStatistics } from '@/shared/types/statistics'
+import { useTranslations } from 'next-intl'
 
 interface StatsSummaryProps {
   statistics: DeepStatistics
 }
 
 export default function StatisticsSummary({ statistics }: StatsSummaryProps) {
+  const t = useTranslations('Index')
   const bestStr = statistics.best.cubeAll > 0 ? formatTime(statistics.best.cubeAll) : '--'
   const overallAvgStr = statistics.average.cubeAll === 0 ? '--' : formatTime(statistics.average.cubeAll)
   const ao5CurrentStr = statistics.stats.cubeSession.ao5 === 0 ? '--' : formatTime(statistics.stats.cubeSession.ao5)
@@ -16,10 +18,18 @@ export default function StatisticsSummary({ statistics }: StatsSummaryProps) {
 
   return (
     <div className="grid grid-cols-2 lg:grid-cols-4 gap-3">
-      <StatItem icon={<TrophyIcon className={'size-5'} />} label="Best Time" value={bestStr} />
-      <StatItem icon={<GaugeIcon className={'size-5'} />} label="Overall Average" value={overallAvgStr} />
-      <StatItem icon={<TimerIcon className={'size-5'} />} label="Ao5 (Current)" value={ao5CurrentStr} />
-      <StatItem icon={<PercentIcon className={'size-5'} />} label="Success Rate" value={successRateStr} />
+      <StatItem icon={<TrophyIcon className={'size-5'} />} label={t('StatsPage.best-time')} value={bestStr} />
+      <StatItem
+        icon={<GaugeIcon className={'size-5'} />}
+        label={t('StatsPage.overall-average')}
+        value={overallAvgStr}
+      />
+      <StatItem icon={<TimerIcon className={'size-5'} />} label={t('StatsPage.ao5-current')} value={ao5CurrentStr} />
+      <StatItem
+        icon={<PercentIcon className={'size-5'} />}
+        label={t('StatsPage.success-rate')}
+        value={successRateStr}
+      />
     </div>
   )
 }
