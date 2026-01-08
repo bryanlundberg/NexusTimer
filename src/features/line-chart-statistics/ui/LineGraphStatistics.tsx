3d612c386764ac7e1c1fda7fe2fac7d71b4675e3
Bryan Lundberg <bryanlundberg@outlook.com>
2026-01-07 18:38:40 -0600

    feat(line-graph): add translations for stats labels

--- src/features/line-chart-statistics/ui/LineGraphStatistics.tsx
+++ src/features/line-chart-statistics/ui/LineGraphStatistics.tsx
@@ -1,8 +1,10 @@
 import { Checkbox } from '@/components/ui/checkbox'
 import useLineGraphStatistics from '@/features/line-chart-statistics/model/useLineGraphStatistics'
 import { Solve } from '@/entities/solve/model/types'
+import { useTranslations } from 'next-intl'
 
 export default function LineGraphStatistics({ solves }: { solves: Solve[] }) {
+  const t = useTranslations('Index')
   const {
     showWorstTime,
     showAverageTime,
@@ -18,15 +20,6 @@ export default function LineGraphStatistics({ solves }: { solves: Solve[] }) {
 
   return (
     <div className="relative w-full">
-      <div className="h-96">
-        <div ref={chartContainerRef} className="w-full h-full"></div>
-        <div
-          ref={tooltipRef}
-          className="absolute hidden p-2 text-sm bg-black bg-opacity-80 rounded shadow-lg z-10 text-white"
-          style={{ pointerEvents: 'none' }}
-        ></div>
-      </div>
-
       <div className="mb-4 flex flex-wrap gap-4">
         <div className="flex items-center space-x-2">
           <Checkbox id="best-time" checked={showBestTime} onCheckedChange={(e: boolean) => setShowBestTime(e)} />
@@ -34,7 +27,7 @@ export default function LineGraphStatistics({ solves }: { solves: Solve[] }) {
             htmlFor="best-time"
             className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
           >
-            Best Time
+            {t('StatsPage.best-time')}
           </label>
         </div>
 
@@ -44,7 +37,7 @@ export default function LineGraphStatistics({ solves }: { solves: Solve[] }) {
             htmlFor="worst-time"
             className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
           >
-            Worst Time
+            {t('StatsPage.worst-time')}
           </label>
         </div>
 
@@ -58,7 +51,7 @@ export default function LineGraphStatistics({ solves }: { solves: Solve[] }) {
             htmlFor="average-time"
             className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
           >
-            Average
+            {t('StatsPage.average')}
           </label>
         </div>
 
@@ -72,9 +65,18 @@ export default function LineGraphStatistics({ solves }: { solves: Solve[] }) {
             htmlFor="standard-deviation"
             className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
           >
-            Standard Deviation
+            {t('StatsPage.standard-deviation')}
           </label>
         </div>
+      </div>
+
+      <div className="h-96">
+        <div ref={chartContainerRef} className="w-full h-full"></div>
+        <div
+          ref={tooltipRef}
+          className="absolute hidden p-2 text-sm bg-black bg-opacity-80 rounded shadow-lg z-10 text-white"
+          style={{ pointerEvents: 'none' }}
+        ></div>
       </div>
     </div>
   )
