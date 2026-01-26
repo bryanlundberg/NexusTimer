633b8637a76e04d982dfda633a0818cc8c8d6d19
Bryan Lundberg <bryanlundberg@outlook.com>
2026-01-26 07:06:36 -0600

    refactor(ui): replace fadein with div in people skeleton

--- src/shared/ui/skeletons/people-skeleton.tsx
+++ src/shared/ui/skeletons/people-skeleton.tsx
@@ -3,7 +3,7 @@ import { Skeleton } from '@/components/ui/skeleton'
 
 export default function PeopleSkeleton() {
   return (
-    <FadeIn className="flex flex-col grow overflow-auto">
+    <div className="flex flex-col grow overflow-auto">
       <div className="max-w-7xl mx-auto p-4 flex flex-col w-full min-h-full">
         <div className={'flex flex-row gap-2'}>
           <Skeleton className={'w-20 h-8'} />
@@ -27,6 +27,6 @@ export default function PeopleSkeleton() {
           </div>
         </div>
       </div>
-    </FadeIn>
+    </div>
   )
 }
