2924d9cbbfd1252c0b4cce2c5fa9d418460c263e
Bryan Lundberg <bryanlundberg@outlook.com>
2026-01-26 07:06:44 -0600

    refactor(ui): remove unused key prop from fade-in

--- src/shared/ui/fade-in/fade-in.tsx
+++ src/shared/ui/fade-in/fade-in.tsx
@@ -12,7 +12,6 @@ export default function FadeIn({
 }) {
   return (
     <motion.div
-      key="content"
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
       exit={{ opacity: 0 }}
