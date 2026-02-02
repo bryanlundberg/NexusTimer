64f75c3f4a25d5cbf9a4c47b8184ab0259824cff
Bryan Lundberg <bryanlundberg@outlook.com>
2026-02-02 11:05:39 -0600

    refactor(ui): replace AccountHeader with CoreHeader in AccountSavePage

--- src/app/(with-sidebar)/(core-timer)/account/save/page.tsx
+++ src/app/(with-sidebar)/(core-timer)/account/save/page.tsx
@@ -3,12 +3,11 @@ import { useTranslations } from 'next-intl'
 import { useRouter } from 'next/navigation'
 import { useEffect } from 'react'
 import { toast } from 'sonner'
-import { Card } from '@/components/ui/card'
 import Link from 'next/link'
 import { Button } from '@/components/ui/button'
 import { Loader2 } from 'lucide-react'
-import AccountHeader from '@/features/account/ui/account-header'
 import { useSyncBackup } from '@/shared/model/backup/useSyncBackup'
+import CoreHeader from '@/shared/ui/core-header/ui/CoreHeader'
 
 export default function AccountSavePage() {
   const t = useTranslations('Index')
@@ -24,8 +23,14 @@ export default function AccountSavePage() {
 
   return (
     <>
-      <AccountHeader back="/app" label={t('SettingsPage.save-data-title')} />
-      <Card className="p-3 bg-secondary/10" aria-busy={isUploading}>
+      <CoreHeader
+        breadcrumbPath={'/account'}
+        breadcrumb={t('SettingsPage.account')}
+        secondaryBreadcrumb={t('SettingsPage.save-data-title')}
+        secondaryBreadcrumbPath={'/account/save'}
+      />
+
+      <div className="p-3 max-w-2xl mx-auto mt-5" aria-busy={isUploading}>
         <p>{t('SettingsPage.save-data-description')}</p>
         <p className="text-yellow-600">{t('SettingsPage.save-data-warning')}</p>
         <div className="flex gap-2 w-full justify-between mt-5 flex-col-reverse sm:flex-row">
@@ -54,7 +59,7 @@ export default function AccountSavePage() {
             )}
           </Button>
         </div>
-      </Card>
+      </div>
     </>
   )
 }
