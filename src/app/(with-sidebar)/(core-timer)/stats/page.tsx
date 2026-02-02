0f7d6fd05e76a3a54b7b782061752c14e95fddf8
Bryan Lundberg <bryanlundberg@outlook.com>
2026-02-01 21:03:55 -0600

    feat(ui): integrate core header in stats page

--- src/app/(with-sidebar)/(core-timer)/stats/page.tsx
+++ src/app/(with-sidebar)/(core-timer)/stats/page.tsx
@@ -6,15 +6,19 @@ import StatisticsSummary from '@/features/deep-statistics/ui/StatisticsSummary'
 import StatisticsViewSwitcher from '@/widgets/statistics-view/ui/StatisticsViewSwitcher'
 import StatisticsChart from '@/features/deep-statistics/ui/StatisticsChart'
 import useDeepStatistics from '@/features/deep-statistics/model/useDeepStatistics'
+import CoreHeader from '@/shared/ui/core-header/ui/CoreHeader'
+import { useTranslations } from 'next-intl'
 
 export default function StatsPage() {
   const { stats, isLoading } = useDeepStatistics()
+  const t = useTranslations('Index.StatsPage')
   return (
     <div className={'max-h-dvh overflow-auto'}>
       <FadeIn>
-        <div className="px-2 pt-2 flex flex-col w-full min-h-full">
+        <CoreHeader breadcrumbPath={'/stats'} breadcrumb={t('title')} />
+        <div className="px-2 flex flex-col w-full min-h-full">
           <StatsPageHeader />
-          <div className="flex flex-col gap-3 grow relative">
+          <div className="flex flex-col gap-2 grow relative">
             <ProcessingOverlay isProcessing={isLoading} />
             <StatisticsSummary statistics={stats} />
             <StatisticsViewSwitcher statistics={stats} />
