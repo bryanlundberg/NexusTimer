ef37b03c41a2b7ca5f75eb213afbad058836a62b
Bryan Lundberg <bryanlundberg@outlook.com>
2026-02-01 20:41:30 -0600

    feat(ui): add core header component with breadcrumb

---
+++ src/shared/ui/core-header/ui/CoreHeader.tsx
@@ -0,0 +1,42 @@
+import { SidebarTrigger } from '@/components/ui/sidebar'
+import { Separator } from '@/components/ui/separator'
+import { Breadcrumb, BreadcrumbItem, BreadcrumbLink, BreadcrumbList } from '@/components/ui/breadcrumb'
+import Link from 'next/link'
+import { NavUser } from '@/widgets/sidebar/ui/nav-user'
+import * as React from 'react'
+import { useSession } from 'next-auth/react'
+
+interface CoreHeaderProps {
+  breadcrumbPath: string
+  breadcrumb: string
+}
+
+export default function CoreHeader({ breadcrumb, breadcrumbPath }: CoreHeaderProps) {
+  const { data: session } = useSession()
+
+  return (
+    <div className="w-full border-b p-2 flex justify-between items-center mb-2 backdrop-blur-lg">
+      <div className="flex items-center gap-2 w-fit">
+        <SidebarTrigger className="-ml-1" />
+        <Separator orientation="vertical" className="mr-2 data-[orientation=vertical]:h-4" />
+        <Breadcrumb>
+          <BreadcrumbList>
+            <BreadcrumbItem>
+              <BreadcrumbLink asChild>
+                <Link href={breadcrumbPath}>{breadcrumb}</Link>
+              </BreadcrumbLink>
+            </BreadcrumbItem>
+          </BreadcrumbList>
+        </Breadcrumb>
+      </div>
+
+      <NavUser
+        user={{
+          name: session?.user?.name || 'Guest',
+          email: session?.user?.email || ',',
+          avatar: session?.user?.image || ''
+        }}
+      />
+    </div>
+  )
+}
