912b8fd364cd356a6029f78a5410331bb6e9a6a2
Bryan Lundberg <bryanlundberg@outlook.com>
2026-02-02 11:05:59 -0600

    refactor(ui): replace AccountHeader with CoreHeader in AccountPage

--- src/app/(with-sidebar)/(core-timer)/account/page.tsx
+++ src/app/(with-sidebar)/(core-timer)/account/page.tsx
@@ -4,11 +4,11 @@ import { useTranslations } from 'next-intl'
 import Link from 'next/link'
 import { Button } from '@/components/ui/button'
 import AccountLastBackup from '@/features/account-last-backup/ui/AccountLastBackup'
-import AccountHeader from '@/features/account/ui/account-header'
 import { AvatarUploader } from '@/features/update-user-avatar/ui/AvatarUploader'
 import AccountInfoForm from '@/features/account-form/ui/AccountInfoForm'
 import { useUser } from '@/entities/user/model/useUser'
 import { SquareArrowOutUpRight } from 'lucide-react'
+import CoreHeader from '@/shared/ui/core-header/ui/CoreHeader'
 
 export default function AccountPage() {
   const { data: session } = useSession()
@@ -17,10 +17,10 @@ export default function AccountPage() {
   const tAccount = useTranslations('Index.AccountPage')
 
   return (
-    <div className="flex flex-col gap-6 pb-10">
-      <AccountHeader back="/app" label={t('SettingsPage.account')} />
+    <div className="flex flex-col gap-6">
+      <CoreHeader breadcrumbPath={'/account'} breadcrumb={t('SettingsPage.account')} />
 
-      <div className="flex flex-col gap-3 justify-center">
+      <div className="flex flex-col gap-3 justify-center max-w-5xl mx-auto">
         <AvatarUploader />
         <div className={'h-2'} />
         <AccountLastBackup session={session!} />
