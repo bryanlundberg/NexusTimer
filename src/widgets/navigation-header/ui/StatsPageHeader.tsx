9e28cb514a67c20b6c89c6a090ac42643541bd03
Bryan Lundberg <bryanlundberg@outlook.com>
2026-02-01 21:04:27 -0600

    refactor(ui): simplify stats page header layout

--- src/widgets/navigation-header/ui/StatsPageHeader.tsx
+++ src/widgets/navigation-header/ui/StatsPageHeader.tsx
@@ -1,16 +1,11 @@
-import { SidebarTrigger } from '@/components/ui/sidebar'
 import DateRangeSelect from '@/features/date-range-select/ui/DateRangeSelect'
-import Navigation from '@/features/navigation/ui/navigation'
 import MainCubeSelector from '@/features/select-cube/ui/MainCubeSelector'
 
 export default function StatsPageHeader() {
   return (
-    <Navigation showMenu={false}>
-      <div className="flex justify-between items-center gap-2 w-full">
-        <SidebarTrigger />
-        <MainCubeSelector />
-        <DateRangeSelect />
-      </div>
-    </Navigation>
+    <div className="flex justify-between items-center gap-2 w-full mb-2">
+      <MainCubeSelector />
+      <DateRangeSelect />
+    </div>
   )
 }
