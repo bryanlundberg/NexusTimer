38f880dc920a4af0d97abac3768bd05a683d9885
Bryan Lundberg <bryanlundberg@outlook.com>
2026-02-02 06:52:55 -0600

    refactor(ui): integrate core header in user header

--- src/widgets/people/ui/UserHeader.tsx
+++ src/widgets/people/ui/UserHeader.tsx
@@ -1,41 +1,21 @@
-import { SidebarTrigger } from '@/components/ui/sidebar'
-import { Separator } from '@/components/ui/separator'
-import {
-  Breadcrumb,
-  BreadcrumbItem,
-  BreadcrumbLink,
-  BreadcrumbList,
-  BreadcrumbPage,
-  BreadcrumbSeparator
-} from '@/components/ui/breadcrumb'
-import Link from 'next/link'
 import { useTranslations } from 'next-intl'
+import CoreHeader from '@/shared/ui/core-header/ui/CoreHeader'
 
 interface Props {
-  user: { name: string }
+  user: {
+    _id: string
+    name: string
+  }
 }
 
 export function UserHeader({ user }: Props) {
   const t = useTranslations('Index.NavMain')
   return (
-    <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12">
-      <div className="flex items-center gap-2 px-4">
-        <SidebarTrigger className="-ml-1" />
-        <Separator orientation="vertical" className="mr-2 data-[orientation=vertical]:h-4" />
-        <Breadcrumb>
-          <BreadcrumbList>
-            <BreadcrumbItem>
-              <BreadcrumbLink asChild>
-                <Link href={'/people'}>{t('people')}</Link>
-              </BreadcrumbLink>
-            </BreadcrumbItem>
-            <BreadcrumbSeparator />
-            <BreadcrumbItem>
-              <BreadcrumbPage>{user.name}</BreadcrumbPage>
-            </BreadcrumbItem>
-          </BreadcrumbList>
-        </Breadcrumb>
-      </div>
-    </header>
+    <CoreHeader
+      breadcrumbPath={'/people'}
+      breadcrumb={t('people')}
+      secondaryBreadcrumbPath={`/people/${user._id}`}
+      secondaryBreadcrumb={user.name}
+    />
   )
 }
