31345ee0afeab9af92572bdc3a5afb4636f63b1f
Bryan Lundberg <bryanlundberg@outlook.com>
2026-02-01 21:04:19 -0600

    fix(ui): adjust padding and background in stats view

--- src/widgets/statistics-view/ui/StatisticsViewContainer.tsx
+++ src/widgets/statistics-view/ui/StatisticsViewContainer.tsx
@@ -1,6 +1,6 @@
 export default function StatisticsViewContainer({ children }: { children: React.ReactNode }) {
   return (
-    <div className="flex flex-col w-full px-3 pt-3 border rounded-md bg-card backdrop-blur-lg min-h-[300px]">
+    <div className="flex flex-col w-full px-3 pt-2 border rounded-md bg-background backdrop-blur-lg min-h-[300px]">
       {children}
     </div>
   )
