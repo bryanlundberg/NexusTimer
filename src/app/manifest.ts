3d8ab8d8a37db54446aaeece81b0098b270cdaac
Bryan Lundberg <bryanlundberg@outlook.com>
2026-02-07 06:24:20 -0600

    feat(manifest): add maskable icon to manifest

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -16,6 +16,12 @@ export default function manifest(): MetadataRoute.Manifest {
         src: '/icon512_rounded.png',
         sizes: '512x512',
         type: 'image/png'
+      },
+      {
+        src: '/icon512_maskable.png',
+        sizes: '512x512',
+        type: 'image/png',
+        purpose: 'maskable'
       }
     ],
     screenshots: [
