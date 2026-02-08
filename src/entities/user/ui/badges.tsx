75c4b049a86b37005b5ffe10db91eb49d9cb89f3
Bryan Lundberg <bryanlundberg@outlook.com>
2026-02-08 13:39:02 -0600

    feat(achievement): improve badges logic with conditions

--- src/entities/user/ui/badges.tsx
+++ src/entities/user/ui/badges.tsx
@@ -3,6 +3,7 @@ import { Cube } from '@/entities/cube/model/types'
 import { UserDocument } from '@/entities/user/model/user'
 import { Achievement } from '@/entities/achievement/model/types'
 import { CUBE_CATEGORIES } from '@/shared/const/cube-categories'
+import moment from 'moment'
 
 interface BadgesProps {
   cubes: Array<Cube>
@@ -14,39 +15,79 @@ export default function Badges({ cubes, user }: BadgesProps) {
 
   const ACHIEVEMENTS_CONFIG: Achievement[] = [
     {
+      id: 'public-sponsor',
+      title: 'Project Patron',
+      description: 'A sponsor helping keep the engine running.',
+      icon: 'icons8-favorite-50.png',
+      color: 'rgba(255,105,180,0.8)',
+      condition: (data) => false
+    },
+    {
       id: 'first-blood',
-      title: 'Alpha Tester',
-      description: 'Became a user in 2024',
-      icon: 'icons8-animated-50.png',
-      condition: (data) => data.cubes.length >= 1
+      title: 'Beta Tester',
+      description: 'Became a user during the first year after launch.',
+      icon: 'icons8-metal-music-50.png',
+      color: 'rgba(0,191,255,0.8)',
+      condition: (data) => {
+        const createdAt = moment(data.user.createdAt)
+        const launchDate = moment('2024-07-11')
+        return createdAt.isBefore(launchDate.add(1, 'year'))
+      }
     },
     {
       id: 'speed-demon',
       title: 'Speed Demon',
       description: 'Fast as lightning! You registered your first sub-10 solve.',
       icon: 'icons8-lightning-48.png',
-      condition: (data) => data.cubes.length >= 1
+      condition: (data) => {
+        return data.cubes.some(
+          (cube) =>
+            cube.category === '3x3' &&
+            (cube.solves.all.some((solve) => solve.time < 10000 && !solve.isDeleted && !solve.dnf) ||
+              cube.solves.session.some((solve) => solve.time < 10000 && !solve.isDeleted && !solve.dnf))
+        )
+      }
     },
     {
       id: 'over-9999-3x3',
       title: "It's over 9000!",
       description: 'You have completed over 9,999 solves on 3x3 cubes.',
       icon: 'icons8-mana-50.png',
-      condition: (data) => data.cubes.length >= 1
+      condition: (data) => {
+        const threeByThreeCubes = data.cubes.filter((cube) => cube.category === '3x3')
+
+        const someCubeHasOver9999Solves = (cube: Cube) => {
+          const totalSolves =
+            cube.solves.all.filter((solve) => !solve.isDeleted && !solve.dnf).length +
+            cube.solves.session.filter((solve) => !solve.isDeleted && !solve.dnf).length
+          return totalSolves >= 9999
+        }
+
+        return threeByThreeCubes.some(someCubeHasOver9999Solves)
+      }
     },
     {
       id: 'career-100k',
       title: 'Cube Legend',
       description: 'A journey of a thousand miles begins with a single solve... you already have 100,000.',
       icon: 'icons8-trophy-50.png',
-      condition: (data) => data.cubes.length >= 1
+      condition: (data) => {
+        const totalSolves = data.cubes.reduce((sum, cube) => {
+          const validAllSolves = cube.solves.all.filter((solve) => !solve.isDeleted && !solve.dnf).length
+          const validSessionSolves = cube.solves.session.filter((solve) => !solve.isDeleted && !solve.dnf).length
+          return sum + validAllSolves + validSessionSolves
+        }, 0)
+        return totalSolves >= 100000
+      }
     },
     {
       id: 'collector',
       title: 'Puzzle Collector',
       description: 'Own at least 25 cubes.',
       icon: 'icons8-money-box-50.png',
-      condition: (data) => data.cubes.length >= 5
+      condition: (data) => {
+        return data.cubes.length >= 25
+      }
     },
     {
       id: 'eventglot',
@@ -55,22 +96,18 @@ export default function Badges({ cubes, user }: BadgesProps) {
       icon: 'icons8-diversity-50.png',
       condition: (data) => {
         const categories = new Set(data.cubes.map((cube) => cube.category))
-        return categories.size >= CUBE_CATEGORIES.length
+
+        const hasAtLeastOneSolveInCategory = (category: string) => {
+          return data.cubes.some(
+            (cube) =>
+              cube.category === category &&
+              (cube.solves.all.some((solve) => !solve.isDeleted && !solve.dnf) ||
+                cube.solves.session.some((solve) => !solve.isDeleted && !solve.dnf))
+          )
+        }
+
+        return CUBE_CATEGORIES.every((category) => categories.has(category) && hasAtLeastOneSolveInCategory(category))
       }
-    },
-    {
-      id: 'early-adopter',
-      title: 'Nexus Pioneer',
-      description: 'You were here before it was cool.',
-      icon: 'icons8-virtual-reality-50.png',
-      condition: (data) => new Date(data.user.createdAt) < new Date('2025-01-01')
-    },
-    {
-      id: 'public-sponsor',
-      title: 'Project Patron',
-      description: 'A sponsor helping keep the engine running.',
-      icon: 'icons8-favorite-50.png',
-      condition: (data) => false
     },
     {
       id: 'bug-hunter',
@@ -84,21 +121,92 @@ export default function Badges({ cubes, user }: BadgesProps) {
       title: 'Marathonist',
       description: 'Performed more than 500 solves in a single day.',
       icon: 'icons8-finish-flag-50.png',
-      condition: (data) => false
+      condition: (data) => {
+        const solvesByDate: { [date: string]: number } = {}
+
+        data.cubes.forEach((cube) => {
+          cube.solves.all.forEach((solve) => {
+            if (!solve.isDeleted) {
+              const date = moment(solve.startTime).format('YYYY-MM-DD')
+              solvesByDate[date] = (solvesByDate[date] || 0) + 1
+            }
+          })
+          cube.solves.session.forEach((solve) => {
+            if (!solve.isDeleted && !solve.dnf) {
+              const date = moment(solve.startTime).format('YYYY-MM-DD')
+              solvesByDate[date] = (solvesByDate[date] || 0) + 1
+            }
+          })
+        })
+
+        return Object.values(solvesByDate).some((count) => count > 500)
+      }
     },
     {
       id: 'consistency-is-key',
       title: 'Consistency is Key',
       description: 'You have maintained a solve streak for 365 consecutive days.',
       icon: 'icons8-workflow-50.png',
-      condition: (data) => false
+      condition: (data) => {
+        const solveDates = new Set<string>()
+
+        data.cubes.forEach((cube) => {
+          cube.solves.all.forEach((solve) => {
+            if (!solve.isDeleted) {
+              const date = moment(solve.startTime).format('YYYY-MM-DD')
+              solveDates.add(date)
+            }
+          })
+          cube.solves.session.forEach((solve) => {
+            if (!solve.isDeleted) {
+              const date = moment(solve.startTime).format('YYYY-MM-DD')
+              solveDates.add(date)
+            }
+          })
+        })
+
+        const sortedDates = Array.from(solveDates).sort()
+        let currentStreak = 1
+
+        for (let i = 1; i < sortedDates.length; i++) {
+          const previousDate = moment(sortedDates[i - 1])
+          const currentDate = moment(sortedDates[i])
+          if (currentDate.diff(previousDate, 'days') === 1) {
+            currentStreak++
+            if (currentStreak >= 365) {
+              return true
+            }
+          } else {
+            currentStreak = 1
+          }
+        }
+
+        return false
+      }
     },
     {
       id: 'zen-master',
       title: 'Zen Master',
       description: 'You performed 100 solves without a single penalty (+2/DNF).',
       icon: 'icons8-wizard-50.png',
-      condition: (data) => false
+      condition: (data) => {
+        let consecutiveCleanSolves = 0
+
+        for (const cube of data.cubes) {
+          for (const solve of [...cube.solves.all, ...cube.solves.session]) {
+            if (!solve.isDeleted && !solve.dnf && !solve.plus2) {
+              consecutiveCleanSolves++
+              if (consecutiveCleanSolves >= 100) {
+                return true
+              }
+            } else {
+              consecutiveCleanSolves = 0
+            }
+          }
+        }
+
+        return false
+      }
     }
   ]
 
@@ -113,10 +221,9 @@ export default function Badges({ cubes, user }: BadgesProps) {
         </span>
       </div>
       <div className="grid grid-cols-5  gap-2">
-        {ACHIEVEMENTS_CONFIG.map((achievement) => {
-          const isUnlocked = false // achievement.condition(achievementsData);
-          return <AchievementItem key={achievement.id} achievement={achievement} />
-        })}
+        {ACHIEVEMENTS_CONFIG.filter((achievement) => achievement.condition(achievementsData)).map((achievement) => (
+          <AchievementItem key={achievement.id} achievement={achievement} />
+        ))}
       </div>
     </div>
   )
