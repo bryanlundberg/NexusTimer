407ac20d137a26d2ecc617f1d6865647ca5313e5
Bryan Lundberg <bryanlundberg@outlook.com>
2026-02-15 13:20:44 -0600

    refactor(ui): remove unused dropdownmenuitem import

--- src/widgets/sidebar/ui/nav-user.tsx
+++ src/widgets/sidebar/ui/nav-user.tsx
@@ -8,7 +8,6 @@ import {
   DropdownMenuContent,
   DropdownMenuGroup,
   DropdownMenuItem,
-  DropdownMenuLabel,
   DropdownMenuSeparator,
   DropdownMenuTrigger
 } from '@/components/ui/dropdown-menu'
