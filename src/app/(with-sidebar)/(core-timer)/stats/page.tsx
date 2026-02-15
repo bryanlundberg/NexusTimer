a9966ee55883434b36642a8cf20a31268616f1a7
Bryan Lundberg <bryanlundberg@outlook.com>
2026-02-15 08:04:17 -0600

    refactor(statistics): remove unused summary component

--- src/app/(with-sidebar)/(core-timer)/stats/page.tsx
+++ src/app/(with-sidebar)/(core-timer)/stats/page.tsx
@@ -1,7 +1,6 @@
 'use client'
 import FadeIn from '@/shared/ui/fade-in/fade-in'
 import StatsPageHeader from '@/widgets/navigation-header/ui/StatsPageHeader'
-import StatisticsSummary from '@/features/deep-statistics/ui/StatisticsSummary'
 import StatisticsViewSwitcher from '@/widgets/statistics-view/ui/StatisticsViewSwitcher'
 import StatisticsChart from '@/features/deep-statistics/ui/StatisticsChart'
 import useDeepStatistics from '@/features/deep-statistics/model/useDeepStatistics'
@@ -18,7 +17,6 @@ export default function StatsPage() {
         <div className="px-2 flex flex-col w-full min-h-full">
           <StatsPageHeader />
           <div className="flex flex-col gap-2 grow relative">
-            <StatisticsSummary statistics={stats} loadingProps={loadingProps} />
             <StatisticsViewSwitcher statistics={stats} loadingProps={loadingProps} />
             <StatisticsChart statistics={stats} loadingProps={loadingProps} />
           </div>
