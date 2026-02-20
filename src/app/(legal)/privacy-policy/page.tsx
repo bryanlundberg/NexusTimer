125474d4b4a4d2d19d0409f23e1e5ae43b143e33
Bryan Lundberg <bryanlundberg@outlook.com>
2026-02-20 06:17:33 -0600

    feat: integrate microsoft clarity for analytics

--- src/app/(legal)/privacy-policy/page.tsx
+++ src/app/(legal)/privacy-policy/page.tsx
@@ -1,4 +1,5 @@
 import { H2, Li, Para } from '@/shared/ui/typography/Typography'
+import Link from 'next/link'
 
 export default function PrivacyPolicyPage() {
   return (
@@ -98,6 +99,16 @@ export default function PrivacyPolicyPage() {
       <Li>
         Analytics: NexusTimer makes use of Third-Party service in order to measure how users utilize our services so
         that we may improve them, and to measure the effectiveness of our advertising campaigns.
+      </Li>
+
+      <Li>
+        We partner with Microsoft Clarity and Microsoft Advertising to capture how you use and interact with our website
+        through behavioral metrics, heatmaps, and session replay to improve and market our products/services. Website
+        usage data is captured using first and third-party cookies and other tracking technologies to determine the
+        popularity of products/services and online activity. Additionally, we use this information for site
+        optimization, fraud/security purposes, and advertising. For more information about how Microsoft collects and
+        uses your data, visit the{' '}
+        <Link href={'https://www.microsoft.com/es-mx/privacy/privacystatement'}>Microsoft Privacy Statement</Link>.
       </Li>
     </div>
   )
--- src/components/preload-app-provider.tsx
+++ src/components/preload-app-provider.tsx
@@ -1,21 +1,28 @@
-"use client"
-import { useAppInit } from '@/shared/model/app-init/useAppInit';
-import { useSettingsStore } from '@/shared/model/settings/useSettingsStore';
-import useWebsiteColors from '@/shared/model/useWebsiteColors';
-import { useEffect } from 'react';
+'use client'
+import { useAppInit } from '@/shared/model/app-init/useAppInit'
+import { useSettingsStore } from '@/shared/model/settings/useSettingsStore'
+import useWebsiteColors from '@/shared/model/useWebsiteColors'
+import clarity from '@microsoft/clarity'
+import { useEffect } from 'react'
+import { useSession } from 'next-auth/react'
 
 export default function PreloadAppProvider({ children }: { children: React.ReactNode }) {
   const { isAppReady } = useAppInit()
   const settings = useSettingsStore((store) => store.settings)
   const { applyColorTheme } = useWebsiteColors()
+  const { data: session } = useSession()
+
+  useEffect(() => {
+    const clarityId = process.env.NEXT_PUBLIC_CLARITY_PROJECT_ID
+    if (clarityId) {
+      clarity.init(clarityId)
+      session?.user?.id && clarity.identify(session?.user?.id)
+    }
+  }, [session?.user?.id])
 
   useEffect(() => {
     applyColorTheme(settings.preferences.colorTheme)
   }, [applyColorTheme, settings.preferences.colorTheme])
 
-  return (
-    <>
-      {isAppReady ? (children) : null}
-    </>
-  )
+  return <>{isAppReady ? children : null}</>
 }
