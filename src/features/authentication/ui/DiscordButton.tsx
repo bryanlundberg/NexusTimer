0f84c2a69424f96718f3329cc27b085d39a27b2c
Bryan Lundberg <bryanlundberg@outlook.com>
2026-02-28 11:07:02 -0600

    feat(discord-button): add localization and ui updates

--- src/features/authentication/ui/DiscordButton.tsx
+++ src/features/authentication/ui/DiscordButton.tsx
@@ -1,20 +1,22 @@
 import { signIn, useSession } from 'next-auth/react'
 import { Button } from '@/components/ui/button'
 import Image from 'next/image'
+import { useTranslations } from 'next-intl'
 
 export default function DiscordButton() {
   const { data: session } = useSession()
+  const t = useTranslations('Index.Auth')
   return (
     <>
       {!session && (
         <>
           <Button
-            variant={'ghost'}
-            className="flex gap-2 items-center w-full"
-            onClick={() => signIn('discord', { redirectTo: '/app' })}
+            variant={'outline'}
+            className="flex gap-3 items-center w-full justify-start h-10 px-4 transition-all hover:bg-muted"
+            onClick={() => signIn('discord')}
           >
-            <Image src={'/timer-logos/discord.png'} alt="google logo" width={20} height={20} />
-            <p className={'overflow-hidden text-ellipsis'}>Discord</p>
+            <Image src={'/timer-logos/discord.png'} alt="discord logo" width={24} height={24} />
+            <span className={'font-medium'}>{t('continue-discord')}</span>
           </Button>
         </>
       )}
