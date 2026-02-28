e569056cc31b570010ffec488effe4a5afef8c4e
Bryan Lundberg <bryanlundberg@outlook.com>
2026-02-28 11:07:08 -0600

    feat(google-button): add localization and ui updates

--- src/features/authentication/ui/GoogleButton.tsx
+++ src/features/authentication/ui/GoogleButton.tsx
@@ -2,20 +2,22 @@
 import { Button } from '@/components/ui/button'
 import Image from 'next/image'
 import { signIn, useSession } from 'next-auth/react'
+import { useTranslations } from 'next-intl'
 
 export default function GoogleButton() {
   const { data: session } = useSession()
+  const t = useTranslations('Index.Auth')
   return (
     <>
       {!session && (
         <>
           <Button
-            variant={'ghost'}
-            className="flex gap-2 items-center w-full"
-            onClick={() => signIn('google', { redirectTo: '/app' })}
+            variant={'outline'}
+            className="flex gap-3 items-center w-full justify-start h-10 px-4 transition-all hover:bg-muted"
+            onClick={() => signIn('google')}
           >
-            <Image src={'/timer-logos/google.svg'} alt="google logo" width={20} height={20} />
-            <p className={'overflow-hidden text-ellipsis'}>Google</p>
+            <Image src={'/timer-logos/google.svg'} alt="google logo" width={24} height={24} />
+            <span className={'font-medium'}>{t('continue-google')}</span>
           </Button>
         </>
       )}
