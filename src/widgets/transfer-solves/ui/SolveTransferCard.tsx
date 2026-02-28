62caf049285b570df012b1acad0fa0ee50b2a0fd
Bryan Lundberg <bryanlundberg@outlook.com>
2026-02-28 06:58:11 -0600

    feat(solve-transfer): localize date formatting

--- src/widgets/transfer-solves/ui/SolveTransferCard.tsx
+++ src/widgets/transfer-solves/ui/SolveTransferCard.tsx
@@ -1,7 +1,8 @@
 import formatTime from '@/shared/lib/formatTime'
 import { Solve } from '@/entities/solve/model/types'
-import { format } from 'date-fns'
 import { Button } from '@/components/ui/button'
+import { useLocale } from 'next-intl'
+import moment from 'moment'
 
 interface SolveTransferCardProps {
   solve: Solve
@@ -10,6 +11,7 @@ interface SolveTransferCardProps {
 }
 
 export default function SolveTransferCard({ solve, isSelected, onToggle }: SolveTransferCardProps) {
+  const locale = useLocale()
   return (
     <Button
       variant={'outline'}
@@ -23,7 +25,7 @@ export default function SolveTransferCard({ solve, isSelected, onToggle }: Solve
         <span className="text-sm">.{formatTime(solve.time).split('.')[1]}</span>
       </div>
       <div className="absolute z-20 text-xs top-1 left-1">
-        {format(new Date(solve.startTime), 'dd/MM/yyyy')}
+        {moment(solve.startTime).locale(locale).format('L')}
         {solve.dnf && <span className="text-red-500 font-bold ml-1">DNF</span>}
         {solve.plus2 && <span className="text-yellow-500 font-bold ml-1">+2</span>}
       </div>
