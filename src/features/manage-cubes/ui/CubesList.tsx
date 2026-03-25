ce5615068735c3cf450b2967c385bb69ca2d07c8
Bryan Lundberg <bryanlundberg@outlook.com>
2026-03-24 22:54:17 -0600

    feat(ui): add animations to cubes list cards

--- src/features/manage-cubes/ui/CubesList.tsx
+++ src/features/manage-cubes/ui/CubesList.tsx
@@ -1,5 +1,6 @@
 import { Cube } from '@/entities/cube/model/types'
 import { CubeCard } from '@/features/manage-cubes/ui/CubeCard'
+import { motion } from 'motion/react'
 
 interface CubesListProps {
   cubes: Array<Cube>
@@ -7,9 +8,20 @@ interface CubesListProps {
 
 export default function CubesList({ cubes }: CubesListProps) {
   return (
-    <div className="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-2">
-      {cubes.map((cube) => (
-        <CubeCard cube={cube} key={cube.id} />
+    <div className="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-3">
+      {cubes.map((cube, index) => (
+        <motion.div
+          key={cube.id}
+          initial={{ opacity: 0, y: 12 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{
+            duration: 0.3,
+            delay: Math.min(index * 0.05, 0.4),
+            ease: 'easeOut'
+          }}
+        >
+          <CubeCard cube={cube} />
+        </motion.div>
       ))}
     </div>
   )
