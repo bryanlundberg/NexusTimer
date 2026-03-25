5e9a8af2e71814a679440a5ceabcc045bd986c34
Bryan Lundberg <bryanlundberg@outlook.com>
2026-03-25 07:18:02 -0600

    feat(ui): add animation to stats page header

--- src/widgets/navigation-header/ui/StatsPageHeader.tsx
+++ src/widgets/navigation-header/ui/StatsPageHeader.tsx
@@ -1,11 +1,17 @@
 import DateRangeSelect from '@/features/date-range-select/ui/DateRangeSelect'
 import MainCubeSelector from '@/features/select-cube/ui/MainCubeSelector'
+import { motion } from 'framer-motion'
 
 export default function StatsPageHeader() {
   return (
-    <div className="flex justify-between items-center gap-2 w-full mb-2">
+    <motion.div
+      initial={{ opacity: 0, y: -8 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ duration: 0.3 }}
+      className="flex justify-between items-center gap-3 w-full mb-4"
+    >
       <MainCubeSelector />
       <DateRangeSelect />
-    </div>
+    </motion.div>
   )
 }
