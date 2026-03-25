cd2bcf97bf2de048ede14d270fda058bfa546d7b
Bryan Lundberg <bryanlundberg@outlook.com>
2026-03-24 22:54:23 -0600

    feat(ui): adjust spacing in cubes page header

--- src/widgets/navigation-header/ui/CubesPageHeader.tsx
+++ src/widgets/navigation-header/ui/CubesPageHeader.tsx
@@ -3,7 +3,7 @@ import MainCubeSelector from '@/features/select-cube/ui/MainCubeSelector'
 
 export default function CubesPageHeader() {
   return (
-    <div className="flex justify-between items-center gap-2 w-full mb-2">
+    <div className="flex justify-between items-center gap-2 w-full mb-4">
       <MainCubeSelector />
       <ButtonCreateCollection />
     </div>
