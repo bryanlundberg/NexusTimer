159e2fbdf25ce960848409d34b1de31b204114d9
Bryan Lundberg <bryanlundberg@outlook.com>
2026-03-30 07:57:41 -0600

    feat(config): add custom security headers

--- next.config.mjs
+++ next.config.mjs
@@ -25,6 +25,35 @@ const nextConfig = {
         hostname: "cdn.jsdelivr.net"
       }
     ]
+  },
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "X-Content-Type-Options",
+            value: "nosniff"
+          },
+          {
+            key: "X-Frame-Options",
+            value: "DENY"
+          },
+          {
+            key: "Referrer-Policy",
+            value: "strict-origin-when-cross-origin"
+          },
+          {
+            key: "Permissions-Policy",
+            value: "camera=(), geolocation=()"
+          },
+          {
+            key: "Strict-Transport-Security",
+            value: "max-age=63072000; includeSubDomains; preload"
+          }
+        ]
+      }
+    ];
   }
 };
 
