dc342350d53afa8315aa6ccd720f817b860e0a75
Bryan Lundberg <bryanlundberg@outlook.com>
2026-04-02 23:18:25 -0600

    feat(hardware): add bluetooth and gancube support

---
+++ public/vendors/cstimer/hardware/bluetooth.js
@@ -0,0 +1,213 @@
+'use strict'
+
+function BtDeviceGroupFactory() {
+  /* { prefix: cubeModel } */
+  var cubeModels = {}
+
+  function regCubeModel(cubeModel) {
+    if ($.isArray(cubeModel.prefix)) {
+      cubeModel.prefix.map((prefix) => {
+        cubeModels[prefix] = cubeModel
+      })
+    } else {
+      cubeModels[cubeModel.prefix] = cubeModel
+    }
+  }
+
+  var cube = undefined
+  var _device = null
+
+  function toUuid128(uuid) {
+    if (/^[0-9A-Fa-f]{4}$/.exec(uuid)) {
+      uuid = '0000' + uuid + '-0000-1000-8000-00805F9B34FB'
+    }
+    return uuid.toUpperCase()
+  }
+
+  function findUUID(elems, uuid) {
+    uuid = toUuid128(uuid)
+    for (var i = 0; i < elems.length; i++) {
+      var elem = elems[i]
+      if (toUuid128(elem.uuid) == uuid) {
+        return elem
+      }
+    }
+    return null
+  }
+
+  function waitForAdvs() {
+    if (!_device || !_device.watchAdvertisements) {
+      return Promise.reject(-1)
+    }
+    var abortController = new AbortController()
+    return new Promise(function (resolve, reject) {
+      var onAdvEvent = function (event) {
+        giikerutil.log('[bluetooth] receive adv event', event)
+        _device && _device.removeEventListener('advertisementreceived', onAdvEvent)
+        abortController.abort()
+        resolve(event.manufacturerData)
+      }
+      _device.addEventListener('advertisementreceived', onAdvEvent)
+      _device.watchAdvertisements({ signal: abortController.signal })
+      setTimeout(function () {
+        // reject if no mac found
+        _device && _device.removeEventListener('advertisementreceived', onAdvEvent)
+        abortController.abort()
+        reject(-2)
+      }, 10000)
+    })
+  }
+
+  function onHardwareEvent(info, event) {
+    var res = Promise.resolve()
+    if (info == 'disconnect') {
+      res = Promise.resolve(stop(true))
+    }
+    return res.then(function () {
+      return typeof evtCallback == 'function' && evtCallback(info, event)
+    })
+  }
+
+  var onDisconnect = onHardwareEvent.bind(null, 'disconnect')
+
+  function init(reconnect) {
+    return giikerutil
+      .chkAvail()
+      .then(function () {
+        if (_device && reconnect) {
+          giikerutil.log('[bluetooth]', 'reconnecting...', _device)
+          return waitUntilDeviceAvailable(_device)
+        }
+        var filters = Object.keys(cubeModels).map((prefix) => ({ namePrefix: prefix }))
+        var opservs = [
+          ...new Set(
+            Array.prototype.concat.apply(
+              [],
+              Object.values(cubeModels).map((cubeModel) => cubeModel.opservs || [])
+            )
+          )
+        ]
+        var cics = [
+          ...new Set(
+            Array.prototype.concat.apply(
+              [],
+              Object.values(cubeModels).map((cubeModel) => cubeModel.cics || [])
+            )
+          )
+        ]
+        giikerutil.log('[bluetooth]', 'scanning...', Object.keys(cubeModels))
+        return window.navigator.bluetooth.requestDevice({
+          filters: filters,
+          optionalServices: opservs,
+          optionalManufacturerData: cics
+        })
+      })
+      .then(function (device) {
+        giikerutil.log('[bluetooth]', 'BLE device is selected, name=' + device.name, device)
+        _device = device
+        device.addEventListener('gattserverdisconnected', onDisconnect)
+        cube = null
+        for (var prefix in cubeModels) {
+          if (device.name.startsWith(prefix)) {
+            cube = cubeModels[prefix]
+            break
+          }
+        }
+        if (!cube) {
+          return Promise.reject('Cannot detect device type')
+        }
+        return cube.init(device)
+      })
+  }
+
+  // Wait until target device start sending bluetooth advertisiment packets
+  function waitUntilDeviceAvailable(device) {
+    var abortController = new AbortController()
+    return new Promise(function (resolve, reject) {
+      if (!device.watchAdvertisements) {
+        reject('Bluetooth Advertisements API is not supported by this browser')
+      } else {
+        var onAdvEvent = function (event) {
+          DEBUG && console.log('[bluetooth] received advertisement packet from device', event)
+          delete device.stopWaiting
+          device.removeEventListener('advertisementreceived', onAdvEvent)
+          abortController.abort()
+          resolve(device)
+        }
+        device.stopWaiting = function () {
+          DEBUG && console.log('[bluetooth] cancel waiting for device advertisements')
+          delete device.stopWaiting
+          device.removeEventListener('advertisementreceived', onAdvEvent)
+          abortController.abort()
+        }
+        device.addEventListener('advertisementreceived', onAdvEvent)
+        device.watchAdvertisements({ signal: abortController.signal })
+        DEBUG && console.log('[bluetooth] start waiting for device advertisement packet')
+      }
+    })
+  }
+
+  function stop(isHardwareEvent) {
+    if (!_device) {
+      return Promise.resolve()
+    }
+    return Promise.resolve(cube && cube.clear(isHardwareEvent)).then(function () {
+      _device.removeEventListener('gattserverdisconnected', onDisconnect)
+      _device.gatt.disconnect()
+      _device = null
+    })
+  }
+
+  var callback = $.noop
+  var evtCallback = $.noop
+
+  return {
+    init: init,
+    stop: stop,
+    isConnected: function () {
+      return _device != null || DEBUGBL
+    },
+    setCallback: function (func) {
+      callback = func
+    },
+    setEventCallback: function (func) {
+      evtCallback = func
+    },
+    getCube: function () {
+      return (
+        cube ||
+        (DEBUGBL && {
+          getBatteryLevel: function () {
+            return Promise.resolve(80)
+          }
+        })
+      )
+    },
+    regCubeModel: regCubeModel,
+    findUUID: findUUID,
+    waitForAdvs: waitForAdvs,
+    onDisconnect: onDisconnect,
+    callback: function () {
+      return callback.apply(null, arguments)
+    }
+  }
+}
+
+var GiikerCube = execMain(BtDeviceGroupFactory)
+
+var BluetoothTimer = execMain(BtDeviceGroupFactory)
+
+BluetoothTimer.CONST = (function () {
+  var State = {}
+  State.DISCONNECT = 0 // Fired when timer is disconnected from bluetooth
+  State.GET_SET = 1 // Grace delay is expired and timer is ready to start
+  State.HANDS_OFF = 2 // Hands removed from the timer before grace delay expired
+  State.RUNNING = 3 // Timer is running
+  State.STOPPED = 4 // Timer is stopped, this event includes recorded time
+  State.IDLE = 5 // Timer is reset and idle
+  State.HANDS_ON = 6 // Hands are placed on the timer
+  State.FINISHED = 7 // Timer moves to this state immediately after STOPPED
+  State.INSPECTION = 8
+  State.GAN_RESET = 9
+  return State
+})()
---
+++ public/vendors/cstimer/hardware/stackmat.js
@@ -0,0 +1,419 @@
+'use strict'
+
+var stackmat = execMain(function () {
+  //========== Hardware Part ==========
+  var audio_context
+  var audio_stream, source, node
+  var sample_rate
+  var bitAnalyzer
+  var curTimer
+
+  function updateInputDevices() {
+    var devices = []
+    var retobj = Promise.resolve(devices)
+    if (!navigator.mediaDevices || !navigator.mediaDevices.enumerateDevices) {
+      return retobj
+    }
+
+    return navigator.mediaDevices.enumerateDevices().then(function (deviceInfos) {
+      for (var i = 0; i < deviceInfos.length; i++) {
+        var deviceInfo = deviceInfos[i]
+        if (deviceInfo.kind === 'audioinput') {
+          devices.push([deviceInfo.deviceId, deviceInfo.label || 'microphone ' + (devices.length + 1)])
+        }
+      }
+      return retobj
+    })
+  }
+
+  function init(timer, deviceId, force) {
+    curTimer = timer
+
+    if (navigator.mediaDevices === undefined) {
+      navigator.mediaDevices = {}
+    }
+
+    if (navigator.mediaDevices.getUserMedia === undefined) {
+      navigator.mediaDevices.getUserMedia = function (constraints) {
+        var getUserMedia = navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia
+        if (!getUserMedia) {
+          return Promise.reject(new Error('getUserMedia is not implemented in this browser'))
+        }
+        return new Promise(function (resolve, reject) {
+          getUserMedia.call(navigator, constraints, resolve, reject)
+        })
+      }
+    }
+
+    var AudioContext = window['AudioContext'] || window['webkitAudioContext']
+
+    audio_context = new AudioContext()
+
+    if (curTimer == 'm') {
+      sample_rate = audio_context['sampleRate'] / 8000
+      bitAnalyzer = appendBitMoyu
+    } else {
+      sample_rate = audio_context['sampleRate'] / 1200
+      bitAnalyzer = appendBit
+    }
+    agc_factor = 0.001 / sample_rate
+    lastVal.length = Math.ceil(sample_rate / 6)
+    bitBuffer = []
+    byteBuffer = []
+
+    var selectObj = {
+      echoCancellation: false,
+      noiseSuppression: false
+    }
+
+    if (deviceId) {
+      selectObj['deviceId'] = {
+        exact: deviceId
+      }
+    }
+
+    if (audio_stream == undefined) {
+      return navigator.mediaDevices
+        .getUserMedia({
+          audio: selectObj
+        })
+        .then(function (stream) {
+          if (audio_context.state == 'suspended' && !force) {
+            return Promise.reject()
+          }
+          success(stream)
+        }, console.log)
+    } else {
+      return Promise.resolve()
+    }
+  }
+
+  function stop() {
+    if (audio_stream != undefined) {
+      try {
+        source['disconnect'](node)
+        node['disconnect'](audio_context['destination'])
+        audio_stream['stop'] && audio_stream['stop']()
+      } catch (e) {}
+      audio_stream = undefined
+    }
+  }
+
+  var last_power = 1
+  var agc_factor = 0.0001
+
+  function success(stream) {
+    audio_stream = stream
+    source = audio_context['createMediaStreamSource'](stream)
+    node = audio_context['createScriptProcessor'](1024, 1, 1)
+
+    node['onaudioprocess'] = function (e) {
+      // console.log(+new Date);
+      var input = e['inputBuffer']['getChannelData'](0)
+      // var output = e["outputBuffer"]["getChannelData"](0);
+      //AGC
+      for (var i = 0; i < input.length; i++) {
+        var power = input[i] * input[i]
+        last_power = Math.max(0.0001, last_power + (power - last_power) * agc_factor)
+        var gain = 1 / Math.sqrt(last_power)
+        procSignal(input[i] * gain)
+        // output[i] = input[i] * gain / 500;
+      }
+      return
+    }
+    source['connect'](node)
+    node['connect'](audio_context['destination'])
+  }
+
+  //========== Audio2Bits Part ==========
+  var lastVal = []
+  var lastSgn = 0
+  var THRESHOLD_SCHM = 0.2
+  var THRESHOLD_EDGE = 0.7
+  var lenVoltageKeep = 0
+  var distortionStat = 0
+
+  function procSignal(signal) {
+    // signal = Math.max(Math.min(signal, 1), -1);
+    // Schmidt trigger
+
+    lastVal.unshift(signal)
+    var isEdge =
+      (lastVal.pop() - signal) * (lastSgn ? 1 : -1) > THRESHOLD_EDGE &&
+      Math.abs(signal - (lastSgn ? 1 : -1)) - 1 > THRESHOLD_SCHM &&
+      lenVoltageKeep > sample_rate * 0.6
+
+    if (isEdge) {
+      for (var i = 0; i < Math.round(lenVoltageKeep / sample_rate); i++) {
+        bitAnalyzer(lastSgn)
+      }
+      lastSgn ^= 1
+      lenVoltageKeep = 0
+    } else if (lenVoltageKeep > sample_rate * 2) {
+      bitAnalyzer(lastSgn)
+      lenVoltageKeep -= sample_rate
+    }
+    lenVoltageKeep++
+
+    //note: signal power has already been normalized. So distortionStat will tends to zero ideally.
+    if (last_bit_length < 10) {
+      distortionStat = Math.max(
+        0.0001,
+        distortionStat + (Math.pow(signal - (lastSgn ? 1 : -1), 2) - distortionStat) * agc_factor
+      )
+    } else if (last_bit_length > 100) {
+      distortionStat = 1
+    }
+    if (sampleRemain > 0) {
+      sampleRemain--
+      sampleData['raw'].push(signal)
+      sampleData['bin'].push(lastSgn)
+      if (sampleRemain == 0) {
+        sampleCallback && sampleCallback(sampleData)
+        sampleCallback = null
+      }
+    }
+  }
+
+  //========== Bits Analyzer ==========
+  var bitBuffer = []
+  var byteBuffer = []
+  var idle_val = 0
+  var last_bit = 0
+  var last_bit_length = 0
+  var no_state_length = 0
+
+  function appendBit(bit) {
+    var newByte = null
+    bitBuffer.push(bit)
+    if (bit != last_bit) {
+      last_bit = bit
+      last_bit_length = 1
+    } else {
+      last_bit_length++
+    }
+    no_state_length++
+    if (last_bit_length > 10) {
+      //IDLE
+      idle_val = bit
+      // console.log(bitBuffer.length);
+      bitBuffer = []
+
+      if (byteBuffer.length != 0) {
+        // console.log(byteBuffer, idle_val);
+        byteBuffer = []
+      }
+
+      if (last_bit_length > 100 && stackmat_state.on) {
+        stackmat_state.on = false
+        stackmat_state.noise = Math.min(1, distortionStat) || 0
+        stackmat_state.power = last_power
+        callback(stackmat_state)
+        // console.log('off');
+      } else if (no_state_length > 700) {
+        no_state_length = 100
+        stackmat_state.noise = Math.min(1, distortionStat) || 0
+        stackmat_state.power = last_power
+        callback(stackmat_state)
+      }
+    } else if (bitBuffer.length == 10) {
+      if (bitBuffer[0] == idle_val || bitBuffer[9] != idle_val) {
+        bitBuffer = bitBuffer.slice(1)
+      } else {
+        var val = 0
+        for (var i = 8; i > 0; i--) {
+          val = (val << 1) | (bitBuffer[i] == idle_val ? 1 : 0)
+        }
+        newByte = String.fromCharCode(val)
+        byteBuffer.push(newByte)
+        decode(byteBuffer)
+        bitBuffer = []
+      }
+    }
+    if (sampleRemain > 0) {
+      sampleData['bits'].push(bit)
+      if (newByte != null) {
+        sampleData['bytes'].push(newByte)
+      }
+    }
+  }
+
+  function decode(byteBuffer) {
+    if (byteBuffer.length != 9 && byteBuffer.length != 10) {
+      return
+    }
+    DEBUG && console.log('[stackmat]', byteBuffer)
+    var re_head = /[ SILRCA]/
+    var re_number = /[0-9]/
+    var head = byteBuffer[0]
+    if (!re_head.exec(head)) {
+      return
+    }
+    var checksum = 64
+    for (var i = 1; i < byteBuffer.length - 3; i++) {
+      if (!re_number.exec(byteBuffer[i])) {
+        return
+      }
+      checksum += ~~byteBuffer[i]
+    }
+    if (checksum != byteBuffer.at(-3).charCodeAt(0)) {
+      return
+    }
+    var time_milli =
+      ~~byteBuffer[1] * 60000 +
+      ~~(byteBuffer[2] + byteBuffer[3]) * 1000 +
+      ~~(byteBuffer[4] + byteBuffer[5] + (byteBuffer.length == 10 ? byteBuffer[6] : '0'))
+    pushNewState(head, time_milli, byteBuffer.length == 9 ? 10 : 1)
+  }
+
+  var last_suc_time = 0
+
+  function pushNewState(head, time_milli, unit) {
+    var suc_time = $.now()
+    if (suc_time - last_suc_time > 200) {
+      DEBUG && console.log('[stackmat] signal miss ', suc_time - last_suc_time)
+    }
+    last_suc_time = suc_time
+    var new_state = {}
+    new_state.time_milli = time_milli
+    new_state.unit = unit
+    new_state.on = true
+    if (!kernel.getProp('stkHead')) {
+      head = 'S'
+    }
+    var is_time_inc =
+      unit == stackmat_state.unit
+        ? new_state.time_milli > stackmat_state.time_milli
+        : Math.floor(new_state.time_milli / 10) > Math.floor(stackmat_state.time_milli / 10)
+    new_state.greenLight = head == 'A'
+    new_state.leftHand = head == 'L' || head == 'A' || head == 'C'
+    new_state.rightHand = head == 'R' || head == 'A' || head == 'C'
+    new_state.running = (head != 'S' || stackmat_state.signalHeader == 'S') && (head == ' ' || is_time_inc)
+    new_state.signalHeader = head
+    new_state.unknownRunning = !stackmat_state.on
+    new_state.noise = Math.min(1, distortionStat) || 0
+    new_state.power = last_power
+
+    stackmat_state = new_state
+
+    no_state_length = 0
+    callback(stackmat_state)
+  }
+
+  function appendBitMoyu(bit) {
+    if (last_bit != idle_val && last_bit_length == 1) {
+      bitBuffer.push(bit)
+      if (bitBuffer.length == 24) {
+        var time_milli = 0
+        for (var i = 5; i >= 0; i--) {
+          time_milli *= 10
+          for (var j = 0; j < 4; j++) {
+            time_milli += bitBuffer[i * 4 + j] << j
+          }
+        }
+        bitBuffer = []
+        pushNewState('S', time_milli, 1)
+      }
+    }
+    if (bit != last_bit) {
+      last_bit = bit
+      last_bit_length = 1
+    } else {
+      last_bit_length++
+    }
+    if (last_bit_length > 10) {
+      //IDLE
+      idle_val = bit
+      bitBuffer = []
+      byteBuffer = []
+      if (last_bit_length > 1000 && stackmat_state.on) {
+        stackmat_state.on = false
+        stackmat_state.noise = Math.min(1, distortionStat) || 0
+        stackmat_state.power = last_power
+        callback(stackmat_state)
+      } else if (last_bit_length > 4000) {
+        last_bit_length = 1000
+        stackmat_state.noise = Math.min(1, distortionStat) || 0
+        stackmat_state.power = last_power
+        callback(stackmat_state)
+      }
+    }
+    if (sampleRemain > 0) {
+      sampleData['bits'].push(bit)
+    }
+  }
+
+  var sampleCallback = null
+  var sampleRemain = 0
+  var sampleData = {}
+
+  function getSample(duration, callback) {
+    sampleCallback = callback
+    sampleRemain = Math.ceil(duration * (audio_context['sampleRate'] || 44100))
+    sampleData = {
+      raw: [], // signal after agc
+      bin: [], // binarized signal
+      bits: [],
+      bytes: []
+    }
+  }
+
+  var stackmat_state = {
+    time_milli: 0,
+    unit: 10,
+    on: false,
+    greenLight: false,
+    leftHand: false,
+    rightHand: false,
+    running: false,
+    unknownRunning: true,
+    signalHeader: 'I',
+    noise: 1,
+    power: 1
+  }
+
+  var callback = $.noop
+
+  return {
+    init: init,
+    stop: stop,
+    updateInputDevices: updateInputDevices,
+    getSample: getSample,
+    setCallBack: function (func) {
+      callback = func
+    }
+  }
+})
+
+execMain(function () {
+  if (!window.nativeStackmat) {
+    return
+  }
+  stackmat = (function () {
+    DEBUG && console.log('Use Native Stackmat')
+    var callbackName = 'stackmat_callback_' + ~~(Math.random() * 10000000)
+    var callback
+    nativeStackmat.setCallback(callbackName)
+    window[callbackName] = function (obj) {
+      DEBUG && console.log(JSON.stringify(obj))
+      callback && callback(obj)
+    }
+    return {
+      init: function () {
+        nativeStackmat.init()
+        return Promise.resolve()
+      },
+      stop: function () {
+        nativeStackmat.stop()
+        return Promise.resolve()
+      },
+      updateInputDevices: function () {
+        return Promise.resolve([[undefined, 'native']])
+      },
+      setCallBack: function (func) {
+        callback = func
+      },
+      getSample: function () {}
+    }
+  })()
+})
