0044379da0b8a8d456083e709a1896cffe2ceea1
Bryan Lundberg <bryanlundberg@outlook.com>
2026-05-11 15:49:37 -0600

    feat(i18n): integrate translations into TrainerHistoryPage for breadcrumb labels

--- src/app/algorithms/trainer/history/page.tsx
+++ src/app/algorithms/trainer/history/page.tsx
@@ -1,15 +1,18 @@
+import { getTranslations } from 'next-intl/server'
 import CoreHeader from '@/shared/ui/core-header/ui/CoreHeader'
 import { ScrollArea } from '@/components/ui/scroll-area'
 import TrainerHistoryView from '@/features/trainer/ui/TrainerHistoryView'
 
-export default function TrainerHistoryPage() {
+export default async function TrainerHistoryPage() {
+  const tTrainer = await getTranslations('Index.TrainerPage')
+  const t = await getTranslations('Index.TrainerHistoryPage')
   return (
     <ScrollArea className="max-h-dvh overflow-auto">
       <CoreHeader
         breadcrumbPath={'/algorithms/trainer'}
-        breadcrumb={'Trainer'}
+        breadcrumb={tTrainer('breadcrumb')}
         secondaryBreadcrumbPath={`#`}
-        secondaryBreadcrumb={'History'}
+        secondaryBreadcrumb={t('breadcrumb')}
       />
 
       <TrainerHistoryView />
