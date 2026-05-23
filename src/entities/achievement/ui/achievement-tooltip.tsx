60716a279fbc4c314010d94d1d87df1935355440
Bryan Lundberg <bryanlundberg@outlook.com>
2026-05-23 05:07:04 -0600

    feat(ui): add AchievementTooltip component for tooltips

---
+++ src/entities/achievement/ui/achievement-tooltip.tsx
@@ -0,0 +1,30 @@
+import Image from 'next/image'
+import { cn } from '@/shared/lib/utils'
+import { TooltipContent } from '@/components/ui/tooltip'
+import { Achievement as AchievementType } from '../model/types'
+
+interface AchievementTooltipProps {
+  achievement: AchievementType
+}
+
+export function AchievementTooltip({ achievement }: AchievementTooltipProps) {
+  return (
+    <TooltipContent className="p-2 bg-black text-white dark:bg-white dark:text-black rounded-md shadow-lg border border-primary">
+      <div className="flex items-center justify-center mb-2">
+        <Image
+          src={`/achievements/${achievement.icon}`}
+          unoptimized
+          alt={achievement.title}
+          width={96}
+          height={96}
+          className={cn('object-cover size-10 dark:invert')}
+          draggable={false}
+        />
+        <div className="ml-3 space-y-1">
+          <p className="font-bold text-sm leading-none">{achievement.title}</p>
+          <p className="text-xs text-muted-foreground leading-snug">{achievement.description}</p>
+        </div>
+      </div>
+    </TooltipContent>
+  )
+}
