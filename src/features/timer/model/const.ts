33e75b14738ab309985e2f22ffbf11426b07aa82
Bryan Lundberg <bryanlundberg@outlook.com>
2026-05-24 14:10:54 -0600

    feat(constants): add constants for scramble and hints

---
+++ src/features/timer/model/const.ts
@@ -0,0 +1,8 @@
+export const SCRAMBLE_MAX_HEIGHT_RATIO = 0.25
+
+export const HINT_CATEGORIES = ['3x3', '3x3 OH'] as const
+
+export const SCRAMBLE_SIZE_CLASSES = {
+  large: 'text-2xl md:text-3xl lg:text-4xl',
+  normal: 'text-xl md:text-2xl lg:text-3xl'
+} as const
