350cea63660f46c05ffabf5688d5c07eb9e7ceed
Bryan Lundberg <bryanlundberg@outlook.com>
2026-05-25 13:46:13 -0600

    refactor(ui): enhance empty-tab styling with mask and opacity

--- src/widgets/people/ui/empty-tab-content.tsx
+++ src/widgets/people/ui/empty-tab-content.tsx
@@ -4,18 +4,19 @@ import { useTranslations } from 'next-intl'
 export default function EmptyTabContent() {
   const t = useTranslations('Index.PeoplePage')
   return (
-    <div className="relative flex flex-col items-center justify-center h-96 bg-card/50 rounded-lg shadow-md overflow-hidden">
+    <div className="relative flex flex-col items-center justify-center h-96 bg-card/50 overflow-hidden">
       <Image
         src="/bg.webp"
         alt="Background Image"
-        className="absolute inset-0 object-cover w-full h-full"
+        className="absolute inset-0 object-cover w-full h-full opacity-30 dark:opacity-10"
         width={1920}
         height={1080}
         draggable={false}
         quality={50}
         style={{
           zIndex: 0,
-          opacity: 0.1
+          maskImage: 'radial-gradient(ellipse at center, black 10%, transparent 100%)',
+          WebkitMaskImage: 'radial-gradient(ellipse at center, black 10%, transparent 100%)'
         }}
       ></Image>
       <div className="relative scroll-m-20 text-xl tracking-tight max-w-prose md:max-w-xl mx-auto text-center px-4">
