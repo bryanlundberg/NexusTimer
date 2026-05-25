2b7c167f16706f7842a6319bb05d05f3e85b04b3
Bryan Lundberg <bryanlundberg@outlook.com>
2026-05-24 21:05:28 -0600

    refactor(tests): add testPathIgnorePatterns for fixtures

--- jest.config.js
+++ jest.config.js
@@ -1,19 +1,20 @@
-const nextJest = require("next/jest");
+const nextJest = require('next/jest')
 
 /** @type {import('jest').Config} */
 const createJestConfig = nextJest({
   // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
-  dir: "./",
-});
+  dir: './'
+})
 
 // Add any custom config to be passed to Jest
 const config = {
-  coverageProvider: "v8",
-  testEnvironment: "jsdom",
-  modulePathIgnorePatterns: ["spec.ts"],
+  coverageProvider: 'v8',
+  testEnvironment: 'jsdom',
+  modulePathIgnorePatterns: ['spec.ts'],
+  testPathIgnorePatterns: ['/node_modules/', '/__tests__/fixtures/']
   // Add more setup options before each test is run
   // setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
-};
+}
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
-module.exports = createJestConfig(config);
+module.exports = createJestConfig(config)
