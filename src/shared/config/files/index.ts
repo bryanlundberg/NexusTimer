bd3992e2910bc7f4fbb4cea8801f97460ae39c87
Bryan Lundberg <bryanlundberg@outlook.com>
2026-05-26 09:27:43 -0600

    refactor(config): support minio emulator in files setup

--- src/shared/config/files/index.ts
+++ src/shared/config/files/index.ts
@@ -1,14 +1,20 @@
 import { Files } from 'files-sdk'
 import { backblazeB2 } from 'files-sdk/backblaze-b2'
+import { minio } from 'files-sdk/minio'
+
+const useEmulator = process.env.FILES_EMULATOR === 'true'
+
+const bucket = process.env.FILES_BUCKET as string
+const accessKeyId = process.env.FILES_ACCESS_KEY_ID
+const secretAccessKey = process.env.FILES_SECRET_ACCESS_KEY
+const endpoint = process.env.FILES_ENDPOINT as string
+const publicBaseUrl = process.env.FILES_PUBLIC_BASE_URL as string
+const region = process.env.FILES_REGION as string
 
 export const files = new Files({
-  adapter: backblazeB2({
-    bucket: process.env.B2_BUCKET as string,
-    region: process.env.B2_REGION as string,
-    accessKeyId: process.env.B2_APPLICATION_KEY_ID,
-    secretAccessKey: process.env.B2_APPLICATION_KEY,
-    publicBaseUrl: `https://${process.env.B2_BUCKET}.s3.${process.env.B2_REGION}.backblazeb2.com`
-  })
+  adapter: useEmulator
+    ? minio({ bucket, endpoint, accessKeyId, secretAccessKey, publicBaseUrl })
+    : backblazeB2({ bucket, region, accessKeyId, secretAccessKey, publicBaseUrl })
 })
 
 export default files
