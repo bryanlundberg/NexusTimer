6dee18b8549bfd327eaae74d5ecda43c75a24a8e
Bryan Lundberg <bryanlundberg@outlook.com>
2026-06-20 06:57:24 -0600

    refactor: remove selectedCube prop from SolvesTabSwitcher

--- src/features/navigation/ui/SolvesTabSwitcher.tsx
+++ src/features/navigation/ui/SolvesTabSwitcher.tsx
@@ -1,7 +1,6 @@
 'use client'
 
 import { useTranslations } from 'next-intl'
-import { useTimerStore } from '@/shared/model/timer/useTimerStore'
 import { useQueryState } from 'nuqs'
 import { STATES } from '@/shared/const/states'
 import { SolveTab } from '@/shared/types/enums'
@@ -10,16 +9,14 @@ import { HistoryIcon, ShellIcon } from 'lucide-react'
 import AnimatedTabsList from '@/shared/ui/animated-tabs/AnimatedTabsList'
 
 export default function SolvesTabSwitcher() {
-  const selectedCube = useTimerStore((state) => state.selectedCube)
   const t = useTranslations('Index')
   const [tabMode, setTabMode] = useQueryState(STATES.SOLVES_PAGE.TAB_MODE.KEY, {
     defaultValue: STATES.SOLVES_PAGE.TAB_MODE.DEFAULT_VALUE
   })
 
-  const disabled = selectedCube === null
   const tabs = [
-    { value: SolveTab.SESSION, icon: ShellIcon, label: t('SolvesPage.session'), disabled },
-    { value: SolveTab.ALL, icon: HistoryIcon, label: t('SolvesPage.historial'), disabled }
+    { value: SolveTab.SESSION, icon: ShellIcon, label: t('SolvesPage.session') },
+    { value: SolveTab.ALL, icon: HistoryIcon, label: t('SolvesPage.historial') }
   ]
 
   return (
