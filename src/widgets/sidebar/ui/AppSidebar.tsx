f7df5f585dd830d784c49fe7f559a47ff51eac83
Bryan Lundberg <bryanlundberg@outlook.com>
2026-06-21 17:01:43 -0600

    chore: update framer-motion import to motion/react

--- src/widgets/sidebar/ui/AppSidebar.tsx
+++ src/widgets/sidebar/ui/AppSidebar.tsx
@@ -2,7 +2,7 @@
 
 import * as React from 'react'
 import { useEffect, useMemo, useRef, useState } from 'react'
-import { motion } from 'framer-motion'
+import { motion } from 'motion/react'
 import { usePathname } from 'next/navigation'
 import { useActiveIndicator } from '@/widgets/sidebar/model/useActiveIndicator'
 import {
