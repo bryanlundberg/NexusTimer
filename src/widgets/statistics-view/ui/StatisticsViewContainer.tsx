4ed1ff1939718324aa36fa856e639a9335b7f3b7
Bryan Lundberg <bryanlundberg@outlook.com>
2026-06-23 11:21:04 -0600

    style: remove bg color from StatisticsViewContainer

--- src/widgets/statistics-view/ui/StatisticsViewContainer.tsx
+++ src/widgets/statistics-view/ui/StatisticsViewContainer.tsx
@@ -1,3 +1,3 @@
 export default function StatisticsViewContainer({ children }: { children: React.ReactNode }) {
-  return <div className="flex flex-col w-full pb-1 bg-background min-h-75">{children}</div>
+  return <div className="flex flex-col w-full pb-1 min-h-75">{children}</div>
 }
