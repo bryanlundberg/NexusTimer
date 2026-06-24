6b73fa6d47b18977a3303417749d768bf6d69cb0
Bryan Lundberg <bryanlundberg@outlook.com>
2026-06-23 18:57:09 -0600

    feat: add TimerSolvesRail to TimerPage layout

--- src/app/(with-sidebar)/(core-timer)/app/page.tsx
+++ src/app/(with-sidebar)/(core-timer)/app/page.tsx
@@ -11,6 +11,7 @@ import ScrambleModal from '@/features/timer/ui/ScrambleModal'
 import CoreHeader from '@/shared/ui/core-header/ui/CoreHeader'
 import { PageBody } from '@/shared/ui/page-body/PageBody'
 import OnboardingTour from '@/features/onboarding-tour/ui/OnboardingTour'
+import TimerSolvesRail from '@/features/timer-solves-rail/ui/TimerSolvesRail'
 
 export default function TimerPage() {
   const resetTimerStore = useTimerStore((state) => state.reset)
@@ -22,16 +23,19 @@ export default function TimerPage() {
   }, [])
 
   return (
-    <div className="flex flex-col grow relative">
+    <div className="flex flex-col grow min-h-0 relative">
       <h1 className="sr-only">{t('description')}</h1>
       <CoreHeader breadcrumbs={[{ label: tHomePage('title'), href: '/app' }]} />
-      <PageBody variant="data" className="flex flex-col grow">
-        <TimerContainer>
-          <HeaderTimer />
-          <MainTimer />
-          <TimerWidgets />
-        </TimerContainer>
-      </PageBody>
+      <div className="flex grow min-h-0">
+        <PageBody variant="data" className="flex flex-col grow min-w-0">
+          <TimerContainer>
+            <HeaderTimer />
+            <MainTimer />
+            <TimerWidgets />
+          </TimerContainer>
+        </PageBody>
+        <TimerSolvesRail />
+      </div>
       <ScrambleModal />
       <OnboardingTour />
     </div>
