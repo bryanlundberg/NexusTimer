5690296c1fdff408da93c889fecfd619102c2c8b
Bryan Lundberg <bryanlundberg@outlook.com>
2026-06-27 20:37:04 -0600

    feat: add algorithms tab to people content

--- src/widgets/people/ui/PeopleContent.tsx
+++ src/widgets/people/ui/PeopleContent.tsx
@@ -4,15 +4,18 @@ import OverviewTabContent from '@/widgets/people/ui/overview-tab-content'
 import CubesTabContent from '@/widgets/people/ui/cubes-tab-content'
 import TimelineTabContent from '@/widgets/people/ui/timeline-tab-content'
 import AchievementsTabContent from '@/widgets/people/ui/achievements-tab-content'
+import AlgorithmsTabContent from '@/widgets/people/ui/algorithms-tab-content'
 import { Cube } from '@/entities/cube/model/types'
 import { UserBadgesResult } from '@/entities/achievement/model/useUserBadges'
+import type { LearnedMethod } from '@/entities/trainer-learned/model/useUserLearned'
 
 interface Props {
   cubes: Array<Cube>
   badges: UserBadgesResult
+  learnedMethods?: LearnedMethod[]
 }
 
-export function PeopleContent({ cubes, badges }: Props) {
+export function PeopleContent({ cubes, badges, learnedMethods }: Props) {
   return (
     <div className="flex flex-col w-full">
       <div className="flex flex-col md:flex-row relative mt-2">
@@ -25,6 +28,9 @@ export function PeopleContent({ cubes, badges }: Props) {
           </TabsContent>
           <TabsContent value={PTabs.TIMELINE}>
             <TimelineTabContent cubes={cubes} />
+          </TabsContent>
+          <TabsContent value={PTabs.ALGORITHMS}>
+            <AlgorithmsTabContent methods={learnedMethods} />
           </TabsContent>
           <TabsContent value={PTabs.ACHIEVEMENTS}>
             <AchievementsTabContent badges={badges} />
