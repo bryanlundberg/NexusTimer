12b0623b545659605f34133cfa215c3716ed835e
Bryan Lundberg <bryanlundberg@outlook.com>
2026-07-13 07:05:41 -0600

    refactor: remove TRAINER_DEFAULT_ROTATION_MODE constant

--- src/features/trainer/lib/constants.ts
+++ src/features/trainer/lib/constants.ts
@@ -1,8 +1,5 @@
-import type { TrainerRotationMode } from '@/features/trainer/model/types'
-
 export const TRAINER_DEFAULT_SLUG = 'oll'
 export const TRAINER_DEFAULT_TARGET_SECONDS = 2
-export const TRAINER_DEFAULT_ROTATION_MODE: TrainerRotationMode = 'shuffle'
 export const TRAINER_RECENT_WINDOW = 12
 export const TRAINER_TARGET_OPTIONS = [1, 2, 3, 4, 5] as const
 export const TRAINER_PAGE_SIZE = 25
