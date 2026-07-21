bf907b3bbd73064be1480a7f96cab0fcdcf4456f
Bryan Lundberg <bryanlundberg@outlook.com>
2026-07-21 09:12:48 -0600

    feat: add Chakra Petch font configuration

--- src/shared/config/fonts/index.ts
+++ src/shared/config/fonts/index.ts
@@ -1,7 +1,14 @@
-import { Space_Grotesk } from 'next/font/google'
+import { Space_Grotesk, Chakra_Petch } from 'next/font/google'
 
 export const spaceGrotesk = Space_Grotesk({
   subsets: ['latin'],
   variable: '--font-space-grotesk',
+  display: 'swap'
+})
+
+export const chakraPetch = Chakra_Petch({
+  subsets: ['latin'],
+  weight: ['500', '600', '700'],
+  variable: '--font-chakra',
   display: 'swap'
 })
