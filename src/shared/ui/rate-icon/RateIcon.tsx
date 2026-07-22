1ecc19d0f986d566f7312463e1fd188e45866ee1
Bryan Lundberg <bryanlundberg@outlook.com>
2026-07-22 05:30:50 -0600

    style: remove redundant class in RateIcon component

--- src/shared/ui/rate-icon/RateIcon.tsx
+++ src/shared/ui/rate-icon/RateIcon.tsx
@@ -5,7 +5,7 @@ export default function RatedIcon({ type }: { type: 'check' | 'cross' | 'partial
     type === 'partial' ? 'bg-yellow-400/10' : type === 'check' ? 'bg-green-400/10' : 'bg-red-400/10'
 
   return (
-    <div className={`size-10 rounded-full flex items-center justify-center mx-auto ${backgroundClass}`}>
+    <div className={`size-10 flex items-center justify-center mx-auto ${backgroundClass}`}>
       {type === 'check' && <Check className={'text-green-400 size-6'} />}
       {type === 'cross' && <XIcon className={'text-red-400 size-6'} />}
       {type === 'partial' && <TriangleAlert className={'size-6 text-yellow-400'} />}
