07c02c954a21451e9ff24709d30b1a7cebf41a72
Bryan Lundberg <bryanlundberg@outlook.com>
2026-08-08 05:06:43 -0600

    feat(data): add l2c-555 algorithms

---
+++ src/shared/data/algs/l2c-555.ts
@@ -0,0 +1,195 @@
+import { AlgorithmCollection } from '@/features/algorithms-list/model/types'
+
+const idMethod = 'L2C_555_ALGS'
+const puzzle = '555'
+const group = 'Last Two Centers'
+
+export const L2C_555_ALGS: AlgorithmCollection[] = [
+  {
+    name: 'L2C 1',
+    id: 'f156e76d-86bc-4be1-8ce8-50160912dd2e',
+    idMethod,
+    puzzle,
+    group,
+    algs: [
+      { moves: "U2 Rw U Rw' U Rw U2 Rw'", id: '1583c595-465a-4bd9-b7ec-d93536be7594' },
+      { moves: "Rw U2 Rw' U' Rw U' Rw'", id: '0c825f79-2414-4070-981b-053d25fd07f3' },
+      { moves: "U2 r U r' U r U2 r'", id: 'fe31ae7e-0f98-4911-a9fa-a1b00da22871' },
+      { moves: "U Rw U' Lw' U Rw' U' Lw", id: 'c155ec9f-5b92-4e46-8c25-94d099d44277' }
+    ]
+  },
+  {
+    name: 'L2C 2',
+    id: 'afe43f06-fbca-491a-b36b-0c2557305c1d',
+    idMethod,
+    puzzle,
+    group,
+    algs: [
+      { moves: "Rw U M' U2 M U Rw'", id: '98a72e3a-e86a-46a8-a710-2d58048f950c' },
+      { moves: "Rw U' Rw' U2 Rw U2 Rw' U Rw U2 Rw'", id: '59291cb9-f914-44a9-939b-5e217f2911dc' },
+      { moves: "U Rw U Rw' U' Rw U Rw' U Rw U2' Rw'", id: '063d4014-98f0-4d47-b2a4-0c238ff36463' },
+      { moves: "U2 Rw U' M' U Rw' U' M", id: 'c10dcd86-266d-4bb4-bcfe-92d9c0fa3801' }
+    ]
+  },
+  {
+    name: 'L2C 3',
+    id: '33c4cd18-10fa-474f-b5be-636bfb58cbf3',
+    idMethod,
+    puzzle,
+    group,
+    algs: [
+      { moves: "Rw U Rw'", id: '8380925a-f6d1-44fa-a7be-4e2e96436240' },
+      { moves: "r U r'", id: '9132f142-be8d-41ee-a9c6-f7cccf8175a2' }
+    ]
+  },
+  {
+    name: 'L2C 4',
+    id: '78af58b7-656c-4a14-8ec5-ab68bc1e4c44',
+    idMethod,
+    puzzle,
+    group,
+    algs: [
+      { moves: "Rw U Rw' U' Rw U' Rw'", id: 'ffc822f3-6861-4370-a7ea-e0e8f81cdf84' },
+      { moves: "U' r U r' U r U' r'", id: 'dc4f1020-5dc1-47fe-a5bd-133263f9b644' }
+    ]
+  },
+  {
+    name: 'L2C 5',
+    id: '60ab9ca9-fc46-4990-b14c-5041193514bc',
+    idMethod,
+    puzzle,
+    group,
+    algs: [
+      { moves: "Rw U' Rw' U' Rw U' Rw'", id: 'f6f4fc81-fef7-4f46-8433-54b0386ac5e4' },
+      { moves: "U 3Rw U Rw' U' 3R'", id: 'f60df2b2-8d5a-405f-92c5-7ee97027bd6e' }
+    ]
+  },
+  {
+    name: 'L2C 6',
+    id: 'b94cbf95-7117-4b97-9e46-daed0fbf550f',
+    idMethod,
+    puzzle,
+    group,
+    algs: [{ moves: "Rw U2 Rw' U Rw U2 Rw'", id: '88fe241c-0d88-4ce3-858e-35d3de829e09' }]
+  },
+  {
+    name: 'L2C 7',
+    id: 'f6958536-175c-42db-8e23-c6e37b1a71a4',
+    idMethod,
+    puzzle,
+    group,
+    algs: [
+      { moves: "Rw U' Rw'", id: '935a6e6e-4b2d-4ea5-a498-3a34327c9386' },
+      { moves: "r U' r'", id: 'f5d08617-cbe9-4882-900e-a58072026cdc' }
+    ]
+  },
+  {
+    name: 'L2C 8',
+    id: 'cd54c17a-40ad-4f50-aeca-ea2268029d07',
+    idMethod,
+    puzzle,
+    group,
+    algs: [{ moves: "Rw U' Rw' U Rw U Rw'", id: 'af93a99d-5aca-4ff0-83d7-38062bf7e9d3' }]
+  },
+  {
+    name: 'L2C 9',
+    id: '9ccbe551-91fd-4938-aa27-da6a51032234',
+    idMethod,
+    puzzle,
+    group,
+    algs: [
+      { moves: "Rw U Rw' U Rw U Rw'", id: '116b5e22-8816-457e-8dd5-359c7c65db28' },
+      { moves: "U' 3Rw U' Rw' U 3R'", id: 'dbd594ad-8f05-430a-8474-c9e0989c3e19' }
+    ]
+  },
+  {
+    name: 'L2C 10',
+    id: '813088d4-4dee-47ef-9d79-7935c670191b',
+    idMethod,
+    puzzle,
+    group,
+    algs: [{ moves: "Rw U2 Rw' U' Rw U2 Rw'", id: '55d43db6-a680-49ff-bbaa-9dfa7d0b9250' }]
+  },
+  {
+    name: 'L2C 11',
+    id: '74b820b9-9c0e-4fae-b12c-ea65aef941f4',
+    idMethod,
+    puzzle,
+    group,
+    algs: [
+      { moves: "U2 Rw U Rw' U' Rw U Rw' U' Rw U Rw' U'", id: '510a55c5-7912-47d8-be74-100dc00d4b69' },
+      { moves: "Rw U M' U2 M U' Rw'", id: 'efeb39e5-c52b-439b-a051-1298a487fafa' },
+      { moves: "U2 Rw U' Rw' U Rw U' Rw' U Rw U' Rw'", id: '30c7931f-7621-4f53-9ef2-2e271ef5d0a7' },
+      { moves: "U Rw U' Rw' U' Rw U' Rw' U Rw U2 Rw'", id: 'af305ffd-5120-4a6c-853e-e5a7b42d0454' }
+    ]
+  },
+  {
+    name: 'L2C 12',
+    id: '47571231-37e9-440b-808f-d31e7ed11254',
+    idMethod,
+    puzzle,
+    group,
+    algs: [{ moves: "Rw U' Rw' U2 Rw U' Rw'", id: '004557c2-1b77-453c-b50e-8e0c2a4ad860' }]
+  },
+  {
+    name: 'L2C 13',
+    id: '72bd7243-1b4f-4941-8b1f-1552a7b53533',
+    idMethod,
+    puzzle,
+    group,
+    algs: [{ moves: "Rw U2 Rw'", id: 'af78fc69-8af8-4647-9808-64478e868d53' }]
+  },
+  {
+    name: 'L2C 14',
+    id: 'dc025516-39cc-40ad-bacf-79660d8bc2d8',
+    idMethod,
+    puzzle,
+    group,
+    algs: [{ moves: "Rw U Rw' U' Rw U2 Rw'", id: '1a52ef8d-67cb-4bd6-b03f-15e790cf7627' }]
+  },
+  {
+    name: 'L2C 15',
+    id: '4abaf2ec-0c2d-413a-924f-1344121f1b98',
+    idMethod,
+    puzzle,
+    group,
+    algs: [
+      { moves: "Rw U Rw' U Rw U2 Rw'", id: '4cf59d5f-c128-49cc-b008-c1dc47ee1f59' },
+      { moves: "Rw U' Rw' U Rw U2 Rw'", id: '44968706-6d89-4032-a9e5-eea5b272352f' },
+      { moves: "U' F r U' r' U' r U2 r'", id: '17cd4d46-c1f2-41b6-b314-cb99cbcba843' },
+      { moves: "U' Rw U' Lw' U Rw' U' Lw", id: '54c7c944-b5fc-46f8-8b38-5a819fa9ea19' }
+    ]
+  },
+  {
+    name: 'L2C 16',
+    id: 'c08684a8-fa2f-473b-82fd-23b86d06a79d',
+    idMethod,
+    puzzle,
+    group,
+    algs: [
+      { moves: "Rw U' Rw' U Rw U' Rw'", id: '89158e3e-abe3-4e2f-8ec1-22b511d35459' },
+      { moves: "U2 Rw U Lw' U Rw' U' Lw", id: '1fb70afd-5973-4898-84b9-22a781af5561' }
+    ]
+  },
+  {
+    name: 'L2C 17',
+    id: 'bcabdb6f-b91b-4320-9ef6-6a335694b21d',
+    idMethod,
+    puzzle,
+    group,
+    algs: [{ moves: "Rw U Rw' U' Rw U Rw'", id: '31841f6d-6f24-4cfa-bb5c-1572bda9ea59' }]
+  },
+  {
+    name: 'L2C 18',
+    id: 'f484a6a8-e3db-402e-ad0d-1a08a39d52fb',
+    idMethod,
+    puzzle,
+    group,
+    algs: [
+      { moves: "M' U2 M U Rw U2 Rw'", id: 'd4987f66-8b43-4772-a0bd-dc763acbf588' },
+      { moves: "r U2 r' U2 r U' r'", id: '4d807de4-bca2-4773-b74e-50eae207b988' },
+      { moves: "U2 r U2 r' U2 r U r'", id: '41a9aefa-8188-4701-a162-9584aa5e008c' },
+      { moves: "U' Rw U' M' U' Rw' U M", id: 'cf4b4a6a-8373-4d72-87d8-e56b3398e49c' }
+    ]
+  }
+]
