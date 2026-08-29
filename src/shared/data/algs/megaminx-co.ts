7212ba59324438baf455b9134025f30a565236f3
Bryan Lundberg <bryanlundberg@outlook.com>
2026-08-29 14:44:58 -0600

    feat: add megaminx CO algorithms set

---
+++ src/shared/data/algs/megaminx-co.ts
@@ -0,0 +1,188 @@
+import { AlgorithmCollection } from '@/features/algorithms-list/model/types'
+
+const idMethod = 'MEGAMINX_CO_ALGS'
+const puzzle = 'mega'
+const group = 'Corner Orientation'
+
+export const MEGAMINX_CO_ALGS: AlgorithmCollection[] = [
+  {
+    name: 'CO 1',
+    id: '0326ccd2-d891-4b79-b62c-02033cb16589',
+    idMethod,
+    puzzle,
+    group,
+    setup: '',
+    algs: [
+      { moves: "R U R' U R U R' U2' R U' R'", id: '3971051c-e6e6-4fcd-ad24-ce2587f2ab2a' },
+      { moves: "y2' R2' DR' R U2 R' DR R U2' R", id: 'fcf82bdb-6eda-42a8-90d1-503aafc198cf' }
+    ]
+  },
+  {
+    name: 'CO 2',
+    id: '8184d619-698a-4545-bd8f-1bbbf21e98ee',
+    idMethod,
+    puzzle,
+    group,
+    setup: '',
+    algs: [
+      { moves: "F R U2 R' U' R U' R' F'", id: '5bb22013-287f-4261-97ca-7f121ac11d18' },
+      { moves: "y2' R BR R' F R BR' R' F'", id: '26d860a0-10e1-4a95-9b83-b33bfe6f233d' },
+      { moves: "y' R U R U2 R' U' R U' R' U' R'", id: '9c62500c-bdf0-4305-958e-102358a64ab6' }
+    ]
+  },
+  {
+    name: 'CO 3',
+    id: 'af63ac29-c19a-4d45-abeb-c49b6d6f0763',
+    idMethod,
+    puzzle,
+    group,
+    setup: '',
+    algs: [
+      { moves: "R U2 R' U R U2 R'", id: 'fba700fd-e057-4d51-a66b-fc02ed71c1bf' },
+      { moves: "y' R U2' R' U' R U2' R'", id: '8bd60386-25b6-4e98-b529-a77fcf465278' },
+      { moves: "L' U2' L U' L' U2' L", id: '0d345111-fe10-4c79-95e5-ebd74997270c' },
+      { moves: "y L' U2 L U L' U2 L", id: '255ea04b-06fd-40c6-a401-c8edfb71efcd' }
+    ]
+  },
+  {
+    name: 'CO 4',
+    id: 'a71f0cd9-5a64-4432-8c82-5bab9548fc12',
+    idMethod,
+    puzzle,
+    group,
+    setup: '',
+    algs: [
+      { moves: "R U R' U' R' F R U R U' R' F'", id: 'c69de576-6014-49d2-b061-062eed274b98' },
+      { moves: "y F R U R' U R U2' R' F'", id: '525b02f4-ff69-410f-b869-a411651549f7' },
+      { moves: "F R' F' L F R F' L'", id: '3d09bef5-e755-4561-a820-f16f9b95ce42' },
+      { moves: "R' F R BR' R' F' R BR", id: '3fecdec4-e13b-4462-bd18-0d3dc1e62b04' }
+    ]
+  },
+  {
+    name: 'CO 5',
+    id: '450fc423-f7b3-44d1-94dd-7a76a12ec56b',
+    idMethod,
+    puzzle,
+    group,
+    setup: '',
+    algs: [
+      { moves: "R U R' U R U2' R'", id: '360f20f8-fa3b-4bf9-8cd0-068c0b7bf19d' },
+      { moves: "y2 L U' R' U L' U' R U", id: '0f9b45cf-1c9a-4e25-9c86-22121b56874a' }
+    ]
+  },
+  {
+    name: 'CO 6',
+    id: '37d6b82c-aaa2-470d-9dfe-219dfeda8774',
+    idMethod,
+    puzzle,
+    group,
+    setup: '',
+    algs: [
+      { moves: "R' U' R U' R' U2 R", id: '4b6e1caf-ccc9-4449-9fbb-2026425006e7' },
+      { moves: "y2 L' U' L U' L' U2 L", id: 'a8121244-cbb2-49bd-b9b8-07b61c19ca3c' }
+    ]
+  },
+  {
+    name: 'CO 7',
+    id: 'b0b8ad1b-f2ec-4351-b48f-2da2148fb13e',
+    idMethod,
+    puzzle,
+    group,
+    setup: '',
+    algs: [
+      { moves: "R U2 R' U' R U' R'", id: 'f854b5bd-2c3d-4509-88f4-c2e07e253d9f' },
+      { moves: "y' R' F R F' L F R' F' R L'", id: 'e8a8ec93-8fa8-415a-8aa1-4e276d7b8eb9' }
+    ]
+  },
+  {
+    name: 'CO 8',
+    id: '3e894106-11bc-4cdd-b75b-6785f107d7f7',
+    idMethod,
+    puzzle,
+    group,
+    setup: '',
+    algs: [
+      { moves: "R U R' U2 R U2 R'", id: '077e4b87-b9de-44bb-a1a0-54861099e4cf' },
+      { moves: "y L' U2' L U L' U L", id: 'eb002224-6e2e-415b-ac8a-6576a83b4235' },
+      { moves: "y' R' L F R F' L' F R' F' R", id: 'b2ae7ddc-8860-40c1-85b2-af736c945550' }
+    ]
+  },
+  {
+    name: 'CO 9',
+    id: '72886efc-1a7b-416b-8c44-9e15c7225931',
+    idMethod,
+    puzzle,
+    group,
+    setup: '',
+    algs: [
+      { moves: "R U2 R' U' R U R' U' R U' R'", id: '71556bf7-1528-4d51-98e2-c448f04ed090' },
+      { moves: "y F R U R' U' R U R' U' R U R' U' F'", id: 'c6f5570c-08bf-4c37-9395-78a0dcb0674a' }
+    ]
+  },
+  {
+    name: 'CO 10',
+    id: '25dcfc0f-077e-4114-9b4c-057b4f592872',
+    idMethod,
+    puzzle,
+    group,
+    setup: '',
+    algs: [{ moves: "R U R' U R U' R' U R U2' R'", id: '0279b8b9-57ec-4182-bc2f-99c7f4e31c6a' }]
+  },
+  {
+    name: 'CO 11',
+    id: '5671cf3a-a4cf-434b-9272-133266cfa745',
+    idMethod,
+    puzzle,
+    group,
+    setup: '',
+    algs: [{ moves: "R U R' U R U R' U' R U2' R'", id: '3d8a8345-4048-4a53-bd0e-dd7e4ca03226' }]
+  },
+  {
+    name: 'CO 12',
+    id: '414671d5-75b3-46f1-bcf0-ac29664bed99',
+    idMethod,
+    puzzle,
+    group,
+    setup: '',
+    algs: [
+      { moves: "R U2 R' U' R U' R2' U' R U' R' U2 R", id: '88049629-ea51-414e-9c9c-ce697da66755' },
+      { moves: "R U2' R' U2' R U2 R' U2' R U' R'", id: '35da480a-0bb6-47c4-8789-5e98f4073c42' }
+    ]
+  },
+  {
+    name: 'CO 13',
+    id: 'ee479efb-2153-4cd1-ac13-1127e5cd6415',
+    idMethod,
+    puzzle,
+    group,
+    setup: '',
+    algs: [{ moves: "R U2 R2' U' R2 U' R2' U2 R", id: '6f599657-0aff-444b-bf72-00567bf32e3a' }]
+  },
+  {
+    name: 'CO 14',
+    id: '392b85a6-49f7-4cb2-b989-17419c97b5cd',
+    idMethod,
+    puzzle,
+    group,
+    setup: '',
+    algs: [{ moves: "R' U2' R2 U R2' U R2 U2' R'", id: 'd993055f-c900-4ba4-b949-c7e47b2cf944' }]
+  },
+  {
+    name: 'CO 15',
+    id: '7849e2c0-4923-48cb-962e-72522d816ed6',
+    idMethod,
+    puzzle,
+    group,
+    setup: '',
+    algs: [{ moves: "R U R' U2 R U2' R' U R U2' R'", id: '588ca9c8-6fb9-43b1-8f17-fe12ac3249a3' }]
+  },
+  {
+    name: 'CO 16',
+    id: '2e2b045e-ba90-42e8-b0ae-41124565d9c8',
+    idMethod,
+    puzzle,
+    group,
+    setup: '',
+    algs: [{ moves: "R U2 R' U' R U2 R' U2' R U' R'", id: 'bc8569dc-2351-4e33-8259-25ab0fea4dc0' }]
+  }
+]
