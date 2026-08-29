9f42a0b354a9eaec7151c1d1dea87190e01179a5
Bryan Lundberg <bryanlundberg@outlook.com>
2026-08-29 14:45:07 -0600

    feat: add megaminx CP algorithms set

---
+++ src/shared/data/algs/megaminx-cp.ts
@@ -0,0 +1,157 @@
+import { AlgorithmCollection } from '@/features/algorithms-list/model/types'
+
+const idMethod = 'MEGAMINX_CP_ALGS'
+const puzzle = 'mega'
+const group = 'Corner Permutation'
+
+export const MEGAMINX_CP_ALGS: AlgorithmCollection[] = [
+  {
+    name: 'CP 1',
+    id: '08b0cbc4-4042-48f2-81b5-9dc9e2db2f9a',
+    idMethod,
+    puzzle,
+    group,
+    setup: '',
+    algs: [{ moves: "R' BR' R BR R' F' R BR' R' BR F R", id: '7ea9494a-a088-4bdc-87f3-588114f28bd0' }]
+  },
+  {
+    name: 'CP 2',
+    id: '66a09f59-88eb-4e64-b403-f5af4d441814',
+    idMethod,
+    puzzle,
+    group,
+    setup: '',
+    algs: [{ moves: "R' F' BR' R BR R' F R BR' R' BR R", id: '2c8adf1c-1e89-46cb-99e6-25d960e8e977' }]
+  },
+  {
+    name: 'CP 3',
+    id: 'ea56269d-3968-41f9-958a-cede8b33e184',
+    idMethod,
+    puzzle,
+    group,
+    setup: '',
+    algs: [{ moves: "BR' R' U L U' R' U L' U' R2 BR", id: '9004509d-8271-4753-8801-b18ea51e2cfe' }]
+  },
+  {
+    name: 'CP 4',
+    id: '394f8bd2-ea84-4498-9a9d-4bd37a3c37ab',
+    idMethod,
+    puzzle,
+    group,
+    setup: '',
+    algs: [{ moves: "BR' R2' U L U' R U L' U' R BR", id: '3ff9056f-39bf-451c-afee-d2ee9c3187c8' }]
+  },
+  {
+    name: 'CP 5',
+    id: 'aa5121d5-c138-4acc-baff-9de00adb1d0e',
+    idMethod,
+    puzzle,
+    group,
+    setup: '',
+    algs: [{ moves: "L' R U2 R' U' R U R' U' R U R' U' R U' R' L", id: '2cb76cb3-7009-4568-b39d-5dc588ab764f' }]
+  },
+  {
+    name: 'CP 6',
+    id: '64ccb010-4405-44d3-877b-295122323230',
+    idMethod,
+    puzzle,
+    group,
+    setup: '',
+    algs: [{ moves: "R U R' U R' U' R F' R U R' U' R' F R2 U' R2' U R U'", id: '7ec6909e-f4cc-4357-96c5-c80312f7afb1' }]
+  },
+  {
+    name: 'CP 7',
+    id: '62c91188-5d41-4610-8f3e-d5014fdfb225',
+    idMethod,
+    puzzle,
+    group,
+    setup: '',
+    algs: [{ moves: "R2 U R' U' y R U R' U' R U R' U' R U R' y' R U' R2'", id: '6263d73d-c84e-4c13-a172-47cde8cc0b0f' }]
+  },
+  {
+    name: 'CP 8',
+    id: '945b45fc-e936-4854-8c35-4165ceb1ef40',
+    idMethod,
+    puzzle,
+    group,
+    setup: '',
+    algs: [
+      { moves: "F R U2 R' U' R U' R' F' R' y' R' U' R U' R' U2 R BR U'", id: '505b050c-5f33-4f15-ad29-b88808b54ff1' }
+    ]
+  },
+  {
+    name: 'CP 9',
+    id: '15a4f911-45a9-4be3-b489-75482352dedb',
+    idMethod,
+    puzzle,
+    group,
+    setup: '',
+    algs: [
+      {
+        moves: "R U R' U R' U' R2 U' R' U R' U R U R U R' U R' U' R2 U' R' U R' U R U",
+        id: 'b5e9e569-fdd9-4c8d-bba6-9ec442318b48'
+      }
+    ]
+  },
+  {
+    name: 'CP 10',
+    id: '3e76a299-8f2a-4397-bc1e-e0172dccf2ce',
+    idMethod,
+    puzzle,
+    group,
+    setup: '',
+    algs: [{ moves: "R2 U2 R2' U' R2 U' R2' y' R2' U' R2 U' R2' U2 R2", id: 'ee6eef41-c258-47dc-b81a-7de08b1cd7c8' }]
+  },
+  {
+    name: 'CP 11',
+    id: '278cf11e-c809-459f-ae18-5880a4dd965e',
+    idMethod,
+    puzzle,
+    group,
+    setup: '',
+    algs: [{ moves: "R2' U2' R2 U R2' U R2 y R2 U R2' U R2 U2' R2'", id: '98d068f7-97aa-4dc5-ade5-ff5ce5b2eac5' }]
+  },
+  {
+    name: 'CP 12',
+    id: 'f64b350a-9f21-4ebd-afe2-7f5a125832f5',
+    idMethod,
+    puzzle,
+    group,
+    setup: '',
+    algs: [
+      {
+        moves: "R2 U2' R2' U' R2 U2' R' U R' U' R' F R2 U' R' U' R U R' F'",
+        id: 'a6ec8b59-4885-4a8d-86bc-36f27b78956f'
+      }
+    ]
+  },
+  {
+    name: 'CP 13',
+    id: '1e5c23af-c06f-4775-ba4b-2ec746c6f070',
+    idMethod,
+    puzzle,
+    group,
+    setup: '',
+    algs: [
+      { moves: "R' U2 R U' R' U2 R U2' R' U' R U2' R' U R U2' R' U R", id: 'd9db419b-270d-4ab7-85c5-a2fadd9e7654' }
+    ]
+  },
+  {
+    name: 'CP 14',
+    id: '410db7f9-10bd-447f-85b1-e654da7df048',
+    idMethod,
+    puzzle,
+    group,
+    setup: '',
+    algs: [{ moves: "R2 U2' R2' U' R2 U R2' U' R2 U R2' U' R2 U2' R2'", id: '9660488a-89f8-4029-9831-8c2b2c1ff1a0' }]
+  },
+  {
+    name: 'CP 15',
+    id: '9e51bd0c-ecee-4852-a696-5cd8c42de656',
+    idMethod,
+    puzzle,
+    group,
+    setup: '',
+    algs: [{ moves: "R2 U2 R2' U R2 U' R2' U R2 U' R2' U R2 U2 R2'", id: '76f0fc9a-ee8c-46b5-af34-3be5db71197d' }]
+  }
+]
