b2d834fd1be194417f3d018612fac74acf32be85
Bryan Lundberg <bryanlundberg@outlook.com>
2026-09-05 10:01:51 -0600

    refactor(chart): remove unused types

--- src/features/bars-chart-categories/model/types.ts
+++
@@ -1,7 +0,0 @@
-import { Cube } from '@/entities/cube/model/types'
-import { CubeCategory } from '@/shared/const/cube-categories'
-
-export type BarsGraphCategoryProps = {
-  cubes: Array<Cube>
-  category?: CubeCategory
-}
