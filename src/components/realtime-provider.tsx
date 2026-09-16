ef60c9d716d42cbe8056c29de6c43f672f312573
Bryan Lundberg <bryanlundberg@outlook.com>
2026-09-16 10:12:36 -0600

    feat(realtime): add presence support to provider

--- src/components/realtime-provider.tsx
+++ src/components/realtime-provider.tsx
@@ -3,10 +3,12 @@
 import { useRealtimeConnection } from '@/features/realtime/model/useRealtimeConnection'
 import { useFriendsRealtime } from '@/features/friends/model/useFriendsRealtime'
 import { useChatRealtime } from '@/features/chat/model/useChatRealtime'
+import { usePresenceRealtime } from '@/features/presence/model/usePresenceRealtime'
 
 export default function RealtimeProvider() {
   useRealtimeConnection()
   useFriendsRealtime()
   useChatRealtime()
+  usePresenceRealtime()
   return null
 }
