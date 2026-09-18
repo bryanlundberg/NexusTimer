ca2ec40328a319999d73bcc10315e55714c620f1
Bryan Lundberg <bryanlundberg@outlook.com>
2026-09-18 08:59:23 -0600

    refactor: remove unnecessary sorting in moveSolvesBatch

--- src/features/manage-solves/api/moveSolvesBatch.ts
+++ src/features/manage-solves/api/moveSolvesBatch.ts
@@ -1,7 +1,6 @@
 import { SolveTab } from '@/shared/types/enums'
 import { Cube } from '@/entities/cube/model/types'
 import { cubesDB } from '@/entities/cube/api/indexdb'
-import { sortSolvesNewestFirst } from '@/entities/solve/lib/sortSolves'
 import { findIndex } from 'es-toolkit/compat'
 
 interface MoveSolvesBatchDTO {
@@ -33,9 +32,6 @@ export async function moveSolvesBatch(dto: MoveSolvesBatchDTO): Promise<Cube> {
       toList.push({ ...solve, isDeleted: false, updatedAt: now + 1 })
     }
   }
-
-  if (fromTab.toLowerCase() === SolveTab.SESSION) cube.solves.all = sortSolvesNewestFirst(cube.solves.all)
-  else cube.solves.session = sortSolvesNewestFirst(cube.solves.session)
 
   return await cubesDB.update(cube)
 }
