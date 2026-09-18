1711fd853fc22741bf53c488c2d3ffcc83e198f2
Bryan Lundberg <bryanlundberg@outlook.com>
2026-09-18 10:04:59 -0600

    refactor: update timer stats worker with reset and append logic

--- src/shared/worker/timer-stats.worker.ts
+++ src/shared/worker/timer-stats.worker.ts
@@ -1,29 +1,31 @@
 // <reference lib="webworker" />
-import calcStatistics from '@/features/deep-statistics/lib/calcStatistics'
+import {
+  appendTimerStats,
+  createTimerStats,
+  TimerStatsState,
+  timerStatsResult
+} from '@/features/deep-statistics/lib/timerStatsEngine'
 import { Cube } from '@/entities/cube/model/types'
+import { Solve } from '@/entities/solve/model/types'
 
-type InMsg = {
-  command: 'start'
-  data: {
-    cubes: Cube[]
-    selectedCube: Cube
-  }
-}
+type InMsg =
+  | { command: 'reset'; data: { cubes: Cube[]; selectedCube: Cube | null } }
+  | { command: 'append'; data: { cubeId: string; solves: Solve[] } }
+
+let state: TimerStatsState | null = null
+const scope = self as unknown as DedicatedWorkerGlobalScope
 
 self.onmessage = (event: MessageEvent<InMsg>) => {
-  const { command, data } = event.data
-  if (command === 'start') {
-    const { global, session, cubeSession } = calcStatistics({
-      cubesDB: data.cubes,
-      selectedCube: data.selectedCube
-    })
-
-    ;(self as unknown as DedicatedWorkerGlobalScope).postMessage({
-      result: {
-        global,
-        session,
-        cubeSession
-      }
-    })
+  const message = event.data
+  if (message.command === 'reset') {
+    state = createTimerStats(message.data.cubes, message.data.selectedCube)
+  } else if (message.command === 'append') {
+    const next = state && appendTimerStats(state, message.data.cubeId, message.data.solves)
+    if (!next) {
+      scope.postMessage({ resync: true })
+      return
+    }
+    state = next
   }
+  scope.postMessage({ result: timerStatsResult(state) })
 }
