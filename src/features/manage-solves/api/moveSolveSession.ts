851e8fd977b34ca245049f7cc614e93589161e99
Bryan Lundberg <bryanlundberg@outlook.com>
2026-09-18 08:59:35 -0600

    refactor: remove redundant sorting in moveSolveSession

--- src/features/manage-solves/api/moveSolveSession.ts
+++ src/features/manage-solves/api/moveSolveSession.ts
@@ -1,7 +1,6 @@
 import { MoveSolveSessionDTO } from '@/features/manage-solves/model/types'
 import { Cube } from '@/entities/cube/model/types'
 import { cubesDB } from '@/entities/cube/api/indexdb'
-import { sortSolvesNewestFirst } from '@/entities/solve/lib/sortSolves'
 import { SolveTab } from '@/shared/types/enums'
 import { findIndex } from 'es-toolkit/compat'
 
@@ -27,9 +26,6 @@ export default async function moveSolveSession(dto: MoveSolveSessionDTO): Promis
   } else {
     toList.push({ ...solve, isDeleted: false, updatedAt: Date.now() + 1 })
   }
-
-  if (fromTab.toLowerCase() === SolveTab.SESSION) cube.solves.all = sortSolvesNewestFirst(cube.solves.all)
-  else cube.solves.session = sortSolvesNewestFirst(cube.solves.session)
 
   return await cubesDB.update(cube)
 }
