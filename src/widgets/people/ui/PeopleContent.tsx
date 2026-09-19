75b0994e9be6edc71fe1725961aec039517a9182
Bryan Lundberg <bryanlundberg@outlook.com>
2026-09-19 13:55:29 -0600

    feat(people): update props to use stats

--- src/widgets/people/ui/PeopleContent.tsx
+++ src/widgets/people/ui/PeopleContent.tsx
@@ -5,29 +5,29 @@ import CubesTabContent from '@/widgets/people/ui/cubes-tab-content'
 import TimelineTabContent from '@/widgets/people/ui/timeline-tab-content'
 import AchievementsTabContent from '@/widgets/people/ui/achievements-tab-content'
 import AlgorithmsTabContent from '@/widgets/people/ui/algorithms-tab-content'
-import { Cube } from '@/entities/cube/model/types'
+import type { UserStatsSummary } from '@/entities/user-stats/model/types'
 import { UserBadgesResult } from '@/entities/achievement/model/useUserBadges'
 import type { LearnedMethod } from '@/entities/trainer-learned/model/useUserLearned'
 
 interface Props {
-  cubes: Array<Cube>
+  stats: UserStatsSummary | null
   badges: UserBadgesResult
   learnedMethods?: LearnedMethod[]
 }
 
-export function PeopleContent({ cubes, badges, learnedMethods }: Props) {
+export function PeopleContent({ stats, badges, learnedMethods }: Props) {
   return (
     <div className="flex flex-col w-full">
       <div className="flex flex-col md:flex-row relative mt-2">
         <div className="flex flex-col grow @container/tab">
           <TabsContent value={PTabs.OVERVIEW}>
-            <OverviewTabContent cubes={cubes} />
+            <OverviewTabContent categories={stats?.categories ?? []} />
           </TabsContent>
           <TabsContent value={PTabs.CUBES}>
-            <CubesTabContent cubes={cubes} />
+            <CubesTabContent cubes={stats?.cubes ?? []} />
           </TabsContent>
           <TabsContent value={PTabs.TIMELINE}>
-            <TimelineTabContent cubes={cubes} />
+            <TimelineTabContent solves={stats?.recentSolves ?? []} totalSolves={stats?.totalSolves ?? 0} />
           </TabsContent>
           <TabsContent value={PTabs.ALGORITHMS}>
             <AlgorithmsTabContent methods={learnedMethods} />
